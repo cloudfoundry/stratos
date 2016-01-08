c468e375ee08a514212e04e75e54691de446bebf
Shaoquan Chen <sean.chen2@hp.com>
2016-01-07 16:10:26 -0800

    Auto-finding host IP for protractor test

--- e2e/smoke.test.js
+++ e2e/smoke.test.js
@@ -1,7 +1,14 @@
+'use strict';
+
+var sh = require('../tools/node_modules/shelljs');
+
+var CMD = "/sbin/ip route|awk '/default/ { print $3 }'";
+var hostIp = sh.exec(CMD, { silent: true }).output.trim();
+
 describe('Smoke test', function () {
 
   it('should have a title', function () {
-    browser.get('http://172.17.0.1/');
+    browser.get('http://' + hostIp);
     expect(browser.getTitle()).toEqual('');
   });
 
