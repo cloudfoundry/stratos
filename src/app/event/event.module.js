6198acd5f1b90b86e29e4686bd6fc02e8c10406b
Shaoquan Chen <sean.chen2@hp.com>
2016-01-13 21:35:11 -0800

    Adding top navigation
    This PR
    - Creates navigation model app.model.navigation to navigation data.
    - Resets navigation data when login/logout user accout.
    - Refactores app.event.eventService for more convenient dependency
      injection.

--- src/app/event/event.module.js
+++ src/app/event/event.module.js
@@ -2,18 +2,6 @@
   'use strict';
 
   angular
-    .module('app.event', [])
-
-    /**
-     * @name app.event.events
-     * @description It is encouraged to define global event name under this
-     * constant map object.
-     */
-    .constant('app.event.events', {
-      HTTP_401: 'HTTP_401',
-      HTTP_403: 'HTTP_403',
-      HTTP_404: 'HTTP_404',
-      HTTP_500: 'HTTP_500'
-    });
+    .module('app.event', []);
 
 })();
