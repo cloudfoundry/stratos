da9edc0c153e70cf7246663556ecc0cba60d313a
Shaoquan Chen <sean.chen2@hp.com>
2016-01-15 14:26:21 -0800

    This PR creates plugin mechanism:
    - each plugin application should be installed or developed inside of src/plugins/
    - each plugin application should be registered into src/config under env.plugins. For example:
    env: {
      plugins: {
         cloudFoundry: {
            moduleName: 'cloud-foundry',
            basePath: 'plugins/cloud-foundry/',
            dependencies: []
         }
      }
    }
    - each plugin should have no more that one root scss file with all the sub scss files @import(ed).

--- src/config.js
+++ src/config.js
@@ -5,7 +5,33 @@
     gettext: gettext,
 
     env: {
-      HELION_UI_FRAMEWORK_BASE_PATH: 'lib/helion-ui-framework/'
+
+      HELION_UI_FRAMEWORK_BASE_PATH: 'lib/helion-ui-framework/',
+
+      plugins: {
+
+        cloudFoundry: {
+          /**
+           * @ngdoc plugins
+           * @property moduleName {String} defines a root angular module name of the
+           * plugin app.
+           *
+           * Each plugin app MUST have one and only one angular module name.
+           * This module will be added to the system as a dependency for the
+           * whole app the be initialized.
+           *
+           */
+          moduleName: 'cloud-foundry',
+
+          /**
+           * @ngdoc plugins
+           * @property basePath {String} defines the base path to the root folder where
+           * the plugin app resides or is installed.  The basePath is relative
+           * to the src folder.
+           */
+          basePath: 'plugins/cloud-foundry/'
+        }
+      }
     }
 
   });
