98868c4a0487debe19e4c22d54387444261426d5
Kelly Domico <kelly.domico@hpe.com>
2016-01-15 08:10:06 -0800

    Added Gulp config files for paths

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -8,129 +8,75 @@ var sass = require('gulp-sass');
 var eslint = require('gulp-eslint');
 var del = require('del');
 
-
-var paths = {
-  src: '../src/',
-  dist: '../dist/'
-};
-
-
-var jsSourceFiles = [
-  paths.src + '**/*.js'
-];
-
-
-var jsLibs = [
-  paths.dist + 'lib/angular/angular.js',
-  paths.dist + 'lib/angular-gettext/dist/angular-gettext.js',
-  paths.dist + 'lib/angular-sanitize/angular-sanitize.js',
-  paths.dist + 'lib/angular-bootstrap/ui-bootstrap.js',
-  paths.dist + 'lib/angular-bootstrap/ui-bootstrap-tpls.js',
-  paths.dist + 'lib/angular-ui-router/release/angular-ui-router.js',
-  paths.dist + 'lib/lodash/lodash.js',
-  paths.dist + 'lib/helion-ui-framework/**/*.module.js',
-  paths.dist + 'lib/helion-ui-framework/**/*.js'
-];
-
-
-var plugins = [
-];
-
-
-var jsFiles = [
-  paths.dist + 'index.module.js',
-  paths.dist + 'app/**/*.module.js',
-  paths.dist + 'app/**/*.js',
-  '!' + paths.dist + '**/*.mock.js',
-  '!' + paths.dist + '**/*.spec.js'
-];
-
-
-var scssSourceFiles = [
-  paths.src + 'index.scss'
-];
-
-
-var scssFiles = [
-  paths.src + '**/*.scss'
-]
-
-
-var cssFiles = [
-   paths.dist + 'index.css'
-];
-
-
-var partials = [
-  paths.src + 'app/**/*.html'
-];
+// Paths are stored in gulp.config.js
+var config = require('./gulp.config')();
 
 
 gulp.task('js', function () {
   return gulp
-    .src(jsSourceFiles, { base: paths.src })
-    .pipe(gulp.dest(paths.dist));
+    .src(config.jsSourceFiles, { base: config.paths.src })
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 
 gulp.task('css', function () {
   return gulp
-    .src(scssSourceFiles, { base: paths.src })
+    .src(config.scssSourceFiles, { base: config.paths.src })
     .pipe(sass())
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 
 gulp.task('html', function () {
   return gulp
-    .src(partials, { base: paths.src })
-    .pipe(gulp.dest(paths.dist));
+    .src(config.partials, { base: config.paths.src })
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 
 gulp.task('lib', function () {
   return gulp
-    .src(paths.src + 'lib/**')
-    .pipe(gulp.dest(paths.dist + 'lib/'));
+    .src(config.paths.src + 'lib/**')
+    .pipe(gulp.dest(config.paths.dist + 'lib/'));
 });
 
 
 gulp.task('index:copy', function () {
   return gulp
-    .src(paths.src + 'index.html')
-    .pipe(gulp.dest(paths.dist));
+    .src(config.paths.src + 'index.html')
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 
 gulp.task('index:inject', [ 'index:copy' ], function () {
   var sources = gulp.src(
-    [ paths.dist + 'config.js' ]
-    .concat(jsLibs)
-    .concat(plugins)
-    .concat(jsFiles)
-    .concat(cssFiles), { read: false });
+    [ config.paths.dist + 'config.js' ]
+    .concat(config.jsLibs)
+    .concat(config.plugins)
+    .concat(config.jsFiles)
+    .concat(config.cssFiles), { read: false });
 
   return gulp
-    .src(paths.dist + 'index.html')
+    .src(config.paths.dist + 'index.html')
     .pipe(gulpinject(sources, { relative: true }))
     .pipe(concat.header())
-    .pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(config.paths.dist));
 });
 
 
 gulp.task('watch', function () {
-  gulp.watch(jsSourceFiles, { interval: 1000, usePoll: true }, [ 'js' ]);
-  gulp.watch(scssFiles, [ 'css' ]);
-  gulp.watch(partials, { interval: 1000, usePoll: true }, [ 'html' ]);
-  gulp.watch(paths.src + 'index.html', [ 'index:inject' ]);
+  gulp.watch(config.jsSourceFiles, { interval: 1000, usePoll: true }, [ 'js' ]);
+  gulp.watch(config.scssFiles, [ 'css' ]);
+  gulp.watch(config.partials, { interval: 1000, usePoll: true }, [ 'html' ]);
+  gulp.watch(config.paths.src + 'index.html', [ 'index:inject' ]);
 });
 
 
 gulp.task('lint', function () {
   return gulp
     .src([
-      paths.src + '**/*.js',
-      '!' + paths.src + 'lib/**/*.js'
+      config.paths.src + '**/*.js',
+      '!' + config.paths.src + 'lib/**/*.js'
     ])
     .pipe(eslint())
     .pipe(eslint.format())
@@ -139,7 +85,7 @@ gulp.task('lint', function () {
 
 
 gulp.task('clean', function (next) {
-  del(paths.dist + '**/*', { force: true }, next);
+  del(config.paths.dist + '**/*', { force: true }, next);
 });
 
 
