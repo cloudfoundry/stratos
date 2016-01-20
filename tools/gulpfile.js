1d4ca52c1d81f0c5383c08d7835b33156857ef03
Kelly Domico <kelly.domico@hpe.com>
2016-01-19 20:03:39 -0800

    Decrease js watch interval

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -99,7 +99,7 @@ gulp.task('index:inject', [ 'index:copy' ], function () {
 
 
 gulp.task('watch', function () {
-  gulp.watch(jsSourceFiles, { interval: 2000, usePoll: true }, [ 'js' ]);
+  gulp.watch(jsSourceFiles, { interval: 1000, usePoll: true }, [ 'js' ]);
   gulp.watch(scssFiles, [ 'css' ]);
   gulp.watch(partials, [ 'html' ]);
   gulp.watch(paths.src + 'index.html', [ 'index:inject' ]);
