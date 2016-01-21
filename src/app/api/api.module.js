c92ddc384959193c769d45f5693f60b70794febb
Shaoquan Chen <sean.chen2@hp.com>
2016-01-21 12:41:43 -0800

    Adding $http interceptor broadcasting HTTP errors

--- src/app/api/api.module.js
+++ src/app/api/api.module.js
@@ -2,6 +2,36 @@
   'use strict';
 
   angular
-    .module('app.api', []);
+    .module('app.api', [], config);
+
+  config.$inject = ['$httpProvider'];
+
+  function config($httpProvider) {
+    $httpProvider.responseInterceptors.push(interceptor);
+  }
+
+  /**
+   * A $http interceptor, which emits a global http error event when
+   * response.status >= 400
+   *
+   * check https://docs.angularjs.org/api/ng/service/$http for details on
+   * $http interceptors.
+   */
+  interceptor.$inject = ['$q', 'app.event.eventService'];
+
+  function interceptor($q, eventService) {
+    return function (promise) {
+      return promise.then(success, error);
+    };
+
+    function success(response) {
+      return response;
+    }
+
+    function error(response) {
+      eventService.$emit('HTTP_' + response.status, response);
+      return $q.reject(response);
+    }
+  }
 
 })();
