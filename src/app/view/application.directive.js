95c1869bf90a2aed0eb6da3ce30e7a9bed0c4223
Kelly Domico <kelly.domico@hpe.com>
2016-01-25 13:26:29 -0800

    Changed controller names to camel case

--- src/app/view/application.directive.js
+++ src/app/view/application.directive.js
@@ -13,43 +13,43 @@
    * @namespace app.view.application
    * @memberof app.view
    * @name application
-   * @property {app.view.application.ApplicationController} controller - the application controller
+   * @property {app.view.application.applicationController} controller - the application controller
    * @property {string} controllerAs - the application controller identifier
    * @property {string} templateUrl - the application template filepath
    */
   function application(path) {
     return {
-      controller: ApplicationController,
+      controller: applicationController,
       controllerAs: 'applicationCtrl',
       templateUrl: path + '/view/application.html'
     };
   }
 
-  ApplicationController.$inject = [
+  applicationController.$inject = [
     'app.event.eventService',
     'app.model.modelManager'
   ];
 
   /**
-   * @namespace app.view.application.ApplicationController
+   * @namespace app.view.application.applicationController
    * @memberof app.view.application
-   * @name ApplicationController
+   * @name applicationController
    * @param {app.event.eventService} eventService - the event bus service
    * @param {app.model.modelManager} modelManager - the application model manager
    * @property {app.event.eventService} eventService - the event bus service
    * @property {app.model.account} account - the account model
    * @property {app.model.navigation} navigation - the navigation model
    */
-  function ApplicationController(eventService, modelManager) {
+  function applicationController(eventService, modelManager) {
     this.eventService = eventService;
     this.account = modelManager.retrieve('app.model.account');
     this.navigation = modelManager.retrieve('app.model.navigation');
   }
 
-  angular.extend(ApplicationController.prototype, {
+  angular.extend(applicationController.prototype, {
     /**
      * @function login
-     * @memberof app.view.application.ApplicationController
+     * @memberof app.view.application.applicationController
      * @description Log in to the application
      * @param {string} name - the username
      * @emits LOGGED_IN
@@ -62,7 +62,7 @@
 
     /**
      * @function logout
-     * @memberof app.view.application.ApplicationController
+     * @memberof app.view.application.applicationController
      * @description Log out of the application
      * @emits LOGGED_OUT
      */
