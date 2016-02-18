a56ac2b82d40ac66c84bfcad78de884facce5b46
Tara Gibbs <tara.gibbs@hp.com>
2016-02-18 15:38:27 -0800

    register the cloud-foundry plugin API

--- src/plugins/cloud-foundry/api/api.module.js
+++ src/plugins/cloud-foundry/api/api.module.js
@@ -1,7 +1,35 @@
 (function () {
   'use strict';
 
+  /**
+   * @namespace cloud-foundry.api
+   * @memberOf cloud-foundry.api
+   * @name cloud-foundry.api
+   * @description Cloud Foundry v2 API
+   */
   angular
-    .module('cloud-foundry.api', []);
+    .module('cloud-foundry.api')
+    .run(registerCFApi);
+
+  registerCFApi.$inject = [
+    '$http',
+    'app.api.apiManager'
+  ];
+
+  function registerCFApi($http, apiManager) {
+    apiManager.register('cloud-foundry.api', new CloudFoundryApi($http));
+  }
+
+  /**
+   * @param {object} $http - the Angular $http service
+   * @property {object} $http - the Angular $http service
+   * @class
+   */
+  function CloudFoundryApi($http) {
+    this.$http = $http;
+  }
+
+  angular.extend(CloudFoundryApi.prototype, {
+  });
 
 })();
