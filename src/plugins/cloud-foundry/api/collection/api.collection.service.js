ee97d25e552d9e910a7013d144a08cafe66847cc
Tara Gibbs <tara.gibbs@hp.com>
2016-02-25 12:11:08 -0800

    52 - add basic collection service

---
+++ src/plugins/cloud-foundry/api/collection/api.collection.service.js
@@ -0,0 +1,111 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('cloud-foundry.api')
+    .factory('cloud-foundry.api.collectionService', CollectionServiceFactory);
+
+  CollectionServiceFactory.$inject = [
+    'app.api.apiManager'
+  ];
+
+  function CollectionServiceFactory(apiManager) {
+
+    var apiVersionPrefix = '/v2/';
+
+    function makeQueryString(options) {
+      options = options || {};
+
+      var query = '';
+      var firstQuery = true;
+
+      if (options.queries) {
+        _.each(options.queries, function (value, key) {
+          query = query + ((firstQuery ? '' : '&') + key + '=' + value);
+          firstQuery = false;
+        });
+      }
+
+      if (options.filter && options.filter.name) {
+        query = query + ((firstQuery ? '' : '&') + 'q=' + options.filter.name + ':' + options.filter.value);
+      }
+
+      return query;
+    }
+
+    // CollectionService constructor
+    function CollectionService(name) {
+      this.name = name;
+      this.api = apiManager.retrieve('cloud-foundry.api');
+
+      this.getCollectionUrl = function () {
+        return apiVersionPrefix + this.name;
+      };
+
+      this.get = function (resourceIdentifier, params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+
+        var path = this.getCollectionUrl() + '/' + resourceIdentifier;
+
+        this.api.get(path, options);
+      };
+
+      this.getSummary = function (resourceIdentifier, params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+
+        var path = this.getCollectionUrl() + '/' + resourceIdentifier + '/summary';
+
+        this.api.get(path, options);
+      };
+
+      this.delete_ = function (resourceIdentifier, params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+
+        var path = this.getCollectionUrl() + '/' + resourceIdentifier;
+
+        this.api.delete_(path, options);
+      };
+
+      this.create = function (data, params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+        options.data = data;
+
+        var path = this.getCollectionUrl();
+
+        this.api.post(path, options);
+      };
+
+      this.update = function (resourceIdentifier, data, params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+        options.data = data;
+
+        var path = this.getCollectionUrl() + '/' + resourceIdentifier;
+
+        this.api.put(path, options);
+      };
+
+      this.list = function (params) {
+        var options = {};
+        options.paramSerializer = makeQueryString;
+        options.params = params;
+
+        var path = apiVersionPrefix + this.collection;
+
+        this.api.get(path, options); // TODO: need to deal with the Page.js collection response type
+      };
+    }
+
+    return CollectionService;
+  }
+
+})();
