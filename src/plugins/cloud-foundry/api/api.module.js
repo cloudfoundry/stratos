665e384e2e619d7c4be706d42635966fc7617403
Tara Gibbs <tara.gibbs@hp.com>
2016-03-10 16:17:43 -0800

    52 - drop collection generation. add interceptors to handle query string generation on requests and as a placeholder for handling orphan relations in responses

--- src/plugins/cloud-foundry/api/api.module.js
+++ src/plugins/cloud-foundry/api/api.module.js
@@ -2,86 +2,81 @@
   'use strict';
 
   angular
-    .module('cloud-foundry.api', [])
+    .module('cloud-foundry.api', [], config)
     .run(registerCFApi);
+
+  config.$inject = [
+    '$httpProvider'
+  ];
+
+  function config($httpProvider) {
+    $httpProvider.interceptors.push(CFAPIInterceptor);
+  }
+
+  CFAPIInterceptor.$inject = [
+    '$q'
+  ];
+
+  function CFAPIInterceptor($q) {
+    return {
+      request: marshalRequest,
+      response: marshalResponse
+    };
+
+    function makeQueryString(params) {
+      params = params || {};
+
+      var query = '';
+      var firstQuery = true;
+
+      if (params.queries) {
+        _.each(params.queries, function (value, key) {
+          query = query + ((firstQuery ? '' : '&') + key + '=' + value);
+          firstQuery = false;
+        });
+
+        if (params.queries['inline-relations-depth']) {
+          query = query + '&orphan-relations=1';
+        }
+      }
+
+      if (params.filter && params.filter.name) {
+        query = query + ((firstQuery ? '' : '&') + 'q=' + params.filter.name + ':' + params.filter.value);
+      }
+
+      return query;
+    }
+
+    function marshalRequest(config) {
+      if (config.url.substr(0,4) === "/v2/") {
+        config.paramSerializer = makeQueryString;
+      }
+      return config || $q.when(config);
+    }
+
+    function marshalResponse(response) {
+      if (response.config.url.substr(0,4) === "/v2/" && response.config.url.indexOf('orphan-relations=1')) {
+        // insert orphan relations back into returned resources
+      }
+      return response;
+    }
+  }
 
   registerCFApi.$inject = [
     '$http',
     'app.api.apiManager',
-    'cloud-foundry.api.collectionService',
-    'cloud-foundry.api.appsService'
+    'cloud-foundry.api.AppsService'
   ];
 
-  function registerCFApi($http, apiManager, CollectionService, AppsAPI) {
-
-    /*
-     Generic CF resources that follow the standard pattern with no unique functionality.
-     For other resources that do have unique functionality or don't support the standard set of verbs then derive an
-     object off of collection and add or remove specific functions.
-     */
-    var genericCollections = [
-      'domains',
-      'private_domains',
-      'shared_domains',
-      'quota_definitions',
-      'service_bindings',
-      'service_plans',
-      'services',
-      'service_instances',
-      'user_provided_service_instances',
-      'buildpacks'
-    ];
+  function registerCFApi($http, apiManager, AppsAPI) {
 
     function CFApi($http) {
       this.$http = $http;
-      CFApiInitializeCollections(this);
-      apiManager.register('cloud-foundry.api.apps', new AppsAPI(this, $http, CollectionService));
+      apiManager.register('cloud-foundry.api.apps', new AppsAPI($http));
     }
-
-    angular.extend(CFApi.prototype, {
-
-      getApiInfo: function (options) {
-        this.get('/v2/info', options);
-      },
-
-      get: function (path, options) {
-        this.$http.get(path, options);
-      },
-
-      delete_: function (path, options) {
-        this.$http.delete(path, options);
-      },
-
-      put: function (path, options) {
-        this.$http.put(path, options);
-      },
-
-      post: function (path, options) {
-        this.$http.post(path, options);
-      }
-
-    });
 
     apiManager.register('cloud-foundry.api', new CFApi($http));
 
-    function CFApiInitializeCollections(api) {
-      /*eslint-disable no-loop-func */
-      for (var i = genericCollections.length - 1; i >= 0; i--) {
-        (function(j) {
-
-          var collectionName = genericCollections[j];
-
-          var collectionAPI = function ($http, CollectionService) {
-            CollectionService.call(this, collectionName);
-            this.$http = $http;
-            this.api = api;
-          };
-
-          apiManager.register('cloud-foundry.api.' + collectionName, new collectionAPI($http, CollectionService));
-        })(i);
-      }
-      /*eslint-enable no-loop-func */
-    }
   }
 
 })();
