965216cfe2ce94457052512153d16fcba67eb9b7
Mike Hagedorn <mike.hagedorn@hp.com>
2016-03-11 15:56:20 -0500

    removed interceptor for http provider

--- src/plugins/cloud-foundry/api/api.module.js
+++ src/plugins/cloud-foundry/api/api.module.js
@@ -10,39 +10,9 @@
   angular
     .module('cloud-foundry.api', [], config);
 
-  config.$inject = [
-    '$httpProvider'
-  ];
-
-  function config($httpProvider) {
-    $httpProvider.interceptors.push(interceptor);
-  }
-
   interceptor.$inject = [
     '$q',
     'app.event.eventService'
   ];
-
-  /**
-   * @name interceptor
-   * @description A $http interceptor, which emits a global HTTP error event when
-   * response.status >= 400
-   *
-   * See https://docs.angularjs.org/api/ng/service/$http for details
-   *
-   * @param {object} $q - the $q service for promise/deferred objects
-   * @param {object} eventService - the event bus service
-   * @returns {object} The response error function
-   */
-  function interceptor($q, eventService) {
-    return {
-      responseError: responseError
-    };
-
-    function responseError(response) {
-      eventService.$emit('HTTP_' + response.status, response);
-      return $q.reject(response);
-    }
-  }
 
 })();
