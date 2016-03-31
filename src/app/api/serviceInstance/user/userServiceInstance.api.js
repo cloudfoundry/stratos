15bc18b065c89bd5a11d2a96d0794b0f0c02ed24
Kelly Domico <kelly.domico@hpe.com>
2016-03-30 17:40:12 -0700

    Moved user service instance API and added service instance API

---
+++ src/app/api/serviceInstance/user/userServiceInstance.api.js
@@ -0,0 +1,86 @@
+(function () {
+  'use strict';
+
+  /**
+   * @namespace app.api.serviceInstance.user
+   * @memberof app.api.serviceInstance
+   * @name user
+   * @description User service instance API
+   */
+  angular
+    .module('app.api')
+    .run(registerUserServiceInstanceApi);
+
+  registerUserServiceInstanceApi.$inject = [
+    '$http',
+    'app.api.apiManager'
+  ];
+
+  function registerUserServiceInstanceApi($http, apiManager) {
+    apiManager.register('app.api.serviceInstance.user', new UserServiceInstanceApi($http));
+  }
+
+  /**
+   * @namespace app.api.serviceInstance.user.UserServiceInstanceApi
+   * @memberof app.api.serviceInstance.user
+   * @name UserServiceInstanceApi
+   * @param {object} $http - the Angular $http service
+   * @property {object} $http - the Angular $http service
+   * @class
+   */
+  function UserServiceInstanceApi($http) {
+    this.$http = $http;
+  }
+
+  angular.extend(UserServiceInstanceApi.prototype, {
+    /**
+     * @function connect
+     * @memberof app.api.serviceInstance.user.UserServiceInstanceApi
+     * @description Connect a service instance
+     * @param {string} url - the service instance endpoint
+     * @returns {promise} A resolved/rejected promise
+     * @public
+     */
+    connect: function (url) {
+      return this.$http.post('/api/service-instances/user/connect', { url: url });
+    },
+
+    /**
+     * @function disconnect
+     * @memberof app.api.serviceInstance.user.UserServiceInstanceApi
+     * @description Disconnect user from service instance
+     * @param {string} url - the service instance endpoint
+     * @returns {promise} A resolved/rejected promise
+     * @public
+     */
+    disconnect: function (url) {
+      return this.$http.post('/api/service-instances/user/disconnect', { url: url });
+    },
+
+    /**
+     * @function list
+     * @memberof app.api.serviceInstance.user.UserServiceInstanceApi
+     * @description Returns a list of service instances for the user
+     * @returns {promise} A resolved/rejected promise
+     * @public
+     */
+    list: function () {
+      return this.$http.get('/api/service-instances/user');
+    },
+
+    /**
+     * @function register
+     * @memberof app.api.serviceInstance.user.UserServiceInstanceApi
+     * @description Set the service instances as registered
+     * @param {array} urls - the service instance endpoints
+     * @returns {promise} A resolved/rejected promise
+     * @public
+     */
+    register: function (urls) {
+      return this.$http.post('/api/service-instances/user/register', {
+        serviceInstances: urls
+      });
+    }
+  });
+
+})();
--- src/app/model/serviceInstance/serviceInstance.model.js
+++ src/app/model/serviceInstance/serviceInstance.model.js
@@ -27,46 +27,45 @@
    * @param {app.api.apiManager} apiManager - the application API manager
    * @property {app.api.apiManager} apiManager - the application API manager
    * @property {array} serviceInstances - the service instances available to user
-   * @property {number} numRegistered - the number of user registered service instances
    * @class
    */
   function ServiceInstance(apiManager) {
     this.apiManager = apiManager;
     this.serviceInstances = [];
-    this.numRegistered = 0;
   }
 
   angular.extend(ServiceInstance.prototype, {
     /**
-     * @function connect
+     * @function create
      * @memberof app.api.serviceInstance.ServiceInstance
-     * @description Connect a service instance
-     * @param {object} serviceInstanceUrl - the service instance endpoint
+     * @description Create a service instance
+     * @param {string} url - the service instance API endpoint
+     * @param {string} name - the service instance friendly name
      * @returns {promise} A resolved/rejected promise
      * @public
      */
-    connect: function (serviceInstanceUrl) {
+    create: function (url, name) {
       var serviceInstanceApi = this.apiManager.retrieve('app.api.serviceInstance');
-      return serviceInstanceApi.connect(serviceInstanceUrl);
+      return serviceInstanceApi.create(url, name);
     },
 
     /**
      * @function disconnect
      * @memberof app.model.serviceInstance.ServiceInstance
-     * @description Disconnect user from service instance
-     * @param {string} serviceInstanceUrl - the service instance endpoint
+     * @description Remove service instance
+     * @param {number} id - the ID of the service instance to remove
      * @returns {promise} A resolved/rejected promise
      * @public
      */
-    disconnect: function (serviceInstanceUrl) {
+    remove: function (id) {
       var serviceInstanceApi = this.apiManager.retrieve('app.api.serviceInstance');
-      return serviceInstanceApi.disconnect(serviceInstanceUrl);
+      return serviceInstanceApi.remove(id);
     },
 
     /**
      * @function list
      * @memberof app.model.serviceInstance.ServiceInstance
-     * @description Returns services instances and number registered
+     * @description Returns a list of service instances (master list)
      * @returns {promise} A resolved/rejected promise
      * @public
      */
@@ -75,44 +74,10 @@
       var serviceInstanceApi = this.apiManager.retrieve('app.api.serviceInstance');
       return serviceInstanceApi.list()
         .then(function (response) {
-          var items = response.data.items;
-
-          // check token expirations
-          var now = (new Date()).getTime() / 1000;
-          angular.forEach(items, function (item) {
-            if (!_.isNil(item.expires_at)) {
-              if (item.expires_at > now) {
-                item.valid = true;
-              } else {
-                item.valid = false;
-              }
-            }
-          });
-
+          var items = response.data.items || [];
           that.serviceInstances.length = 0;
           that.serviceInstances.push.apply(that.serviceInstances, _.sortBy(items, 'name'));
-          that.numRegistered = _.sumBy(items, function (o) { return o.valid ? 1 : 0; }) || 0;
-          var numCompleted = _.sumBy(items, function (o) { return o.registered ? 1 : 0; }) || 0;
-
-          return {
-            serviceInstances: that.serviceInstances,
-            numCompleted: numCompleted,
-            numRegistered: that.numRegistered
-          };
         });
-    },
-
-    /**
-     * @function register
-     * @memberof app.model.serviceInstance.ServiceInstance
-     * @description Set the service instances as registered
-     * @param {string} serviceInstanceUrls - the service instance endpoints
-     * @returns {promise} A resolved/rejected promise
-     * @public
-     */
-    register: function (serviceInstanceUrls) {
-      var serviceInstanceApi = this.apiManager.retrieve('app.api.serviceInstance');
-      return serviceInstanceApi.register(serviceInstanceUrls);
     }
   });
 
