03dfdacb2fbeb828ee121d1aec8e8651bdcd29fd
Mike Hagedorn <mike.hagedorn@hp.com>
2016-03-31 12:50:58 -0400

    WIP e2e tests for gallery card

--- e2e/applications-gallery-page.spec.js
+++ e2e/applications-gallery-page.spec.js
@@ -7,23 +7,11 @@ describe('Application Gallery Page', function () {
   beforeAll(function () {
     helpers.setBrowserNormal();
     helpers.loadApp();
-    galleryPage.login();
-    browser.driver.sleep(1000);
   });
 
   describe('content', function () {
-   describe('applications tab', function(){
-     it("should show the applications gallery", function(){
-      galleryPage.showApplicationsGallery();  
-      expect(browser.getCurrentUrl()).toBe('http://' + helpers.getHost() +'/#/cf/applications/list/gallery-view');
-     });
-     describe("and you click a card", function(){
-        it('should go to application detail', function () {
-          galleryPage.showApplicationDetails();
-          expect(browser.getCurrentUrl()).toMatch(/summary$/);
-        });
-     })
-   });
-   
+    it('should go to application detail if card clicked', function () {
+      galleryPage.showAppDetails();
+    });
   });
 });
--- e2e/po/application-card-gallery.po.js
+++ e2e/po/application-card-gallery.po.js
@@ -3,14 +3,15 @@ var loginPage = require('./login-page.po');
 
 // Navbar helpers
 module.exports = {
-  login : login,
-  showApplicationsGallery : showApplicationsGallery,
-  showApplicationDetails : showApplicationDetails
+
+  showAppDetails: showAppDetails
+
 };
 
 function applicationGalleryCard() {
   return element.all(by.css('application-gallery-card')).get(0)
     .element(by.css('gallery-card'));
+
 }
 
 function applicationsGalleryButton() {
@@ -22,10 +23,17 @@ function showApplicationsGallery() {
   applicationsGalleryButton().click();
 }
 
-function showApplicationDetails(){
+function showApplicationDetail(){
   applicationGalleryCard().click();
 }
 
+
+
+function showAppDetails() {
+  login();
+  showApplicationsGallery();
+  showApplicationDetail();
+}
 
 function login(){
   var fields = loginPage.loginFormFields();
