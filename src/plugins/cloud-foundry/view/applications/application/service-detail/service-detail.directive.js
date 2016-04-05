2a9bb41078cd66f9ea4acb9d9fd01a74815f5b92
Mike Hagedorn <mike.hagedorn@hp.com>
2016-04-05 16:54:25 -0400

    initial cut at styling service detail

---
+++ src/plugins/cloud-foundry/view/applications/application/service-detail/service-detail.directive.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('cloud-foundry.view.applications.application')
+    .directive('applicationServiceDetail', applicationServiceDetail);
+
+  applicationServiceDetail.$inject = [];
+
+  function applicationServiceDetail() {
+    return {
+      bindToController: {
+        service: '=',
+        app: '='
+      },
+      controller: ApplicationServiceDetailController,
+      controllerAs: 'applicationServiceDetailCtrl',
+      scope: {},
+      templateUrl: 'plugins/cloud-foundry/view/applications/application/service-detail/service-detail.html'
+    };
+  }
+
+  ApplicationServiceDetailController.$inject = [
+    '$state'
+  ];
+
+  function ApplicationServiceDetailController($state) {
+    this.$state = $state;
+  }
+
+  angular.extend(ApplicationServiceDetailController.prototype, {
+    showServiceDetail: function () {
+      alert(this.service);
+    }
+  });
+
+})();
