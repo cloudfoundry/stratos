d97e5ee199a3bdb1bca9147fefb8edec428133d9
Mike Hagedorn <mike.hagedorn@hp.com>
2016-04-11 06:46:54 -0400

    restore e2e to disabled temporarily

--- e2e/applications-service-preview-page.spec.js
+++ e2e/applications-service-preview-page.spec.js
@@ -3,45 +3,31 @@
 var helpers = require('./po/helpers.po');
 var galleryPage = require('./po/application-card-gallery.po');
 var registration = require('./po/service-instance-registration.po');
-var fs = require('fs');
-
-// abstract writing screen shot to a file
-    function writeScreenShot(data, filename) {
-        var stream = fs.createWriteStream(filename);
-        stream.write(new Buffer(data, 'base64'));
-        stream.end();
-    }
 
 describe('Applications - Gallery View', function () {
-  beforeEach(function () {
+  beforeAll(function () {
     helpers.setBrowserNormal();
     helpers.loadApp();
     registration.loginAndConnect();
   });
 
-  afterEach(function () {
+  afterAll(function () {
     registration.disconnectAndLogout();
     helpers.resetDatabase();
   });
 
-
-  describe("while viewing the application gallery", function(){
-    beforeEach(function () {
+  xdescribe("while viewing the application gallery", function(){
+    beforeAll(function () {
       galleryPage.showApplications();
-      browser.driver.sleep(1000);
     });
-
-    it('should show applications as cards', function() {
-      expect(browser.getCurrentUrl()).toBe('http://' + helpers.getHost() +'/#/cf/applications/list/gallery-view');
-    });
-
     describe('and you click a card', function () {
-     beforeEach(function () {
+     beforeAll(function () {
         galleryPage.showApplicationDetails(0);
      });
      describe('and you click on the services tab',function(){
-       beforeEach(function () {
+       beforeAll(function () {
          galleryPage.showServices();
+         browser.driver.sleep(1000);
        });
        it('should show application services URL', function () {
          expect(browser.getCurrentUrl()).toMatch(/services$/);
@@ -50,12 +36,9 @@ describe('Applications - Gallery View', function () {
          expect(galleryPage.servicePanelsAddServiceButtons().count()).toBeGreaterThan(0);
        });
        describe("and you click on 'add service'",function() {
-         beforeEach(function() {
+         beforeAll(function() {
            galleryPage.showServiceDetails();
            browser.driver.sleep(1000);
-           browser.takeScreenshot().then(function (png) {
-              writeScreenShot(png, 'exception.png');
-           });
          });
          it("shows the service preview panel", function() {
            expect(galleryPage.applicationServiceFlyout().isDisplayed()).toBe(true);
