e6cc790ccfeeedc2eff27d529de7963c5b36bcd4
Kelly Domico <kelly.domico@hpe.com>
2016-04-13 10:44:53 -0700

    Demo for radio button

--- src/plugins/cloud-foundry/view/workspaces/workspaces.module.js
+++ src/plugins/cloud-foundry/view/workspaces/workspaces.module.js
@@ -12,8 +12,22 @@
   function registerRoute($stateProvider) {
     $stateProvider.state('cf.workspaces', {
       url: '/workspaces',
-      templateUrl: 'plugins/cloud-foundry/view/workspaces/workspaces.html'
+      templateUrl: 'plugins/cloud-foundry/view/workspaces/workspaces.html',
+      controller: WorkspacesController,
+      controllerAs: 'workspacesCtrl'
     });
+  }
+
+  WorkspacesController.$inject = [];
+
+  function WorkspacesController() {
+    this.selectedWorkspace = null;
+    this.options = [
+      { label: 'Workspace One', value: 'one' },
+      { label: 'Workspace Two', value: 'two' },
+      { label: 'Workspace Three', value: 'three' },
+      { label: 'Disabled Workspace', value: 'disabled', disabled: true }
+    ];
   }
 
 })();
