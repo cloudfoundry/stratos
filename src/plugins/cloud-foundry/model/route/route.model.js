97caf8fb4afc4c1ff4b741c38264b9f50d595a0d
Shaoquan Chen <sean.chen2@hp.com>
2016-05-18 11:19:44 -0700

    Adding Route model with mock and unit tests

---
+++ src/plugins/cloud-foundry/model/route/route.model.js
@@ -0,0 +1,69 @@
+(function () {
+  'use strict';
+
+  /**
+   * @namespace cloud-foundry.model
+   * @memberOf cloud-foundry.model
+   * @description Route model
+   */
+  angular
+    .module('cloud-foundry.model')
+    .run(registerRouteModel);
+
+  registerRouteModel.$inject = [
+    'app.model.modelManager',
+    'app.api.apiManager'
+  ];
+
+  function registerRouteModel(modelManager, apiManager) {
+    modelManager.register('cloud-foundry.model.route', new Route(apiManager));
+  }
+
+  /**
+   * @memberof cloud-foundry.model.route
+   * @name Route
+   * @param {app.api.apiManager} apiManager - the API manager
+   * @property {app.api.apiManager} apiManager - the API manager
+   * @property {object} data - the data bag
+   * @class
+   */
+  function Route(apiManager) {
+    this.apiManager = apiManager;
+    this.route = {};
+  }
+
+  angular.extend(Route.prototype, {
+   /**
+    * @function deleteApp
+    * @memberof cloud-foundry.model.route
+    * @description Deletes a particular route
+    * @param {string} guid - Route identifier
+    * @returns {promise} A resolved/rejected promise
+    * @public
+    */
+    deleteRoute: function (guid) {
+      return this.apiManager.retrieve('cloud-foundry.api.Routes')
+        .DeleteRoute(guid);
+    },
+
+   /**
+    * @function listAllAppsForRoute
+    * @memberof cloud-foundry.model.route
+    * @description Lists all apps for the route
+    * @param {string} guid - Route identifier
+    * @param {object} params - optional parameters
+    * @returns {promise} A resolved/rejected promise
+    * @public
+    */
+    listAllAppsForRoute: function (guid, params) {
+      var that = this;
+      return this.apiManager.retrieve('cloud-foundry.api.Routes')
+        .ListAllAppsForRoute(guid, params)
+        .then(function (response) {
+          that.route.id = guid;
+          that.route.apps = response.data;
+        });
+    }
+  });
+
+})();
---
+++ src/plugins/cloud-foundry/model/route/route.model.spec.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  describe('cloud-foundry route model', function () {
+    var $httpBackend, routeModel;
+
+    beforeEach(module('templates'));
+    beforeEach(module('green-box-console'));
+    beforeEach(inject(function ($injector) {
+      $httpBackend = $injector.get('$httpBackend');
+      var modelManager = $injector.get('app.model.modelManager');
+      routeModel = modelManager.retrieve('cloud-foundry.model.route');
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('deleteRoute', function () {
+      var DeleteRoute = mock.cloudFoundryAPI.Routes.DeleteRoute('123');
+      $httpBackend.whenDELETE(DeleteRoute.url).respond(204, DeleteRoute.response['204'].body);
+      $httpBackend.expectDELETE(DeleteRoute.url);
+      routeModel.deleteRoute('123');
+      $httpBackend.flush();
+      expect(DeleteRoute.response['204'].body).toBeDefined();
+    });
+
+    it('ListAllAppsForRoute', function () {
+      var ListAllAppsForRoute = mock.cloudFoundryAPI.Routes.ListAllAppsForRoute('123');
+      $httpBackend.whenGET(ListAllAppsForRoute.url).respond(200, ListAllAppsForRoute.response['200'].body);
+      $httpBackend.expectGET(ListAllAppsForRoute.url);
+      routeModel.listAllAppsForRoute('123');
+      $httpBackend.flush();
+      expect(routeModel.route.id).toBe('123');
+      expect(routeModel.route.apps).toEqual(ListAllAppsForRoute.response['200'].body);
+    });
+  });
+
+})();
