2ae9edaf2257f9f9a45baa4aa31fe2f2d2564b50
Chris Johnson <wchrisjohnson@gmail.com>
2016-05-26 22:37:16 -0400

    Database Configuration at deployment

---
+++ db/README.md
@@ -0,0 +1,5 @@
+## Database Migrations
+
+We are currently using [Goose](https://bitbucket.org/liamstask/goose/) in development for database migrations.
+
+For the demo we are currently preparing for, we'll just execute the migrations directly.
---
+++ db/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+execStatement() {
+    stmt=$1
+    PGPASSFILE=/tmp/pgpass psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d postgres -w -tc "$stmt"
+}
+
+execStatementsFromFile() {
+    file=$1
+    PGPASSFILE=/tmp/pgpass psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d postgres -w -f "$file"
+}
+
+echo "$DB_HOST:$DB_PORT:$DB:$DB_USER:$(cat $DB_PW_FILE)" > /tmp/pgpass
+chmod 0600 /tmp/pgpass
+
+stratosExists=$(execStatement "SELECT 1 FROM pg_database WHERE datname = '$STRATOS_DB';")
+if [ -z "$stratosExists" ] ; then
+    execStatement "CREATE DATABASE $STRATOS_DB;"
+    execStatement "CREATE USER $STRATOS_USER WITH ENCRYPTED PASSWORD '$STRATOS_PWD';"
+    execStatement "GRANT ALL PRIVILEGES ON DATABASE $STRATOS_PWD TO $STRATOS_USER;"
+    execStatementsFromFile "$SQL"
+else
+    echo "$STRATOS_DB already exists"
+fi
+
+exit 0
