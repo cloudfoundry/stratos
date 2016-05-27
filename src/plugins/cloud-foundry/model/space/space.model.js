729ab2b27efff24cb7b83b0291633e84940b2e86
Shaoquan Chen <sean.chen2@hp.com>
2016-05-26 19:59:44 -0700

    - Adding org and space dropdown to add-app workflow - Adding organization model and methods

---
+++ src/plugins/cloud-foundry/model/space/space.model.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  /**
+   * @namespace cloud-foundry.model
+   * @description Space model
+   */
+  angular
+    .module('cloud-foundry.model')
+    .run(registerRouteModel);
+
+  registerRouteModel.$inject = [
+    'app.model.modelManager',
+    'app.api.apiManager'
+  ];
+
+  function registerRouteModel(modelManager, apiManager) {
+    modelManager.register('cloud-foundry.model.space', new Space(apiManager));
+  }
+
+  /**
+   * @memberof cloud-foundry.model
+   * @name Space
+   * @param {app.api.apiManager} apiManager - the API manager
+   * @property {app.api.apiManager} apiManager - the API manager
+   * @class
+   */
+  function Space(apiManager) {
+    this.apiManager = apiManager;
+  }
+
+  angular.extend(Space.prototype, {
+   /**
+    * @function listAllSpaces
+    * @memberof cloud-foundry.model.space
+    * @description lists all spaces
+    * @param {object} params - optional parameters
+    * @returns {promise} A resolved/rejected promise
+    * @public
+    */
+    listAllSpaces: function (params) {
+      return this.apiManager.retrieve('cloud-foundry.api.Spaces')
+        .ListAllSpaces(params)
+    }
+  });
+
+})();
