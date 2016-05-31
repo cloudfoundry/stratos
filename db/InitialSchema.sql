346c5270f3647507a2587b0f6c351beb22ff1548
Chris Johnson <wchrisjohnson@gmail.com>
2016-05-30 23:02:12 -0400

    Latest

---
+++ db/InitialSchema.sql
@@ -0,0 +1,32 @@
+
+-- For storage of UAA and CNSI tokens
+CREATE TABLE IF NOT EXISTS tokens (
+  id            BIGSERIAL   NOT NULL UNIQUE,
+  user_guid     VARCHAR(36) NOT NULL,
+  cnsi_guid     VARCHAR(36),
+  token_type    VARCHAR(10) NOT NULL,
+  auth_token    TEXT        NOT NULL,
+  refresh_token TEXT        NOT NULL,
+  token_expiry  BIGINT      NOT NULL,
+  last_updated  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
+  PRIMARY KEY (id)
+);
+
+CREATE INDEX tokens_user_guid ON tokens (user_guid);
+CREATE INDEX tokens_cnsi_guid ON tokens (cnsi_guid);
+CREATE INDEX tokens_token_type ON tokens (token_type);
+
+-- For storage of CNSI records
+CREATE TABLE IF NOT EXISTS cnsis (
+  guid            VARCHAR(36)   NOT NULL UNIQUE,
+  name            VARCHAR(255)  NOT NULL,
+  cnsi_type       VARCHAR(3)    NOT NULL,
+  api_endpoint    VARCHAR(255)  NOT NULL,
+  auth_endpoint   VARCHAR(255)  NOT NULL,
+  token_endpoint  VARCHAR(255)  NOT NULL,
+  last_updated    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE 'utc'),
+  PRIMARY KEY (guid)
+);
+
+CREATE INDEX cnsis_name ON cnsis (name);
+CREATE INDEX cnsis_cnsi_type ON cnsis (cnsi_type);
--- db/run.sh
+++ db/run.sh
@@ -1,27 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 execStatement() {
     stmt=$1
-    PGPASSWORD='stolon' psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d postgres -w -tc "$stmt"
+    psql -U postgres -h $DB_HOST -p $DB_PORT -d $DB -w -tc "$stmt"
 }
 
 execStatementsFromFile() {
     file=$1
-    PGPASSWORD='stratos' psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $STRATOS_DB -w -f "$file"
+    PGPASSWORD='stratos' psql -U $STRATOS_USER -h $DB_HOST -p $DB_PORT -d $STRATOS_DB -w -f "$file"
 }
-
-echo "$DB_HOST:$DB_PORT:$DB:$DB_USER:$(cat $DB_PW_FILE)" > /tmp/pgpass
-chmod 0600 /tmp/pgpass
 
 stratosExists=$(execStatement "SELECT 1 FROM pg_database WHERE datname = '$STRATOS_DB';")
 if [ -z "$stratosExists" ] ; then
     execStatement "CREATE DATABASE $STRATOS_DB;"
     execStatement "CREATE USER $STRATOS_USER WITH ENCRYPTED PASSWORD '$STRATOS_PWD';"
-    execStatement "GRANT ALL PRIVILEGES ON DATABASE $STRATOS_PWD TO $STRATOS_USER;"
-    execStatementsFromFile "$SQL"
+    execStatement "GRANT ALL PRIVILEGES ON DATABASE $STRATOS_DB TO $STRATOS_USER;"
+    # execStatementsFromFile "$SQL"
 else
     echo "$STRATOS_DB already exists"
 fi
+
+echo "Success!"
 
 exit 0
