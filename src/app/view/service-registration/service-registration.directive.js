b8c9873d11898bd4c66b944d0bfbeb3feeac279e
woodnt <woodm1979@gmail.com>
2016-06-04 23:26:11 -0600

    448_gettext

--- src/app/view/service-registration/service-registration.directive.js
+++ src/app/view/service-registration/service-registration.directive.js
@@ -170,7 +170,7 @@
       this.detailView(
         {
           templateUrl: 'app/view/hce-registration/hce-registration.html',
-          title: 'Register Code Engine Endpoint'
+          title: gettext('Register Code Engine Endpoint')
         },
         {
           data: data
