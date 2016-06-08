364af058d38578ed15f656a6f5beb91b03393548
woodnt <woodm1979@gmail.com>
2016-06-08 13:57:15 -0600

    513 delint github/plugin.config.js

--- src/plugins/github/plugin.config.js
+++ src/plugins/github/plugin.config.js
@@ -1,8 +1,13 @@
 (function () {
   'use strict';
 
-  env &&
-    env.registerApplication &&
-    env.registerApplication('github', 'github', 'plugins/github');
+  // register this plugin application to the platform
+  if (env && env.registerApplication) {
+    env.registerApplication(
+      'github',         // plugin application identity
+      'github',         // plugin application's root angular module name
+      'plugins/github'  // plugin application's base path
+    );
+  }
 
 })();
