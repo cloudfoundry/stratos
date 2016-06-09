fe06aa348df4d4c4814c465cd0ff30d1b0f6e9f5
Chris Johnson <wchrisjohnson@gmail.com>
2016-06-08 23:22:41 -0400

    Initial commit
    Migrate the OAuth feature from the NodeJS server to the portal proxy

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ var (
 
 func main() {
 	log.SetOutput(os.Stdout)
+	log.Println("Started Portal Proxy")
 
 	var portalConfig portalConfig
 	portalConfig, err := loadPortalConfig(portalConfig)
@@ -32,6 +33,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	log.Println("Proxy Configuration loaded up")
 	var databaseConfig datastore.DatabaseConfig
 	databaseConfig, err = loadDatabaseConfig(databaseConfig)
 	if err != nil {
@@ -39,6 +41,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	log.Println("Database Configuration loaded up")
 	var databaseConnectionPool *sql.DB
 	databaseConnectionPool, err = datastore.GetConnection(databaseConfig)
 	if err != nil {
@@ -47,13 +50,17 @@ func main() {
 	}
 	defer databaseConnectionPool.Close()
 
+	log.Println("Spinning up a new Portal Proxy")
 	portalProxy := newPortalProxy(portalConfig, databaseConnectionPool)
 
+	log.Println("Initializing the HTTP client")
 	initializeHTTPClient(portalConfig.SkipTLSVerification,
 		time.Duration(portalConfig.HTTPClientTimeoutInSecs)*time.Second)
 
+	log.Printf("Starting the proxy on IP %s and port %s", portalConfig.TLSAddress, 80)
+	log.Printf("%v", portalConfig)
 	if err := start(portalProxy); err != nil {
-		fmt.Println(err)
+		log.Printf("Unable to start the proxy: %v", err)
 		os.Exit(1)
 	}
 }
@@ -168,6 +175,10 @@ func (p *portalProxy) registerRoutes(e *echo.Echo) {
 
 	// Disconnect HCF cluster
 	sessionGroup.Post("/auth/logout/cnsi", p.logoutOfCNSI)
+
+	// GitHub OAuth
+	sessionGroup.Get("/github/oauth/auth", p.githubAuth)
+	sessionGroup.Get("/github/oauth/redirect", p.githubRedirect)
 
 	// Register clusters
 	sessionGroup.Post("/register/hcf", p.registerHCFCluster)
