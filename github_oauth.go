0bad42c4a84b8975254e0bfb09da225c11f350bd
Chris Johnson <wchrisjohnson@gmail.com>
2016-06-10 20:42:21 -0400

    Finish the initial implementation effort

--- github_oauth.go
+++ github_oauth.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
@@ -10,24 +11,7 @@ import (
 	"github.com/labstack/echo"
 )
 
-var successHTML = `
-  <!doctype html>
-  <html>
-  <head><link rel="stylesheet" href="/index.css"></head>
-  <body id="github-auth-callback-page">
-  <h1 class="text-center">GitHub authorization is done.</h1>
-  <p class="text-center"><button class="btn btn-primary" onclick="window.close()">Close window and continue</button></p>
-  <script>
-    (function () {
-      window.opener.postMessage(JSON.stringify({
-        name: 'GitHub Oauth - token',
-        data: __TOKEN__
-      }), window.location.origin);
-    })();
-  </script>
-  </body>
-  </html>`
-
+// handleGitHubAuth <TBD>
 func (p *portalProxy) handleGitHubAuth(c echo.Context) error {
 
 	log.Println("handleGitHubAuth")
@@ -43,63 +27,80 @@ func (p *portalProxy) handleGitHubAuth(c echo.Context) error {
 		oauthStateString = p.Config.GitHubOAuthState
 	)
 
-	// func handleGitHubLogin(w http.ResponseWriter, r *http.Request) {
-	url := oauthConf.AuthCodeURL(oauthStateString, oauth2.AccessTypeOnline)
-	c.Redirect(200, url)
-	// http.Redirect(w,e r, url, http.StatusTemporaryRedirect)
+	// TODO (wchrisjohnson) SECURITY RISK - remove this log statement once running
+	log.Printf("oauthConf: %v", oauthConf)
 
-	return nil
+	// TODO (wchrisjohnson) SECURITY RISK - remove this log statement once running
+	log.Printf("oauthStateString: %s", oauthStateString)
+
+	url := oauthConf.AuthCodeURL(oauthStateString, oauth2.AccessTypeOnline)
+
+	log.Printf("OAuth url: %s", url)
+
+	return c.Redirect(302, url)
 }
 
+// handleGitHubCallback <TBD>
 func (p *portalProxy) handleGitHubCallback(c echo.Context) error {
 
 	log.Println("handleGitHubCallback")
 
 	var (
+		// Define the necessary OAuth scopes needed by the application
+		scopes = []string{"admin:repo_hook", "repo", "repo:status "}
+
+		// OAuth2 configuration expected by the OAuth2 package
 		oauthConf = &oauth2.Config{
 			ClientID:     p.Config.GitHubOauthClientID,
 			ClientSecret: p.Config.GitHubOAuthClientSecret,
-			Scopes:       []string{"admin:repo_hook", "repo", "repo:status "},
+			Scopes:       scopes,
 			Endpoint:     github.Endpoint,
 		}
+
 		// random string for oauth2 API calls to protect against CSRF
 		oauthStateString = p.Config.GitHubOAuthState
+
+		// The markup that defines the reponse back to the user after succcessfully
+		// authenticating against GitHub
+		successHTML = `
+      <!doctype html>
+      <html>
+      <head><link rel="stylesheet" href="/index.css"></head>
+      <body id="github-auth-callback-page">
+      <h1 class="text-center">GitHub authorization is done.</h1>
+      <p class="text-center"><button class="btn btn-primary" onclick="window.close()">Close window and continue</button></p>
+      <script>
+        (function () {
+          window.opener.postMessage(JSON.stringify({
+            name: 'GitHub Oauth - token',
+            data: %s
+          }), window.location.origin);
+        })();
+      </script>
+      </body>
+      </html>`
 	)
 
 	state := c.FormValue("state")
 	if state != oauthStateString {
-		log.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
-		// http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		c.Redirect(500, "/")
-		return fmt.Errorf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		msg := fmt.Sprintf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		log.Printf(msg)
+		return c.String(http.StatusBadRequest, msg)
 	}
 
+	// TODO (wchrisjohnson) SECURITY RISK - remove this log statement once running
 	log.Printf("Got state: %s", state)
 
 	code := c.FormValue("code")
 	token, err := oauthConf.Exchange(oauth2.NoContext, code)
 	if err != nil {
-		log.Printf("oauthConf.Exchange() failed with '%s'\n", err)
-		// http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		c.Redirect(500, "/")
-		return fmt.Errorf("oauthConf.Exchange() failed with '%s'\n", err)
+		msg := fmt.Sprintf("oauthConf.Exchange() failed with '%s'\n", err)
+		log.Printf(msg)
+		return c.String(http.StatusBadRequest, msg)
 	}
 
+	// TODO (wchrisjohnson) SECURITY RISK - remove this log statement once running
 	log.Printf("Got token: %s", token)
 
-	// oauthClient := oauthConf.Client(oauth2.NoContext, token)
-	// client := github.NewClient(oauthClient)
-	// user, _, err := client.Users.Get("")
-	// if err != nil {
-	// 	fmt.Printf("client.Users.Get() faled with '%s'\n", err)
-	// 	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-	// 	return
-	// }
-
-	// log.Printf("Logged in as GitHub user: %s\n", *user.Login)
-	// http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-
-	//
-	// send conf page to user
-	return nil
+	return c.String(http.StatusOK, fmt.Sprintf(successHTML, token))
 }
