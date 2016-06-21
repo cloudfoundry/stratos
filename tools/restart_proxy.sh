25d313179919250c59e56c53e5c4c741f302bef0
woodnt <woodm1979@gmail.com>
2016-06-21 11:40:55 -0600

    created restart_proxy.sh

---
+++ tools/restart_proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MAINDIR="$( cd "$( dirname "${TOOLSDIR}" )" && pwd )"
+
+pushd "${MAINDIR}"
+docker-compose -f docker-compose.development.yml stop proxy
+docker-compose -f docker-compose.development.yml rm -fa proxy
+
+pushd $GOPATH/src/github.com/hpcloud/portal-proxy
+./tools/build_portal_proxy.sh
+popd
+
+docker-compose -f docker-compose.development.yml up -d proxy
+popd
