c1c09dcfceff516fe8f25b9f2d3730eb2e79660d
Julien Bramary <julien.bramary@hpe.com>
2016-07-27 13:48:15 +0100

    Made input form names unique, started duplicate space name detection

--- src/app/view/endpoints/clusters/cluster/detail/actions/cluster-actions.directive.js
+++ src/app/view/endpoints/clusters/cluster/detail/actions/cluster-actions.directive.js
@@ -53,113 +53,141 @@
       return _.get(org, 'details.org.entity.name');
     }
 
+    var createOrg = {
+      name: gettext('Create Organization'),
+      disabled: true,
+      execute: function () {
+        return asyncTaskDialog(
+          {
+            title: gettext('Create Organization'),
+            templateUrl: 'app/view/endpoints/clusters/cluster/detail/actions/create-organization.html',
+            buttonTitles: {
+              submit: gettext('Create')
+            }
+          },
+          {
+            data: {
+              // Make the form invalid if the name is already taken
+              organizationNames: organizationModel.organizationNames[that.clusterGuid]
+            }
+          },
+          function (orgData) {
+            if (orgData.name && orgData.name.length > 0) {
+              return organizationModel.createOrganization(that.clusterGuid, orgData.name);
+            } else {
+              return $q.reject('Invalid Name!');
+            }
+
+          }
+        );
+      },
+      icon: 'helion-icon-lg helion-icon helion-icon-Tree'
+    };
+
+    var createSpace = {
+      name: gettext('Create Space'),
+      disabled: true,
+      execute: function () {
+
+        var contextData;
+        var existingSpaceNames;
+
+        // Create organization is context sensitive!
+        var selectedOrg;
+        if ($stateParams.organization) {
+          selectedOrg = organizationModel.organizations[that.clusterGuid][$stateParams.organization];
+        } else {
+          // Pre-select the most recently created organization
+          var sortedOrgs = _.sortBy(organizationModel.organizations[that.clusterGuid], function (org) {
+            return -org.details.created_at;
+          });
+          selectedOrg = sortedOrgs[0];
+        }
+
+        var orgSpaces = organizationModel.organizations[that.clusterGuid][selectedOrg.details.guid].spaces;
+        existingSpaceNames = _.map(orgSpaces, function (space) {
+          return space.entity.name;
+        });
+
+        function setOrganization() {
+          console.log('Org changed to: ' + contextData.organization);
+        }
+
+        function createSpaceDisabled() {
+          if (contextData.spaces.length >= 10) {
+            return true;
+          }
+          // Make sure all spaces have a valid name before allowing creating another
+          for (var i = 0; i < contextData.spaces.length; i++) {
+            var spaceName = contextData.spaces[i];
+            if (angular.isUndefined(spaceName) || spaceName.length < 1) {
+              return true;
+            }
+          }
+          return false;
+        }
+
+        function addSpace() {
+          if (createSpaceDisabled()) {
+            return;
+          }
+          contextData.spaces.push('');
+        }
+
+        function removeSpace() {
+          contextData.spaces.length--;
+        }
+
+        contextData = {
+          organization: selectedOrg,
+          organizations: _.map(organizationModel.organizations[that.clusterGuid], function (org) {
+            return {
+              label: getOrgName(org),
+              value: org
+            };
+          }),
+          existingSpaceNames: existingSpaceNames,
+          spaces: [''],
+          setOrganization: setOrganization,
+          createSpaceDisabled: createSpaceDisabled,
+          addSpace: addSpace,
+          removeSpace: removeSpace
+        };
+        return asyncTaskDialog(
+          {
+            title: gettext('Create Space'),
+            templateUrl: 'app/view/endpoints/clusters/cluster/detail/actions/create-space.html',
+            buttonTitles: {
+              submit: gettext('Create')
+            }
+          },
+          {
+            data: contextData
+          },
+          function (spaceData) {
+            if (spaceData.name && spaceData.name.length > 0) {
+              return $q.resolve('Valid Name!');
+            } else {
+              return $q.reject('Invalid Name!');
+            }
+          }
+        );
+      },
+      icon: 'helion-icon-lg helion-icon helion-icon-Tree'
+    };
+
+    var assignUsers = {
+      name: gettext('Assign User(s)'),
+      disabled: true,
+      execute: function () {
+      },
+      icon: 'helion-icon-lg helion-icon helion-icon-Add_user'
+    };
+
     this.clusterActions = [
-      {
-        name: gettext('Create Organization'),
-        disabled: true,
-        execute: function () {
-          return asyncTaskDialog(
-            {
-              title: gettext('Create Organization'),
-              templateUrl: 'app/view/endpoints/clusters/cluster/detail/actions/create-organization.html',
-              buttonTitles: {
-                submit: gettext('Create')
-              }
-            },
-            {
-              data: {
-                // Make the form invalid if the name is already taken
-                organizationNames: organizationModel.organizationNames[that.clusterGuid]
-              }
-            },
-            function (orgData) {
-              if (orgData.name && orgData.name.length > 0) {
-                return organizationModel.createOrganization(that.clusterGuid, orgData.name);
-              } else {
-                return $q.reject('Invalid Name!');
-              }
-
-            }
-          );
-        },
-        icon: 'helion-icon-lg helion-icon helion-icon-Tree'
-      },
-      {
-        name: gettext('Create Space'),
-        disabled: true,
-        execute: function () {
-
-          var contextData;
-
-          // Create organization is context sensitive!
-          var selectedOrg;
-          if ($stateParams.organization) {
-            selectedOrg = getOrgName(organizationModel.organizations[that.clusterGuid][$stateParams.organization]);
-          } else {
-            // Pre-select the most recently created organization
-            var sortedOrgs = _.sortBy(organizationModel.organizations[that.clusterGuid], function (org) {
-              return -org.details.created_at;
-            });
-            selectedOrg = getOrgName(sortedOrgs[0]);
-          }
-
-          function setOrganization() {
-            console.log('Org changed!' + contextData.organization);
-          }
-
-          function addSpace() {
-            if (contextData.spaces.length < 10) {
-              contextData.spaces.push('');
-            }
-          }
-
-          function removeSpace() {
-            contextData.spaces.length--;
-          }
-
-          contextData = {
-            organization: selectedOrg,
-            organizations: _.map(organizationModel.organizationNames[that.clusterGuid], function (name) {
-              return {
-                label: name,
-                value: name
-              };
-            }),
-            spaceNames: [],
-            spaces: [''],
-            setOrganization: setOrganization,
-            addSpace: addSpace,
-            removeSpace: removeSpace
-          };
-          return asyncTaskDialog(
-            {
-              title: gettext('Create Space'),
-              templateUrl: 'app/view/endpoints/clusters/cluster/detail/actions/create-space.html',
-              buttonTitles: {
-                submit: gettext('Create')
-              }
-            },
-            {
-              data: contextData
-            },
-            function (spaceData) {
-              if (spaceData.name && spaceData.name.length > 0) {
-                return $q.resolve('Valid Name!');
-              } else {
-                return $q.reject('Invalid Name!');
-              }
-            }
-          );
-        },
-        icon: 'helion-icon-lg helion-icon helion-icon-Tree'
-      },
-      {
-        name: gettext('Assign User(s)'),
-        disabled: true,
-        execute: function () {
-        },
-        icon: 'helion-icon-lg helion-icon helion-icon-Add_user'
-      }
+      createOrg,
+      createSpace,
+      assignUsers
     ];
 
     /**
