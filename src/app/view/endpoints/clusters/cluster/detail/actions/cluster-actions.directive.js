6ef645294291a4fb041a6e26e32109538b3ca95a
Kelly Domico <kelly.domico@hpe.com>
2016-07-28 11:23:44 -0700

    Fix lint issue

--- src/app/view/endpoints/clusters/cluster/detail/actions/cluster-actions.directive.js
+++ src/app/view/endpoints/clusters/cluster/detail/actions/cluster-actions.directive.js
@@ -236,7 +236,7 @@
       require: 'ngModel',
       link: function (scope, elm, attrs, ctrl) {
         var index = parseInt(attrs.uniqueSpaceName, 10);
-        ctrl.$validators.dupeName = function (modelValue, viewValue) {
+        ctrl.$validators.dupeName = function (modelValue) {
           if (ctrl.$isEmpty(modelValue)) {
             return true;
           }
