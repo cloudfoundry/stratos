7fb2e107a8a85b16a2c6447704dffb0150cd3c73
Shaoquan Chen <sean.chen2@hp.com>
2016-08-04 11:42:14 -0700

    695 - Creating add-pipeline workflow
    This is the first baby step to create a add-pile-workflow prototype spot that the code can be shareable between aft-pipeline workflow and add-app workflow.

---
+++ src/plugins/cloud-foundry/view/applications/workflows/workflows.module.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('cloud-foundry.view.applications.workflows', [
+      'cloud-foundry.view.applications.workflows.add-pipeline-workflow'
+    ]);
+
+})();
