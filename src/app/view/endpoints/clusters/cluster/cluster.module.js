727bf034bb0844aca36975982950745064c60167
Julien Bramary <julbra@users.noreply.github.com>
2016-08-10 13:15:09 +0100

    Endpoints init after login (#547)
    * Delay initialization of endpoints dashboard until the login event is emitted
    
    * Filter out spaces from different organizations in org user tab
    
    * lint

--- src/app/view/endpoints/clusters/cluster/cluster.module.js
+++ src/app/view/endpoints/clusters/cluster/cluster.module.js
@@ -37,7 +37,6 @@
     var organizationModel = modelManager.retrieve('cloud-foundry.model.organization');
     var serviceBindingModel = modelManager.retrieve('cloud-foundry.model.service-binding');
     var appModel = modelManager.retrieve('cloud-foundry.model.application');
-    var stackatoInfoModel = modelManager.retrieve('app.model.stackatoInfo');
 
     this.initialized = false;
     this.guid = $stateParams.guid;
@@ -81,16 +80,13 @@
       // Needed to show a Space's list of service instances (requires app name, from app guid, from service binding)
       var serviceBindingPromise = serviceBindingModel.listAllServiceBindings(that.guid);
 
-      var stackatoInfoPromise = stackatoInfoModel.getStackatoInfo();
-
       // Reset any cache we may be interested in
       delete appModel.appSummary;
 
       return $q.all([
         orgPromise,
         servicesPromise,
-        serviceBindingPromise,
-        stackatoInfoPromise])
+        serviceBindingPromise])
         .finally(function () {
           that.initialized = true;
         });
