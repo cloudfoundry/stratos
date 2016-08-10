a993ed80ed71ccffb28ae43572fbe6b872b225de
Tara Gibbs <tara.gibbs@hp.com>
2016-08-10 15:45:37 -0700

    create goosedbversion package, pull db version from database

--- version_info.go
+++ version_info.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
+
+	"github.com/hpcloud/portal-proxy/repository/goose-db-version"
 )
 
 // Allow us to mock out os.Getenv
@@ -13,30 +16,40 @@ var osGetEnv = os.Getenv
 // Versions - response returned to caller from a getVersions action
 type Versions struct {
 	ProxyVersion    string `json:"proxy_version"`
-	DatabaseVersion string `json:"database_version"`
+	DatabaseVersion int64  `json:"database_version"`
 }
 
-func (p *portalProxy) getVersionsData() *Versions {
+func (p *portalProxy) getVersionsData() (*Versions, error) {
 	proxyVersion := osGetEnv("CONSOLE_VERSION")
 	// Default CONSOLE_VERSION to be "dev"
 	if proxyVersion == "" {
 		proxyVersion = "dev"
 	}
 
-	databaseVersion := osGetEnv("DATABASE_VERSION")
-	// Default DATABASE_VERSION to be the same as proxy_version
-	if databaseVersion == "" {
-		databaseVersion = proxyVersion
+	dbVersionRepo, err := goosedbversion.NewPostgresGooseDBVersionRepository(p.DatabaseConnectionPool)
+	if err != nil {
+		return &Versions{}, fmt.Errorf("getVersionsData: %s", err)
 	}
+
+	databaseVersionRec, _ := dbVersionRepo.GetCurrentVersion()
+	if err != nil {
+		return &Versions{}, fmt.Errorf("getVersionsData: %s", err)
+	}
+
+	databaseVersion := databaseVersionRec.VersionID
 
 	resp := &Versions{
 		ProxyVersion:    proxyVersion,
 		DatabaseVersion: databaseVersion,
 	}
 
-	return resp
+	return resp, nil
 }
 
 func (p *portalProxy) getVersions(c echo.Context) error {
-	return c.JSON(http.StatusOK, p.getVersionsData())
+	v, err := p.getVersionsData()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, v)
 }
