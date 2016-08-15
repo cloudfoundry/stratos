e8e4ea28797ac46076c3aa9f51a4a43d202cb625
Julien Bramary <julbra@users.noreply.github.com>
2016-08-15 13:26:23 +0100

    Correctly update cached names after last week's optimizations broke this (#567)

--- src/plugins/cloud-foundry/model/space/space.model.js
+++ src/plugins/cloud-foundry/model/space/space.model.js
@@ -508,10 +508,13 @@
       var spaceApi = this.apiManager.retrieve('cloud-foundry.api.Spaces');
       return spaceApi.UpdateSpace(spaceGuid, spaceData, {}, this.makeHttpConfig(cnsiGuid)).then(function (val) {
         // Refresh the org!
+        that.organizationModel.uncacheOrganizationSpaces(cnsiGuid, orgGuid);
         var org = that.organizationModel.organizations[cnsiGuid][orgGuid].details.org;
         var orgRefreshedP = that.organizationModel.getOrganizationDetails(cnsiGuid, org);
+
         // Refresh the space!
         var spaceRefreshedP = that.getSpaceDetails(cnsiGuid, val.data, {});
+
         return that.$q.all([orgRefreshedP, spaceRefreshedP]);
       });
     }
