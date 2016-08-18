f2a5efabce79df05bdd61b0ba2935236225ba7e8
Shaoquan Chen <sean.chen2@hp.com>
2016-08-17 20:20:34 -0700

    595 - Toast Notifications End of Add App flow

--- src/plugins/cloud-foundry/view/applications/workflows/add-app-workflow/add-app-workflow.directive.js
+++ src/plugins/cloud-foundry/view/applications/workflows/add-app-workflow/add-app-workflow.directive.js
@@ -476,6 +476,29 @@
         [].push.apply(this.data.workflow.steps, this.data.subflows.cli);
       },
 
+      /**
+       * @function notify
+       * @memberOf cloud-foundry.view.applications.AddAppWorkflowController
+       * @description notify success
+       */
+      notify: function () {
+        if (!this.userInput.application) {
+          return;
+        }
+
+        var href = [
+          '#/cf/applications',
+          this.userInput.serviceInstance.guid,
+          'app',
+          this.userInput.application.summary.guid,
+          'summary'
+        ].join('/');
+
+        this.eventService.$emit('cf.events.NOTIFY_SUCCESS', {
+          message: gettext('A new app has been created: <a href="' + href + '">' + this.userInput.name + '</a>')
+        });
+      },
+
       startWorkflow: function () {
         var that = this;
         this.addingApplication = true;
@@ -495,6 +518,7 @@
       },
 
       stopWorkflow: function () {
+        this.notify();
         this.addingApplication = false;
       },
 
@@ -502,6 +526,7 @@
         if (this.options.subflow === 'pipeline') {
           this.triggerPipeline();
         }
+        this.notify();
         this.addingApplication = false;
       }
     });
