9a66e4acf35be0264dff3316c9adcb48c8574b63
Chris Johnson <wchrisjohnson@gmail.com>
2016-08-24 18:41:10 -0400

    Clean up and document init script

--- containers/etcd/run.sh
+++ containers/etcd/run.sh
@@ -3,26 +3,37 @@ set -o pipefail
 
 pushd etcd-v2.3.7-linux-amd64
 
+# Define ports and their use
+CLIENT_COMM_PORT="2379"
+PEER_COMM_PORT="2380"
+
+# These ENV VARs are network related and are based on the hostname
+# that HCP gives each component at runtime.
 HOST_NAME="hsc-etcd-${HCP_COMPONENT_INDEX}-int"
 echo "This container is named: ${HOST_NAME}"
 
-ADVERTISE_PEER_URLS="http://${HOST_NAME}:2380"
+ADVERTISE_PEER_URLS="http://${HOST_NAME}:${PEER_COMM_PORT}"
 echo "Advertise peer URLs: ${ADVERTISE_PEER_URLS}"
 
-ADVERTISE_CLIENT_URLS="http://${HOST_NAME}:2379"
+ADVERTISE_CLIENT_URLS="http://${HOST_NAME}:${CLIENT_COMM_PORT}"
 echo "Advertise client URLs: ${ADVERTISE_CLIENT_URLS}"
 
-
-LISTEN_PEER_URLS="http://0.0.0.0:2380"
+# These ENV VARs are generally applicable for each etcd node that comes up
+LISTEN_PEER_URLS="http://0.0.0.0:${PEER_COMM_PORT}"
 echo "Listen peer URLs: ${LISTEN_PEER_URLS}"
 
-LISTEN_CLIENT_URLS="http://0.0.0.0:2379"
+LISTEN_CLIENT_URLS="http://0.0.0.0:${CLIENT_COMM_PORT}"
 echo "Listen client URLs: ${LISTEN_CLIENT_URLS}"
 
 INITIAL_CLUSTER_TOKEN="hsc-etcd-cluster-1"
 echo "Initial cluster token: ${INITIAL_CLUSTER_TOKEN}"
 
-INITIAL_CLUSTER="hsc-etcd-0-int=http://hsc-etcd-0-int:2380,hsc-etcd-1-int=http://hsc-etcd-1-int:2380,hsc-etcd-2-int=http://hsc-etcd-2-int:2380"
+# Define the members of the cluster
+CM0="hsc-etcd-0-int"
+CM1="hsc-etcd-1-int"
+CM2="hsc-etcd-2-int"
+
+INITIAL_CLUSTER="${CM0}=http://${CM0}:${PEER_COMM_PORT},${CM1}=http://${CM1}:${PEER_COMM_PORT},${CM2}=http://${CM2}:${PEER_COMM_PORT}"
 echo "Initial cluster: ${INITIAL_CLUSTER}"
 
 INITIAL_CLUSTER_STATE="new"
@@ -31,7 +42,7 @@ echo "Initial cluster state: ${INITIAL_CLUSTER_STATE}"
 
 log_cmd=''
 if [[ -n "$HCP_FLIGHTRECORDER_HOST" && -n "$HCP_FLIGHTRECORDER_PORT" ]]; then
-  log_cmd="2>&1 | tee >(logger -t helion-stackato-console-etcd -n 127.0.0.1 -P 514)"
+  log_cmd="2>&1 | tee >(logger -t hsc-etcd -n 127.0.0.1 -P 514)"
   mkdir -p /etc/rsyslog.d
   echo "*.* @@${HCP_FLIGHTRECORDER_HOST}:${HCP_FLIGHTRECORDER_PORT}" > /etc/rsyslog.d/flight-recorder.conf
   service rsyslog start
