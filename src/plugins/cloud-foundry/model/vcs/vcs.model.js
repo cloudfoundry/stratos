6dd3a729198b18b056cb049dcccddd9576948dc8
Kelly Domico <kelly.domico@hpe.com>
2016-08-26 13:39:59 -0700

    Cache VCS clients

--- src/plugins/cloud-foundry/model/vcs/vcs.model.js
+++ src/plugins/cloud-foundry/model/vcs/vcs.model.js
@@ -12,23 +12,29 @@
     .run(registerVcsModel);
 
   registerVcsModel.$inject = [
+    '$q',
     'app.model.modelManager',
     'app.api.apiManager'
   ];
 
-  function registerVcsModel(modelManager, apiManager) {
-    modelManager.register('cloud-foundry.model.vcs', new VcsModel(apiManager));
+  function registerVcsModel($q, modelManager, apiManager) {
+    modelManager.register('cloud-foundry.model.vcs', new VcsModel($q, apiManager));
   }
 
   /**
    * @memberof cloud-foundry.model.hce
    * @name VcsModel
+   * @param {object} $q - the Angular $q service
    * @param {app.api.apiManager} apiManager - the application API manager
+   * @property {object} $q - the Angular $q service
    * @property {app.api.apiManager} apiManager - the application API manager
+   * @property {array} vcsClients - the list of VCS clients
    * @class
    */
-  function VcsModel(apiManager) {
+  function VcsModel($q, apiManager) {
+    this.$q = $q;
     this.apiManager = apiManager;
+    this.vcsClients = null;
   }
 
   angular.extend(VcsModel.prototype, {
@@ -36,12 +42,24 @@
      * @function listVcsClients
      * @memberof cloud-foundry.model.vcs.VcsModel
      * @description Get the list of valid VCS clients
+     * @param {boolean} forceFetch - force fetch VCS clients
      * @returns {promise} A promise object
      * @public
      */
-    listVcsClients: function () {
-      return this.apiManager.retrieve('cloud-foundry.api.Vcs')
-        .listVcsClients();
+    listVcsClients: function (forceFetch) {
+      if (forceFetch || this.vcsClients === null) {
+        var that = this;
+        return this.apiManager.retrieve('cloud-foundry.api.Vcs')
+          .listVcsClients()
+          .then(function (response) {
+            that.vcsClients = response.data;
+            return response;
+          });
+      } else {
+        var deferred = this.$q.defer();
+        deferred.resolve({data: this.vcsClients});
+        return deferred.promise;
+      }
     }
   });
 
