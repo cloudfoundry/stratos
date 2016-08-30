7a5c6e05211f21bde5a27854b6af7ba460548ac3
Neil MacDougall <neil.macdougall@hpe.com>
2016-08-30 15:24:34 +0100

    TEAMFOUR-980: Fix footer links
    Removed controller and used inline URLs with translate filter

--- src/app/view/page-footer/page-footer.directive.js
+++ src/app/view/page-footer/page-footer.directive.js
@@ -19,25 +19,8 @@
    */
   function pageFooter(path) {
     return {
-      templateUrl: path + 'view/page-footer/page-footer.html',
-      controller: FooterController,
-      controllerAs: 'footerCtrl'
+      templateUrl: path + 'view/page-footer/page-footer.html'
     };
   }
 
-  FooterController.$inject = [];
-
-  /**
-   * @namespace app.view.pageFooter
-   * @memberof app.view
-   * @name FooterController
-   * @description Controller for the footer - provides footer links
-   * @constructor
-   */
-  function FooterController() {
-    this.links = {
-      privacy: gettext('https://www.hpe.com/us/en/legal/privacy.html'),
-      terms: gettext('http://docs.hpcloud.com/permalink/helion-openstack/3.0/eula')
-    };
-  }
 })();
