cd322d4e05234a6b859a3128c500ef1c184ee951
Chris Johnson <wchrisjohnson@gmail.com>
2016-09-01 12:01:09 -0400

    expand from 3-5 instances

--- containers/etcd2/run.sh
+++ containers/etcd2/run.sh
@@ -10,7 +10,7 @@ PEER_COMM_PORT="2380"
 # These ENV VARs are network related and are based on the hostname
 # that HCP gives each component at runtime.
 HOST_NAME="hsc-etcd-${HCP_COMPONENT_INDEX}-int"
-echo "This container is named: ${HOST_NAME}"
+echo "ETCD container hostname setting: ${HOST_NAME}"
 
 ADVERTISE_PEER_URLS="http://${HOST_NAME}:${PEER_COMM_PORT}"
 echo "Advertise peer URLs: ${ADVERTISE_PEER_URLS}"
@@ -28,36 +28,40 @@ echo "Listen client URLs: ${LISTEN_CLIENT_URLS}"
 INITIAL_CLUSTER_TOKEN="hsc-etcd-cluster-1"
 echo "Initial cluster token: ${INITIAL_CLUSTER_TOKEN}"
 
+INITIAL_CLUSTER_STATE="new"
+echo "Initial cluster state: ${INITIAL_CLUSTER_STATE}"
+
 # Define the members of the cluster
 CM0="hsc-etcd-0-int"
 CM1="hsc-etcd-1-int"
 CM2="hsc-etcd-2-int"
+CM3="hsc-etcd-3-int"
+CM4="hsc-etcd-4-int"
 
-INITIAL_CLUSTER="${CM0}=http://${CM0}:${PEER_COMM_PORT},${CM1}=http://${CM1}:${PEER_COMM_PORT},${CM2}=http://${CM2}:${PEER_COMM_PORT}"
+INITIAL_CLUSTER="${CM0}=http://${CM0}:${PEER_COMM_PORT},${CM1}=http://${CM1}:${PEER_COMM_PORT},${CM2}=http://${CM2}:${PEER_COMM_PORT},${CM3}=http://${CM3}:${PEER_COMM_PORT},${CM4}=http://${CM4}:${PEER_COMM_PORT}"
 echo "Initial cluster: ${INITIAL_CLUSTER}"
-
-INITIAL_CLUSTER_STATE="new"
-echo "Initial cluster state: ${INITIAL_CLUSTER_STATE}"
-
 
 log_cmd=''
 if [[ -n "$HCP_FLIGHTRECORDER_HOST" && -n "$HCP_FLIGHTRECORDER_PORT" ]]; then
-  log_cmd="2>&1 | tee >(logger -t hsc-etcd -n 127.0.0.1 -P 514)"
+  log_cmd="2>&1 | tee >(logger -t ${HOST_NAME} -n 127.0.0.1 -P 514)"
   mkdir -p /etc/rsyslog.d
   echo "*.* @@${HCP_FLIGHTRECORDER_HOST}:${HCP_FLIGHTRECORDER_PORT}" > /etc/rsyslog.d/flight-recorder.conf
   service rsyslog start
 fi
 
-cmd="
-ETCD_NAME=${HOST_NAME}
-ETCD_INITIAL_ADVERTISE_PEER_URLS=${ADVERTISE_PEER_URLS}
-ETCD_LISTEN_PEER_URLS=${LISTEN_PEER_URLS}
-ETCD_LISTEN_CLIENT_URLS=${LISTEN_CLIENT_URLS}
-ETCD_ADVERTISE_CLIENT_URLS=${ADVERTISE_CLIENT_URLS}
-ETCD_INITIAL_CLUSTER_TOKEN=${INITIAL_CLUSTER_TOKEN}
-ETCD_INITIAL_CLUSTER_STATE=${INITIAL_CLUSTER_STATE}
-ETCD_INITIAL_CLUSTER=${INITIAL_CLUSTER}
-./etcd
-"
+# cmd="
+# ETCD_NAME=${HOST_NAME}
+# ETCD_INITIAL_ADVERTISE_PEER_URLS=${ADVERTISE_PEER_URLS}
+# ETCD_LISTEN_PEER_URLS=${LISTEN_PEER_URLS}
+# ETCD_LISTEN_CLIENT_URLS=${LISTEN_CLIENT_URLS}
+# ETCD_ADVERTISE_CLIENT_URLS=${ADVERTISE_CLIENT_URLS}
+# ETCD_INITIAL_CLUSTER_TOKEN=${INITIAL_CLUSTER_TOKEN}
+# ETCD_INITIAL_CLUSTER_STATE=${INITIAL_CLUSTER_STATE}
+# ETCD_INITIAL_CLUSTER=${INITIAL_CLUSTER}
+# ./etcd
+# "
 
+cmd="./etcd --name ${HOST_NAME} --initial-advertise-peer-urls ${ADVERTISE_PEER_URLS} --listen-peer-urls ${LISTEN_PEER_URLS} --listen-client-urls ${LISTEN_CLIENT_URLS} --advertise-client-urls ${ADVERTISE_CLIENT_URLS} --initial-cluster-token ${INITIAL_CLUSTER_TOKEN} --initial-cluster-state ${INITIAL_CLUSTER_STATE} --initial-cluster ${INITIAL_CLUSTER}"
+
+echo "Command to be executed: $cmd $log_cmd"
 eval "$cmd $log_cmd"
