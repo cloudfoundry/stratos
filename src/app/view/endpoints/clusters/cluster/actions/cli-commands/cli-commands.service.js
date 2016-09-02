a5b2b5c9e133f01ffe0351c89f1a8e2008f36360
richard-cox <richard.cox@hpe.com>
2016-09-02 15:45:50 +0100

    Tweaked text, added ngdoc

--- src/app/view/endpoints/clusters/cluster/actions/cli-commands/cli-commands.service.js
+++ src/app/view/endpoints/clusters/cluster/actions/cli-commands/cli-commands.service.js
@@ -9,9 +9,27 @@
     'helion.framework.widgets.detailView'
   ];
 
+  /**
+   * @memberof app.view.endpoints.clusters.cluster
+   * @name CliCommandsFactory
+   * @description Factory to provide a way to show the cli commands for HCF
+   * @constructor
+   * @param {helion.framework.widgets.detailView} detailView - The console's detailView service
+   */
   function CliCommandsFactory(detailView) {
 
     return {
+      /**
+       * @function show
+       * @memberof app.view.endpoints.clusters.cluster
+       * @description Show a detail view containing basic cli instructions
+       * @param {string=} api - The api url used to connect the HCF cli tool to the required HCF
+       * @param {string=} username - Username used to connect to HCF
+       * @param {string=} orgName - Target organization's name
+       * @param {string=} spaceName - Target space name
+       * @returns {promise} A detail view promise
+       * @public
+       */
       show: function (api, username, orgName, spaceName) {
         return detailView(
           {
