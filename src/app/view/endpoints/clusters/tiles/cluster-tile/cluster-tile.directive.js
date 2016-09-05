7c6c496902b78b748554194b751095248c239b56
Julien Bramary <julien.bramary@hpe.com>
2016-09-05 15:08:42 +0100

    Removed redundant _.get inside watch

--- src/app/view/endpoints/clusters/tiles/cluster-tile/cluster-tile.directive.js
+++ src/app/view/endpoints/clusters/tiles/cluster-tile/cluster-tile.directive.js
@@ -85,7 +85,7 @@
       return cardData;
     };
 
-    $scope.$watch(function () { return _.get(that.service, 'guid'); }, function (newVal) {
+    $scope.$watch(function () { return that.service.guid; }, function (newVal) {
       if (!newVal) {
         return;
       }
