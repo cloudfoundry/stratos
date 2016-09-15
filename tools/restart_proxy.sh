27f53939619206bed59657a88cb3f0301bc34882
Tara Gibbs <tara.gibbs@hp.com>
2016-09-15 12:44:22 -0700

    make sure to use the environment variables in development.rc when restarting the proxy

--- tools/restart_proxy.sh
+++ tools/restart_proxy.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+ENV_RC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+ENV_RC="development.rc"
 TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MAINDIR="$( cd "$( dirname "${TOOLSDIR}" )" && pwd )"
 
+function env_vars {
+    echo "===== Environment variables"
+    if [ -f $ENV_RC ] ; then
+        echo "Found environment variables file: $ENV_RC"
+        source $ENV_RC
+    else
+        echo "Did not find environment variables file: $ENV_RC"
+        echo "Your build may fail if the proper environment variables are missing."
+    fi
+}
+
+pushd "${ENV_RC_DIR}"
+env_vars
 pushd "${MAINDIR}"
 docker-compose -f docker-compose.development.yml stop proxy
 docker-compose -f docker-compose.development.yml rm -fa proxy
