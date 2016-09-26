b932cacf399978d8ca61de27cae2f61d9475605a
Julien Bramary <julien.bramary@hpe.com>
2016-09-26 17:15:17 +0100

    Fix test

--- src/plugins/cloud-foundry/view/applications/application/delivery-pipeline/delivery-pipeline.module.spec.js
+++ src/plugins/cloud-foundry/view/applications/application/delivery-pipeline/delivery-pipeline.module.spec.js
@@ -53,7 +53,7 @@
 
     describe('Check HCE status', function () {
       it('Non-admin user with no services', function () {
-        account.data = {isAdmin: false};
+        account.accountData = {isAdmin: false};
         createController();
         expect(controller.hceServices.fetching).toBe(false);
         expect(controller.hceServices.valid).toBe(0);
@@ -62,7 +62,7 @@
       });
 
       it('Admin user with no services', function () {
-        account.data = {isAdmin: true};
+        account.accountData = {isAdmin: true};
         createController();
         expect(controller.hceServices.fetching).toBe(false);
         expect(controller.hceServices.valid).toBe(0);
