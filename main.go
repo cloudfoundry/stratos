9b69609c64d3e25fa5be8b84352a5516ca687cf2
Julien Bramary <julien.bramary@hpe.com>
2016-10-05 13:01:50 +0100

    Will separate secure cookie into another PR

--- main.go
+++ main.go
@@ -129,7 +129,9 @@ func main() {
 	// Setup cookie-store options
 	sessionStore.Options.MaxAge = SessionExpiry
 	sessionStore.Options.HttpOnly = true
-	sessionStore.Options.Secure = true
+	// TODO (wchrisjohnson) HSC-1160 Determine how we can enable this option and yet
+	// not disrupt our UI development workflow, specifically wrt the use of gulp.
+	// sessionStore.Options.Secure = true
 
 	defer func() {
 		logger.Info(`--- Closing sessionStore`)
