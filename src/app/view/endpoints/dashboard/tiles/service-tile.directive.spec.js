db67912494d875bbbc1da766aec0a6b49d15cdc1
Richard <richard-cox@users.noreply.github.com>
2016-10-17 13:47:33 +0100

    HSC-1241: Add tests for cluster-actions and service-tile directives (#767)
    * Expanded cluster-actions.directive + service-tile.directive
    
    * Merge Fix

--- src/app/view/endpoints/dashboard/tiles/service-tile.directive.spec.js
+++ src/app/view/endpoints/dashboard/tiles/service-tile.directive.spec.js
@@ -3,17 +3,9 @@
 
   describe('service-registration directive', function () {
     var $compile, $httpBackend, $scope, $q;
-    var detailViewCalled = false;
 
     beforeEach(module('templates'));
     beforeEach(module('green-box-console'));
-    beforeEach(module(function ($provide) {
-      var mock = function () {
-        detailViewCalled = true;
-        return {rendered: $q.resolve(), result: $q.reject()};
-      };
-      $provide.value('helion.framework.widgets.detailView', mock);
-    }));
     beforeEach(inject(function ($injector) {
       $compile = $injector.get('$compile');
       $httpBackend = $injector.get('$httpBackend');
@@ -51,12 +43,80 @@
 
       it('should be defined', function () {
         expect(element).toBeDefined();
+        expect(serviceTileCtrl).toBeDefined();
       });
 
-      it('should show cluster registration detail view when showClusterAddForm is invoked', function () {
+      it('should show register hcf if tile is hcf', function () {
+        serviceTileCtrl.serviceType = 'hcf';
+        spyOn(serviceTileCtrl.hcfRegistration, 'add').and.returnValue($q.resolve());
         serviceTileCtrl.showClusterAddForm();
-        expect(detailViewCalled).toBe(true);
+        expect(serviceTileCtrl.hcfRegistration.add).toHaveBeenCalled();
       });
+
+      it('should show register hce if tile is hce', function () {
+        serviceTileCtrl.serviceType = 'hce';
+        spyOn(serviceTileCtrl.hceRegistration, 'add').and.returnValue($q.resolve());
+        serviceTileCtrl.showClusterAddForm();
+        expect(serviceTileCtrl.hceRegistration.add).toHaveBeenCalled();
+      });
+
+      it('_listServiceInstances - with cached data', function () {
+        spyOn(serviceTileCtrl, '_updateChart');
+
+        expect(serviceTileCtrl.resolvedPromise).toBeFalsy();
+        serviceTileCtrl.useCachedData = true;
+        serviceTileCtrl._listServiceInstances();
+        $scope.$digest();
+
+        expect(serviceTileCtrl.resolvedPromise).toBeTruthy();
+        expect(serviceTileCtrl._updateChart).toHaveBeenCalled();
+      });
+
+      it('_updateInstances', function () {
+        spyOn(serviceTileCtrl, '_updateChart');
+
+        var service = {
+          cnsi_type: 'service_type',
+          guid: 'serviceGuid'
+        };
+        serviceTileCtrl.serviceType = service.cnsi_type;
+        _.set(serviceTileCtrl, 'serviceInstanceModel.serviceInstances', [service]);
+        serviceTileCtrl._updateInstances();
+        var result = _.set({}, service.guid, service);
+
+        expect(serviceTileCtrl.serviceInstances).toEqual(result);
+        expect(serviceTileCtrl._updateChart).toHaveBeenCalled();
+      });
+
+      it('getInstancesCountByStatus - connected', function () {
+
+        expect(serviceTileCtrl.getInstancesCountByStatus('connected')).toBe(0);
+
+        serviceTileCtrl.serviceInstances = {
+          cnsiGuid: {}
+        };
+
+        expect(serviceTileCtrl.getInstancesCountByStatus('connected')).toBe(0);
+
+        serviceTileCtrl.userServiceInstanceModel.serviceInstances = {
+          cnsiGuid: {
+            valid: true
+          }
+        };
+        expect(serviceTileCtrl.getInstancesCountByStatus('connected')).toBe(1);
+      });
+
+      it('getInstancesCountByStatus - dsiconnected', function () {
+
+        expect(serviceTileCtrl.getInstancesCountByStatus('disconnected')).toBe(0);
+
+        serviceTileCtrl.serviceInstances = {
+          cnsiGuid: {}
+        };
+
+        expect(serviceTileCtrl.getInstancesCountByStatus('disconnected')).toBe(1);
+      });
+
     });
   });
 
