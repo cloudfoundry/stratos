c01602d38eb348763e49aa92a06c056afe619bc6
Richard <richard-cox@users.noreply.github.com>
2016-10-20 16:11:28 +0100

    HSC-124: Increase unit coverage #2 (#778)
    * Expanded cluster-actions.directive + service-tile.directive
    
    * Merge Fix
    
    * Assorted unit test, one change to user-selection.service
    user-selection.service change is an improvement on an undefined check
    
    * Assorted unit tests + tweaked npm targets
    - Change pretest so as to not run before unit tests in 'test' target
    - Added unit tests for cluster/org/space user table states
    - Added tests for user-selection service
    - Added some basic tests for space.model

--- src/plugins/cloud-foundry/api/hcf/Spaces.mock.js
+++ src/plugins/cloud-foundry/api/hcf/Spaces.mock.js
@@ -4,6 +4,70 @@
   /* eslint-disable quote-props */
   mock.cloudFoundryAPI = mock.cloudFoundryAPI || {};
 
+  var ListAllAppsForSpace = {
+    200: {
+
+      body: {
+        "total_results": 1,
+        "total_pages": 1,
+        "prev_url": null,
+        "next_url": null,
+        "resources": [
+          {
+            "metadata": {
+              "guid": "91587747-004b-4a44-9adf-61c764519f71",
+              "url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71",
+              "created_at": "2016-02-19T02:04:05Z",
+              "updated_at": "2016-02-19T02:04:05Z"
+            },
+            "entity": {
+              "name": "name-2500",
+              "production": false,
+              "space_guid": "b71b5455-6d01-4846-bab9-f2af6e4e7a4f",
+              "stack_guid": "07f55326-9ced-4ff2-bb42-a23525ff9425",
+              "buildpack": null,
+              "detected_buildpack": null,
+              "environment_json": null,
+              "memory": 1024,
+              "instances": 1,
+              "disk_quota": 1024,
+              "state": "STOPPED",
+              "version": "f6bb92c2-107e-41fc-9705-9d9892e30845",
+              "command": null,
+              "console": false,
+              "debug": null,
+              "staging_task_id": null,
+              "package_state": "PENDING",
+              "health_check_type": "port",
+              "health_check_timeout": null,
+              "staging_failed_reason": null,
+              "staging_failed_description": null,
+              "diego": false,
+              "docker_image": null,
+              "package_updated_at": "2016-02-19T02:04:05Z",
+              "detected_start_command": "",
+              "enable_ssh": true,
+              "docker_credentials_json": {
+                "redacted_message": "[PRIVATE DATA HIDDEN]"
+              },
+              "ports": null,
+              "space_url": "/v2/spaces/b71b5455-6d01-4846-bab9-f2af6e4e7a4f",
+              "stack_url": "/v2/stacks/07f55326-9ced-4ff2-bb42-a23525ff9425",
+              "events_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/events",
+              "service_bindings_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/service_bindings",
+              "routes_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/routes",
+              "route_mappings_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/route_mappings"
+            }
+          }
+        ]
+      }
+    },
+
+    500: {
+      body: {}
+    }
+  };
+
   mock.cloudFoundryAPI.Spaces = {
 
     ListAllAppsForSpace: function (guid) {
@@ -15,67 +79,22 @@
           200: {
 
             body: {
-              "guid": {
-                "total_results": 1,
-                "total_pages": 1,
-                "prev_url": null,
-                "next_url": null,
-                "resources": [
-                  {
-                    "metadata": {
-                      "guid": "91587747-004b-4a44-9adf-61c764519f71",
-                      "url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71",
-                      "created_at": "2016-02-19T02:04:05Z",
-                      "updated_at": "2016-02-19T02:04:05Z"
-                    },
-                    "entity": {
-                      "name": "name-2500",
-                      "production": false,
-                      "space_guid": "b71b5455-6d01-4846-bab9-f2af6e4e7a4f",
-                      "stack_guid": "07f55326-9ced-4ff2-bb42-a23525ff9425",
-                      "buildpack": null,
-                      "detected_buildpack": null,
-                      "environment_json": null,
-                      "memory": 1024,
-                      "instances": 1,
-                      "disk_quota": 1024,
-                      "state": "STOPPED",
-                      "version": "f6bb92c2-107e-41fc-9705-9d9892e30845",
-                      "command": null,
-                      "console": false,
-                      "debug": null,
-                      "staging_task_id": null,
-                      "package_state": "PENDING",
-                      "health_check_type": "port",
-                      "health_check_timeout": null,
-                      "staging_failed_reason": null,
-                      "staging_failed_description": null,
-                      "diego": false,
-                      "docker_image": null,
-                      "package_updated_at": "2016-02-19T02:04:05Z",
-                      "detected_start_command": "",
-                      "enable_ssh": true,
-                      "docker_credentials_json": {
-                        "redacted_message": "[PRIVATE DATA HIDDEN]"
-                      },
-                      "ports": null,
-                      "space_url": "/v2/spaces/b71b5455-6d01-4846-bab9-f2af6e4e7a4f",
-                      "stack_url": "/v2/stacks/07f55326-9ced-4ff2-bb42-a23525ff9425",
-                      "events_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/events",
-                      "service_bindings_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/service_bindings",
-                      "routes_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/routes",
-                      "route_mappings_url": "/v2/apps/91587747-004b-4a44-9adf-61c764519f71/route_mappings"
-                    }
-                  }
-                ]
-              }
+              "guid": ListAllAppsForSpace[200].body
             }
           },
 
           500: {
-            body: {guid: {}}
+            body: {guid: ListAllAppsForSpace[500].body}
           }
         }
+      };
+    },
+
+    ListAllAppsForSpaceNoPassthrough: function (guid) {
+      return {
+        url: '/pp/v1/proxy/v2/spaces/' + guid + '/apps?results-per-page=100',
+
+        response: ListAllAppsForSpace
       };
     },
 
@@ -326,9 +345,88 @@
             }
           }
         }
-      }
-    }
+      };
+    },
 
+    RetrievingRolesOfAllUsersInSpace: function (guid) {
+      return {
+        url: '/pp/v1/proxy/v2/spaces/' + guid + '/user_roles?results-per-page=100',
+        response: {
+          200: {
+            body: {
+              "total_results": 3,
+              "total_pages": 1,
+              "prev_url": null,
+              "next_url": null,
+              "resources": [
+                {
+                  "metadata": {
+                    "guid": "hcf_auto_config",
+                    "url": "/v2/users/hcf_auto_config",
+                    "created_at": "2016-10-11T17:00:51Z",
+                    "updated_at": null
+                  },
+                  "entity": {
+                    "admin": false,
+                    "active": true,
+                    "default_space_guid": null,
+                    "spaces_url": "/v2/users/hcf_auto_config/spaces",
+                    "organizations_url": "/v2/users/hcf_auto_config/organizations",
+                    "managed_organizations_url": "/v2/users/hcf_auto_config/managed_organizations",
+                    "billing_managed_organizations_url": "/v2/users/hcf_auto_config/billing_managed_organizations",
+                    "audited_organizations_url": "/v2/users/hcf_auto_config/audited_organizations",
+                    "managed_spaces_url": "/v2/users/hcf_auto_config/managed_spaces",
+                    "audited_spaces_url": "/v2/users/hcf_auto_config/audited_spaces"
+                  }
+                },
+                {
+                  "metadata": {
+                    "guid": "280391c9-335d-4f03-83d9-af64fc315b6c",
+                    "url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c",
+                    "created_at": "2016-10-11T17:57:39Z",
+                    "updated_at": null
+                  },
+                  "entity": {
+                    "admin": false,
+                    "active": true,
+                    "default_space_guid": null,
+                    "username": "admin",
+                    "spaces_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/spaces",
+                    "organizations_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/organizations",
+                    "managed_organizations_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/managed_organizations",
+                    "billing_managed_organizations_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/billing_managed_organizations",
+                    "audited_organizations_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/audited_organizations",
+                    "managed_spaces_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/managed_spaces",
+                    "audited_spaces_url": "/v2/users/280391c9-335d-4f03-83d9-af64fc315b6c/audited_spaces"
+                  }
+                },
+                {
+                  "metadata": {
+                    "guid": "0683b8d1-767d-4063-9ae6-bc2194d7f3da",
+                    "url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da",
+                    "created_at": "2016-10-12T19:07:32Z",
+                    "updated_at": null
+                  },
+                  "entity": {
+                    "admin": false,
+                    "active": false,
+                    "default_space_guid": null,
+                    "username": "rcox",
+                    "spaces_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/spaces",
+                    "organizations_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/organizations",
+                    "managed_organizations_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/managed_organizations",
+                    "billing_managed_organizations_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/billing_managed_organizations",
+                    "audited_organizations_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/audited_organizations",
+                    "managed_spaces_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/managed_spaces",
+                    "audited_spaces_url": "/v2/users/0683b8d1-767d-4063-9ae6-bc2194d7f3da/audited_spaces"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      };
+    }
   };
 
   /* eslint-enable quote-props */
