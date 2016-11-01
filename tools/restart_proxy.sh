17542bdfd3cb8e959b4c6c5a4f7b99265cf0dd19
Julien Bramary <julien.bramary@hpe.com>
2016-11-01 12:25:32 +0000

    Env dir is the main dir

--- tools/restart_proxy.sh
+++ tools/restart_proxy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-ENV_RC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 ENV_RC="development.rc"
 TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MAINDIR="$( cd "$( dirname "${TOOLSDIR}" )" && pwd )"
 
 function env_vars {
     echo "===== Environment variables"
-    if [ -f $ENV_RC ] ; then
-        echo "Found environment variables file: $ENV_RC"
-        source $ENV_RC
+    if [ -f ${ENV_RC} ]; then
+        echo -e "\033[32mFound environment variables file: ${ENV_RC}\033[0m"
+        source ${ENV_RC}
     else
-        echo "Did not find environment variables file: $ENV_RC"
-        echo "Your build may fail if the proper environment variables are missing."
+        echo -e "\033[31mDid not find environment variables file: ${ENV_RC}"
+        echo -e "Your build may fail if the proper environment variables are missing.\033[0m"
     fi
 }
 
-pushd "${ENV_RC_DIR}"
-env_vars
 pushd "${MAINDIR}"
+env_vars
+
 docker-compose -f docker-compose.development.yml stop proxy
 docker-compose -f docker-compose.development.yml rm -fa proxy
 
