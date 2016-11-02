e604b53c15f0740cf9c947c56f936863c5b49a4c
Irfan Habib <irfan.habib@hpe.com>
2016-11-02 11:14:00 +0000

    HSC-1292 - Optimising E2E tests

--- provision.sh
+++ provision.sh
@@ -4,7 +4,12 @@ WORK_DIR=$(pwd)
 TOOLS_DIR="$WORK_DIR/tools"
 
 cd ${TOOLS_DIR}
-npm install
+if [ "$(md5sum /package.json | cut -d' ' -f1)" != "$(md5sum package.json| cut -d' ' -f1)" ]; then
+  echo -e "\033[31mpackage.json was updated, please update the image. Running npm install\033[0m"
+  npm install
+else
+  echo -e "\033[32mpackage.json has not changed. Skipping npm install\033[0m"
+fi
 PATH=$PATH:$TOOLS_DIR/node_modules/.bin
 bower install --allow-root --force
 npm run build
