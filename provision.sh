83815fb1e8590d7ec338bfa9f3c3ac3d1b98f9e2
Irfan Habib <irfan.habib@hpe.com>
2016-11-02 13:59:37 +0000

    Fixed prep-proxy-image

--- provision.sh
+++ provision.sh
@@ -2,8 +2,8 @@
 
 WORK_DIR=$(pwd)
 TOOLS_DIR="$WORK_DIR/tools"
-
 cd ${TOOLS_DIR}
+cp -r /node_modules .
 if [ "$(md5sum /package.json | cut -d' ' -f1)" != "$(md5sum package.json| cut -d' ' -f1)" ]; then
   echo -e "\033[31mpackage.json was updated, please update the image. Running npm install\033[0m"
   npm install
