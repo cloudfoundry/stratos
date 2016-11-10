50199f954aa4dea09c3845462c1817ae4b2aa73d
richard-cox <richard.cox@hpe.com>
2016-11-10 15:20:16 +0000

    Fixed failing unit tests

--- e2e/po/widgets/cluster-settings.po.js
+++ e2e/po/widgets/cluster-settings.po.js
@@ -1,35 +1,36 @@
-'use strict';
+(function () {
+  'use strict';
 
-module.exports = {
-  getClusterSettings: getClusterSettings,
-  getItems: getItems,
-  getItem: getItem,
-  isExpired: isExpired,
-  canReconnect: canReconnect,
-  reconnect: reconnect
-};
+  module.exports = {
+    getClusterSettings: getClusterSettings,
+    getItems: getItems,
+    getItem: getItem,
+    isExpired: isExpired,
+    canReconnect: canReconnect,
+    reconnect: reconnect
+  };
 
-function getClusterSettings() {
-  return element.all(by.css('.cluster-settings'));
-}
+  function getClusterSettings() {
+    return element.all(by.css('.cluster-settings'));
+  }
 
-function getItems(clusterSettings) {
-  return clusterSettings.all(by.repeater('(name, cnsi) in clusterSettingsCtrl.serviceInstances'));
-}
+  function getItems(clusterSettings) {
+    return clusterSettings.all(by.repeater('(name, cnsi) in clusterSettingsCtrl.serviceInstances'));
+  }
 
-function getItem(clusterSettings, row) {
-  return clusterSettings.all(by.repeater('(name, cnsi) in clusterSettingsCtrl.serviceInstances')).get(row);
-}
+  function getItem(clusterSettings, row) {
+    return clusterSettings.all(by.repeater('(name, cnsi) in clusterSettingsCtrl.serviceInstances')).get(row);
+  }
 
-function isExpired(item) {
-  return item.element(by.css('.cluster-expired')).isPresent();
-}
+  function isExpired(item) {
+    return item.element(by.css('.cluster-expired')).isPresent();
+  }
 
-function canReconnect(item) {
-  return item.element(by.linkText('Reconnect')).isDisplayed();
-}
+  function canReconnect(item) {
+    return item.element(by.linkText('Reconnect')).isDisplayed();
+  }
 
-function reconnect(item) {
-  return item.element(by.linkText('Reconnect')).click();
-}
-
+  function reconnect(item) {
+    return item.element(by.linkText('Reconnect')).click();
+  }
+})();
