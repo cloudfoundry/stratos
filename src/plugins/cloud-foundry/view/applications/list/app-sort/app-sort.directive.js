0caedd8e0ab825f20c4e426f41c242dc0076763a
Irfan Habib <irfan.habib@hpe.com>
2016-12-01 12:26:44 +0000

    Responsive styling

--- src/plugins/cloud-foundry/view/applications/list/app-sort/app-sort.directive.js
+++ src/plugins/cloud-foundry/view/applications/list/app-sort/app-sort.directive.js
@@ -21,6 +21,15 @@
     'app.model.modelManager'
   ];
 
+  /**
+   * @name ApplicationsSortingController
+   * @description Controller for app-sort directive
+   * @constructor
+   * @param {object} $scope - the Angular $scope service
+   * @param {app.model.modelManager} modelManager - the Model management service
+   * @property {object} $scope - the Angular $scope service
+   * @property {app.model.modelManager} modelManager - the Model management service
+   */
   function ApplicationsSortingController($scope, modelManager) {
 
     var that = this;
@@ -31,26 +40,35 @@
       {label: 'Status', value: 'state.label'},
       {label: 'Instances', value: 'instanceCount'},
       {label: 'Disk Quota', value: 'entity.disk_quota'},
-      {label: 'Memory Utilization', value: 'entity.memory'},
+      {label: 'Memory Utilization', value: 'entity.memory'}
+      // Currently disabled sorting by Creation Date,
+      // since this is not displayed in table-view
       // {label: 'Creation Date', value: 'metadata.created_at'}
     ];
+
     this.model.currentSortOption = this.sortOptions[0].value;
+    // Flag for signaling that an update to
+    // filteredApplications is currently going on
     this.updatingFilteredApplications = false;
     this.model.sortAscending = true;
+    // Used to toggle display of sort action buttons when screen width is too small
+    this.showSortActions = true;
 
+    // If currentSortOption is updated update filteredApplications
     $scope.$watch(function () {
       return that.model.currentSortOption;
     }, function () {
       that.sortFilteredApplications();
     });
 
+    // If sort order is updated, update filteredApplications
     $scope.$watch(function () {
       return that.model.sortAscending;
     }, function () {
       that.sortFilteredApplications();
     });
 
-    // // In case filter is updated
+    // In case user applied a filter, resort filteredApplications
     $scope.$watchCollection(function () {
       return that.model.filteredApplications;
     }, function () {
@@ -63,10 +81,20 @@
 
   angular.extend(ApplicationsSortingController.prototype, {
 
+    /**
+     * @name setSortOrder
+     * @description Set sort order (asc/desc)
+     * @param {boolean} isAscending - Set sort order
+     */
     setSortOrder: function (isAscending) {
       this.model.sortAscending = isAscending;
     },
 
+    /**
+     * @name sortFilteredApplications
+     * @description Sort model.filteredApplications based on the required sort parameters
+     *
+     */
     sortFilteredApplications: function () {
 
       this.updatingFilteredApplications = true;
@@ -87,18 +115,41 @@
       this.updatingFilteredApplications = false;
     },
 
+    /**
+     * @name isCurrentSort
+     * @description Helper to apply appropriate style to sort button
+     * @param {boolean} option - sort option
+     * @returns {boolean}
+     */
     isCurrentSort: function (option) {
       return option.value === this.model.currentSortOption;
     },
 
+    /**
+     * @name showAscending
+     * @description Helper to show appropriate sort order button
+     * @param {boolean} option - sort option
+     * @returns {boolean}
+     */
     showAscending: function (option) {
       return this.isCurrentSort(option) && this.model.sortAscending;
     },
 
+    /**
+     * @name showDescending
+     * @description Helper to show appropriate sort order button
+     * @param {boolean} option - sort option
+     * @returns {boolean}
+     */
     showDescending: function (option) {
       return this.isCurrentSort(option) && !this.model.sortAscending;
     },
 
+    /**
+     * @name sort
+     * @description Helper to set the appropriate model sort options
+     * @param {boolean} option - sort option
+     */
     sort: function (option) {
       // when current sort is different by default switch to ascending
       // when current sort is the same toggle order
@@ -110,6 +161,23 @@
         this.model.sortAscending = !this.model.sortAscending;
       }
 
+    },
+
+    /**
+     * @name toggleSortActions
+     * @description Helper to toggle
+     * @param {boolean} option - sort option
+     */
+    toggleSortActions: function () {
+      this.showSortActions = !this.showSortActions;
+    },
+
+    getMessage: function () {
+      if (this.showSortActions) {
+        return gettext('Show Sort');
+      } else {
+        return gettext('Hide Sort');
+      }
     }
 
   });
