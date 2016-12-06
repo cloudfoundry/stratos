c5520b7d96bf6b03d990dba3445cad27c1b96dfa
richard-cox <richard.cox@hpe.com>
2016-12-06 10:36:03 +0000

    Added jsdoc to stFilteredCollection

--- framework/src/utils/st-filtered-collection/st-filtered-collection.directive.js
+++ framework/src/utils/st-filtered-collection/st-filtered-collection.directive.js
@@ -3,14 +3,22 @@
 
   angular
     .module('helion.framework.utils')
-    .directive('stFilteredCollection', function () {
-      return {
-        require: '^stTable',
-        link: function (scope, element, attr, ctrl) {
-          scope.$watch(ctrl.getFilteredCollection, function (val) {
-            scope.filteredCollection = val;
-          });
-        }
-      };
-    });
+    .directive('stFilteredCollection', stFilteredCollection);
+
+  /**
+   * @name stFilteredCollection
+   * @description Provides scope access to angular-smart-table's filtered collection.
+   * This is useful in conjunction with st-search.
+   * @returns {*}
+   */
+  function stFilteredCollection() {
+    return {
+      require: '^stTable',
+      link: function (scope, element, attr, ctrl) {
+        scope.$watch(ctrl.getFilteredCollection, function (val) {
+          scope.filteredCollection = val;
+        });
+      }
+    };
+  }
 })();
