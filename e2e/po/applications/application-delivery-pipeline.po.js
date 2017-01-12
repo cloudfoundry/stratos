d8b90c28732fa450aa410b68acea835307ab6193
Irfan Habib <irfan.habib@hpe.com>
2017-01-12 11:07:16 +0000

    Delivery Pipeline summary tests

--- e2e/po/applications/application-delivery-pipeline.po.js
+++ e2e/po/applications/application-delivery-pipeline.po.js
@@ -9,6 +9,20 @@
 
     getSetupElement: getSetupElement,
     getSetupWizard: getSetupWizard,
+    getTokenLink: getTokenLink,
+    getRepositoryLink: getRepositoryLink,
+    getSourceText: getSourceText,
+    getBranchText: getBranchText,
+    getBuildContainerText: getBuildContainerText,
+    getHceEndpointUrlText: getHceEndpointUrlText,
+    getAddTargetButton: getAddTargetButton,
+    getAddPostDeployActionButton: getAddPostDeployActionButton,
+
+    getNotificationTargets: getNotificationTargets,
+    getNoNotificationTargetsMessage: getNoNotificationTargetsMessage,
+
+    getPostDeployActions: getPostDeployActions,
+    getNoPostDeployActionsMessage: getNoPostDeployActionsMessage,
 
     getVCSServer: getVCSServer,
     isVCSServerEnabled: isVCSServerEnabled,
@@ -51,5 +65,64 @@
   function manageVcsTokenButton() {
     return element(by.css('.manage-tokens-link'));
   }
+
+  function getDeliveryPipelineSummary() {
+    return element(by.css('.delivery-pipeline-summary'));
+  }
+
+
+  function getSummaryRow(colIndex, rowIndex) {
+    return getDeliveryPipelineSummary()
+      .element.all(by.css('dl')).get(colIndex)
+      .element.all(by.css('dd')).get(rowIndex);
+  }
+
+  function getTokenLink() {
+    return getSummaryRow(0, 1).element(by.css('a'));
+  }
+
+  function getRepositoryLink() {
+    return getSummaryRow(0, 2).element(by.css('a'));
+  }
+
+  function getSourceText() {
+    return getSummaryRow(0, 0).getText();
+  }
+
+  function getBranchText() {
+    return getSummaryRow(1, 0).getText();
+  }
+
+  function getBuildContainerText() {
+    return getSummaryRow(1, 1).getText();
+  }
+
+  function getHceEndpointUrlText() {
+    return getSummaryRow(1, 2).getText();
+  }
+
+  function getNotificationTargetsSection(){
+    return element(by.id('notification-targets'));
+  }
+  function getAddTargetButton() {
+    return getNotificationTargetsSection()
+      .element(by.css('.btn.btn-link'));
+  }
+
+  function getAddPostDeployActionButton() {
+    return element(by.id('post-deploy-actions'))
+      .element(by.css('.btn.btn-link'));
+  }
+
+  function getNotificationTargets() {
+    return getNotificationTargetsSection().
+    element.all(by.repeater('target in applicationDeliveryPipelineCtrl.notificationTargets'));
+  }
+
+  function getNoNotificationTargetsMessage(){
+    return getNotificationTargetsSection().
+
+  }
+
 
 })();
