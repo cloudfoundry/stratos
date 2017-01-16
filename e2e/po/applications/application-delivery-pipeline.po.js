692fb790b99ca3855bcda4d5d14ef44fd56ba959
Irfan Habib <irfan.habib@hpe.com>
2017-01-16 12:12:29 +0000

    HSC-1469 - E2E tests: Add tests for Delivery Pipeline summary page (#955)
    * Delivery Pipeline summary tests
    
    * Fix leakage of apps
    
    * Delivery Pipeline summary page E2E tests
    
    * Fixed actions-menu click
    
    * HSC-1469 - E2E tests: Add tests for Delivery Pipeline summary page
    
    * remove debug file
    
    * Delivery Pipeline E2E tests: Allow concurrent tests to run
    
    * minor refactor
    
    * Delivery Pipeline E2E tests: Allow concurrent tests to run
    
    * Fix test

--- e2e/po/applications/application-delivery-pipeline.po.js
+++ e2e/po/applications/application-delivery-pipeline.po.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var wizard = require('../widgets/wizard.po');
+  var confirmationModal = require('../widgets/confirmation-modal.po');
 
   module.exports = {
     setupPipelineButton: setupPipelineButton,
@@ -9,11 +10,34 @@
 
     getSetupElement: getSetupElement,
     getSetupWizard: getSetupWizard,
+    getDeliveryPipelineSummary: getDeliveryPipelineSummary,
+    getTokenLink: getTokenLink,
+    getRepositoryLink: getRepositoryLink,
+    getSourceText: getSourceText,
+    getBranchText: getBranchText,
+    getBuildContainerText: getBuildContainerText,
+    getHceEndpointUrlText: getHceEndpointUrlText,
+    getAddTargetButton: getAddTargetButton,
+    getAddPostDeployActionButton: getAddPostDeployActionButton,
+
+    getNotificationTargetsSection: getNotificationTargetsSection,
+    getNotificationTargets: getNotificationTargets,
+    getNotificationTargetDeleteAction: getNotificationTargetDeleteAction,
+    getNoNotificationTargetsMessage: getNoNotificationTargetsMessage,
+    getNoTargetsMessage: getNoTargetsMessage,
+
+    getPostDeployActionsSection: getPostDeployActionsSection,
+    getPostDeployActions: getPostDeployActions,
+    getPostDeployActionDeleteAction: getPostDeployActionDeleteAction,
+    getNoPostDeployActionsMessage: getNoPostDeployActionsMessage,
+    getNoActionsMessage: getNoActionsMesasge,
 
     getVCSServer: getVCSServer,
     isVCSServerEnabled: isVCSServerEnabled,
+    acknowledgeDeletion: acknowledgeDeletion,
     addNewTokenButton: addNewTokenButton,
-    manageVcsTokenButton: manageVcsTokenButton
+    manageVcsTokenButton: manageVcsTokenButton,
+    deletePipelineButton: deletePipelineButton
   };
 
   function setupPipelineButton() {
@@ -52,4 +76,99 @@
     return element(by.css('.manage-tokens-link'));
   }
 
+  function getDeliveryPipelineSummary() {
+    return element(by.css('.delivery-pipeline-summary'));
+  }
+
+  function getSummaryRow(colIndex, rowIndex) {
+    return getDeliveryPipelineSummary()
+      .all(by.css('dl')).get(colIndex)
+      .all(by.css('dd')).get(rowIndex);
+  }
+
+  function getTokenLink() {
+    return getSummaryRow(0, 1).element(by.css('a'));
+  }
+
+  function getRepositoryLink() {
+    return getSummaryRow(0, 2).element(by.css('a'));
+  }
+
+  function getSourceText() {
+    return getSummaryRow(0, 0).getText();
+  }
+
+  function getBranchText() {
+    return getSummaryRow(1, 0).getText();
+  }
+
+  function getBuildContainerText() {
+    return getSummaryRow(1, 1).getText();
+  }
+
+  function getHceEndpointUrlText() {
+    return getSummaryRow(1, 2).getText();
+  }
+
+  function getNotificationTargetsSection() {
+    return element(by.id('notification-targets'));
+  }
+
+  function getNoTargetsMessage() {
+    return 'You have no notification targets.';
+  }
+
+  function getPostDeployActionsSection() {
+    return element(by.id('post-deploy-actions'));
+  }
+
+  function getAddTargetButton() {
+    return getNotificationTargetsSection()
+      .element(by.css('.btn.btn-link'));
+  }
+
+  function getAddPostDeployActionButton() {
+    return getPostDeployActionsSection()
+      .element(by.css('.btn.btn-link'));
+  }
+
+  function getNotificationTargets() {
+    return getNotificationTargetsSection().all(by.repeater('target in applicationDeliveryPipelineCtrl.notificationTargets'));
+  }
+
+  function getNoNotificationTargetsMessage() {
+    return getNotificationTargetsSection()
+      .element(by.css('.panel-body > span'));
+  }
+
+  function getNotificationTargetDeleteAction(rowIndex) {
+    return getNotificationTargets().get(rowIndex)
+      .element(by.css('actions-menu'));
+  }
+
+  function acknowledgeDeletion() {
+    return confirmationModal.commit();
+  }
+
+  function getPostDeployActions() {
+    return getPostDeployActionsSection().all(by.repeater('postDeployAction in applicationDeliveryPipelineCtrl.postDeployActions'));
+  }
+
+  function getNoActionsMesasge() {
+    return 'You have no post deploy actions.';
+  }
+
+  function getNoPostDeployActionsMessage() {
+    return getPostDeployActionsSection()
+      .element(by.css('.panel-body > span'));
+  }
+
+  function getPostDeployActionDeleteAction(rowIndex) {
+    return getPostDeployActions().get(rowIndex)
+      .element(by.css('actions-menu'));
+  }
+
+  function deletePipelineButton() {
+    return element(by.css('.panel-heading > .btn.btn-link'));
+  }
 })();
