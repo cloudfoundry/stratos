fd6778ac0c2d25f449954ce4a289b981c63ad432
Richard <richard-cox@users.noreply.github.com>
2017-01-16 12:12:43 +0000

    HSC-1472: Handle new HCE project based permissions (#956)
    * HSC-1472: Handle new HCE project based permissions
    Basic handling of new hce project permissions
    - Do not allow application to be deleted if user does not have project permission
    - Display permissions notice on delivery pipeline + logs pages
    
    Note - We may want to refactor delivery-pipeline-status.html in the future,
    there's a fair few ng-if conditions in the template
    
    * Fix project role holes
    - Delete application button state is updated given changes to project roles while app pages are shown
    - Delete application process will now correctly fail and stop progression if..
    -- The delete project request fails (user is only a project developer)
    -- The request project previously failed due to a 403 (user has no project role)
    - Delete pipeline process remains as before
    
    Note - We should look to improve the project developer use case. Need
    to check beforehand what roles user has for project.

--- src/plugins/cloud-foundry/view/applications/application/delivery-pipeline/delivery-pipeline-status/delivery-pipeline-status.directive.js
+++ src/plugins/cloud-foundry/view/applications/application/delivery-pipeline/delivery-pipeline-status/delivery-pipeline-status.directive.js
@@ -10,7 +10,7 @@
   /**
    * @memberof cloud-foundry.view.applications
    * @name deliveryPipelineStatus
-   * @description A dierctive for showing the delivery pipeline status
+   * @description A directive for showing the delivery pipeline status
    * @returns {object} The delivery-pipeline-status directive definition object
    */
   function deliveryPipelineStatus() {
