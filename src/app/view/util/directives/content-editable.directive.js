b5dae71d1c51b61357411ab65838ed27efa9db25
Julien Bramary <julien.bramary@hpe.com>
2017-02-15 11:10:45 +0000

    Add directive to link contenteditable to ng-model

---
+++ src/app/view/util/directives/content-editable.directive.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.view')
+    .directive('contenteditable', contentEditable);
+
+  // See: https://docs.angularjs.org/api/ng/type/ngModel.NgModelController#custom-control-example
+  contentEditable.$inject = ['$sce'];
+  function contentEditable($sce) {
+    return {
+      restrict: 'A',
+      require: '?ngModel',
+      link: function (scope, element, attrs, ngModel) {
+        if (!ngModel) { return; } // do nothing if no ng-model
+
+        // Specify how UI should be updated
+        ngModel.$render = function () {
+          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+        };
+
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function () {
+          scope.$evalAsync(read);
+        });
+
+        read(); // initialize
+
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable certain browsers (e.g. Firefox) leave a <br> behind
+          if (html.endsWith('<br>')) {
+            html = html.slice(0, -4);
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  }
+})();
