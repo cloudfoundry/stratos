e8ddacf4cd64fe9a1bf82b3fb6845fbdb08ca84d
Julien Bramary <julien.bramary@hpe.com>
2017-02-15 12:31:51 +0000

    Text filter fixes: Prevent rich text paste. Prevent wrapping. Works in IE

--- src/app/view/util/directives/content-editable.directive.js
+++ src/app/view/util/directives/content-editable.directive.js
@@ -24,13 +24,25 @@
           scope.$evalAsync(read);
         });
 
+        // Prevent crazy HTML paste inside content-editable elements
+        element.on('paste', function (e) {
+          // cancel paste
+          e.preventDefault();
+
+          // get text representation of clipboard
+          var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+
+          // insert text manually
+          document.execCommand('insertHTML', false, text);
+        });
+
         read(); // initialize
 
         // Write data to the model
         function read() {
           var html = element.html();
           // When we clear the content editable certain browsers (e.g. Firefox) leave a <br> behind
-          if (html.endsWith('<br>')) {
+          if (_.endsWith(html, '<br>')) {
             html = html.slice(0, -4);
           }
           ngModel.$setViewValue(html);
