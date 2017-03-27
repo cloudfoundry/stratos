9dc6501c542b0637489ba758dcd95c1befee895c
Irfan Habib <irfan@irfanhabib.com>
2017-03-27 16:20:08 +0100

    Tests for token storage/retrieval (#163)
    * Fixed version_ info and pgsql_vcs_token tests
    
    * Fixed version_ info and pgsql_vcs_token tests
    
    * Fixed passthrough tests
    
    * Fixed version_ info and pgsql_vcs_token tests
    
    * Fixed passthrough tests
    
    * Fixed passthrough & auth_tests tests
    
    * Migrated OAuth requests tests to GoConvey
    
    * Added tests for CNSI/UAA Token storage/retrieval
    
    * Added for VCS Tokens
    
    * Fixed typos

--- repository/vcstokens/pgsql_vcs_tokens_test.go
+++ repository/vcstokens/pgsql_vcs_tokens_test.go
@@ -1,101 +1,367 @@
 package vcstokens
 
 import (
-	// "errors"
-	// "fmt"
-	// "net/url"
 	"testing"
-	// "time"
+	"errors"
+	"database/sql"
 
-	"github.com/hpcloud/portal-proxy/repository/crypto"
-	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 	. "github.com/smartystreets/goconvey/convey"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-func TestPgsqlVCSTokens(t *testing.T) {
+const (
+	mockGuid = "foo-guid"
+	mockUserGuid = "foo-bar"
+	mockVcsGuid = "foo-bar"
+	mockVcsTokenName = "myToken"
+	mockToken = "mock-foohub-token"
+	mockUrl = "https://foohub.com"
+	findVcsTokenQuery = "SELECT guid, user_guid, vcs_guid, name, token FROM vcs_tokens"
+	saveVcsTokenQuery = "INSERT INTO vcs_tokens "
+	renameVcsTokenQuery = "UPDATE vcs_tokens"
+	deleteVcsTokenQuery = "DELETE FROM vcs_tokens"
+	listVcsTokensByUserQuery = "SELECT t.guid, t.user_guid, t.vcs_guid, t.name, t.token, v.guid, v.label, v.type, v.browse_url, v.api_url, v.skip_ssl_validation FROM vcs v, vcs_tokens t"
+)
 
-	const mockToken = `eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI2ZGIyYTI5NGYyYWE0OGNlYjI1NDgzMDk4ZDNjY2Q3YyIsInN1YiI6Ijg4YmNlYWE1LWJkY2UtNDdiOC04MmYzLTRhZmMxNGYyNjZmOSIsInNjb3BlIjpbIm9wZW5pZCIsInNjaW0ucmVhZCIsImNsb3VkX2NvbnRyb2xsZXIuYWRtaW4iLCJ1YWEudXNlciIsImNsb3VkX2NvbnRyb2xsZXIucmVhZCIsInBhc3N3b3JkLndyaXRlIiwicm91dGluZy5yb3V0ZXJfZ3JvdXBzLnJlYWQiLCJjbG91ZF9jb250cm9sbGVyLndyaXRlIiwiZG9wcGxlci5maXJlaG9zZSIsInNjaW0ud3JpdGUiXSwiY2xpZW50X2lkIjoiY2YiLCJjaWQiOiJjZiIsImF6cCI6ImNmIiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9pZCI6Ijg4YmNlYWE1LWJkY2UtNDdiOC04MmYzLTRhZmMxNGYyNjZmOSIsIm9yaWdpbiI6InVhYSIsInVzZXJfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbiIsImF1dGhfdGltZSI6MTQ2Nzc2OTgxNiwicmV2X3NpZyI6IjE0MGUwMjZiIiwiaWF0IjoxNDY3NzY5ODE2LCJleHAiOjE0Njc3NzA0MTYsImlzcyI6Imh0dHBzOi8vdWFhLmV4YW1wbGUuY29tL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiYXVkIjpbImNmIiwib3BlbmlkIiwic2NpbSIsImNsb3VkX2NvbnRyb2xsZXIiLCJ1YWEiLCJwYXNzd29yZCIsInJvdXRpbmcucm91dGVyX2dyb3VwcyIsImRvcHBsZXIiXX0.q2u0JX42Qiwr0ZsBU5Y6bF74_0URWmmBYTLf8l7of_6huFoMkyqvirEYcbYbATt6Hz2zcN6xlXcInALxQ6nt6Jk01kZHRNYfuu6QziLHHw2o_dJWk9iipiermUze7BvSGtU_JXx45BSBNVFxvRxG9Yv54Lwa9FvyhMSmK3CI5S8NtVDchzrsH3sMsIjlTAb-L7sch-OOQ7ncWH1JoGMtw8sTbiaHvfNJQclSq8Ro11NUtRHiWeGFFxYIerzKO-TrSpDojFJrYVuK1m0YPmBDa_dY3cneRuppagRIn8oI0VFHF8BckrIqNCHvOMoVz6uzHebo9LK7H5z5SluxJ2vYUgPiHE_Tyo-7gELnNSy8qL4Bk9yTxNseeGiq13TSTGOtNnbrv1eq4ZeW7eafseLceKIZH2QZlXVzwd_aWbuKRv9ApDwy4AcSbpM0XtU89IjUEDoOf3IDWV2YZTZkEaXZ52Mhztb1O_IVpHyyks88P67RoANFt83MnCai9U3stCX45LEsg9oz2djrVnfHDzRNQVlg9hKJYbxsa2R5tpnftjhz-hfpsoPRxBkJDKM2islyd-gLqHtsERiZEoifu93VRE0Jvk6vaCNdStw7y4mq73Co6ykNUYA78SlT9lCwDJRQHTJiDWg33EeKpXne8joZbElwrKNcv93X1qxxvmp1wXQ bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI2ZGIyYTI5NGYyYWE0OGNlYjI1NDgzMDk4ZDNjY2Q3Yy1yIiwic3ViIjoiODhiY2VhYTUtYmRjZS00N2I4LTgyZjMtNGFmYzE0ZjI2NmY5Iiwic2NvcGUiOlsib3BlbmlkIiwic2NpbS5yZWFkIiwiY2xvdWRfY29udHJvbGxlci5hZG1pbiIsInVhYS51c2VyIiwiY2xvdWRfY29udHJvbGxlci5yZWFkIiwicGFzc3dvcmQud3JpdGUiLCJyb3V0aW5nLnJvdXRlcl9ncm91cHMucmVhZCIsImNsb3VkX2NvbnRyb2xsZXIud3JpdGUiLCJkb3BwbGVyLmZpcmVob3NlIiwic2NpbS53cml0ZSJdLCJpYXQiOjE0Njc3Njk4MTYsImV4cCI6MTQ3MDM2MTgxNiwiY2lkIjoiY2YiLCJjbGllbnRfaWQiOiJjZiIsImlzcyI6Imh0dHBzOi8vdWFhLmV4YW1wbGUuY29tL29hdXRoL3Rva2VuIiwiemlkIjoidWFhIiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9uYW1lIjoiYWRtaW4iLCJvcmlnaW4iOiJ1YWEiLCJ1c2VyX2lkIjoiODhiY2VhYTUtYmRjZS00N2I4LTgyZjMtNGFmYzE0ZjI2NmY5IiwicmV2X3NpZyI6IjE0MGUwMjZiIiwiYXVkIjpbImNmIiwib3BlbmlkIiwic2NpbSIsImNsb3VkX2NvbnRyb2xsZXIiLCJ1YWEiLCJwYXNzd29yZCIsInJvdXRpbmcucm91dGVyX2dyb3VwcyIsImRvcHBsZXIiXX0.K5M_isGkEBAN_MaXqkVvJfHG86rGIUkDgsHaFnoKOA1x5FNC4APDvhImWJZ8zbFHhXT3PYHTyeSf_HQaFDFUHFvGZUhSSry2ID4kdU5kRyZ-y3ydkv2mq32BlUQBSC9ap0r5vFTv7BY1yf2EcDaKGe4v4ODMhTm2SIkdTyk2ZcLXHIucS0xgSZdjgxNqh3pnKtmcFkw72-CyREW4_2Nbvn_7U2UNUCb2SeAuWmYaNAOkuGveB8jAhg9ftTrxn5GNtNe1sdVycm51X1O0dGPt_rLbwkRDCdNpm0La_xzLqZEl60_YUqwo33eOChFgqXB5y_0Pzs9gD__uExrIXYIgMsltFELXryyRUDKTTHZEEw1bnLTbQfF-GAnS0E0CaTU_kcDVqDYcqfh0TCcr7nGCEozExMPm3J0OGUSP3FQAD5mDICsKKcSIi_kIjggkJ87tuNAY6QOW1WzBoRizXJVS4jb3QOnrii2LmH786qBYJMX0nH__JRYEU-HWLi_OGXVTo03Pe9QcB8qJvbu2DGRfQdBfjhvgt2AItY4voJnZcjwT29q144C5wvJ2_W8cUzNY-Xw_tN_fWK4LWCu6KRNLVLO2MNbl0aOfkvb1U5NZJUpUUC2jG3cZM2c8232YNFKVjdjbf-Mlx17OxOYQ5XtG5BiSEj7BA6s5hWftUXEUchg`
+var tokenRecord = VcsTokenRecord{
+	Guid: mockGuid,
+	UserGuid: mockUserGuid,
+	VcsGuid: mockVcsGuid,
+	Name: mockVcsTokenName,
+	Token: mockToken,
+}
 
-	var (
-		mockUserGUID    = "abcd-1234-efgh"
-		mockAPIEndpoint = "https://api.127.0.0.1"
-		insertVCSTokens = `INSERT INTO vcstokens`
+var mockEncryptionKey = make([]byte, 32)
 
-		mockEncryptionKey     = make([]byte, 32)
-		mockEncryptedToken, _ = crypto.EncryptToken(mockEncryptionKey, mockToken)
-	)
+func initialiseRepo(t *testing.T) (*sql.DB, sqlmock.Sqlmock, Repository) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	repository, _ := NewPgsqlVcsTokenRepository(db)
+	return db, mock, repository
+}
 
-	Convey("Given a request for a new reference to a VCS Token Repository", t, func() {
+func TestSaveVcsTokens(t *testing.T) {
 
-		db, _, err := sqlmock.New()
-		if err != nil {
-			t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
-		}
-		defer db.Close()
+	Convey("SaveVcsTokens Tests", t, func() {
 
-		Convey("A valid VCS Token Repository should be returned without error.", func() {
-			repository, err := NewPgsqlVcsTokenRepository(db)
-			So(repository, ShouldHaveSameTypeAs, &PgsqlVcsTokenRepository{})
-			So(err, ShouldBeNil)
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should fail to find token with an invalid user GUID", func() {
+			tokenRecord.UserGuid = ""
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
 		})
-	})
 
-	Convey("Given a request to save a VCS Token", t, func() {
+		Convey("should fail to find token with an invalid GUID", func() {
+			tokenRecord.Guid = ""
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
 
-		db, mock, err := sqlmock.New()
-		if err != nil {
-			t.Errorf("an error '%s' was not expected when opening a stub database connection", err)
-		}
-		defer db.Close()
+		Convey("should fail to find token with an VCS GUID", func() {
+			tokenRecord.VcsGuid = ""
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
 
-		SkipConvey("if successful", func() {
+		Convey("should fail to find token with an invalid VCS token name", func() {
+			tokenRecord.Name = ""
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
 
-			// General setup
-			mockTokenRecord := VcsTokenRecord{UserGuid: mockUserGUID,  Token: mockToken}
+		Convey("should fail to find token with an invalid VCS token value", func() {
+			tokenRecord.Token = ""
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
 
-			// Database Expectations
-			mock.ExpectExec(insertVCSTokens).
-				WithArgs(mockAPIEndpoint, mockUserGUID, mockEncryptedToken).
+		Convey("should throw exception when a token with the same name already exists", func() {
+
+			row := sqlmock.NewRows([]string{"guid", "user_guid", "vcs_guid", "name", "token"}).
+				AddRow(mockGuid, mockUserGuid, mockVcsGuid, mockVcsTokenName, mockToken)
+
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnRows(row)
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("should fail to decrypt with invalid encryptionKey", func() {
+
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnError(errors.New("failed to find token"))
+			err := repository.SaveVcsToken(&tokenRecord, nil)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("Success case", func() {
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnError(errors.New("failed to find token"))
+			mock.ExpectExec(saveVcsTokenQuery).
 				WillReturnResult(sqlmock.NewResult(1, 1))
+			err := repository.SaveVcsToken(&tokenRecord, mockEncryptionKey)
 
-			Convey("there should be no error returned", func() {
-				repository, _ := NewPgsqlVcsTokenRepository(db)
-				err := repository.SaveVcsToken(&mockTokenRecord, mockEncryptionKey)
-				So(err, ShouldBeNil)
-
-				dberr := mock.ExpectationsWereMet()
-				So(dberr, ShouldBeNil)
-			})
+			So(err, ShouldBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
 		})
 
-		SkipConvey("if unsuccessful", func() {
-
-			// // General setup
-			// u, _ := url.Parse(mockAPIEndpoint)
-			// cnsi := CNSIRecord{GUID: mockHCFGUID, Name: "Some fancy HCF Cluster", CNSIType: CNSIType("hcf"), APIEndpoint: u, AuthorizationEndpoint: mockAuthEndpoint, TokenEndpoint: mockAuthEndpoint, DopplerLoggingEndpoint: mockDopplerEndpoint, SkipSSLValidation: true}
-			// expectedErrorMessage := fmt.Sprintf("Unable to Save CNSI record: %s", unknownDBError)
-			//
-			// mock.ExpectExec(insertIntoCNSIs).
-			//   WithArgs(mockHCFGUID, "Some fancy HCF Cluster", "hcf", mockAPIEndpoint, mockAuthEndpoint, mockAuthEndpoint, mockDopplerEndpoint, true).
-			//   WillReturnError(errors.New(unknownDBError))
-			//
-			// Convey("there should be an error returned", func() {
-			//   repository, _ := NewPostgresCNSIRepository(db)
-			//   err := repository.Save(mockHCFGUID, cnsi)
-			//   So(err, ShouldResemble, errors.New(expectedErrorMessage))
-			//
-			//   dberr := mock.ExpectationsWereMet()
-			//   So(dberr, ShouldBeNil)
-			// })
-
+		Reset(func() {
+			tokenRecord = VcsTokenRecord{
+				Guid: mockGuid,
+				UserGuid: mockUserGuid,
+				VcsGuid: mockVcsGuid,
+				Name: mockVcsTokenName,
+				Token: mockToken,
+			}
+			db.Close()
 		})
-		//
-		//
-		//
-		//
-		//
+
 	})
 
-	SkipConvey("FindVCSToken", t, func() {
+}
+
+func TestFindVcsToken(t *testing.T) {
+
+	Convey("FindVcsToken Tests", t, func() {
+
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should fail to find token with an invalid user GUID", func() {
+			_, err := repository.FindVcsToken("", tokenRecord.Guid, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("should fail to find token with an invalid token GUID", func() {
+			_, err := repository.FindVcsToken(tokenRecord.UserGuid, "", mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("should throw exception when finding a non-existent token", func() {
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnError(errors.New("doesnt exist"))
+			_, err := repository.FindVcsToken(tokenRecord.UserGuid, tokenRecord.Guid, mockEncryptionKey)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("success case", func() {
+			row := sqlmock.NewRows([]string{"guid", "user_guid", "vcs_guid", "name", "token"}).
+				AddRow(mockGuid, mockUserGuid, mockVcsGuid, mockVcsTokenName, mockToken)
+
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnRows(row)
+			tk, err := repository.FindVcsToken(tokenRecord.UserGuid, tokenRecord.Guid, mockEncryptionKey)
+
+			So(err, ShouldBeNil)
+			So(tk, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Reset(func() {
+			db.Close()
+		})
 	})
+
+}
+
+func TestFindMatchingVcsToken(t *testing.T) {
+
+	Convey("FindMatchingVcsToken Tests", t, func() {
+
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should fail to find token with an invalid user GUID", func() {
+			tokenRecord.UserGuid = ""
+			_, err := repository.FindMatchingVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("should fail to find token with an invalid GUID", func() {
+			tokenRecord.Guid = ""
+			_, err := repository.FindMatchingVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("should fail to find token with an VCS token name", func() {
+			tokenRecord.Name = ""
+			_, err := repository.FindMatchingVcsToken(&tokenRecord, mockEncryptionKey)
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("should throw exception when finding a non-existent token", func() {
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnError(errors.New("doesnt exist"))
+			_, err := repository.FindMatchingVcsToken(&tokenRecord, mockEncryptionKey)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("success case", func() {
+			row := sqlmock.NewRows([]string{"guid", "user_guid", "vcs_guid", "name", "token"}).
+				AddRow(mockGuid, mockUserGuid, mockVcsGuid, mockVcsTokenName, mockToken)
+
+			mock.ExpectQuery(findVcsTokenQuery).
+				WillReturnRows(row)
+			tk, err := repository.FindMatchingVcsToken(&tokenRecord, mockEncryptionKey)
+
+			So(err, ShouldBeNil)
+			So(tk, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Reset(func() {
+			tokenRecord = VcsTokenRecord{
+				Guid: mockGuid,
+				UserGuid: mockUserGuid,
+				VcsGuid: mockVcsGuid,
+				Name: mockVcsTokenName,
+				Token: mockToken,
+			}
+			db.Close()
+		})
+	})
+
+}
+
+func TestRenameVcsToken(t *testing.T) {
+
+	Convey("RenameVcsToken Tests", t, func() {
+
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should throw exception when attemping to rename a non-existent token", func() {
+			mock.ExpectExec(renameVcsTokenQuery).
+				WillReturnError(errors.New("doesnt exist"))
+			err := repository.RenameVcsToken(mockUserGuid, mockGuid, mockVcsTokenName)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("should throw an exception if number of modified rows is less than 1", func() {
+			mock.ExpectExec(renameVcsTokenQuery).
+				WillReturnResult(sqlmock.NewResult(1, 0))
+			err := repository.RenameVcsToken(mockUserGuid, mockGuid, mockVcsTokenName)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("success case", func() {
+			mock.ExpectExec(renameVcsTokenQuery).
+				WillReturnResult(sqlmock.NewResult(1, 1))
+			err := repository.RenameVcsToken(mockUserGuid, mockGuid, mockVcsTokenName)
+
+			So(err, ShouldBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Reset(func() {
+			db.Close()
+		})
+	})
+
+}
+func TestDeleteVcsToken(t *testing.T) {
+
+	Convey("DeleteVcsToken Tests", t, func() {
+
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should throw exception when attemping to delete a non-existent token", func() {
+			mock.ExpectExec(deleteVcsTokenQuery).
+				WillReturnError(errors.New("doesnt exist"))
+			err := repository.DeleteVcsToken(mockUserGuid, mockGuid)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("should throw an exception if number of modified rows is less than 1", func() {
+			mock.ExpectExec(deleteVcsTokenQuery).
+				WillReturnResult(sqlmock.NewResult(1, 0))
+			err := repository.DeleteVcsToken(mockUserGuid, mockGuid)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("success case", func() {
+			mock.ExpectExec(deleteVcsTokenQuery).
+				WillReturnResult(sqlmock.NewResult(1, 1))
+			err := repository.DeleteVcsToken(mockUserGuid, mockGuid)
+
+			So(err, ShouldBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Reset(func() {
+			db.Close()
+		})
+	})
+
+}
+
+func TestListVcsTokenByUser(t *testing.T) {
+
+	Convey("ListVcsTokenByUser Tests", t, func() {
+
+		row := sqlmock.NewRows([]string{"t.guid", "t.user_guid",
+			"t.vcs_guid", "t.name", "t.token", "v.guid", "v.label",
+			"v.type", "v.browse_url", "v.api_url", "v.skip_ssl_validation"}).
+			AddRow(mockGuid, mockUserGuid, mockVcsGuid, mockVcsTokenName, mockToken,
+			mockGuid, mockVcsTokenName, "github", mockUrl, mockUrl, true)
+
+
+		db, mock, repository := initialiseRepo(t)
+
+		Convey("should throw exception when no tokens exist for user", func() {
+			mock.ExpectQuery(listVcsTokensByUserQuery).
+				WillReturnError(errors.New("doesnt exist"))
+			_, err := repository.ListVcsTokenByUser(mockUserGuid, mockEncryptionKey)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("should fail to decrypt with invalid encryptionKey", func() {
+
+			mock.ExpectQuery(listVcsTokensByUserQuery).
+				WillReturnRows(row)
+			_, err := repository.ListVcsTokenByUser(mockUserGuid, nil)
+
+			So(err, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Convey("success case", func() {
+			row := sqlmock.NewRows([]string{"t.guid", "t.user_guid",
+				"t.vcs_guid", "t.name", "t.token", "v.guid", "v.label",
+				"v.type", "v.browse_url", "v.api_url", "v.skip_ssl_validation"}).
+				AddRow(mockGuid, mockUserGuid, mockVcsGuid, mockVcsTokenName, mockToken,
+				mockGuid, mockVcsTokenName, "github", mockUrl, mockUrl, true)
+
+			mock.ExpectQuery(listVcsTokensByUserQuery).
+				WillReturnRows(row)
+			rs, err := repository.ListVcsTokenByUser(mockUserGuid, mockEncryptionKey)
+
+			So(err, ShouldBeNil)
+			So(rs, ShouldNotBeNil)
+			So(mock.ExpectationsWereMet(), ShouldBeNil)
+		})
+
+		Reset(func() {
+			db.Close()
+		})
+	})
+
 }
