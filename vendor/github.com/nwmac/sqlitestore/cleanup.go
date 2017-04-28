fdcb2e41080d9a550622ce0586a163ba37f2731f
Neil MacDougall <neil.macdougall@suse.com>
2017-04-28 10:41:26 +0100

    Update vendor folder

---
+++ vendor/github.com/nwmac/sqlitestore/cleanup.go
@@ -0,0 +1,59 @@
+package sqlitestore
+
+import (
+	"log"
+	"time"
+)
+
+var defaultInterval = time.Minute * 5
+
+// Cleanup runs a background goroutine every interval that deletes expired
+// sessions from the database.
+//
+// The design is based on https://github.com/yosssi/boltstore
+func (m *SqliteStore) Cleanup(interval time.Duration) (chan<- struct{}, <-chan struct{}) {
+	if interval <= 0 {
+		interval = defaultInterval
+	}
+
+	quit, done := make(chan struct{}), make(chan struct{})
+	go m.cleanup(interval, quit, done)
+	return quit, done
+}
+
+// StopCleanup stops the background cleanup from running.
+func (m *SqliteStore) StopCleanup(quit chan<- struct{}, done <-chan struct{}) {
+	quit <- struct{}{}
+	<-done
+}
+
+// cleanup deletes expired sessions at set intervals.
+func (m *SqliteStore) cleanup(interval time.Duration, quit <-chan struct{}, done chan<- struct{}) {
+	ticker := time.NewTicker(interval)
+
+	defer func() {
+		ticker.Stop()
+	}()
+
+	for {
+		select {
+		case <-quit:
+			// Handle the quit signal.
+			done <- struct{}{}
+			return
+		case <-ticker.C:
+			// Delete expired sessions on each tick.
+			err := m.deleteExpired()
+			if err != nil {
+				log.Printf("pgstore: unable to delete expired sessions: %v", err)
+			}
+		}
+	}
+}
+
+// deleteExpired deletes expired sessions from the database.
+func (m *SqliteStore) deleteExpired() error {
+	var deleteStmt = "DELETE FROM " + m.table + " WHERE expires_on < datetime('now')"
+	_, err := m.db.Exec(deleteStmt)
+	return err
+}
