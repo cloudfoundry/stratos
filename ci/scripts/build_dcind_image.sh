a75bb909b387aa6f4eb6bd4501f72cacd6aec309
Irfan Habib <irfan@irfanhabib.com>
2017-05-05 11:28:10 +0100

    Make logs from concourse pipelines available (#138)
    * log test
    
    * Publish log artefacts from concourse
    
    * Updated tag to force refresh
    
    * Fix private key path
    
    * Upload directory
    
    * Tar the file
    
    * Updated `console-tests-pr.yml`
    
    * Tidy ups
    
    * Updated properties

--- ci/scripts/build_dcind_image.sh
+++ ci/scripts/build_dcind_image.sh
@@ -11,10 +11,23 @@ echo FROM amidos/dcind:latest > Dockerfile.dcind
 
 echo COPY daemon.json /etc/docker/ >> Dockerfile.dcind
 
+# Get registry.paas-ui host key
+TMP_FILE=$(mktemp)
+ssh-keyscan registry.paas-ui > $TMP_FILE 2> /dev/null
+ECDSA_FINGERPRINT=$(cat $TMP_FILE | grep ecdsa)
+cat << EOT >> Dockerfile.dcind
+RUN apk update && \\
+        apk add dropbear-scp && \\
+	apk add dropbear
+RUN  dropbearkey -t rsa -f id_rsa
+RUN mkdir /root/.ssh
+RUN echo ${ECDSA_FINGERPRINT} > /root/.ssh/known_hosts
+EOT
 for tarball in $(ls tmp/); do
   echo COPY ./tmp/$tarball /tarballs/$tarball >> Dockerfile.dcind
 done
 
-docker build -f Dockerfile.dcind ./ stackatotest/hsc-dcind:${TAG} ${BUILD_ARGS}
+docker build -f Dockerfile.dcind ./ -t ${REGISTRY}stackatotest/hsc-dcind:${TAG} ${BUILD_ARGS}
 rm -rf tmp
 rm -f Dockerfile.dcind
+
