ac1241ec2300cb98fea84907cb4fe40f04403adf
Neil MacDougall <neil.macdougall@suse.com>
2017-05-30 13:49:03 +0100

    fix lint issue

--- components/app-shell/test/view/application.directive.spec.js
+++ components/app-shell/test/view/application.directive.spec.js
@@ -28,7 +28,7 @@
       $httpBackend.when('GET', '/pp/v1/proxy/v2/info').respond(200, {});
       $httpBackend.when('GET', '/pp/v1/proxy/v2/apps?page=1&results-per-page=48').respond(200, {guid: {}});
       $httpBackend.when('GET', '/pp/v1/cnsis/registered').respond([]);
-      
+
       // For some tests, the redirected state depends on whether the endpoints dashboard is available
       redirectStateName = $state.get('endpoint.dashboard') ? 'endpoint.dashboard' : 'error-page';
     }));
