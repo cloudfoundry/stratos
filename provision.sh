59f614cade1d7443b4922bbcfd8f7c8888e91c40
Irfan Habib <irfan.habib@suse.com>
2017-06-01 18:13:52 +0100

    Clean native built node_modules after proxy compile

--- provision.sh
+++ provision.sh
@@ -2,7 +2,6 @@
 
 WORK_DIR=$(pwd)
 
-npm run clean
 npm install
 PATH=$PATH:$WORK_DIR/node_modules/.bin
 bower install --allow-root --force
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,5 +6,9 @@ set -e
 rm -rf node_modules
 npm install
 npm run build-backend
+
 adduser -D -G users -u ${USER_ID} ${USER_NAME}
 chown -R ${USER_NAME}:${GROUP_ID} outputs/
+
+# Clean node_modules
+rm -rf node_modules
