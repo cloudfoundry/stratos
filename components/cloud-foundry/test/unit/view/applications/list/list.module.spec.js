3652f3b945e6296b155260ba4365ee12ca2755ec
Richard <richard-cox@users.noreply.github.com>
2017-06-07 15:26:17 +0100

    Fix issue where app wall filters were only semi cleared after being reset #2 (#1077)
    * Fix issue where app wall filters were only semi cleared after being reset
    Bug was...
    - 1 cf, in app wall select cluster, org and space
    - Add a second cf and return to app wall
    - Filters all showed as being reset while under the hood old filter was
    still being applied.
    
    * Change to existing fix for 2nd cf and app-wall
    Ensure existing filters for org + space are used except when new cf
    had been detected
    
    * gate check
    
    * gate check

--- components/cloud-foundry/test/unit/view/applications/list/list.module.spec.js
+++ components/cloud-foundry/test/unit/view/applications/list/list.module.spec.js
@@ -438,8 +438,7 @@
           appModel.filterParams.spaceGuid = 'junk3';
 
           setUp();
-          // Kick of a digest, we only make a call when we have a valid org
-          $scope.$digest();
+          $httpBackend.flush();
 
           check(allFilterValue, 3, allFilterValue, 1, allFilterValue, 1);
         });
@@ -449,7 +448,6 @@
           appModel.filterParams.cnsiGuid = 'junk1';
 
           setUp();
-          // Kick of a digest, we only make a call when we have a valid org
           $scope.$digest();
 
           check(allFilterValue, 3, allFilterValue, 1, allFilterValue, 1);
@@ -461,7 +459,6 @@
           appModel.filterParams.orgGuid = 'junk2';
 
           setUp();
-          // Kick of a digest, we only make a call when we have a valid org
           $scope.$digest();
 
           check(cnsiGuid, 3, allFilterValue, 3, allFilterValue, 1);
