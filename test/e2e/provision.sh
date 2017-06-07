10001354bd3b4dcc723ea4623bf0eaa3fe3c7d25
Irfan Habib <irfan.habib@suse.com>
2017-06-07 14:27:27 +0100

    minor tweaks

--- test/e2e/provision.sh
+++ test/e2e/provision.sh
@@ -2,7 +2,6 @@
 
 # TODO: combine with the stratos-ui/provision.sh, the only difference is the conditional
 WORK_DIR=$(pwd)
-TOOLS_DIR="$WORK_DIR/tools"
 cp -r /node_modules .
 if [ "$(md5sum /package.json | cut -d' ' -f1)" != "$(md5sum package.json| cut -d' ' -f1)" ]; then
   echo -e "\033[31mpackage.json was updated, please update the hsc-concourse:latest image. Running npm install\033[0m"
@@ -11,5 +10,6 @@ else
   echo -e "\033[32mpackage.json has not changed. Skipping npm install\033[0m"
 fi
 PATH=$PATH:${WORK_DIR}/node_modules/.bin
+ls build
 bower install --allow-root --force
 npm run build
