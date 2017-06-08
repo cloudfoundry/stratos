b94622db814afe11e904cd7e30335dfba2ba8294
Neil MacDougall <neil.macdougall@suse.com>
2017-06-08 17:25:45 +0100

    tweak

--- tools/cloud-foundry/package.sh
+++ tools/cloud-foundry/package.sh
@@ -24,8 +24,7 @@ EOF
 # Delete endpoints-dashboard from bower.json
 sed -i '/"endpoints-dashboard.*/d' bower.json
 
-npm install -g gulp
-npm install -g bower
+npm install -g gulp bower
 
 cd ${TOP_LEVEL}
 
