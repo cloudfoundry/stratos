ae2fd3d87f48b51579de83d06adb68acaada5d7e
Irfan Habib <irfan.habib@suse.com>
2017-06-08 15:21:31 +0100

    Remove stackato

--- test/e2e/provision.sh
+++ test/e2e/provision.sh
@@ -4,7 +4,7 @@
 WORK_DIR=$(pwd)
 cp -r /node_modules .
 if [ "$(md5sum /package.json | cut -d' ' -f1)" != "$(md5sum package.json| cut -d' ' -f1)" ]; then
-  echo -e "\033[31mpackage.json was updated, please update the hsc-concourse:latest image. Running npm install\033[0m"
+  echo -e "\033[31mpackage.json was updated, please update the concourse:latest image. Running npm install\033[0m"
   npm install
 else
   echo -e "\033[32mpackage.json has not changed. Skipping npm install\033[0m"
