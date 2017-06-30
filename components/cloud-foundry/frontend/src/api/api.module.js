578ebe48b0abebcd20ce2011607d114c20ead184
Richard Cox <richard.cox@suse.com>
2017-06-30 13:55:44 +0100

    Remove obsolete and unused CFAPIInterceptor from api.module

--- components/cloud-foundry/frontend/src/api/api.module.js
+++ components/cloud-foundry/frontend/src/api/api.module.js
@@ -8,55 +8,6 @@
    * @description The API layer of the CF platform that handles HTTP requests
    */
   angular
-    .module('cloud-foundry.api', [], config);
-
-  function config($httpProvider) {
-    $httpProvider.interceptors.push(CFAPIInterceptor);
-  }
-
-  function CFAPIInterceptor($q) {
-    return {
-      request: marshalRequest,
-      response: marshalResponse
-    };
-
-    function makeQueryString(params) {
-      params = params || {};
-
-      var query = '';
-      var firstQuery = true;
-
-      if (params.queries) {
-        _.each(params.queries, function (value, key) {
-          query = query + ((firstQuery ? '' : '&') + key + '=' + value);
-          firstQuery = false;
-        });
-
-        if (params.queries['inline-relations-depth']) {
-          query = query + '&orphan-relations=1';
-        }
-      }
-
-      if (params.filter && params.filter.name) {
-        query = query + ((firstQuery ? '' : '&') + 'q=' + params.filter.name + ':' + params.filter.value);
-      }
-
-      return query;
-    }
-
-    function marshalRequest(config) {
-      if (config.url.substr(0,11) === '/api/cf/v2/') {
-        config.paramSerializer = makeQueryString;
-      }
-      return config || $q.when(config);
-    }
-
-    function marshalResponse(response) {
-      if (response.config.url.substr(0,4) === '/v2/' && response.config.url.indexOf('orphan-relations=1')) {
-        // insert orphan relations back into returned resources
-      }
-      return response;
-    }
-  }
+    .module('cloud-foundry.api', []);
 
 })();
