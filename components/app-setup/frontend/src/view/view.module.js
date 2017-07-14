85bf79c33c006be51df101460d69f3ca77249ea7
Richard Cox <richard.cox@suse.com>
2017-07-14 14:12:39 +0100

    General tidy up

--- components/app-setup/frontend/src/view/view.module.js
+++ components/app-setup/frontend/src/view/view.module.js
@@ -9,7 +9,6 @@
   var SETUP_STATE = 'setup';
 
   function registerState($stateProvider) {
-
     $stateProvider.state(SETUP_STATE, {
       url: '/setup',
       templateUrl: 'plugins/app-setup/view/setup.html',
@@ -21,7 +20,7 @@
   }
 
   function registerSetup(consoleSetupCheck) {
-    // Apply a setup up state so the console knows to come here when it's not setup
+    // The console will navigate to this state when it detects setup is required (instead of the default error page)
     consoleSetupCheck.setupState = SETUP_STATE;
   }
 
