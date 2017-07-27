187cf22cebaebe1758ecbef8477ba279c391b2ba
Irfan Habib <irfan.habib@suse.com>
2017-07-27 09:48:55 +0100

    Add a glide-cache and an npm cache to the portal proxy builder to speed up builds (#1195)

--- deploy/tools/build-push-proxy-builder-image.sh
+++ deploy/tools/build-push-proxy-builder-image.sh
@@ -15,8 +15,46 @@ SHARED_IMAGE_URL=${DOCKER_REGISTRY}/${DOCKER_ORG}/${NAME}:${TAG}
 
 echo "Building Docker Image for $NAME"
 pushd deploy
+
+# Generate Glide cache
+cat << EOT > ../run-glide.sh
+#!/bin/sh
+BACKEND_PATHS=\$(find /stratos-ui/components -name backend)
+for backend in \${BACKEND_PATHS};
+do
+    cd \$backend
+    glide install;
+done
+EOT
+chmod +x ../run-glide.sh
+
+docker run \
+       -ti \
+       --rm \
+       -e GLIDE_HOME=/.glide \
+       -e HOME=/stratos-ui \
+       --volume ${PWD}/glide-cache:/.glide \
+       --volume $PWD/../:/stratos-ui \
+       splatform/stratos-bk-build-base:latest \
+       sh /stratos-ui/run-glide.sh
+
+# Generate NPM cache
+docker run \
+       -ti \
+       --rm \
+       --volume ${PWD}/npm-cache:/root/.npm \
+       --volume $PWD/..:/stratos-ui \
+       splatform/stratos-bk-build-base:latest \
+       sh  -c "cd /stratos-ui && npm install"
+
 docker build --tag ${NAME} \
              --file Dockerfile.bk.build .
+
+sudo rm -rf ./glide-cache
+sudo rm -rf ./npm-cache
+rm -rf ../run-glide.sh
+rm -rf ../vendor/
+
 popd
 
 echo "Tag and push the shared image"
