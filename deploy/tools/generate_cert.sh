492c392b15d7fab84a6f6bc9f1d90964183c7f38
Irfan Habib <irfan.habib@suse.com>
2017-08-03 13:15:42 +0100

    SSL certs handling

--- deploy/tools/generate_cert.sh
+++ deploy/tools/generate_cert.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # Settings
-devcerts_path=${DEV_CERTS_PATH:-portal-proxy-output/dev-certs}
-domain=pproxy
-commonname=192.168.99.100
-country=US
-state=Washington
-locality=Seattle
+devcerts_path=${CERTS_PATH:-portal-proxy-output/dev-certs}
+domain=${DEV_CERTS_DOMAIN:-pproxy}
+commonname=127.0.0.1
+country=UK
+state=Bristol
+locality=Bristol
 organization=SUSE
-organizationalunit=HDP
+organizationalunit=CAP
 email=SUSE
 
 # Generate a key and cert
 echo "Generating key and cert for $domain"
-mkdir -p ${devcerts_path}
+if [ ! -d ${devcerts_path} ]; then
+  mkdir -p ${devcerts_path}
+fi
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${devcerts_path}/$domain.key -out ${devcerts_path}/$domain.crt \
   -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
