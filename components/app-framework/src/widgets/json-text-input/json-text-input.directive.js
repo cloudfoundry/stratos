38967c19351d69b456a39a890b73b50d4b99169c
Neil MacDougall <neil.macdougall@suse.com>
2017-09-08 12:46:24 +0100

    Allow parameters to be supplied when creating a service instance and binding to an application

---
+++ components/app-framework/src/widgets/json-text-input/json-text-input.directive.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.framework.widgets')
+    .directive('jsonTextInput', jsonTextInput);
+
+  /**
+   * @name jsonTextInput
+   * @description A directive that displays JSON.
+   * @returns {object} The directive definition object
+   */
+  function jsonTextInput() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      scope: {
+        ngModel: '='
+      },
+      link: function ($scope, elem, attr, ngModelCtrl) {
+        ngModelCtrl.$parsers.push(function (viewValue) {
+          try {
+            return angular.fromJson(viewValue);
+          } catch (e) {
+            return undefined;
+          }
+        });
+      }
+    };
+  }
+})();
