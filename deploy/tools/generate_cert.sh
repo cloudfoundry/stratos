162ba8e550045c18b30c07321bcf1408381be1a1
Irfan Habib <irfan.habib@suse.com>
2017-09-18 13:46:07 +0100

    Update generate_cert.sh

--- deploy/tools/generate_cert.sh
+++ deploy/tools/generate_cert.sh
@@ -14,6 +14,5 @@ email=SUSE
 # Generate a key and cert
 echo "Generating key and cert for $domain"
 mkdir -p ${devcerts_path}
-
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${devcerts_path}/$domain.key -out ${devcerts_path}/$domain.crt \
   -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
