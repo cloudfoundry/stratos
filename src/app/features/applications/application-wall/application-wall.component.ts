31eb2b31ea00017581efe46cc6afda092e1c1f1d
Nathan Jones <nathanmjones1@gmail.com>
2017-09-22 11:03:24 +0100

    Added animation to application wall

--- src/app/features/applications/application-wall/application-wall.component.ts
+++ src/app/features/applications/application-wall/application-wall.component.ts
@@ -1,21 +1,30 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { denormalize } from 'normalizr';
-
-import { Observable } from 'rxjs/Rx';
-import { skipWhile } from 'rxjs/operator/skipWhile';
-
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Rx';
+
 import { getAPIResourceEntity } from '../../../store/actions/api.actions';
-import { EntitiesState } from '../../../store/reducers/entity.reducer';
-import { getCurrentPage, PaginationState, PaginationEntityState } from './../../../store/reducers/pagination.reducer';
-import { GetAllApplications, ApplicationSchema } from '../../../store/actions/application.actions';
+import { ApplicationSchema, GetAllApplications } from '../../../store/actions/application.actions';
 import { AppState } from '../../../store/app-state';
+import { getCurrentPage } from './../../../store/reducers/pagination.reducer';
 
 
 @Component({
   selector: 'app-application-wall',
   templateUrl: './application-wall.component.html',
-  styleUrls: ['./application-wall.component.scss']
+  styleUrls: ['./application-wall.component.scss'],
+  animations: [
+    trigger(
+      'cardEnter', [
+        transition('* => *', [
+          query(':enter', [
+            style({ opacity: 0, transform: 'translateY(10px)' }),
+            stagger(50, animate('150ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })))
+          ], { optional: true })
+        ])
+      ]
+    )
+  ]
 })
 export class ApplicationWallComponent implements OnInit {
 
@@ -33,16 +42,17 @@ export class ApplicationWallComponent implements OnInit {
       action: new GetAllApplications(paginationKey),
       schema: [ApplicationSchema]
     });
-    this.isFetching = getObs$.mergeMap(({paginationEntity}) => {
+    this.isFetching = getObs$.mergeMap(({ paginationEntity }) => {
       return Observable.of(paginationEntity.fetching);
     });
 
     getObs$
-    .subscribe(({ paginationEntity, data }) => {
-      if (!paginationEntity.fetching) {
-        this.applications = data.map(getAPIResourceEntity);
-      }
-    });
+      .delay(100)
+      .subscribe(({ paginationEntity, data }) => {
+        if (!paginationEntity.fetching) {
+          this.applications = data.map(getAPIResourceEntity);
+        }
+      });
   }
 
 }
