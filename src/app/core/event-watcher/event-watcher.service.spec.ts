4922dd6c33a67ac8e14137c668f5123d64778b28
Nathan Jones <nathanmjones1@gmail.com>
2017-09-25 11:38:32 +0100

    Hide nav on small screens

---
+++ src/app/core/event-watcher/event-watcher.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { EventWatcherService } from './event-watcher.service';
+
+describe('EventWatcherService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [EventWatcherService]
+    });
+  });
+
+  it('should be created', inject([EventWatcherService], (service: EventWatcherService) => {
+    expect(service).toBeTruthy();
+  }));
+});
