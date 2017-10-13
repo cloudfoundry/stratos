082e8ae3c209f8f4ad7238fd7ed400039e6eeb56
Irfan Habib <irfan.habib@suse.com>
2017-10-13 16:42:08 +0100

    Increase timeout for automated deployments

--- build/protractor.conf.js
+++ build/protractor.conf.js
@@ -172,7 +172,7 @@
     },
 
     jasmineNodeOpts: {
-      defaultTimeoutInterval: 45000,
+      defaultTimeoutInterval: 90000,
       // disable default jasmine report (using jasmine-spec-reporter)
       print: function () {
       }
