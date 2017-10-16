ef313e8cefe2c7244b7be002d3e6f157f3777bb0
Richard Cox <richard.cox@suse.com>
2017-10-16 13:01:43 +0100

    Fix app actions

--- src/app/store/actions/application.actions.ts
+++ src/app/store/actions/application.actions.ts
@@ -44,12 +44,6 @@ export const ApplicationSchema = new schema.Entity('application', {
     idAttribute: getAPIResourceGuid
   });
 
-  entity: {
-    stack: StackSchema,
-    space: SpaceSchema
-  }
-    idAttribute: getAPIResourceGuid
-  });
 export class GetAllApplications implements APIAction {
   constructor(public paginationKey?: string) {
     this.options = new RequestOptions();
@@ -137,11 +131,11 @@ export class AssociateRouteWithAppApplication implements APIAction {
 }
 
 export interface UpdateApplication {
-    name?: string;
-    instances?: number;
-    memory?: number;
-    enable_ssh?: boolean;
-    environment_json?: any;
+  name?: string;
+  instances?: number;
+  memory?: number;
+  enable_ssh?: boolean;
+  environment_json?: any;
 }
 
 export class UpdateExistingApplicationBase implements APIAction {
