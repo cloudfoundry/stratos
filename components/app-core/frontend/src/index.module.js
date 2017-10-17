2b7be121f74f47c49d5f18c0cfefe3f9e5d8297f
Irfan Habib <irfan.habib@suse.com>
2017-10-17 11:48:30 +0100

    Fix unit tests

--- components/app-core/frontend/src/index.module.js
+++ components/app-core/frontend/src/index.module.js
@@ -26,7 +26,7 @@
   angular
     .module('console-app', angularModules.concat(otherModules, ['app'], pluginModules), config);
 
-  function config($compileProvider, $logProvider) {
+  function config($compileProvider, $logProvider, $qProvider) {
 
     /**
      * Disabling Debug Data
@@ -44,6 +44,8 @@
 
     $logProvider.debugEnabled(false);
     $compileProvider.preAssignBindingsEnabled(true);
+    // Skip errors on unhandled rejections (promise chains that don't deal with errors)
+    $qProvider.errorOnUnhandledRejections(false);
 
   }
 
