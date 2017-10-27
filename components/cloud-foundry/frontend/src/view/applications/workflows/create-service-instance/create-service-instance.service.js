9e80313a56f7f5b46a9e12067cec3ece86c9b362
Neil MacDougall <nwmac@users.noreply.github.com>
2017-10-27 11:20:30 +0100

    Add ability to edit a service instance (#1324)
    * Adds ability to edit a service instance
    
    * Add name uniqueness support
    
    * Fix problem with enter in params editor
    
    * Fixed lint issue
    
    * Address PR Feedback

--- components/cloud-foundry/frontend/src/view/applications/workflows/create-service-instance/create-service-instance.service.js
+++ components/cloud-foundry/frontend/src/view/applications/workflows/create-service-instance/create-service-instance.service.js
@@ -16,6 +16,7 @@
    */
   function cfServiceCreateServiceInstanceWorkflow($q, modelManager, frameworkAsyncTaskDialog) {
     var instanceModel = modelManager.retrieve('cloud-foundry.model.service-instance');
+    var serviceModel = modelManager.retrieve('cloud-foundry.model.service');
 
     /**
      * @function addService
@@ -79,6 +80,83 @@
             options: options
           },
           doCreate
+        ).result;
+      },
+
+      //    function asyncDialog(config, context, submitAction, invalidityCheck, initPromise) {
+
+      edit: function (cnsiGuid, serviceInstance) {
+        var path = 'plugins/cloud-foundry/view/applications/workflows/create-service-instance/create-service-instance.html';
+        var options = {
+          instanceNames: [],
+          servicePlans: [],
+          userInput: {
+            plan: null
+          }
+        };
+
+        var doEdit = function () {
+          var instanceData = {
+            name: options.userInput.name,
+            tags: _.map(options.userInput.tags, function (tag) { return tag.text; })
+          };
+
+          if (serviceInstance.entity.service_plan_guid !== options.userInput.plan.metadata.guid) {
+            instanceData.service_plan_guid = options.userInput.plan.metadata.guid;
+          }
+
+          if (options.userInput.params) {
+            instanceData.parameters = options.userInput.params;
+          }
+          return instanceModel.updateServiceInstance(cnsiGuid, serviceInstance.metadata.guid, instanceData);
+        };
+
+        var doInitEdit = function () {
+          options.userInput = {
+            name: serviceInstance.entity.name,
+            tags: _.map(serviceInstance.entity.tags, function (tag) {
+              return {
+                text: tag
+              };
+            })
+          };
+
+          return serviceModel.allServicePlans(cnsiGuid, serviceInstance.entity.service_guid).then(function (servicePlans) {
+            var plans = _.map(servicePlans, function (o) { return { label: o.entity.name, value: o }; });
+            [].push.apply(options.servicePlans, plans);
+
+            if (options.servicePlans.length) {
+              options.userInput.plan = options.servicePlans[0].value;
+            }
+
+            // Get all of the instances so that we can check for name uniqueness
+            var queryOptions = { q:  'space_guid:' + serviceInstance.entity.space_guid };
+            return instanceModel.all(cnsiGuid, queryOptions).then(function (spaceInstances) {
+              options.instanceNames = _.map(spaceInstances, function (instance) { return instance.entity.name; });
+            });
+          });
+        };
+
+        return frameworkAsyncTaskDialog(
+          {
+            title: 'app.app-info.app-tabs.services.edit.title',
+            templateUrl: path,
+            submitCommit: true,
+            buttonTitles: {
+              submit: 'app.app-info.app-tabs.services.edit.button.yes'
+            },
+            class: 'dialog-form-larger create-service-instance-dialog edit-service-instance',
+            dialog: true,
+            allowEnter: function () {
+              return false;
+            }
+          },
+          {
+            options: options
+          },
+          doEdit,
+          undefined,
+          doInitEdit
         ).result;
       }
     };
