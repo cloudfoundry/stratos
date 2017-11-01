9919cb78eee64e0f7e998321c7f080e95fdaf4f4
Nathan Jones <njones@suse.com>
2017-11-01 10:32:34 +0000

    Added log viewer tests

--- src/app/shared/components/log-viewer/log-viewer.component.spec.ts
+++ src/app/shared/components/log-viewer/log-viewer.component.spec.ts
@@ -3,7 +3,6 @@ import { MDAppModule } from '../../../core/md.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CommonModule } from '@angular/common';
 import { CoreModule } from '../../../core/core.module';
-import { it } from '@angular/cli/lib/ast-tools/spec-utils';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogViewerComponent } from './log-viewer.component';
@@ -13,6 +12,7 @@ fdescribe('LogViewerComponent', () => {
   let component: TestHostComponent;
   let fixture: ComponentFixture<TestHostComponent>;
   let stream: Subject<String>;
+  let contentEl: HTMLDivElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,6 +32,7 @@ fdescribe('LogViewerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    contentEl = component.logViewer.content.nativeElement as HTMLDivElement;
     stream = new Subject();
     component.logViewer.logStream = stream;
     fixture.detectChanges();
@@ -43,10 +44,63 @@ fdescribe('LogViewerComponent', () => {
   });
 
   it('should add logline from observable', (done) => {
-    expect(component.logViewer.content.nativeElement.children.length).toEqual(0);
+    const logText = 'Log';
+    expect(contentEl.children.length).toEqual(0);
     stream.next('Log');
     fixture.detectChanges();
-    expect(component.logViewer.content.nativeElement.children.length).toEqual(1);
+    setTimeout(() => {
+      expect(contentEl.children.length).toEqual(1);
+      expect(contentEl.children[0].children[0].innerHTML).toEqual(logText);
+      done();
+    });
+  });
+
+  it('should be in high throughput mode', (done) => {
+    expect(contentEl.children.length).toEqual(0);
+    let log = true;
+
+    component.logViewer.isHighThroughput$.take(1).subscribe(high => {
+      expect(high).toEqual(false);
+    });
+
+    while (log) {
+      setTimeout(() => {
+        stream.next('Log');
+      }, 100);
+    }
+
+    setTimeout(() => {
+      fixture.detectChanges();
+      log = false;
+      component.logViewer.isHighThroughput$.take(1).subscribe(high => {
+        expect(high).toEqual(true);
+      });
+      done();
+    }, 5500);
+
+  });
+
+  it('should only allow max rows', (done) => {
+    expect(contentEl.children.length).toEqual(0);
+
+    component.logViewer.isHighThroughput$.take(1).subscribe(high => {
+      expect(high).toEqual(false);
+    });
+
+    const maxLines = component.logViewer.maxLogLines + 100;
+    let i = 0;
+
+    while (i < maxLines) {
+      stream.next('Log');
+      ++i;
+    }
+
+    setTimeout(() => {
+      fixture.detectChanges();
+      expect(contentEl.children.length).toEqual(component.logViewer.maxLogLines);
+      done();
+    });
+
   });
   @Component({
     selector: `app-host-component`,
