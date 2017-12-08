087a5d22e52cf260fed5220e4926fb5e411712a9
Nathan Jones <njones@suse.com>
2017-12-08 12:31:02 +0000

    codecov intergration

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'json'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
