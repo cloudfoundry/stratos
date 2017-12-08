0e3f4e35dbdae5695cc0cfc5455dd79236f5204a
Nathan Jones <njones@suse.com>
2017-12-08 15:00:30 +0000

    Added disconnect

--- src/app/shared/list-configs/endpoints-list-config.service.ts
+++ src/app/shared/list-configs/endpoints-list-config.service.ts
@@ -1,3 +1,5 @@
+import { selectUpdateInfo } from '../../store/selectors/api.selectors';
+import { CNSISEffect } from '../../store/effects/cnsis.effects';
 import {
   ConnectEndpointDialogComponent,
 } from '../../features/endpoints/connect-endpoint-dialog/connect-endpoint-dialog.component';
@@ -7,13 +9,14 @@ import { TableCellActionsComponent } from '../components/table/table-cell-action
 import { TableCellSelectComponent } from '../components/table/table-cell-select/table-cell-select.component';
 import { TableHeaderSelectComponent } from '../components/table/table-header-select/table-header-select.component';
 import { Action, Store } from '@ngrx/store';
-import { CNSISModel } from '../../store/types/cnsis.types';
+import { CNSISModel, cnsisStoreNames } from '../../store/types/cnsis.types';
 import { RouterNav } from '../../store/actions/router.actions';
 import { ConnectCnis, DisconnectCnis } from '../../store/actions/cnsis.actions';
 import { EndpointsDataSource } from '../data-sources/endpoints-data-source';
 import { IGlobalListAction, IListAction, IListConfig, IMultiListAction } from '../components/list/list.component';
 import { Injectable } from '@angular/core';
 import { MdDialog } from '@angular/material';
+import { GetSystemInfo } from '../../store/actions/system.actions';
 
 
 function getEndpointTypeString(endpoint: CNSISModel): string {
@@ -61,6 +64,20 @@ export class EndpointsListConfigService implements IListConfig<CNSISModel> {
       this.store.dispatch(new DisconnectCnis(
         item.guid
       ));
+      const disSub = this.store.select(selectUpdateInfo(
+        cnsisStoreNames.type,
+        item.guid,
+        CNSISEffect.disconnectingKey,
+        cnsisStoreNames.section
+      ))
+        .pairwise()
+        .subscribe(([oldVal, newVal]) => {
+          if (!newVal.error && (oldVal.busy && !newVal.busy)) {
+            // Has finished fetching
+            this.store.dispatch(new GetSystemInfo());
+            disSub.unsubscribe();
+          }
+        });
     },
     icon: 'remove_from_queue',
     label: 'Disconnect',
