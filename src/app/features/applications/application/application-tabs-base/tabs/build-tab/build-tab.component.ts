6f2868b56b045ab7a960613601767412acf6f05e
Neil MacDougall <neil.macdougall@suse.com>
2018-01-18 16:18:23 +0000

    Fix import path

--- src/app/features/applications/application/application-tabs-base/tabs/build-tab/build-tab.component.ts
+++ src/app/features/applications/application/application-tabs-base/tabs/build-tab/build-tab.component.ts
@@ -1,4 +1,4 @@
-import { AppState } from '../../../../store/app-state';
+import { AppState } from '../../../../../../store/app-state';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
