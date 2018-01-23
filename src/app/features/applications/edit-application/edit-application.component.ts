920c127816b42c97d87f4012c19ca24b44f1b394
Neil MacDougall <neil.macdougall@suse.com>
2018-01-23 09:15:09 +0000

    Edit APP WIP

--- src/app/features/applications/edit-application/edit-application.component.ts
+++ src/app/features/applications/edit-application/edit-application.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { ApplicationService } from '../application.service';
+import { EntityService } from '../../../core/entity-service';
+import { AppState } from '../../../store/app-state';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 // import { UpdateApplication } from '../../../store/actions/application.actions';
 
@@ -7,8 +13,81 @@ import { Component } from '@angular/core';
   templateUrl: './edit-application.component.html',
   styleUrls: ['./edit-application.component.scss']
 })
-export class EditApplicationComponent {
+export class EditApplicationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  editAppForm: FormGroup;
+
+  constructor(
+    private applicationService: ApplicationService,
+    private entityService: EntityService,
+    private store: Store<AppState>,
+    private fb: FormBuilder
+  ) {
+    this.editAppForm = this.fb.group({
+      name:['',  Validators.required],
+      instances: 0,
+      disk_quota: 0,
+      memory: 0,
+      enable_ssh: false,
+      production: false
+    });
+  }
+
+  private app: any = {
+    entity: {}
+  };
+
+  private sub: Subscription;
+
+  ngOnInit() {
+    this.sub = this.applicationService.appSummary$.subscribe(app => {
+      console.log('APP');
+      console.log(app);
+      if(app.metadata) {
+        this.app = app.metadata;
+        this.editAppForm.setValue({
+          name:    this.app.name,
+          instances: this.app.instances,
+          memory: this.app.memory,
+          disk_quota: this.app.disk_quota,
+          production: this.app.production,
+          enable_ssh: this.app.enable_ssh
+        });
+        // Don't want the values to change while the user is editing
+        this.clearSub();
+      }
+    });
+  }
+
+  updateApp() {
+    console.log('Apply Application Edit');
+    console.log(this.editAppForm);
+
+    var updates = {};
+    // We will only send the values that were actually edited
+    for (var key of Object.keys(this.editAppForm.value)) {
+      console.log(key);
+      if (!this.editAppForm.controls[key].pristine) {
+        updates[key] = this.editAppForm.value[key];
+      }
+    }
+    console.log(updates);
+
+    if (Object.keys(updates).length) {
+      // We had at least one value to change - send update action
+      this.applicationService.updateApplication(updates);
+    }
+  }
+ 
+  clearSub() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+      this.sub = undefined;
+    }
+  }
+
+  ngOnDestroy() {
+    this.clearSub();
+  }
 
 }
