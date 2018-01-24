c4996b34f71a3bd823d9b9fd90f0b673513b30ee
Neil MacDougall <neil.macdougall@suse.com>
2018-01-24 17:18:13 +0000

    Unit test fix

--- src/app/features/applications/edit-application/edit-application.component.spec.ts
+++ src/app/features/applications/edit-application/edit-application.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
- 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { EditApplicationComponent } from './edit-application.component';
 import { CoreModule } from '../../../core/core.module';
 import { SharedModule } from '../../../shared/shared.module';
@@ -10,9 +10,6 @@ import { ApplicationServiceMock, generateTestApplicationServiceProvider } from '
 import { createBasicStoreModule } from '../../../test-framework/store-test-helper';
 import { generateTestEntityServiceProvider } from '../../../test-framework/entity-service.helper';
 import { ApplicationSchema, GetApplication } from '../../../store/actions/application.actions';
-import { XHRBackend, ResponseOptions } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { EntityService } from '../../../core/entity-service';
 import { ApplicationEnvVarsService } from '../application/application-tabs-base/tabs/build-tab/application-env-vars.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
