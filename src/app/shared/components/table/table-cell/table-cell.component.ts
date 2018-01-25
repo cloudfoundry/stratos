ad37c5b2ba5c871ccf84402a7c4149b9b20a1d2e
Richard Cox <richard.cox@suse.com>
2018-01-25 18:12:50 +0000

    Fix linting

--- src/app/shared/components/table/table-cell/table-cell.component.ts
+++ src/app/shared/components/table/table-cell/table-cell.component.ts
@@ -1,6 +1,17 @@
 import { TableCellTCPRouteComponent } from '../custom-cells/table-cell-tcproute/table-cell-tcproute.component';
 import { TableCellRouteComponent } from '../custom-cells/table-cell-route/table-cell-route.component';
-import { Component, ComponentFactoryResolver, Input, OnInit, Type, ViewContainerRef, ViewChild, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  Input,
+  OnInit,
+  Type,
+  ViewContainerRef,
+  ViewChild,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges
+} from '@angular/core';
 import { TableCellSelectComponent } from '../table-cell-select/table-cell-select.component';
 import { TableHeaderSelectComponent } from '../table-header-select/table-header-select.component';
 import { TableCellEditComponent } from '../table-cell-edit/table-cell-edit.component';
