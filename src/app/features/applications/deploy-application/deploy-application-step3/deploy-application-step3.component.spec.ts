9d57793915af839eee930ffde63343091517f45c
Irfan Habib <irfan.habib@suse.com>
2018-01-26 15:35:59 +0000

    Fix unit tests

--- src/app/features/applications/deploy-application/deploy-application-step3/deploy-application-step3.component.spec.ts
+++ src/app/features/applications/deploy-application/deploy-application-step3/deploy-application-step3.component.spec.ts
@@ -1,4 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CoreModule } from '../../../../core/core.module';
+import { SharedModule } from '../../../../shared/shared.module';
+import { inject, TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { Store, StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialogModule } from '@angular/material';
 
 import { DeployApplicationStep3Component } from './deploy-application-step3.component';
 
@@ -8,7 +16,14 @@ describe('DeployApplicationStep3Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeployApplicationStep3Component ]
+      declarations: [ DeployApplicationStep3Component ],
+      imports: [
+        CoreModule,
+        SharedModule,
+        RouterTestingModule,
+        createBasicStoreModule(),
+        BrowserAnimationsModule
+      ]
     })
     .compileComponents();
   }));
--- src/app/features/applications/deploy-application/deploy-application.component.spec.ts
+++ src/app/features/applications/deploy-application/deploy-application.component.spec.ts
@@ -1,14 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, inject, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../../../shared/shared.module';
+import { CfOrgSpaceDataService } from '../../../shared/data-services/cf-org-space-service.service';
+import { CoreModule } from '../../../core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from '../../../store/reducers.module';
+import { getInitialTestStoreState } from '../../../test-framework/store-test-helper';
+import { CreateApplicationModule } from '../create-application/create-application.module';
 import { DeployApplicationComponent } from './deploy-application.component';
+import { DeployApplicationStep2Component } from './deploy-application-step2/deploy-application-step2.component';
+import { DeployApplicationStep3Component } from './deploy-application-step3/deploy-application-step3.component';
 
 describe('DeployApplicationComponent', () => {
   let component: DeployApplicationComponent;
   let fixture: ComponentFixture<DeployApplicationComponent>;
+  const initialState = { ...getInitialTestStoreState() };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeployApplicationComponent ]
+          declarations: [
+            DeployApplicationComponent,
+            DeployApplicationStep2Component,
+            DeployApplicationStep3Component,
+          ],
+        providers: [CfOrgSpaceDataService],
+        imports: [
+          SharedModule,
+          CoreModule,
+          RouterTestingModule,
+          CreateApplicationModule,
+          BrowserAnimationsModule,
+          StoreModule.forRoot(
+            appReducers,
+            {
+              initialState
+            }
+          ),
+        ]
     })
     .compileComponents();
   }));
