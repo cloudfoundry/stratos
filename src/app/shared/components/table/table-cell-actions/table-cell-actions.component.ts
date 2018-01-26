d4353ca582af7602d78b4076efffe2ca93a9299e
Richard Cox <richard.cox@suse.com>
2018-01-26 16:58:16 +0000

    Removed unused import, moves tslint:disable to top of file

--- src/app/shared/components/table/table-cell-actions/table-cell-actions.component.ts
+++ src/app/shared/components/table/table-cell-actions/table-cell-actions.component.ts
@@ -1,6 +1,5 @@
-import { CardComponent } from '../../cards/card/card.component';
-import { IListAction, ListConfig } from '../../list/list.component';
 /* tslint:disable:no-access-missing-member https://github.com/mgechev/codelyzer/issues/191/ */
+import { IListAction, ListConfig } from '../../list/list.component';
 import { Component, OnInit } from '@angular/core';
 import { TableCellCustom } from '../table-cell/table-cell-custom';
 import { Store } from '@ngrx/store';
