23c484368bd2f4b93f4a43fd0d0c7c21231401d6
Richard Cox <richard.cox@suse.com>
2018-01-30 13:43:40 +0000

    Removed test data

--- src/app/shared/data-sources/endpoints-data-source.ts
+++ src/app/shared/data-sources/endpoints-data-source.ts
@@ -23,28 +23,6 @@ export class EndpointsDataSource extends ListDataSource<CNSISModel> {
   ) {
     const action = new GetAllCNSIS();
     const paginationKey = GetAllCNSIS.storeKey;
-    const rowStateManager = new TableRowStateManager({
-      '2fa75a76-c2e6-490f-acac-02eabb1bbf6a': {
-        error: true,
-        message: '<a href="#">Lorem ipsum dolor</a> sit amet, consectetur adipiscing elit. Nulla malesuada ullamcorper massa eu euismod.',
-        blocked: false
-      },
-      '6dd897cb-2e93-422d-b707-15ce24e76bdb': {
-        error: false,
-        message: 'Me too',
-        blocked: true
-      }
-    });
-    interval(5000).pipe(
-      map(() => {
-        rowStateManager.updateRowState('2fa75a76-c2e6-490f-acac-02eabb1bbf6a', {
-          error: false
-        });
-        rowStateManager.updateRowState('6dd897cb-2e93-422d-b707-15ce24e76bdb', {
-          error: true
-        });
-      })
-    ).subscribe();
     super({
       store,
       action,
@@ -81,7 +59,6 @@ export class EndpointsDataSource extends ListDataSource<CNSISModel> {
           field: 'api_endpoint.Host'
         },
       ],
-      rowsState: rowStateManager.observable
     });
     this.store = store;
     store.dispatch(new SetListStateAction(
