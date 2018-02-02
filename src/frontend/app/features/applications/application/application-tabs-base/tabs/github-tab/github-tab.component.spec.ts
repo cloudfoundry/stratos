4260a63f2b849f78ba84087f1d1e9cd00f31c689
Irfan Habib <irfan.habib@suse.com>
2018-02-02 14:46:25 +0000

    Fix unit tests

--- src/frontend/app/features/applications/application/application-tabs-base/tabs/github-tab/github-tab.component.spec.ts
+++ src/frontend/app/features/applications/application/application-tabs-base/tabs/github-tab/github-tab.component.spec.ts
@@ -1,16 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { GithubTabComponent } from './github-tab.component';
-import { CoreModule } from '../../../../../../core/core.module';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ApplicationService } from '../../../../application.service';
-import { ApplicationServiceMock } from '../../../../../../test-framework/application-service-helper';
 import { StoreModule } from '@ngrx/store';
-import { appReducers } from '../../../../../../store/reducers.module';
-import { getInitialTestStoreState } from '../../../../../../test-framework/store-test-helper';
-import { SharedModule } from '../../../../../../shared/shared.module';
 
-describe('GithubTabComponent', () => {
+import { CoreModule } from '../../../../../../core/core.module';
+import { SharedModule } from '../../../../../../shared/shared.module';
+import { appReducers } from '../../../../../../store/reducers.module';
+import { ApplicationServiceMock } from '../../../../../../test-framework/application-service-helper';
+import { getInitialTestStoreState } from '../../../../../../test-framework/store-test-helper';
+import { ApplicationService } from '../../../../application.service';
+import { GithubTabComponent } from './github-tab.component';
+
+fdescribe('GithubTabComponent', () => {
   let component: GithubTabComponent;
   let fixture: ComponentFixture<GithubTabComponent>;
   const initialState = getInitialTestStoreState();
