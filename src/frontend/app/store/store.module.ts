beb89d2d3dc53907fa4ec53375e0e25435d8bfa0
Irfan Habib <irfan.habib@suse.com>
2018-02-05 09:29:59 +0000

    Remove no longer required `routes.effects`

--- src/frontend/app/store/store.module.ts
+++ src/frontend/app/store/store.module.ts
@@ -1,5 +1,3 @@
-import { SetAPIFilterEffect } from './effects/set-api-filter.effect';
-import { SetClientFilterEffect } from './effects/set-client-filter.effect';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -11,16 +9,17 @@ import { AppVariablesEffect } from './effects/app-variables.effects';
 import { AuthEffect } from './effects/auth.effects';
 import { CNSISEffect } from './effects/cnsis.effects';
 import { CreateAppPageEffects } from './effects/create-app-effects';
+import { DeployAppEffects } from './effects/deploy-app.effects';
+import { GithubEffects } from './effects/github.effects';
 import { PaginationEffects } from './effects/pagination.effects';
 import { RouterEffect } from './effects/router.effects';
+import { SetAPIFilterEffect } from './effects/set-api-filter.effect';
+import { SetClientFilterEffect } from './effects/set-client-filter.effect';
 import { SnackBarEffects } from './effects/snackBar.effects';
 import { SystemEffects } from './effects/system.effects';
 import { UAASetupEffect } from './effects/uaa-setup.effects';
 import { UpdateAppEffects } from './effects/update-app-effects';
 import { AppReducersModule } from './reducers.module';
-import { DeployAppEffects } from './effects/deploy-app.effects';
-import { GithubEffects } from './effects/github.effects';
-import { RoutesEffects } from './effects/routes.effects';
 
 @NgModule({
   imports: [
@@ -43,9 +42,8 @@ import { RoutesEffects } from './effects/routes.effects';
       SetClientFilterEffect,
       SetAPIFilterEffect,
       DeployAppEffects,
-      GithubEffects,
-      RoutesEffects
-    ]),
+      GithubEffects
+    ])
   ]
 })
-export class AppStoreModule { }
+export class AppStoreModule {}
