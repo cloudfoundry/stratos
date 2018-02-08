2fa7d4c9d48e2350b4ec4c2f232a74b4f4c415a7
Irfan Habib <irfan.habib@suse.com>
2018-02-08 14:02:41 +0000

    merge

--- src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-summary-tab/cloud-foundry-summary-tab.component.ts
+++ src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-summary-tab/cloud-foundry-summary-tab.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cloud-foundry-summary-tab.component.scss']
 })
 export class CloudFoundrySummaryTabComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
