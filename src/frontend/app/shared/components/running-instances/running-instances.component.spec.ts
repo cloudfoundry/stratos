2455bc6ddf3b7562307824b80a0215886e6c0714
Nathan Jones <njones@suse.com>
2018-02-08 16:35:05 +0000

    Test fixes

--- src/frontend/app/shared/components/running-instances/running-instances.component.spec.ts
+++ src/frontend/app/shared/components/running-instances/running-instances.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RunningInstancesComponent } from './running-instances.component';
 import { createBasicStoreModule } from '../../../test-framework/store-test-helper';
+import { PaginationMonitorFactory } from '../../monitors/pagination-monitor.factory';
 
 describe('RunningInstancesComponent', () => {
   let component: RunningInstancesComponent;
@@ -14,6 +15,9 @@ describe('RunningInstancesComponent', () => {
       imports: [
         CoreModule,
         createBasicStoreModule(),
+      ],
+      providers: [
+        PaginationMonitorFactory
       ]
     })
       .compileComponents();
