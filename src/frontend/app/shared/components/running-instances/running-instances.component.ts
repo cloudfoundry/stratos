abcdbf5a7f69207fd2988f51996eb12743c731a2
Nathan Jones <njones@suse.com>
2018-02-08 15:55:40 +0000

    Make instances component use new pagination monitor

--- src/frontend/app/shared/components/running-instances/running-instances.component.ts
+++ src/frontend/app/shared/components/running-instances/running-instances.component.ts
@@ -6,7 +6,8 @@ import { map } from 'rxjs/operators';
 import { GetAppStatsAction } from '../../../store/actions/app-metadata.actions';
 import { AppState } from '../../../store/app-state';
 import { getPaginationPages } from '../../../store/reducers/pagination-reducer/pagination-reducer.helper';
-import { AppStatsSchema } from '../../../store/types/app-metadata.types';
+import { AppStatSchema, AppStatsSchema } from '../../../store/types/app-metadata.types';
+import { PaginationMonitorFactory } from '../../monitors/pagination-monitor.factory';
 
 @Component({
   selector: 'app-running-instances',
@@ -22,11 +23,19 @@ export class RunningInstancesComponent implements OnInit {
   // Observable on the running instances count for the application
   private runningInstances$: Observable<number>;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(
+    private store: Store<AppState>,
+    private paginationMonitorFactory: PaginationMonitorFactory
+  ) { }
 
   ngOnInit() {
+    const dummyAction = new GetAppStatsAction(this.appGuid, this.cfGuid);
+    const paginationMonitor = this.paginationMonitorFactory.create(
+      dummyAction.paginationKey,
+      AppStatSchema
+    );
     this.runningInstances$ =
-      getPaginationPages(this.store, new GetAppStatsAction(this.appGuid, this.cfGuid), AppStatsSchema)
+      paginationMonitor.currentPage$
         .pipe(
         map(appInstancesPages => {
           const allInstances = [].concat.apply([], Object.values(appInstancesPages || [])).filter(instance => !!instance);
