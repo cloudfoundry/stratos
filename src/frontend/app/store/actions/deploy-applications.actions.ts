6ee942310d6a73bfe2355a50a5b4334a2a4d653b
Nathan Jones <njones@suse.com>
2018-02-09 10:21:58 +0000

    Fix entity service and add actions to pagination action

--- src/frontend/app/store/actions/deploy-applications.actions.ts
+++ src/frontend/app/store/actions/deploy-applications.actions.ts
@@ -19,23 +19,27 @@ export const SET_BRANCH = '[Deploy App] Set branch';
 export const SET_DEPLOY_BRANCH = '[Deploy App] Set deploy branch';
 export const DELETE_COMMIT = '[Deploy App] Delete commit';
 
+export const FETCH_BRANCH_START = '[GitHub] Fetch branch start';
+export const FETCH_BRANCH_SUCCESS = '[GitHub] Fetch branch succeeded';
+export const FETCH_BRANCH_FAILED = '[GitHub] Fetch branch failed';
+
 export class SetAppSourceDetails implements Action {
-  constructor(public sourceType: SourceType) {}
+  constructor(public sourceType: SourceType) { }
   type = SET_APP_SOURCE_DETAILS;
 }
 
 export class SetAppSourceSubType implements Action {
-  constructor(public subType: SourceType) {}
+  constructor(public subType: SourceType) { }
   type = SET_APP_SOURCE_SUB_TYPE;
 }
 
 export class CheckProjectExists implements Action {
-  constructor(public projectName: any) {}
+  constructor(public projectName: any) { }
   type = CHECK_PROJECT_EXISTS;
 }
 
 export class ProjectDoesntExist implements Action {
-  constructor(public projectName: string) {}
+  constructor(public projectName: string) { }
   type = PROJECT_DOESNT_EXIST;
 }
 
@@ -48,14 +52,19 @@ export class ProjectExists implements Action {
 }
 
 export class FetchBranchesForProject implements PaginatedAction {
-  constructor(public projectName: string) {}
+  constructor(public projectName: string) { }
+  actions = [
+    FETCH_BRANCH_START,
+    FETCH_BRANCH_SUCCESS,
+    FETCH_BRANCH_FAILED
+  ];
   type = FETCH_BRANCHES_FOR_PROJECT;
   entityKey = GithubBranchSchema.key;
   paginationKey: 'branches';
 }
 
 export class SaveAppDetails implements Action {
-  constructor(public appDetails: GitAppDetails) {}
+  constructor(public appDetails: GitAppDetails) { }
   type = SAVE_APP_DETAILS;
 }
 
@@ -73,21 +82,21 @@ export class FetchCommit implements IRequestAction {
 }
 
 export class StoreCFSettings implements Action {
-  constructor(public cloudFoundryDetails: any) {}
+  constructor(public cloudFoundryDetails: any) { }
   type = SET_DEPLOY_CF_SETTINGS;
 }
 
 export class DeleteDeployAppSection implements Action {
-  constructor() {}
+  constructor() { }
   type = DELETE_DEPLOY_APP_SECTION;
 }
 
 export class SetBranch implements Action {
-  constructor(private branch: GitBranch) {}
+  constructor(private branch: GitBranch) { }
   type = SET_BRANCH;
 }
 
 export class SetDeployBranch implements Action {
-  constructor(private branch: string) {}
+  constructor(private branch: string) { }
   type = SET_DEPLOY_BRANCH;
 }
