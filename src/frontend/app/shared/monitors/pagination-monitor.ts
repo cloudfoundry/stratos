81c7ae7900e98eb391e3e26bca168c35d353afed
Richard Cox <richard.cox@suse.com>
2018-02-09 14:09:02 +0000

    Fix, reviewed with Nathan

--- src/frontend/app/shared/monitors/pagination-monitor.ts
+++ src/frontend/app/shared/monitors/pagination-monitor.ts
@@ -114,7 +114,7 @@ export class PaginationMonitor<T = any> {
       withLatestFrom(this.store.select(getAPIRequestDataState)),
       map(([[pagination, entities], allEntities]) => {
         const page = pagination.ids[pagination.currentPage] || [];
-        return page.length ? denormalize(page, [schema], allEntities).filter(ent => !!ent) : null;
+        return page.length ? denormalize(page, [schema], allEntities).filter(ent => !!ent) : [];
       }),
       shareReplay(1)
       );
