4acf27b49e4f42dd681150ea837a3e93c92b5165
Irfan Habib <irfan.habib@suse.com>
2018-02-13 16:32:16 +0000

    Undo app wall changes

--- src/frontend/app/features/applications/application-wall/application-wall.component.ts
+++ src/frontend/app/features/applications/application-wall/application-wall.component.ts
@@ -1,26 +1,14 @@
-import {
-  animate,
-  query,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { tag } from 'rxjs-spy/operators/tag';
-import {
-  distinctUntilChanged,
-  tap,
-  withLatestFrom,
-  delay,
-  debounceTime
-} from 'rxjs/operators';
+import { distinctUntilChanged, tap, withLatestFrom, delay, debounceTime } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Rx';
 
 import { EndpointsService } from '../../../core/endpoints.service';
 import {
   distinctPageUntilChanged,
-  ListDataSource
+  ListDataSource,
 } from '../../../shared/components/list/data-sources-controllers/list-data-source';
 import { CardAppComponent } from '../../../shared/components/list/list-types/app/card/card-app.component';
 import { CfAppConfigService } from '../../../shared/components/list/list-types/app/cf-app-config.service';
@@ -34,65 +22,54 @@ import { APIResource } from '../../../store/types/api.types';
   templateUrl: './application-wall.component.html',
   styleUrls: ['./application-wall.component.scss'],
   animations: [
-    trigger('cardEnter', [
-      transition('* => *', [
-        query(
-          ':enter',
-          [
+    trigger(
+      'cardEnter', [
+        transition('* => *', [
+          query(':enter', [
             style({ opacity: 0, transform: 'translateY(10px)' }),
-            animate(
-              '150ms ease-out',
-              style({ opacity: 1, transform: 'translateY(0)' })
-            )
-          ],
-          { optional: true }
-        )
-      ])
-    ])
+            animate('150ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ], { optional: true })
+        ])
+      ]
+    )
   ],
-  providers: [
-    {
-      provide: ListConfig,
-      useClass: CfAppConfigService
-    }
-  ]
+  providers: [{
+    provide: ListConfig,
+    useClass: CfAppConfigService
+  }]
 })
 export class ApplicationWallComponent implements OnDestroy {
+
   private statsSub: Subscription;
 
   constructor(
     public endpointsService: EndpointsService,
     private store: Store<AppState>,
-    private appListConfig: ListConfig<APIResource>
+    private appListConfig: ListConfig<APIResource>,
   ) {
-    const dataSource: ListDataSource<
-      APIResource
-    > = appListConfig.getDataSource();
+    const dataSource: ListDataSource<APIResource> = appListConfig.getDataSource();
 
-    this.statsSub = dataSource.page$
-      .pipe(
-        // The page observable will fire often, here we're only interested in updating the stats on actual page changes
-        distinctUntilChanged(distinctPageUntilChanged(dataSource)),
-        withLatestFrom(dataSource.pagination$),
-        // Ensure we keep pagination smooth
-        debounceTime(250),
-        tap(([page, pagination]) => {
-          if (!page) {
-            return;
+    this.statsSub = dataSource.page$.pipe(
+      // The page observable will fire often, here we're only interested in updating the stats on actual page changes
+      distinctUntilChanged(distinctPageUntilChanged(dataSource)),
+      withLatestFrom(dataSource.pagination$),
+      // Ensure we keep pagination smooth
+      debounceTime(250),
+      tap(([page, pagination]) => {
+        if (!page) {
+          return;
+        }
+        page.forEach(app => {
+          const appState = app.entity.state;
+          const appGuid = app.entity.guid;
+          const cfGuid = app.entity.cfGuid;
+          const dispatching = false;
+          if (appState === 'STARTED') {
+            this.store.dispatch(new GetAppStatsAction(appGuid, cfGuid));
           }
-          page.forEach(app => {
-            const appState = app.entity.state;
-            const appGuid = app.entity.guid;
-            const cfGuid = app.entity.cfGuid;
-            const dispatching = false;
-            if (appState === 'STARTED') {
-              this.store.dispatch(new GetAppStatsAction(appGuid, cfGuid));
-            }
-          });
-        }),
-        tag('stat-obs')
-      )
-      .subscribe();
+        });
+      }),
+      tag('stat-obs')).subscribe();
   }
 
   cardComponent = CardAppComponent;
