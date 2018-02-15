e4616524b6d672eeaf7ea7e3c25e0448281343ea
Irfan Habib <irfan.habib@suse.com>
2018-02-15 13:14:26 +0000

    Mulitply metric by no. of instances

--- src/frontend/app/features/cloud-foundry/cloud-foundry-base/cloud-foundry-endpoint.service.ts
+++ src/frontend/app/features/cloud-foundry/cloud-foundry-base/cloud-foundry-endpoint.service.ts
@@ -8,18 +8,30 @@ import { EntityServiceFactory } from '../../../core/entity-service-factory.servi
 import { CfOrgSpaceDataService } from '../../../shared/data-services/cf-org-space-service.service';
 import { CfUserService } from '../../../shared/data-services/cf-user.service';
 import { PaginationMonitorFactory } from '../../../shared/monitors/pagination-monitor.factory';
-import { CF_INFO_ENTITY_KEY, CFInfoSchema, GetEndpointInfo } from '../../../store/actions/cloud-foundry.actions';
-import { EndpointSchema, GetAllEndpoints } from '../../../store/actions/endpoint.actions';
+import {
+  CF_INFO_ENTITY_KEY,
+  CFInfoSchema,
+  GetEndpointInfo
+} from '../../../store/actions/cloud-foundry.actions';
+import {
+  EndpointSchema,
+  GetAllEndpoints
+} from '../../../store/actions/endpoint.actions';
 import { AppState } from '../../../store/app-state';
 import { APIResource, EntityInfo } from '../../../store/types/api.types';
-import { CfApplication, CfApplicationState } from '../../../store/types/application.types';
-import { EndpointModel, EndpointUser } from '../../../store/types/endpoint.types';
+import {
+  CfApplication,
+  CfApplicationState
+} from '../../../store/types/application.types';
+import {
+  EndpointModel,
+  EndpointUser
+} from '../../../store/types/endpoint.types';
 import { CfOrg } from '../../../store/types/org-and-space.types';
 import { CfUser } from '../../../store/types/user.types';
 
 @Injectable()
 export class CloudFoundryEndpointService {
-
   allApps$: Observable<APIResource<CfApplication>[]>;
   users$: Observable<APIResource<CfUser>[]>;
   orgs$: Observable<APIResource<CfOrg>[]>;
@@ -69,25 +81,25 @@ export class CloudFoundryEndpointService {
 
     this.info$ = this.cfInfoEntityService.waitForEntity$.pipe(shareReplay(1));
 
-    this.allApps$ =  this.orgs$.pipe(
+    this.allApps$ = this.orgs$.pipe(
       // This should go away once https://github.com/cloudfoundry-incubator/stratos/issues/1619 is fixed
       map(orgs => orgs.filter(org => org.entity.spaces)),
       map(p => {
-        return p.map(o => o.entity.spaces.map(space =>  space.entity.apps));
+        return p.map(o => o.entity.spaces.map(space => space.entity.apps));
       }),
       map(a => {
         let flatArray = [];
-        a.forEach(appsInSpace => flatArray = flatArray.concat(...appsInSpace));
+        a.forEach(
+          appsInSpace => (flatArray = flatArray.concat(...appsInSpace))
+        );
         return flatArray;
-      }),
+      })
     );
-
   }
 
   getAppsInOrg = (
     org: APIResource<CfOrg>
   ): Observable<APIResource<CfApplication>[]> => {
-
     // This should go away once https://github.com/cloudfoundry-incubator/stratos/issues/1619 is fixed
     if (!org.entity.spaces) {
       return Observable.of([]);
@@ -98,23 +110,24 @@ export class CloudFoundryEndpointService {
         return apps.filter(a => orgSpaces.indexOf(a.entity.space_guid) !== -1);
       })
     );
-  }
+  };
 
   getAggregateStat(
-    org: APIResource<CfOrg > ,
+    org: APIResource<CfOrg>,
     statMetric: string
-  ): Observable<number > {
+  ): Observable<number> {
     return this.getAppsInOrg(org).pipe(
-      map(apps =>
-        this.getMetricFromApps(apps, statMetric)
-      )
+      map(apps => this.getMetricFromApps(apps, statMetric))
     );
   }
 
-  public getMetricFromApps(apps: APIResource<CfApplication>[], statMetric: string): any {
+  public getMetricFromApps(
+    apps: APIResource<CfApplication>[],
+    statMetric: string
+  ): any {
     return apps
       .filter(a => a.entity.state !== CfApplicationState.STOPPED)
-      .map(a => a.entity[statMetric])
+      .map(a => a.entity[statMetric] * a.entity.instances)
       .reduce((a, t) => a + t, 0);
   }
 }
