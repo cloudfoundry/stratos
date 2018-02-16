e9dc1fc4467e5bd7ec22df4ba520db91514214fe
Richard Cox <richard.cox@suse.com>
2018-02-16 17:25:08 +0000

    Fix CC

--- src/frontend/app/features/applications/routes/routes.helper.ts
+++ src/frontend/app/features/applications/routes/routes.helper.ts
@@ -22,12 +22,9 @@ export const getRoute = (
   if (route.entity.port) {
     // Note: Hostname and path are not supported for TCP routes
     return `${protocol}${domain.entity.name}:${route.entity.port}`;
-  } else if (route.entity.path) {
-    const pathPrefix = route.entity.path && route.entity.path.length && route.entity.path[0] !== '/' ? '/' : '';
-    return `${protocol}${route.entity.host}.${domain.entity.name}${pathPrefix}${route.entity.path}`;
-  } else {
-    return `${protocol}${route.entity.host}.${domain.entity.name}`;
   }
+  const path = route.entity.path && route.entity.path.length && route.entity.path[0] !== `/` ? `/${route.entity.path}` : route.entity.path;
+  return `${protocol}${route.entity.host}.${domain.entity.name}${path}`;
 };
 
 export const getMappedApps = (route: APIResource): APIResource[] => {
