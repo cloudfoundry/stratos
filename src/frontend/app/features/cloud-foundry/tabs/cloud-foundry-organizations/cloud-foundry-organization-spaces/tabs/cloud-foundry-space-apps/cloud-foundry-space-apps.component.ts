91531cacc45fbc7989eb12ebc94e5b6ebd49838f
Irfan Habib <irfan.habib@suse.com>
2018-02-22 17:08:13 +0000

    Fix lint

--- src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-organizations/cloud-foundry-organization-spaces/tabs/cloud-foundry-space-apps/cloud-foundry-space-apps.component.ts
+++ src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-organizations/cloud-foundry-organization-spaces/tabs/cloud-foundry-space-apps/cloud-foundry-space-apps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+/* tslint:disable:max-line-length */
 import { CfSpaceAppsListConfigService } from '../../../../../../../shared/components/list/list-types/cf-space-apps/cf-space-apps-list-config.service';
+/* tslint:enable:max-line-length */
+
 import { ListConfig } from '../../../../../../../shared/components/list/list.component.types';
 @Component({
   selector: 'app-cloud-foundry-space-apps',
