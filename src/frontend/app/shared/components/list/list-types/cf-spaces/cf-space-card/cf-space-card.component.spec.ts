b9ff0d836724fec94f304db1aa1ee0e6abce74c8
Irfan Habib <irfan.habib@suse.com>
2018-02-26 11:01:37 +0000

    Uncomment test

--- src/frontend/app/shared/components/list/list-types/cf-spaces/cf-space-card/cf-space-card.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-spaces/cf-space-card/cf-space-card.component.spec.ts
@@ -1,66 +1,68 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { EntityServiceFactory } from '../../../../../../core/entity-service-factory.service';
-// import {
-//   generateTestCfEndpointServiceProvider,
-//   generateTestCfUserServiceProvider,
-//   getBaseTestModulesNoShared,
-// } from '../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
-// import { CfOrgSpaceDataService } from '../../../../../data-services/cf-org-space-service.service';
-// import { EntityMonitorFactory } from '../../../../../monitors/entity-monitor.factory.service';
-// import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
-// import {
-//   ApplicationStateIconComponent,
-// } from '../../../../application-state/application-state-icon/application-state-icon.component';
-// import { ApplicationStateIconPipe } from '../../../../application-state/application-state-icon/application-state-icon.pipe';
-// import { CardStatusComponent } from '../../../../cards/card-status/card-status.component';
-// import { MetaCardComponent } from '../../../list-cards/meta-card/meta-card-base/meta-card.component';
-// import { MetaCardItemComponent } from '../../../list-cards/meta-card/meta-card-item/meta-card-item.component';
-// import { MetaCardKeyComponent } from '../../../list-cards/meta-card/meta-card-key/meta-card-key.component';
-// import { MetaCardTitleComponent } from '../../../list-cards/meta-card/meta-card-title/meta-card-title.component';
-// import { MetaCardValueComponent } from '../../../list-cards/meta-card/meta-card-value/meta-card-value.component';
+import { EntityServiceFactory } from '../../../../../../core/entity-service-factory.service';
+import {
+  generateTestCfEndpointServiceProvider,
+  generateTestCfUserServiceProvider,
+  getBaseTestModulesNoShared,
+  getMetadataCardComponents,
+} from '../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
+import { CfOrgSpaceDataService } from '../../../../../data-services/cf-org-space-service.service';
+import { EntityMonitorFactory } from '../../../../../monitors/entity-monitor.factory.service';
+import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
+import {
+  ApplicationStateIconComponent,
+} from '../../../../application-state/application-state-icon/application-state-icon.component';
+import { ApplicationStateIconPipe } from '../../../../application-state/application-state-icon/application-state-icon.pipe';
+import { CardStatusComponent } from '../../../../cards/card-status/card-status.component';
+import { MetaCardComponent } from '../../../list-cards/meta-card/meta-card-base/meta-card.component';
+import { MetaCardItemComponent } from '../../../list-cards/meta-card/meta-card-item/meta-card-item.component';
+import { MetaCardKeyComponent } from '../../../list-cards/meta-card/meta-card-key/meta-card-key.component';
+import { MetaCardTitleComponent } from '../../../list-cards/meta-card/meta-card-title/meta-card-title.component';
+import { MetaCardValueComponent } from '../../../list-cards/meta-card/meta-card-value/meta-card-value.component';
+import { CfOrgCardComponent } from '../../cf-orgs/cf-org-card/cf-org-card.component';
 
-// describe('CfOrgCardComponent', () => {
-//   let component: CfOrgCardComponent;
-//   let fixture: ComponentFixture<CfOrgCardComponent>;
+describe('CfOrgCardComponent', () => {
+  let component: CfOrgCardComponent;
+  let fixture: ComponentFixture<CfOrgCardComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [CfOrgCardComponent, MetaCardComponent, MetaCardItemComponent,
-//         MetaCardKeyComponent, ApplicationStateIconPipe, ApplicationStateIconComponent,
-//         MetaCardTitleComponent, CardStatusComponent, MetaCardValueComponent],
-//       imports: [...getBaseTestModulesNoShared],
-//       providers: [PaginationMonitorFactory, EntityMonitorFactory, generateTestCfUserServiceProvider(),
-//         CfOrgSpaceDataService, generateTestCfEndpointServiceProvider(), EntityServiceFactory]
-//     })
-//       .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CfOrgCardComponent, ...getMetadataCardComponents],
+      imports: [...getBaseTestModulesNoShared],
+      providers: [PaginationMonitorFactory, EntityMonitorFactory, generateTestCfUserServiceProvider(),
+        CfOrgSpaceDataService, generateTestCfEndpointServiceProvider(), EntityServiceFactory]
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CfOrgCardComponent);
-//     component = fixture.componentInstance;
-//     component.row = {
-//       entity: {
-//         spaces: [],
-//         guid: '',
-//         cfGuid: '',
-//         quota_definition: {
-//           entity: {
-//             memory_limit: 1000,
-//             app_instance_limit: -1,
-//             instance_memory_limit: -1,
-//             name: ''
-//           },
-//           metadata: null
-//         }
-//       },
-//       metadata: null
-//     };
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CfOrgCardComponent);
+    component = fixture.componentInstance;
+    component.row = {
+      entity: {
+        spaces: [],
+        guid: '',
+        cfGuid: '',
+        name: 'test',
+        private_domains: [],
+        quota_definition: {
+          entity: {
+            memory_limit: 1000,
+            app_instance_limit: -1,
+            instance_memory_limit: -1,
+            name: ''
+          },
+          metadata: null
+        }
+      },
+      metadata: null
+    };
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
+  it('should create', () => {
 
-//     expect(component).toBeTruthy();
-//   });
-// });
+    expect(component).toBeTruthy();
+  });
+});
