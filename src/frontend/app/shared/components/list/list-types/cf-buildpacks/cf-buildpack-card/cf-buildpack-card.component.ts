d99bd9ee18c9409d06f03e6c775d06cc61aa65d8
Irfan Habib <irfan.habib@suse.com>
2018-03-05 09:57:46 +0000

    Styling fixes

--- src/frontend/app/shared/components/list/list-types/cf-buildpacks/cf-buildpack-card/cf-buildpack-card.component.ts
+++ src/frontend/app/shared/components/list/list-types/cf-buildpacks/cf-buildpack-card/cf-buildpack-card.component.ts
@@ -5,7 +5,7 @@ import { IBuildpack } from '../../../../../../core/cf-api.types';
 @Component({
   selector: 'app-cf-buildpack-card',
   templateUrl: './cf-buildpack-card.component.html',
-  styleUrls: ['./cf-buildpack-card.component.scss']
+  styleUrls: ['./cf-buildpack-card.component.theme.scss']
 })
 export class CfBuildpackCardComponent implements OnInit {
 
@@ -15,4 +15,7 @@ export class CfBuildpackCardComponent implements OnInit {
   ngOnInit() {
   }
 
+  isLocked = () => this.row.entity.locked;
+
+  isEnabled = () => this.row.entity.enabled;
 }
