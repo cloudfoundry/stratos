06c10aeef17b592aa02abf389fb0028b8888196f
Irfan Habib <irfan.habib@suse.com>
2018-03-05 12:55:02 +0000

    lint & test fixes

--- src/frontend/app/shared/components/list/list-types/cf-stacks/cf-stacks-card/cf-stacks-card.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-stacks/cf-stacks-card/cf-stacks-card.component.spec.ts
@@ -1,5 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  getBaseTestModulesNoShared,
+  getMetadataCardComponents,
+} from '../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
 import { CfStacksCardComponent } from './cf-stacks-card.component';
 
 describe('CfStacksCardComponent', () => {
@@ -8,14 +12,27 @@ describe('CfStacksCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CfStacksCardComponent ]
+      declarations: [CfStacksCardComponent, getMetadataCardComponents],
+      imports: [...getBaseTestModulesNoShared],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CfStacksCardComponent);
     component = fixture.componentInstance;
+    component.row = {
+      entity: {
+        name: '',
+        description: ''
+      },
+      metadata: {
+        created_at: '',
+        updated_at: '',
+        guid: '',
+        url: ''
+      }
+    };
     fixture.detectChanges();
   });
 
