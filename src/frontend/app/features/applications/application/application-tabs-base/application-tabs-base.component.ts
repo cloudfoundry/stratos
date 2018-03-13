4147bb15334ae532d4c1c171d7c272741fb2405d
Neil MacDougall <neil.macdougall@suse.com>
2018-03-13 13:12:41 +0000

    Fix for github tab not showing on first load

--- src/frontend/app/features/applications/application/application-tabs-base/application-tabs-base.component.ts
+++ src/frontend/app/features/applications/application/application-tabs-base/application-tabs-base.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { take } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Rx';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { EntityService } from '../../../../core/entity-service';
 import { ConfirmationDialogService } from '../../../../shared/components/confirmation-dialog.service';
@@ -14,6 +15,7 @@ import { AppState } from '../../../../store/app-state';
 import { ApplicationService } from '../../application.service';
 import { APIResource } from '../../../../store/types/api.types';
 import { ConfirmationDialogConfig } from '../../../../shared/components/confirmation-dialog.config';
+import { ISubHeaderTabs } from '../../../../shared/components/page-subheader/page-subheader.types';
 
 // Confirmation dialogs
 const appStopConfirmation = new ConfirmationDialogConfig(
@@ -47,7 +49,7 @@ export class ApplicationTabsBaseComponent implements OnInit, OnDestroy {
     private applicationService: ApplicationService,
     private entityService: EntityService<APIResource>,
     private store: Store<AppState>,
-    private confirmDialog: ConfirmationDialogService
+    private confirmDialog: ConfirmationDialogService,
   ) { }
 
   public async: any;
@@ -65,14 +67,17 @@ export class ApplicationTabsBaseComponent implements OnInit, OnDestroy {
     update => update[this.autoRefreshString] || { busy: false }
   );
 
-  tabLinks = [
+  tabLinks: ISubHeaderTabs[] = [
     { link: 'summary', label: 'Summary' },
     { link: 'instances', label: 'Instances' },
     { link: 'log-stream', label: 'Log Stream' },
     { link: 'services', label: 'Services' },
     { link: 'variables', label: 'Variables' },
-    { link: 'events', label: 'Events' }
+    { link: 'events', label: 'Events' },
+    { link: 'github', label: 'GitHub', hidden: true },
   ];
+
+  tabs$ = new BehaviorSubject<any>(this.tabLinks);
 
   stopApplication() {
     this.confirmDialog.open(appStopConfirmation, () => {
@@ -163,7 +168,10 @@ export class ApplicationTabsBaseComponent implements OnInit, OnDestroy {
           stratProject.deploySource &&
           stratProject.deploySource.type === 'github'
         ) {
-          this.tabLinks.push({ link: 'github', label: 'GitHub' });
+          // Make the GitHub tab visible
+          const githubTab = this.tabLinks.find((tab) => tab.link === 'github');
+          githubTab.hidden = false;
+          this.tabs$.next(this.tabLinks);
         }
       });
   }
