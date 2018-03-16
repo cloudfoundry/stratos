e9b26a51562b499f00d8967c63a7da4879fed2d8
Nathan Jones <nathanmjones1@gmail.com>
2018-03-16 17:58:17 +0000

    Added left and right scroll buttons when nav is hidden

--- src/frontend/app/shared/components/page-subheader/page-subheader.component.ts
+++ src/frontend/app/shared/components/page-subheader/page-subheader.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { startWith } from 'rxjs/operators/startWith';
+import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { MatTabNav } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
 
 import { ISubHeaderTabs } from './page-subheader.types';
 
@@ -7,8 +12,17 @@ import { ISubHeaderTabs } from './page-subheader.types';
   templateUrl: './page-subheader.component.html',
   styleUrls: ['./page-subheader.component.scss']
 })
-export class PageSubheaderComponent {
+export class PageSubheaderComponent implements AfterViewInit {
   cssClass: string;
+
+  @ViewChild('nav')
+  nav: MatTabNav;
+
+  @ViewChild('navOuter')
+  navOuter: ElementRef;
+
+  @ViewChild('navScroller')
+  navScroller: ElementRef;
 
   @Input('tabs')
   tabs: ISubHeaderTabs[];
@@ -16,12 +30,38 @@ export class PageSubheaderComponent {
   @Input('nested')
   nested: boolean;
 
+  public navOverflowing$: Observable<boolean>;
+
   className: string;
+
+  private scrollSpeed = 10;
 
   constructor() {
     this.className = this.nested ? 'nested-tab' : 'page-subheader';
     if (!!this.tabs) {
       this.cssClass = this.nested ? 'nested-tab__tabs' : 'page-subheader__tabs';
+    }
+  }
+  ngAfterViewInit() {
+    this.navOverflowing$ = fromEvent(window, 'resize').pipe(
+      debounceTime(100),
+      map(this.navIsOverflowing),
+      startWith(this.navIsOverflowing()),
+      distinctUntilChanged()
+    );
+  }
+  private navIsOverflowing = () => {
+    const { offsetWidth } = this.navOuter.nativeElement;
+    const navWith = this.nav._elementRef.nativeElement.offsetWidth;
+    console.log(navWith, offsetWidth);
+    return navWith > offsetWidth;
+  }
+
+  public scrollNav(direction: 'left' | 'right') {
+    if (direction === 'left') {
+      this.navScroller.nativeElement.scrollLeft -= this.scrollSpeed;
+    } else {
+      this.navScroller.nativeElement.scrollLeft += this.scrollSpeed;
     }
   }
 }
