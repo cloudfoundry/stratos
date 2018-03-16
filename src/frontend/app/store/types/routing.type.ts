ad1578b2a9123df2c6d79f5f2b5bde6a9b610a92
Irfan Habib <irfan.habib@suse.com>
2018-03-16 09:14:00 +0000

    Fix typing of Routing state

--- src/frontend/app/store/types/routing.type.ts
+++ src/frontend/app/store/types/routing.type.ts
@@ -1,10 +1,24 @@
 import { RoutesRecognized } from '@angular/router';
 import { AppState } from '../app-state';
 
+export interface RoutingEvent {
+  id: number;
+  url: string;
+  urlAfterRedirects: string;
+  state: {
+    url: string;
+    params: {
+      [key: string]: string;
+    }
+    queryParams: {
+      [key: string]: string;
+    }
+  };
+}
 export interface RoutingHistory {
-  history?: RoutesRecognized[];
-  currentState: RoutesRecognized;
-  previousState?: RoutesRecognized;
+  history?: RoutingEvent[];
+  currentState: RoutingEvent;
+  previousState?: RoutingEvent;
 }
 
 export const defaultRoutingState: RoutingHistory = {
