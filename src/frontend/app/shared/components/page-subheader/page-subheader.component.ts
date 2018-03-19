2cc7602b0611f3e658061cf9a041b6ccfc9bb4d2
Nathan Jones <nathanmjones1@gmail.com>
2018-03-19 11:22:36 +0000

    Added smooth scrolling to the nav scroll

--- src/frontend/app/shared/components/page-subheader/page-subheader.component.ts
+++ src/frontend/app/shared/components/page-subheader/page-subheader.component.ts
@@ -3,16 +3,18 @@ import { AfterViewInit, Component, Input, ViewChild, ElementRef } from '@angular
 import { MatTabNav } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, map, distinctUntilChanged, timeInterval, tap, switchMap } from 'rxjs/operators';
 
 import { ISubHeaderTabs } from './page-subheader.types';
+import { interval } from 'rxjs/observable/interval';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-page-subheader',
   templateUrl: './page-subheader.component.html',
   styleUrls: ['./page-subheader.component.scss']
 })
-export class PageSubheaderComponent implements AfterViewInit {
+export class PageSubheaderComponent {
   cssClass: string;
 
   @ViewChild('nav')
@@ -34,6 +36,8 @@ export class PageSubheaderComponent implements AfterViewInit {
 
   className: string;
 
+  scrollSub: Subscription;
+
   private scrollSpeed = 10;
 
   constructor() {
@@ -41,8 +45,6 @@ export class PageSubheaderComponent implements AfterViewInit {
     if (!!this.tabs) {
       this.cssClass = this.nested ? 'nested-tab__tabs' : 'page-subheader__tabs';
     }
-  }
-  ngAfterViewInit() {
     this.navOverflowing$ = fromEvent(window, 'resize').pipe(
       debounceTime(100),
       map(this.navIsOverflowing),
@@ -50,18 +52,53 @@ export class PageSubheaderComponent implements AfterViewInit {
       distinctUntilChanged()
     );
   }
+
   private navIsOverflowing = () => {
-    const { offsetWidth } = this.navOuter.nativeElement;
-    const navWith = this.nav._elementRef.nativeElement.offsetWidth;
-    console.log(navWith, offsetWidth);
-    return navWith > offsetWidth;
+    if (this.navOuter) {
+      const { offsetWidth } = this.navOuter.nativeElement;
+      const navWith = this.nav._elementRef.nativeElement.offsetWidth;
+      console.log(navWith, offsetWidth);
+      return navWith > offsetWidth;
+    }
+    return false;
   }
 
-  public scrollNav(direction: 'left' | 'right') {
+  public scrollNav(direction: 'left' | 'right', event: Event) {
+    event.preventDefault();
+    const initialScrollTime = 81;
+    let scrollTime = 80;
+    this._scrollNav(direction);
+    this.scrollSub = interval(initialScrollTime)
+      .pipe(
+        switchMap(() => {
+          return interval(scrollTime).pipe(
+            tap(() => {
+              this._scrollNav(direction);
+              if (scrollTime >= 20) {
+                scrollTime -= 5;
+              }
+            })
+          );
+        })
+      ).subscribe();
+  }
+
+  public scrollNavClick(direction: 'left' | 'right', event: Event) {
+    event.preventDefault();
+    this._scrollNav(direction);
+  }
+
+  private _scrollNav(direction: 'left' | 'right') {
     if (direction === 'left') {
       this.navScroller.nativeElement.scrollLeft -= this.scrollSpeed;
     } else {
       this.navScroller.nativeElement.scrollLeft += this.scrollSpeed;
+    }
+  }
+
+  endScrollNav() {
+    if (this.scrollSub) {
+      this.scrollSub.unsubscribe();
     }
   }
 }
