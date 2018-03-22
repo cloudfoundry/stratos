ff1997802132ac34fe4d5abe58847a3e24bab652
Richard Cox <richard.cox@suse.com>
2018-03-22 16:23:01 +0000

    Ensure all list cards extend new class

--- src/frontend/app/shared/components/list/list-types/cf-stacks/cf-stacks-card/cf-stacks-card.component.ts
+++ src/frontend/app/shared/components/list/list-types/cf-stacks/cf-stacks-card/cf-stacks-card.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { APIResource } from '../../../../../../store/types/api.types';
-import { IStack } from '../../../../../../core/cf-api.types';
+import { CardCell } from '../../../list.types';
 
 @Component({
   selector: 'app-cf-stacks-card',
   templateUrl: './cf-stacks-card.component.html',
   styleUrls: ['./cf-stacks-card.component.scss']
 })
-export class CfStacksCardComponent implements OnInit {
-
-  @Input('row') row: APIResource<IStack>;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CfStacksCardComponent extends CardCell<APIResource> { }
