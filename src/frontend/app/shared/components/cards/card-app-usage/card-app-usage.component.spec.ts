a8b70ec2eeb1b533cda704ed2d4b4a3d2bc0985b
Richard Cox <richard.cox@suse.com>
2018-03-22 14:43:34 +0000

    Ensure we retain the result of fetching inline data during an api call - To reproduce have an org with 50+ spaces (so fetching an org will not return the spaces) - Visit the console orgs list page directly - We were succesfully discovering the missing spaces property, fetching, saving to org however we were overwritting the org with the result of the initial call without spaces - Also ensure we don't return a pagination page if it's busy (i.e. fetching inline params)

--- src/frontend/app/shared/components/cards/card-app-usage/card-app-usage.component.spec.ts
+++ src/frontend/app/shared/components/cards/card-app-usage/card-app-usage.component.spec.ts
@@ -1,22 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CardAppUsageComponent } from './card-app-usage.component';
-import { CardStatusComponent } from '../card-status/card-status.component';
-import { ApplicationStateComponent } from '../../application-state/application-state.component';
-import { ApplicationStateIconComponent } from '../../application-state/application-state-icon/application-state-icon.component';
-import { ApplicationStateIconPipe } from '../../application-state/application-state-icon/application-state-icon.pipe';
-import { CoreModule } from '../../../../core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
-import { ApplicationService } from '../../../../features/applications/application.service';
-import { ApplicationServiceMock } from '../../../../test-framework/application-service-helper';
-import { ApplicationStateService } from '../../application-state/application-state.service';
-import { CardAppStatusComponent } from '../card-app-status/card-app-status.component';
-import { TableCellAppStatusComponent } from '../../list/list-types/app/table-cell-app-status/table-cell-app-status.component';
-import { PercentagePipe } from '../../../pipes/percentage.pipe';
+
+import { CoreModule } from '../../../../core/core.module';
 import { UtilsService } from '../../../../core/utils.service';
 import { ApplicationMonitorService } from '../../../../features/applications/application-monitor.service';
+import { ApplicationService } from '../../../../features/applications/application.service';
+import { ApplicationServiceMock } from '../../../../test-framework/application-service-helper';
+import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
+import { PercentagePipe } from '../../../pipes/percentage.pipe';
+import {
+  ApplicationStateIconComponent,
+} from '../../application-state/application-state-icon/application-state-icon.component';
+import { ApplicationStateIconPipe } from '../../application-state/application-state-icon/application-state-icon.pipe';
+import { ApplicationStateComponent } from '../../application-state/application-state.component';
+import { ApplicationStateService } from '../../application-state/application-state.service';
 import { TableCellStatusDirective } from '../../list/list-table/table-cell-status.directive';
+import { CardAppStatusComponent } from '../card-app-status/card-app-status.component';
+import { CardStatusComponent } from '../card-status/card-status.component';
+import { CardAppUsageComponent } from './card-app-usage.component';
 
 describe('CardAppUsageComponent', () => {
   let component: CardAppUsageComponent;
