7c89bd33dc83f46012c73ecd2ac6df8ecbb5c6c6
Richard Cox <richard.cox@suse.com>
2018-04-04 11:34:32 +0100

    Add extra orgs, make type of service selection easier

--- deploy/tools/populate-cf/cf-summit-april-2018.sh
+++ deploy/tools/populate-cf/cf-summit-april-2018.sh
@@ -2,6 +2,11 @@
 set -e
 
 ORGNAME=cf-summit
+ORGNAME2="SUSE Hackweek"
+ORGNAME3="['hip','hip']"
+ORGNAME4="its_a_feature"
+ORGNAME5="greek_questionmark"
+ORGNAME6="K8"
 SPACENAME=dev
 
 ORGQUOTA_NAME=cf-summit-org-quota
@@ -18,28 +23,25 @@ SPACEQUOTA_ROUTES=10
 SPACEQUOTA_SERVICEINSTANCES=5
 SPACEQUOTA_APPINSTANCES=10
 
-#SERVICEINSTANCE_1_TYPE=p-mysql
-#SERVICEINSTANCE_1_PLAN=512mb
-#SERVICEINSTANCE_1_NAME=cf-summit-p-mysql
-#SERVICEINSTANCE_1_PARAMS='{"name":"value1","name":"value1"}'
+SERVICEINSTANCE_PCF_1_TYPE=p-mysql
+SERVICEINSTANCE_PCF_1_PLAN=512mb
+SERVICEINSTANCE_PCF_1_NAME=cf-summit-p-mysql
+SERVICEINSTANCE_PCF_1_PARAMS='{"name":"value1","name":"value1"}'
 
-#SERVICEINSTANCE_2_TYPE=p-rabbitmq
-#SERVICEINSTANCE_2_PLAN=standard
-#SERVICEINSTANCE_2_NAME=cf-summit-p-rabbitmq
-#SERVICEINSTANCE_2_PARAMS='{"name":"value2","name":"value2"}'
+SERVICEINSTANCE_PCF_2_TYPE=p-rabbitmq
+SERVICEINSTANCE_PCF_2_PLAN=standard
+SERVICEINSTANCE_PCF_2_NAME=cf-summit-p-rabbitmq
+SERVICEINSTANCE_PCF_2_PARAMS='{"name":"value2","name":"value2"}'
 
-SERVICEINSTANCE_3_TYPE=mysql-dev
-SERVICEINSTANCE_3_PLAN=10mb
-SERVICEINSTANCE_3_NAME=cf-summit-mysql-dev-1
-SERVICEINSTANCE_3_PARAMS='{"name":"value3","name":"value3"}'
+SERVICEINSTANCE_SCF_1_TYPE=mysql-dev
+SERVICEINSTANCE_SCF_1_PLAN=10mb
+SERVICEINSTANCE_SCF_1_NAME=cf-summit-mysql-dev-1
+SERVICEINSTANCE_SCF_1_PARAMS='{"name":"value3","name":"value3"}'
 
-SERVICEINSTANCE_4_TYPE=mysql-dev
-SERVICEINSTANCE_4_PLAN=20mb
-SERVICEINSTANCE_4_NAME=cf-summit-mysql-dev-2
-SERVICEINSTANCE_4_PARAMS='{"name":"value4","name":"value4"}'
-
-SERVICEINSTANCE_NAME=$SERVICEINSTANCE_3_NAME
-SERVICEINSTANCE_NAME2=$SERVICEINSTANCE_4_NAME
+SERVICEINSTANCE_SCF_2_TYPE=mysql-dev
+SERVICEINSTANCE_SCF_2_PLAN=20mb
+SERVICEINSTANCE_SCF_2_NAME=cf-summit-mysql-dev-2
+SERVICEINSTANCE_SCF_2_PARAMS='{"name":"value4","name":"value4"}'
 
 APP_1_NAME=cf-summit-app-1
 APP_1_DISK=5M
@@ -70,97 +72,133 @@ function createQuota {
   
   if [ "$7" = true ]; then
     #cf create-quota QUOTA [-m TOTAL_MEMORY] [-i INSTANCE_MEMORY] [-r ROUTES] [-s SERVICE_INSTANCES] [-a APP_INSTANCES] [--allow-paid-service-plans] [--reserved-route-ports RESERVED_ROUTE_PORTS]
-    cf create-quota $1 -m $2 -i $3 -r $4 -s $5 -a $6
+    cf create-quota "$1" -m "$2" -i "$3" -r "$4" -s "$5" -a "$6"
     cf quotas
   else
     #cf create-space-quota QUOTA [-i INSTANCE_MEMORY] [-m MEMORY] [-r ROUTES] [-s SERVICE_INSTANCES] [-a APP_INSTANCES] [--allow-paid-service-plans] [--reserved-route-ports RESERVED_ROUTE_PORTS]
-    cf target -o $ORGNAME
-    cf create-space-quota $1 -m $2 -i $3 -r $4 -s $5 -a $6
+    cf target -o "$ORGNAME"
+    cf create-space-quota "$1" -m "$2" -i "$3" -r "$4" -s "$5" -a "$6"
     cf space-quotas
   fi
 }
 
 function createOrg {
   #cf create-org ORG
-  cf create-org $ORGNAME -q $ORGQUOTA_NAME
+  cf create-org "$1" -q "$2"
 }
 
 function createSpace {
   #cf create-space SPACE [-o ORG] [-q SPACE_QUOTA]
-  cf create-space $SPACENAME -o $ORGNAME -q $SPACEQUOTA_NAME
+  cf create-space "$SPACENAME" -o "$ORGNAME" -q "$SPACEQUOTA_NAME"
 }
 
 function createServiceInstance {
-  echo Creating service: $1, Type: $2, Plan: $3
+  echo Creating service: "$1", Type: "$2", Plan: "$3"
   #cf create-service SERVICE PLAN SERVICE_INSTANCE -c '{"name":"value","name":"value"}'
-  cf create-service $2 $3 $1 -c $4
+  cf create-service "$2" "$3" "$1" -c "$4"
 }
 
 function createServiceInstances {
-  cf target -o $ORGNAME -s $SPACENAME
-  #createServiceInstance $SERVICEINSTANCE_1_NAME $SERVICEINSTANCE_1_TYPE $SERVICEINSTANCE_1_PLAN $SERVICEINSTANCE_1_PARAMS
-  #createServiceInstance $SERVICEINSTANCE_2_NAME $SERVICEINSTANCE_2_TYPE $SERVICEINSTANCE_2_PLAN $SERVICEINSTANCE_2_PARAMS
-  createServiceInstance $SERVICEINSTANCE_NAME $SERVICEINSTANCE_3_TYPE $SERVICEINSTANCE_3_PLAN $SERVICEINSTANCE_3_PARAMS
-  createServiceInstance $SERVICEINSTANCE_NAME2 $SERVICEINSTANCE_4_TYPE $SERVICEINSTANCE_4_PLAN $SERVICEINSTANCE_4_PARAMS
+  cf target -o "$ORGNAME" -s "$SPACENAME"
+  createServiceInstance "$SERVICEINSTANCE_NAME" "$SERVICEINSTANCE_TYPE" "$SERVICEINSTANCE_PLAN" "$SERVICEINSTANCE_PARAMS"
+  createServiceInstance "$SERVICEINSTANCE_NAME2" "$SERVICEINSTANCE_TYPE2" "$SERVICEINSTANCE_PLAN2" "$SERVICEINSTANCE_PARAMS2"
 }
 
 function createApp {
-  echo Creating App: $1
-  echo Disk Limit: $2
-  echo Memory Limit: $3
-  echo Number of instances: $4
+  echo Creating App: "$1"
+  echo Disk Limit: "$2"
+  echo Memory Limit: "$3"
+  echo Number of instances: "$4"
   
-  cf push $1 -k $2 -m $3 -i $4 --no-manifest --no-start
+  cf push "$1" -k "$2" -m "$3" -i "$4" --no-manifest --no-start
 }
 
 function createApps {
   TEMP_PUSH_FOLDER=temp-push-folder
-  mkdir $TEMP_PUSH_FOLDER -p
-  pushd $TEMP_PUSH_FOLDER
+  mkdir "$TEMP_PUSH_FOLDER" -p
+  pushd "$TEMP_PUSH_FOLDER"
   touch delete-me
-  createApp $APP_1_NAME $APP_1_DISK $APP_1_MEMORY $APP_1_INSTANCES
-  createApp $APP_2_NAME $APP_2_DISK $APP_2_MEMORY $APP_2_INSTANCES
-  createApp $APP_3_NAME $APP_3_DISK $APP_3_MEMORY $APP_3_INSTANCES
+  createApp "$APP_1_NAME" "$APP_1_DISK" "$APP_1_MEMORY" "$APP_1_INSTANCES"
+  createApp "$APP_2_NAME" "$APP_2_DISK" "$APP_2_MEMORY" "$APP_2_INSTANCES"
+  createApp "$APP_3_NAME" "$APP_3_DISK" "$APP_3_MEMORY" "$APP_3_INSTANCES"
   popd
 }
 
 function bindServiceInstancesToApp {
-  cf bind-service $APP_1_NAME $SERVICEINSTANCE_NAME
-  cf bind-service $APP_2_NAME $SERVICEINSTANCE_NAME2
-  cf bind-service $APP_3_NAME $SERVICEINSTANCE_NAME
-  cf bind-service $APP_3_NAME $SERVICEINSTANCE_NAME2
-  echo nope
+  cf bind-service "$APP_1_NAME" "$SERVICEINSTANCE_NAME"
+  cf bind-service "$APP_2_NAME" "$SERVICEINSTANCE_NAME2"
+  cf bind-service "$APP_3_NAME" "$SERVICEINSTANCE_NAME"
+  cf bind-service "$APP_3_NAME" "$SERVICEINSTANCE_NAME2"
 }
 
 function create {
-  createQuota $ORGQUOTA_NAME $ORGQUOTA_TOTALMEMORY $ORGQUOTA_APPINSTANCEMEMORY $ORGQUOTA_ROUTES $ORGQUOTA_SERVICEINSTANCES $ORGQUOTA_APPINSTANCES true
-  createOrg
-  createQuota $SPACEQUOTA_NAME $SPACEQUOTA_TOTALMEMORY $SPACEQUOTA_APPINSTANCEMEMORY $SPACEQUOTA_ROUTES $SPACEQUOTA_SERVICEINSTANCES $SPACEQUOTA_APPINSTANCES false
+  createQuota "$ORGQUOTA_NAME" "$ORGQUOTA_TOTALMEMORY" "$ORGQUOTA_APPINSTANCEMEMORY" "$ORGQUOTA_ROUTES" "$ORGQUOTA_SERVICEINSTANCES" "$ORGQUOTA_APPINSTANCES" true
+  createOrg "$ORGNAME" "$ORGQUOTA_NAME"
+  createOrg "$ORGNAME2" "$ORGQUOTA_NAME"
+  createOrg "$ORGNAME3" "$ORGQUOTA_NAME"
+  createOrg "$ORGNAME4" "$ORGQUOTA_NAME"
+  createOrg "$ORGNAME5" "$ORGQUOTA_NAME"
+  createOrg "$ORGNAME6" "$ORGQUOTA_NAME"
+  createQuota "$SPACEQUOTA_NAME" "$SPACEQUOTA_TOTALMEMORY" "$SPACEQUOTA_APPINSTANCEMEMORY" "$SPACEQUOTA_ROUTES" "$SPACEQUOTA_SERVICEINSTANCES" "$SPACEQUOTA_APPINSTANCES" false
   createSpace
   createServiceInstances
-  ## bind to apps as well
   createApps
   bindServiceInstancesToApp
 }
 
 function clean {
-  cf target -o $ORGNAME
-  cf delete-space $SPACENAME -f
-  cf delete-space-quota $SPACEQUOTA_NAME -f
+  echo Targeting $ORGNAME and deleting it\'s content
+  cf target -o "$ORGNAME"
+  cf delete-space "$SPACENAME" -f
+  cf delete-space-quota "$SPACEQUOTA_NAME" -f
   
+  echo Deleting Orgs
   # Delete org will also delete spaces, apps, service instances, routes, private domains and space-scoped service brokers
-  cf delete-org $ORGNAME -f
-  cf delete-quota $ORGQUOTA_NAME -f
+  cf delete-org "$ORGNAME" -f
+  cf delete-org "$ORGNAME2" -f
+  cf delete-org "$ORGNAME3" -f
+  cf delete-org "$ORGNAME4" -f
+  cf delete-org "$ORGNAME5" -f
+  cf delete-org "$ORGNAME6" -f
+  cf delete-quota "$ORGQUOTA_NAME" -f
 }
 
 CLEAN=false
-while getopts ":c" opt ; do
+CF=SCF
+while getopts ":cp" opt ; do
   case $opt in
     c)
       CLEAN=true
     ;;
+    p)
+      CF=PCF
+    ;;
   esac
 done
+
+if [ "$CF" = "SCF" ]; then
+  echo Using SCF
+  SERVICEINSTANCE_NAME=$SERVICEINSTANCE_SCF_1_NAME
+  SERVICEINSTANCE_TYPE=$SERVICEINSTANCE_SCF_1_TYPE
+  SERVICEINSTANCE_PLAN=$SERVICEINSTANCE_SCF_1_PLAN
+  SERVICEINSTANCE_PARAMS=$SERVICEINSTANCE_SCF_1_PARAMS
+
+  SERVICEINSTANCE_NAME2=$SERVICEINSTANCE_SCF_2_NAME
+  SERVICEINSTANCE_TYPE2=$SERVICEINSTANCE_SCF_2_TYPE
+  SERVICEINSTANCE_PLAN2=$SERVICEINSTANCE_SCF_2_PLAN
+  SERVICEINSTANCE_PARAMS2=$SERVICEINSTANCE_SCF_2_PARAMS
+else
+  echo Using PCF
+  SERVICEINSTANCE_NAME=$SERVICEINSTANCE_PCF_1_NAME
+  SERVICEINSTANCE_TYPE=$SERVICEINSTANCE_PCF_1_TYPE
+  SERVICEINSTANCE_PLAN=$SERVICEINSTANCE_PCF_1_PLAN
+  SERVICEINSTANCE_PARAMS=$SERVICEINSTANCE_PCF_1_PARAMS
+
+  SERVICEINSTANCE_NAME2=$SERVICEINSTANCE_PCF_2_NAME
+  SERVICEINSTANCE_TYPE2=$SERVICEINSTANCE_PCF_2_TYPE
+  SERVICEINSTANCE_PLAN2=$SERVICEINSTANCE_PCF_2_PLAN
+  SERVICEINSTANCE_PARAMS2=$SERVICEINSTANCE_PCF_2_PARAMS
+fi
 
 #login
 if [ "$CLEAN" = true ]; then
@@ -168,7 +206,7 @@ if [ "$CLEAN" = true ]; then
 fi
 create
 
-echo This script has...
+echo This script has
 echo - Created an org \($ORGNAME\) and space \($SPACENAME\), both with custom quotas \($ORGQUOTA_NAME, $SPACEQUOTA_NAME\)
 echo - Created 2 service instances \($SERVICEINSTANCE_NAME, $SERVICEINSTANCE_NAME2\)
 echo - Created 3 apps \($APP_1_NAME, $APP_2_NAME, $APP_3_NAME\) and bound the service instances to them
