2ef39ef70eab113173d5cae0d265b28abade1d3c
Richard Cox <richard.cox@suse.com>
2018-04-05 10:09:45 +0100

    Fix tests + cc

--- src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/cf-spaces-service-instances-list-config.service.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/cf-spaces-service-instances-list-config.service.spec.ts
@@ -2,13 +2,15 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { CfSpacesServiceInstancesListConfigService } from './cf-spaces-service-instances-list-config.service';
 import { BaseTestModules, getCfSpaceServiceMock } from '../../../../../test-framework/cloud-foundry-endpoint-service.helper';
+import { DatePipe } from '@angular/common';
 
 describe('CfSpacesServiceInstancesListConfigService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         CfSpacesServiceInstancesListConfigService,
-        getCfSpaceServiceMock
+        getCfSpaceServiceMock,
+        DatePipe
       ],
       imports: [...BaseTestModules]
     });
