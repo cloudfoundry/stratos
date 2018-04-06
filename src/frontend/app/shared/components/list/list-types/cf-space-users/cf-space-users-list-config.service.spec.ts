2ace01a1d28eebf8563ab843f6920f24d750b928
Irfan Habib <irfan.habib@suse.com>
2018-04-06 13:22:45 +0100

    Fix unit tests

--- src/frontend/app/shared/components/list/list-types/cf-space-users/cf-space-users-list-config.service.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-space-users/cf-space-users-list-config.service.spec.ts
@@ -1,11 +1,21 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
+import { CloudFoundrySpaceService } from '../../../../../features/cloud-foundry/services/cloud-foundry-space.service';
+import { BaseTestModules } from '../../../../../test-framework/cloud-foundry-endpoint-service.helper';
+import { CloudFoundrySpaceServiceMock } from '../../../../../test-framework/cloud-foundry-space.service.mock';
 import { CfSpaceUsersListConfigService } from './cf-space-users-list-config.service';
+import { ActiveRouteCfOrgSpace } from '../../../../../features/cloud-foundry/cf-page.types';
 
 describe('CfSpaceUsersListConfigService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CfSpaceUsersListConfigService]
+      providers: [
+        CfSpaceUsersListConfigService,
+        { provide: CloudFoundrySpaceService, useClass: CloudFoundrySpaceServiceMock },
+        ActiveRouteCfOrgSpace
+
+      ],
+      imports: [...BaseTestModules]
     });
   });
 
