ad5f2a699782647cddc7d70c65752871543274f4
Neil MacDougall <neil.macdougall@suse.com>
2018-04-11 10:58:50 +0100

    Fix for unit test

--- src/frontend/app/features/about/about-page/about-page.component.spec.ts
+++ src/frontend/app/features/about/about-page/about-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutPageComponent } from './about-page.component';
+import { CoreModule } from '../../../core/core.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { createBasicStoreModule } from '../../../test-framework/store-test-helper';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent;
@@ -8,7 +12,13 @@ describe('AboutPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutPageComponent ]
+      declarations: [ AboutPageComponent ],
+      imports: [
+        CoreModule,
+        RouterTestingModule,
+        SharedModule,
+        createBasicStoreModule(),
+      ]
     })
     .compileComponents();
   }));
