00f7084d5ddcc11352d0aa2a5b92602c93e358cf
Neil MacDougall <neil.macdougall@suse.com>
2018-04-12 07:58:33 +0100

    Hide user mgmt action when in production as its not implemented

--- src/frontend/app/features/cloud-foundry/cloud-foundry-tabs-base/cloud-foundry-tabs-base.component.ts
+++ src/frontend/app/features/cloud-foundry/cloud-foundry-tabs-base/cloud-foundry-tabs-base.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { CloudFoundryEndpointService } from '../services/cloud-foundry-endpoint.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-cloud-foundry-tabs-base',
@@ -19,6 +20,9 @@ export class CloudFoundryTabsBaseComponent implements OnInit {
     { link: 'stacks', label: 'Stacks' },
     { link: 'security-groups', label: 'Security Groups' }
   ];
+
+  // Used to hide tab that is not yet implemented when in production
+  isDevEnvironment = !environment.production;
 
   isFetching$: Observable<boolean>;
   constructor(private cfEndpointService: CloudFoundryEndpointService) { }
