7ce6d3d2816ff32ff6106c56b8181a54d2e8322f
Irfan Habib <irfan.habib@suse.com>
2018-04-23 15:29:36 +0100

    Address CC issue

--- src/frontend/app/shared/components/list/list-types/service-instances/service-instances-list-config.service.ts
+++ src/frontend/app/shared/components/list/list-types/service-instances/service-instances-list-config.service.ts
@@ -12,7 +12,7 @@ import { TableCellServiceInstanceTagsComponent } from '../cf-spaces-service-inst
 import { TableCellServiceInstanceAppsAttachedComponent } from '../cf-spaces-service-instances/table-cell-service-instance-apps-attached/table-cell-service-instance-apps-attached.component';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../../../store/app-state';
-import {ServicesService} from '../../../../../features/service-catalog/services.service';
+import { ServicesService } from '../../../../../features/service-catalog/services.service';
 import { DatePipe } from '@angular/common';
 
 @Injectable()
@@ -20,64 +20,42 @@ export class ServiceInstancesListConfigService
   extends ListConfig<APIResource<IServiceInstance>>
   implements IListConfig<APIResource<IServiceInstance>> {
 
-    viewType = ListViewTypes.TABLE_ONLY;
-    dataSource: ServiceInstancesDataSource;
-    defaultView = 'table' as ListView;
-    text = {
-      title: null,
-      noEntries: 'There are no service instances'
-    };
+  viewType = ListViewTypes.TABLE_ONLY;
+  dataSource: ServiceInstancesDataSource;
+  defaultView = 'table' as ListView;
+  text = {
+    title: null,
+    noEntries: 'There are no service instances'
+  };
 
-    static getColumns(datePipe: DatePipe): ITableColumn<APIResource<IServiceInstance>>[]{
-        return  [
-          {
-            columnId: 'name',
-            headerCell: () => 'Service Instances',
-            cellDefinition: {
-              getValue: (row) => `${row.entity.name}`
-            },
-            cellFlex: '2'
-          },
-          {
-            columnId: 'service',
-            headerCell: () => 'Service',
-            cellComponent: TableCellServiceNameComponent,
-            cellFlex: '1'
-          },
-          {
-            columnId: 'servicePlan',
-            headerCell: () => 'Plan',
-            cellComponent: TableCellServicePlanComponent,
-            cellFlex: '1'
-          },
-          {
-            columnId: 'tags',
-            headerCell: () => 'Tags',
-            cellComponent: TableCellServiceInstanceTagsComponent,
-            cellFlex: '2'
-          },
-          {
-            columnId: 'attachedApps',
-            headerCell: () => 'Application Attached',
-            cellComponent: TableCellServiceInstanceAppsAttachedComponent,
-            cellFlex: '3'
-          },
-          {
-            columnId: 'creation', headerCell: () => 'Creation Date',
-            cellDefinition: {
-              getValue: (row: APIResource) => `${datePipe.transform(row.metadata.created_at, 'medium')}`
-            },
-            sort: {
-              type: 'sort',
-              orderKey: 'creation',
-              field: 'metadata.created_at'
-            },
-            cellFlex: '2'
-          },
-        ];
-    }
+  serviceInstanceColumns: ITableColumn<APIResource<IServiceInstance>>[] = [
+    {
+      columnId: 'name', headerCell: () => 'Service Instances',
+      cellDefinition: { getValue: (row) => `${row.entity.name}` }, cellFlex: '2'
+    },
+    {
+      columnId: 'service', headerCell: () => 'Service', cellComponent: TableCellServiceNameComponent, cellFlex: '1'
+    },
+    {
+      columnId: 'servicePlan', headerCell: () => 'Plan', cellComponent: TableCellServicePlanComponent, cellFlex: '1'
+    },
+    {
+      columnId: 'tags', headerCell: () => 'Tags', cellComponent: TableCellServiceInstanceTagsComponent, cellFlex: '2'
+    },
+    {
+      columnId: 'attachedApps',
+      headerCell: () => 'Application Attached', cellComponent: TableCellServiceInstanceAppsAttachedComponent, cellFlex: '3'
+    },
+    {
+      columnId: 'creation', headerCell: () => 'Creation Date', cellDefinition: {
+        getValue: (row: APIResource) => `${this.datePipe.transform(row.metadata.created_at, 'medium')}`
+      }, sort: {
+        type: 'sort', orderKey: 'creation', field: 'metadata.created_at'
+      }, cellFlex: '2'
+    },
+  ];
 
-  constructor(private store: Store<AppState > , servicesService: ServicesService, private datePipe: DatePipe) {
+  constructor(private store: Store<AppState>, servicesService: ServicesService, private datePipe: DatePipe) {
     super();
     this.dataSource = new ServiceInstancesDataSource(servicesService.cfGuid, servicesService.serviceGuid, store, this);
   }
