b4341d91b36f456eb4204945cf0fb87c7d46675a
Irfan Habib <irfan.habib@suse.com>
2018-04-23 16:47:19 +0100

    Fix lint issue

--- src/frontend/app/features/service-catalog/service-catalog.module.ts
+++ src/frontend/app/features/service-catalog/service-catalog.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
+import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
 import { ServiceBaseComponent } from './service-base/service-base.component';
 import { ServiceCatalogPageComponent } from './service-catalog-page/service-catalog-page.component';
 import { ServiceCatalogRoutingModule } from './service-catalog.routing';
 import { ServiceInstancesComponent } from './service-instances/service-instances.component';
-import { CoreModule } from "../../core/core.module";
 
 @NgModule({
   imports: [
