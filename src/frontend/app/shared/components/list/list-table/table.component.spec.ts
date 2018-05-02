4f8dc785a5d325119321283e155e2994a71f2acc
Nathan Jones <njones@suse.com>
2018-05-02 11:09:19 +0100

    Lint fox

--- src/frontend/app/shared/components/list/list-table/table.component.spec.ts
+++ src/frontend/app/shared/components/list/list-table/table.component.spec.ts
@@ -64,7 +64,7 @@ describe('TableComponent', () => {
       trackBy: () => '1',
       connect: () => Observable.empty(),
       disconnect: () => null
-    }
+    };
     fixture.detectChanges();
   });
 
