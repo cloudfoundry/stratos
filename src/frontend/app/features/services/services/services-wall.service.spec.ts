a4c1ec540fa447c7531dc969149259f0b61fa8e9
Irfan Habib <irfan.habib@suse.com>
2018-05-10 16:27:57 +0100

    Fix unit tests

--- src/frontend/app/features/services/services/services-wall.service.spec.ts
+++ src/frontend/app/features/services/services/services-wall.service.spec.ts
@@ -1,11 +1,17 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
+import { EntityServiceFactory } from '../../../core/entity-service-factory.service';
+import { PaginationMonitorFactory } from '../../../shared/monitors/pagination-monitor.factory';
 import { ServicesWallService } from './services-wall.service';
+import { BaseTestModules } from '../../../test-framework/cloud-foundry-endpoint-service.helper';
 
 describe('ServicesWallService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ServicesWallService]
+      providers: [ServicesWallService,
+        EntityServiceFactory,
+        PaginationMonitorFactory],
+      imports: [BaseTestModules]
     });
   });
 
--- src/frontend/app/shared/components/list/list-types/services-wall/service-instance-card/service-instance-card.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/services-wall/service-instance-card/service-instance-card.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ServicesWallService } from '../../../../../../features/services/services/services-wall.service';
+import {
+  BaseTestModulesNoShared,
+  MetadataCardTestComponents,
+} from '../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
 import { ServiceInstanceCardComponent } from './service-instance-card.component';
+import { AppChipsComponent } from '../../../../chips/chips.component';
+import { EntityServiceFactory } from '../../../../../../core/entity-service-factory.service';
+import { EntityMonitorFactory } from '../../../../../monitors/entity-monitor.factory.service';
+import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
 
 describe('ServiceInstanceCardComponent', () => {
   let component: ServiceInstanceCardComponent;
@@ -8,14 +17,75 @@ describe('ServiceInstanceCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ServiceInstanceCardComponent ]
+      declarations: [ServiceInstanceCardComponent,
+        ...MetadataCardTestComponents,
+        AppChipsComponent],
+      imports: [
+        BaseTestModulesNoShared,
+      ],
+      providers: [
+        ServicesWallService,
+        EntityServiceFactory,
+        EntityMonitorFactory,
+        PaginationMonitorFactory
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ServiceInstanceCardComponent);
     component = fixture.componentInstance;
+    component.row = {
+      entity: {
+        service_plan_guid: '',
+        space_guid: '',
+        dashboard_url: '',
+        type: '',
+        tags: [],
+        service_guid: '',
+        service_plan_url: '',
+        service_bindings: [],
+        service_bindings_url: '',
+        service_keys_url: '',
+        last_operation: {
+          type: '',
+          state: '',
+          description: '',
+          updated_at: '',
+          created_at: ''
+        },
+        service_plan: {
+          entity: {
+            name: '',
+            free: true,
+            description: '',
+            service_guid: '',
+            extra: '',
+            unique_id: '',
+            public: false,
+            bindable: 0,
+            active: false,
+            service_url: '',
+            service_instances_url: '',
+          },
+          metadata: {
+            created_at: '',
+            guid: '',
+            updated_at: '',
+            url: ''
+          }
+        },
+        routes_url: '',
+        service_url: ''
+      },
+      metadata: {
+        created_at: '',
+        guid: '',
+        updated_at: '',
+        url: ''
+      }
+    };
     fixture.detectChanges();
   });
 
