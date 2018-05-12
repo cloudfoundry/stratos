8b731ed4e3a39b47e0280bce350d720b4b09e664
Neil MacDougall <neil.macdougall@suse.com>
2018-05-12 11:32:06 +0100

    WIP SUSE login page

--- custom-src/frontend/app/custom.module.ts
+++ custom-src/frontend/app/custom.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
+import { CoreModule } from './core/core.module';
+import { MDAppModule } from './core/md.module';
 import { SuseLoginComponent } from './custom/suse-login/suse-login.component';
 import { EULA_PROVIDER } from './features/about/about-page/about-page.component';
-import { Router } from '@angular/router';
+import { SIDENAV_COPYRIGHT } from './features/dashboard/side-nav/side-nav.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    SuseLoginComponent
+    SuseLoginComponent,
   ],
   imports: [
-    Router
+    CoreModule,
+    SharedModule,
+    MDAppModule,
   ],
   providers: [
-    { provide: EULA_PROVIDER, useValue: true }
+    { provide: EULA_PROVIDER, useValue: true },
+    { provide: SIDENAV_COPYRIGHT, useValue: '&copy; 2018 SUSE' }
   ],
   entryComponents: [
     SuseLoginComponent
@@ -21,7 +28,7 @@ export class CustomModule {
 
   constructor(private router: Router) {
     // Override the component used for the login route
-    const routeConfig = { ...router.config };
+    const routeConfig = [ ...router.config ];
     const loginRoute = routeConfig.find(r => r.path === 'login') || {};
     loginRoute.component = SuseLoginComponent;
     router.resetConfig(routeConfig);
