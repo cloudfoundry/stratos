497640187be2314bb0f2216546229a5af2286bd1
Neil MacDougall <neil.macdougall@suse.com>
2018-05-15 09:05:57 +0100

    Fix unit test

--- src/frontend/app/features/about/eula-page/eula-page.component.spec.ts
+++ src/frontend/app/features/about/eula-page/eula-page.component.spec.ts
@@ -1,6 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { EulaPageComponent } from './eula-page.component';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CoreModule } from '../../../core/core.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { createBasicStoreModule } from '../../../test-framework/store-test-helper';
+import { EulaPageComponent, EulaPageContentComponent } from './eula-page.component';
 
 describe('EulaPageComponent', () => {
   let component: EulaPageComponent;
@@ -8,7 +11,13 @@ describe('EulaPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EulaPageComponent ]
+      declarations: [ EulaPageComponent, EulaPageContentComponent ],
+      imports: [
+        CoreModule,
+        RouterTestingModule,
+        SharedModule,
+        createBasicStoreModule(),
+      ]
     })
     .compileComponents();
   }));
