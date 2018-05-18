b9e2f548b95316b4ed4899d133ce26a266aa8924
Irfan Habib <irfan.habib@suse.com>
2018-05-18 15:52:38 +0100

    Fix issue #2195.1

--- src/frontend/app/features/service-catalog/add-service-instance/select-service/select-service.component.ts
+++ src/frontend/app/features/service-catalog/add-service-instance/select-service/select-service.component.ts
@@ -1,8 +1,10 @@
 import { AfterContentInit, Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
-import { filter, first, map, share, switchMap, tap } from 'rxjs/operators';
+import { filter, first, share, switchMap, tap } from 'rxjs/operators';
 import { Subscription } from 'rxjs/Subscription';
 
 import { IService } from '../../../../core/cf-api-svc.types';
@@ -12,12 +14,10 @@ import { SetCreateServiceInstanceServiceGuid } from '../../../../store/actions/c
 import { AppState } from '../../../../store/app-state';
 import { selectCreateServiceInstance } from '../../../../store/selectors/create-service-instance.selectors';
 import { APIResource } from '../../../../store/types/api.types';
-import { ServicesWallService } from '../../../services/services/services-wall.service';
-import { ServicesService } from '../../services.service';
 import { CreateServiceInstanceState } from '../../../../store/types/create-service-instance.types';
-import { CreateServiceInstanceHelperService, Mode } from '../create-service-instance-helper.service';
-import { ActivatedRoute } from '@angular/router';
 import { getIdFromRoute } from '../../../cloud-foundry/cf.helpers';
+import { ServicesWallService } from '../../../services/services/services-wall.service';
+import { CreateServiceInstanceHelperService, Mode } from '../create-service-instance-helper.service';
 
 @Component({
   selector: 'app-select-service',
@@ -32,8 +32,7 @@ export class SelectServiceComponent implements OnDestroy, AfterContentInit {
   serviceSubscription: Subscription;
   services$: Observable<APIResource<IService>[]>;
   stepperForm: FormGroup;
-  validate: Observable<boolean>;
-
+  validate = new BehaviorSubject(false);
 
   constructor(
     private store: Store<AppState>,
@@ -46,39 +45,30 @@ export class SelectServiceComponent implements OnDestroy, AfterContentInit {
     this.stepperForm = new FormGroup({
       service: new FormControl(''),
     });
-
     this.services$ = this.store.select(selectCreateServiceInstance).pipe(
       filter(p => !!p && !!p.cfGuid),
       tap((p: CreateServiceInstanceState) => this.cfGuid = p.cfGuid),
-      switchMap(p => servicesWallService.getServicesInCf(p.cfGuid)),
+      switchMap(p => this.servicesWallService.getServicesInCf(p.cfGuid)),
       filter(p => !!p),
       first(),
       share()
     );
-
   }
 
   onNext = () => {
-
     const serviceGuid = this.stepperForm.controls.service.value;
     this.store.dispatch(new SetCreateServiceInstanceServiceGuid(serviceGuid));
-
     const appId = getIdFromRoute(this.activatedRoute, 'id');
     this.cSIHelperService.initService(this.cfGuid, serviceGuid, appId ? Mode.APPSERVICE : Mode.DEFAULT);
     return Observable.of({ success: true });
   }
 
   ngAfterContentInit() {
-
-    this.validate = this.stepperForm.statusChanges.pipe(
-      map(() => this.stepperForm.valid)
-    );
-
     this.serviceSubscription = this.services$.pipe(
       tap(services => {
         const guid = services[0].metadata.guid;
         this.stepperForm.controls.service.setValue(guid);
-        this.store.dispatch(new SetCreateServiceInstanceServiceGuid(guid));
+        this.validate.next(true);
       })
     ).subscribe();
   }
