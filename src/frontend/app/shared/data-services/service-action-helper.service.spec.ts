6f5b6080ab318d213f1051017fc58d3346e813ca
Neil MacDougall <neil.macdougall@suse.com>
2018-05-21 16:05:54 +0100

    Bump more test packages

--- src/frontend/app/shared/data-services/service-action-helper.service.spec.ts
+++ src/frontend/app/shared/data-services/service-action-helper.service.spec.ts
@@ -1,11 +1,10 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
-import { ServiceActionHelperService } from './service-action-helper.service';
 import { BaseTestModules } from '../../test-framework/cloud-foundry-endpoint-service.helper';
-import { ConfirmationDialogConfig } from '../components/confirmation-dialog.config';
 import { ConfirmationDialogService } from '../components/confirmation-dialog.service';
+import { ServiceActionHelperService } from './service-action-helper.service';
 
-fdescribe('ServiceActionHelperService', () => {
+describe('ServiceActionHelperService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ServiceActionHelperService, ConfirmationDialogService],
