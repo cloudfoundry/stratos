96ba72b57fce612bed04f4b136f63d84a55913f4
Nathan Jones <njones@suse.com>
2018-05-24 16:16:07 +0100

    Angular cli upgrade

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,14 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-spec-reporter'),
-      require('@angular/cli/plugins/karma'),
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('./build/karma.test.reporter.js')
     ],
     client: {
@@ -18,7 +18,7 @@ module.exports = function (config) {
       captureConsole: true,
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'json'],
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly', 'json'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
