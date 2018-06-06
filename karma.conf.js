0fe99a63b518072eb7a558e18e1648f9bf075eb7
Neil MacDougall <neil.macdougall@suse.com>
2018-06-06 12:03:25 +0100

    Remove codecov reporter for now

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+//      require('karma-coverage-istanbul-reporter'),
       require('karma-spec-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('./build/karma.test.reporter.js')
