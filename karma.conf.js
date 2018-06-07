f93c2996366119a54f30707f3f3c888260530a0c
Nathan Jones <njones@suse.com>
2018-06-07 14:18:36 +0100

    Ensure tests are not run in a random order

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,9 @@ module.exports = function (config) {
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
       captureConsole: true,
+      jasmine: {
+        random: false
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'),
