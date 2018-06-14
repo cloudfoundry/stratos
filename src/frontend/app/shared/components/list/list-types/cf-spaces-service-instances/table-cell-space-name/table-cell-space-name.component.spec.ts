b8c4f0fdf019bfb8754fbd757dd4764c72a7345b
Irfan Habib <irfan.habib@suse.com>
2018-06-14 11:37:52 +0100

    Fix entity issue & fix unit tests

--- src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-space-name/table-cell-space-name.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-space-name/table-cell-space-name.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableCellSpaceNameComponent } from './table-cell-space-name.component';
+import { BaseTestModulesNoShared } from '../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
 
 describe('TableCellSpaceNameComponent', () => {
   let component: TableCellSpaceNameComponent<any>;
@@ -8,7 +9,8 @@ describe('TableCellSpaceNameComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableCellSpaceNameComponent ]
+      declarations: [ TableCellSpaceNameComponent ],
+      imports: [BaseTestModulesNoShared]
     })
     .compileComponents();
   }));
@@ -16,6 +18,23 @@ describe('TableCellSpaceNameComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TableCellSpaceNameComponent);
     component = fixture.componentInstance;
+    component.row = {
+      entity: {
+        service_bindings: [],
+        active: true,
+        bindable: true,
+        description: 'test',
+        extra: '',
+        label: '',
+        info_url: '',
+        long_description: '',
+        plan_updateable: false,
+        tags: [],
+        url: '',
+        version: ''
+      },
+      metadata: null
+    };
     fixture.detectChanges();
   });
 
