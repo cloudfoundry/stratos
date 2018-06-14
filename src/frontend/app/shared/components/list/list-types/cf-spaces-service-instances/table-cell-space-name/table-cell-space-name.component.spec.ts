09528b9e34405059203b48b27727f560d226ac18
Irfan Habib <irfan.habib@suse.com>
2018-06-14 12:39:23 +0100

    Fix unit tests

--- src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-space-name/table-cell-space-name.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-space-name/table-cell-space-name.component.spec.ts
@@ -20,20 +20,94 @@ describe('TableCellSpaceNameComponent', () => {
     component = fixture.componentInstance;
     component.row = {
       entity: {
-        service_bindings: [],
-        active: true,
-        bindable: true,
-        description: 'test',
-        extra: '',
-        label: '',
-        info_url: '',
-        long_description: '',
-        plan_updateable: false,
+        service_plan_guid: 'test',
+        space_guid: '',
+        space: {
+          entity: {
+            name: '',
+            organization_guid: '',
+            allow_ssh: false,
+            organization_url: '',
+            developers_url: '',
+            managers_url: '',
+            auditors_url: '',
+            apps_url: '',
+            routes_url: '',
+            domains_url: '',
+            service_instances_url: '',
+            app_events_url: '',
+            security_groups_url: '',
+            staging_security_groups_url: '',
+          },
+          metadata: {
+            created_at: '',
+            guid: '',
+            updated_at: '',
+            url: ''
+          }
+        },
+        dashboard_url: '',
+        type: '',
         tags: [],
-        url: '',
-        version: ''
+        service_guid: '',
+        service_plan_url: '',
+        service_bindings: [],
+        service_bindings_url: '',
+        service_keys_url: '',
+        last_operation: {
+          type: '',
+          state: '',
+          description: '',
+          updated_at: '',
+          created_at: ''
+        },
+        service_plan: {
+          entity: {
+            name: '',
+            free: true,
+            description: '',
+            service_guid: '',
+            extra: '',
+            unique_id: '',
+            public: false,
+            bindable: 0,
+            active: false,
+            service_url: '',
+            service: {
+              entity: {
+                label: '',
+                description: '',
+                active: 1,
+                bindable: 1,
+                unique_id: '',
+                extra: '',
+                tags: [''],
+                requires: [''],
+                service_broker_guid: '',
+                plan_updateable: 1,
+                service_plans_url: '',
+                service_plans: [],
+              },
+              metadata: null
+            },
+            service_instances_url: '',
+          },
+          metadata: {
+            created_at: '',
+            guid: '',
+            updated_at: '',
+            url: ''
+          }
+        },
+        routes_url: '',
+        service_url: ''
       },
-      metadata: null
+      metadata: {
+        created_at: '',
+        guid: '',
+        updated_at: '',
+        url: ''
+      }
     };
     fixture.detectChanges();
   });
