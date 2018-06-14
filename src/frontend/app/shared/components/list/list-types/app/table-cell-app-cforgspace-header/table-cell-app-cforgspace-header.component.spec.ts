70e9299ba11dcf27d73a21721045a52e85ee4142
Richard Cox <richard.cox@suse.com>
2018-06-14 10:09:10 +0100

    Fix tests

--- src/frontend/app/shared/components/list/list-types/app/table-cell-app-cforgspace-header/table-cell-app-cforgspace-header.component.spec.ts
+++ src/frontend/app/shared/components/list/list-types/app/table-cell-app-cforgspace-header/table-cell-app-cforgspace-header.component.spec.ts
@@ -1,20 +1,16 @@
-import { appReducers } from '../../../../../../store/reducers.module';
-import { getInitialTestStoreState, createBasicStoreModule } from '../../../../../../test-framework/store-test-helper';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
-import { TableCellAppStatusComponent } from './table-cell-app-status.component';
-import { AppState } from '../../../../../../store/app-state';
+import { CoreModule } from '../../../../../../core/core.module';
+import { createBasicStoreModule } from '../../../../../../test-framework/store-test-helper';
+import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
+import {
+  ApplicationStateIconComponent,
+} from '../../../../application-state/application-state-icon/application-state-icon.component';
+import { ApplicationStateIconPipe } from '../../../../application-state/application-state-icon/application-state-icon.pipe';
 import { ApplicationStateComponent } from '../../../../application-state/application-state.component';
 import { ApplicationStateService } from '../../../../application-state/application-state.service';
-import {
-  ApplicationStateIconComponent
-} from '../../../../application-state/application-state-icon/application-state-icon.component';
-import {
-  ApplicationStateIconPipe
-} from '../../../../application-state/application-state-icon/application-state-icon.pipe';
-import { CoreModule } from '../../../../../../core/core.module';
-import { StoreModule } from '@ngrx/store';
-import { PaginationMonitorFactory } from '../../../../../monitors/pagination-monitor.factory';
+import { TableCellAppStatusComponent } from '../table-cell-app-status/table-cell-app-status.component';
 
 describe('TableCellAppStatusComponent', () => {
   let component: TableCellAppStatusComponent<any>;
