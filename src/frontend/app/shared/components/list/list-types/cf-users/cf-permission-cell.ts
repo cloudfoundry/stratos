82dbd3e2611cac154e4f8ecc82e4c80212a7ee3c
Richard Cox <richard.cox@suse.com>
2018-06-20 15:46:51 +0100

    Fix unit tests

--- src/frontend/app/shared/components/list/list-types/cf-users/cf-permission-cell.ts
+++ src/frontend/app/shared/components/list/list-types/cf-users/cf-permission-cell.ts
@@ -1,7 +1,7 @@
 import { Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { BehaviorSubject, Observable, of as observableOf } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { first, map, filter } from 'rxjs/operators';
 
 import { IUserRole } from '../../../../../features/cloud-foundry/cf.helpers';
 import { AppState } from '../../../../../store/app-state';
@@ -46,7 +46,10 @@ export abstract class CfPermissionCell<T> extends TableCellCustom<APIResource<Cf
   protected guid: string;
 
   protected rowSubject = new BehaviorSubject<APIResource<CfUser>>(null);
-  protected configSubject = new BehaviorSubject<any>(null);
+  private configSubject = new BehaviorSubject<any>(null);
+  protected config$ = this.configSubject.asObservable().pipe(
+    filter(config => !!config)
+  );
 
   constructor(public store: Store<AppState>, private confirmDialog: ConfirmationDialogService) {
     super();
