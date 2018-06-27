28aa183fb1568962e6a8cd7767594f2737fd40f7
Irfan Habib <irfan.habib@suse.com>
2018-06-27 11:56:46 +0100

    Update `meta_data` field

--- src/backend/app-core/datastore/20180627111300_UpdateMetadata.go
+++ src/backend/app-core/datastore/20180627111300_UpdateMetadata.go
@@ -14,12 +14,22 @@ func (s *StratosMigrations) Up_20180627111300(txn *sql.Tx, conf *goose.DBConf) {
 	if err != nil {
 		fmt.Printf("Failed to migrate due to: %v", err)
 	}
+	updateMetadata = "UPDATE tokens SET meta_data='' where meta_data is NULL"
+	_, err = txn.Exec(updateMetadata)
+	if err != nil {
+		fmt.Printf("Failed to migrate due to: %v", err)
+	}
 
 }
 
 func Down_20180627111300(txn *sql.Tx) {
 	dropTables := "ALTER TABLE tokens modify meta_data TEXT DEFAULT ''"
 	_, err := txn.Exec(dropTables)
+	if err != nil {
+		fmt.Printf("Failed to migrate due to: %v", err)
+	}
+	dropTables = "UPDATE tokens SET meta_data=NULL where meta_data is ''"
+	_, err = txn.Exec(dropTables)
 	if err != nil {
 		fmt.Printf("Failed to migrate due to: %v", err)
 	}
