cf38ce5465589d9b50d5eb2dabfa1d911cc7a10a
Nathan Jones <nathanmjones1@gmail.com>
2018-06-29 12:46:06 +0100

    Change schedular of application step 1 validated observable to avoid zone issues

--- src/frontend/app/shared/components/create-application/create-application-step1/create-application-step1.component.ts
+++ src/frontend/app/shared/components/create-application/create-application-step1/create-application-step1.component.ts
@@ -1,8 +1,8 @@
 import { AfterContentInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { Observable, of as observableOf } from 'rxjs';
-import { map, startWith, switchMap, withLatestFrom } from 'rxjs/operators';
+import { Observable, of as observableOf, asapScheduler } from 'rxjs';
+import { map, startWith, switchMap, withLatestFrom, observeOn } from 'rxjs/operators';
 
 import { ISpace } from '../../../../core/cf-api.types';
 import { PermissionStrings } from '../../../../core/current-user-permissions.config';
@@ -72,6 +72,7 @@ export class CreateApplicationStep1Component implements OnInit, AfterContentInit
     this.validate = this.cfForm.statusChanges.pipe(
       startWith(this.cfForm.valid || this.isRedeploy),
       map(() => this.cfForm.valid || this.isRedeploy),
+      observeOn(asapScheduler)
     );
   }
 
