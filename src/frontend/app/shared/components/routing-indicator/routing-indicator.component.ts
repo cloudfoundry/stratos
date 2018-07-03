ac287e185150172d686b8d8dec767407866b41b7
Nathan Jones <nathanmjones1@gmail.com>
2018-07-03 13:17:28 +0100

    Added over-engineered 'natural' loading bar for pages that take a while to load

--- src/frontend/app/shared/components/routing-indicator/routing-indicator.component.ts
+++ src/frontend/app/shared/components/routing-indicator/routing-indicator.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { interval, Observable, of as observableOf } from 'rxjs';
-import { filter, map, startWith, switchMap, delay } from 'rxjs/operators';
+import { filter, map, startWith, switchMap, delay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-routing-indicator',
@@ -11,41 +11,57 @@ import { filter, map, startWith, switchMap, delay } from 'rxjs/operators';
 export class RoutingIndicatorComponent {
   public value$: Observable<number>;
 
+  public HIDE_VALUE = 101;
+
   constructor(private router: Router) {
+    let started = false;
     this.value$ = this.router.events.pipe(
       filter(event => {
-        return event instanceof NavigationStart ||
+        return (event instanceof NavigationStart && !started) ||
           event instanceof NavigationCancel ||
           event instanceof NavigationEnd;
       }),
       switchMap(event => {
         if (event instanceof NavigationStart) {
+          started = true;
           return this.getValueEmitter();
         }
-        return observableOf(0);
+        started = false;
+        return observableOf(this.HIDE_VALUE).pipe(
+          delay(100),
+          startWith(100)
+        );
       })
     );
   }
 
   private getValueEmitter() {
     const getValue = this.getValue();
-    return interval(500).pipe(
+    return interval(80).pipe(
       delay(500),
-      map(() => getValue())
+      map(() => getValue()),
     );
   }
-
-  private getValue(minStep: number = 1, maxStep: number = 3) {
-    let value = 1;
-    const maxValue = 90;
+  // Fakes a natual loading indicator
+  private getValue(minStep: number = 0.1, maxStep: number = 2) {
+    let value = 0.1;
+    const slowDownValue = 60;
+    const top = 95;
     return () => {
-      if (value >= maxValue) {
-        return value;
+      if (value >= top) {
+        return top;
       }
-      const increase = Math.floor(Math.random() * maxStep) + minStep;
-      value = Math.min(value + increase, maxValue);
+      if (value >= slowDownValue) {
+        return value += this.getRandomNumber(0.1, 0.6);
+      }
+      const increase = this.getRandomNumber(minStep, maxStep);
+      value = Math.min(value + increase, top);
       return value;
     };
+  }
+
+  private getRandomNumber(min: number, max: number) {
+    return (Math.random() * max) + min;
   }
 
 }
