c7e12a7025566673c022625065b034a4b8a59833
Neil MacDougall <nwmac@users.noreply.github.com>
2018-07-06 14:46:39 +0100

    Remove unused afterviewinit

--- src/frontend/app/features/cloud-foundry/add-organization/create-organization-step/create-organization-step.component.ts
+++ src/frontend/app/features/cloud-foundry/add-organization/create-organization-step/create-organization-step.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
