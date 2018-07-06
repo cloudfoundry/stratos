8b7da7cd93a3e7bfaacb3a563b8d83a47c2c0652
Nathan Jones <njones@suse.com>
2018-07-06 13:57:38 +0100

    Fixed exception that made it's way onto master

--- src/frontend/app/shared/components/list/list-types/cf-users/cf-space-permission-cell/cf-space-permission-cell.component.ts
+++ src/frontend/app/shared/components/list/list-types/cf-users/cf-space-permission-cell/cf-space-permission-cell.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { combineLatest, of as observableOf } from 'rxjs';
+import { combineLatest, of as observableOf, never, empty } from 'rxjs';
 import { map, switchMap, first, filter } from 'rxjs/operators';
 
 import { IOrganization, ISpace } from '../../../../../../core/cf-api.types';
@@ -48,11 +48,11 @@ export class CfSpacePermissionCellComponent extends CfPermissionCell<SpaceUserRo
     );
   }
 
-  private prefixOrgName(permissionList) {
+  private prefixOrgName(permissionList: ICellPermissionList<SpaceUserRoleNames>[]) {
     // Find all unique org guids
     const orgGuids = permissionList.map(permission => permission.orgGuid).filter((value, index, self) => self.indexOf(value) === index);
     // Find names of all orgs
-    const orgNames$ = combineLatest(
+    const orgNames$ = orgGuids.length ? observableOf([]) : combineLatest(
       orgGuids.map(orgGuid => this.store.select<APIResource<IOrganization>>(selectEntity(organizationSchemaKey, orgGuid)))
     ).pipe(
       filter(org => !!org),
