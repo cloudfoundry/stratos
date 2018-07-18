63f3944fb9b42df7fabc72a45b6885b696a12ed1
Irfan Habib <irfan.habib@suse.com>
2018-07-18 15:47:35 +0100

    Services Marketplace E2E (part-1)

--- src/test-e2e/marketplace/marketplace-e2e.spec.ts
+++ src/test-e2e/marketplace/marketplace-e2e.spec.ts
@@ -1,15 +1,14 @@
 import { MarketplacePage } from './marketplace.po';
-import { E2EHelpers, ConsoleUserType } from '../helpers/e2e-helpers';
-import { browser } from 'protractor';
+import { ConsoleUserType } from '../helpers/e2e-helpers';
 import { e2e } from '../e2e';
+import { SecretsHelpers } from '../helpers/secrets-helpers';
+import { browser } from 'protractor';
 
-// Dashboard page not implemented
-fdescribe('Marketplace', () => {
-  const helpers = new E2EHelpers();
+describe('Marketplace', () => {
   const marketplacePage = new MarketplacePage();
-
+  const secretsHelper = new SecretsHelpers();
+  const serviceSearchText = 'app';
   beforeAll(() => {
-    helpers.setupApp(ConsoleUserType.admin);
     e2e.setup(ConsoleUserType.admin)
       .clearAllEndpoints()
       .registerDefaultCloudFoundry()
@@ -22,5 +21,54 @@ fdescribe('Marketplace', () => {
 
   it('- should reach marketplace page', () => {
     expect(marketplacePage.isMarketplacePage()).toBeTruthy();
+  });
+
+  it('- should have correct title', () => {
+    expect(marketplacePage.header.getTitleText()).toEqual('Services Marketplace');
+  });
+
+  it('- should have visible services', () => {
+    marketplacePage.getServices().then(services => {
+      expect(services.length).toBeGreaterThan(0);
+    });
+  });
+
+  it('- should have filters', () => {
+    marketplacePage.servicesList.header.getFilterOptions().then(options => {
+      expect(options.length).toBeGreaterThan(0);
+    });
+    marketplacePage.servicesList.header.getPlaceholderText().then(text => {
+      expect(text).toEqual('Cloud Foundry');
+    });
+
+  });
+
+  it('- should change filter text when an option is selected', () => {
+    marketplacePage.servicesList.header.selectFilterOption(1);
+    marketplacePage.servicesList.header.getFilterText().then(text => {
+      expect(text).toEqual(secretsHelper.getDefaultCFEndpoint().name);
+    });
+  });
+
+  it('- should have a search box', () => {
+    expect(marketplacePage.servicesList.header.getSearchInputField()).toBeDefined();
+  });
+
+  it('- should be able to search', () => {
+    marketplacePage.getServices().then(services => {
+      const initialCount = services.length;
+      marketplacePage.servicesList.header.setSearchText(serviceSearchText).then(
+        () => {
+          expect(marketplacePage.servicesList.header.getSearchText()).toEqual(serviceSearchText);
+          marketplacePage.getServices().then(s => {
+            expect(s.length).toBeLessThan(initialCount);
+          });
+        }
+      );
+    });
+  });
+
+  it('- should have a refresh button', () => {
+    expect(marketplacePage.servicesList.header.getRefreshListButton().isPresent()).toBeTruthy();
   });
 });
--- src/test-e2e/po/list.po.ts
+++ src/test-e2e/po/list.po.ts
@@ -1,7 +1,8 @@
-import { by, element, promise } from 'protractor';
+import { by, element, promise, Key } from 'protractor';
 import { ElementArrayFinder, ElementFinder } from 'protractor/built';
 import { Component } from './component.po';
 import { MetaCard } from './meta-card.po';
+import { VOLUME_UP } from '@angular/cdk/keycodes';
 
 /**
  * Page Object for the List component
@@ -12,10 +13,13 @@ export class ListComponent extends Component {
 
   public cards: ListCardComponent;
 
+  public header: ListHeaderComponent;
+
   constructor(locator: ElementFinder = element(by.tagName('app-list'))) {
     super(locator);
     this.table = new ListTableComponent(locator);
     this.cards = new ListCardComponent(locator);
+    this.header = new ListHeaderComponent(locator);
   }
 
   isTableView(): promise.Promise<boolean> {
@@ -62,4 +66,59 @@ export class ListCardComponent extends Component {
   getCard(index: number): MetaCard {
     return new MetaCard(this.getCards().get(index));
   }
+}
+// List Header (filter/search bar)
+export class ListHeaderComponent extends Component {
+
+  constructor(locator: ElementFinder) {
+    super(locator);
+  }
+
+  getListHeader(): ElementFinder {
+    return this.locator.element(by.css('.list-component__header'));
+  }
+
+  getFilterFormField(): ElementFinder {
+    return this.getListHeader().element(by.tagName('mat-form-field'));
+  }
+
+  getRightHeaderSection(): ElementFinder {
+    return this.getListHeader().element(by.css('.list-component__header__right'));
+  }
+  getSearchInputField(): ElementFinder {
+    return this.getRightHeaderSection().all(by.css('.filter')).first().element(by.css('input'));
+  }
+
+  setSearchText(text: string): promise.Promise<void> {
+    const searchField = this.getSearchInputField();
+    searchField.click();
+    searchField.sendKeys(text);
+    return searchField.sendKeys(Key.RETURN);
+  }
+
+  getSearchText(): promise.Promise<string> {
+    return this.getSearchInputField().getAttribute('ng-reflect-model');
+  }
+  getPlaceholderText(): promise.Promise<string> {
+    return this.getFilterFormField().element(by.tagName('mat-placeholder')).getText();
+  }
+
+  getFilterOptions(): promise.Promise<ElementFinder[]> {
+    this.getFilterFormField().click();
+    return element.all(by.tagName('mat-option')).then((matOptions: ElementFinder[]) => {
+      return matOptions;
+    });
+  }
+
+  getFilterText(): promise.Promise<string> {
+    return this.locator.element(by.css('.mat-select-value')).getText();
+  }
+  selectFilterOption(index: number): promise.Promise<any> {
+    return this.getFilterOptions().then(options => options[index].click());
+  }
+
+  getRefreshListButton(): ElementFinder {
+    return this.getRightHeaderSection().element(by.css('button'));
+  }
+
 }
