2e1e637e7481b94b6de30c974332a1bfa6a4735c
Irfan Habib <irfan.habib@suse.com>
2018-07-18 15:47:35 +0100

    Services Marketplace E2E (part-1)

--- src/test-e2e/marketplace/marketplace.po.ts
+++ src/test-e2e/marketplace/marketplace.po.ts
@@ -1,9 +1,13 @@
-import { protractor } from 'protractor';
+import { protractor, ElementArrayFinder, ElementFinder } from 'protractor';
 import { E2EHelpers } from '../helpers/e2e-helpers';
 import { browser, promise } from 'protractor';
+import { Page } from '../po/page.po';
+import { ListComponent } from '../po/list.po';
 
-export class MarketplacePage {
+export class MarketplacePage extends Page {
   helpers = new E2EHelpers();
+
+  servicesList = new ListComponent();
 
   navigateTo() {
     return browser.get('/marketplace');
@@ -11,9 +15,12 @@ export class MarketplacePage {
 
   isMarketplacePage(strictCheck: boolean = true): promise.Promise<boolean> {
     return browser.getCurrentUrl().then(url => {
-      console.log(`URL is: ${url}`);
       return url === browser.baseUrl + '/marketplace' || (strictCheck && url === browser.baseUrl + '/');
     });
+  }
+
+  getServices = (): ElementArrayFinder => {
+    return this.servicesList.cards.getCards();
   }
 
 }
