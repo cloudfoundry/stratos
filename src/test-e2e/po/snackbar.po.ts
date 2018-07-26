653ce26076609676ce8449c0c5a9e89c012d44eb
Neil MacDougall <neil.macdougall@suse.com>
2018-07-26 13:52:06 +0100

    Add delay for snackbar animations

--- src/test-e2e/po/snackbar.po.ts
+++ src/test-e2e/po/snackbar.po.ts
@@ -1,6 +1,9 @@
-import { protractor, ElementFinder, ElementArrayFinder } from 'protractor/built';
+import { protractor } from 'protractor';
 import { browser, element, by, promise } from 'protractor';
 import { Component } from './component.po';
+import { e2e } from '../e2e';
+
+const until = protractor.ExpectedConditions;
 
 /**
  * Page Object for snack bar component
@@ -28,5 +31,18 @@ export class SnackBarComponent extends Component {
 
   getMessage(): promise.Promise<string> {
     return this.locator.getText();
+  }
+
+  // Pass an optional description to help when debugging test issues
+  waitUntilShown(description = 'Snackbar'): promise.Promise<void> {
+    return browser.wait(until.presenceOf(this.locator), 5000,
+      description + ' taking too long to appear in the DOM').then(() => {
+        // Short delay for snackbar animations to finish
+        e2e.sleep(250);
+        return browser.wait(until.visibilityOf(this.locator), 5000, description + ' not visible timing out').then(v => {
+          // Slight delay for animations
+          return browser.driver.sleep(100);
+        });
+      });
   }
 }
