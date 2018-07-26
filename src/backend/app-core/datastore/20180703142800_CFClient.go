f52143e41ab0a4c0c74e6f6f7170fe217c8377f3
Neil MacDougall <neil.macdougall@suse.com>
2018-07-26 10:23:08 +0100

    Fix merge issue

--- src/backend/app-core/datastore/20180703142800_CFClient.go
+++ src/backend/app-core/datastore/20180703142800_CFClient.go
@@ -14,19 +14,19 @@ func init() {
 		if strings.Contains(conf.Driver.Name, "mysql") {
 			binaryDataType = "BLOB"
 		}
-	
+
 		alterCnsis := "ALTER TABLE cnsis ADD COLUMN client_id VARCHAR(255) NOT NULL DEFAULT 'cf';"
 		_, err := txn.Exec(alterCnsis)
 		if err != nil {
 			return err
 		}
-	
+
 		alterCnsis = "ALTER TABLE cnsis ADD COLUMN client_secret " + binaryDataType + ";"
 		_, err = txn.Exec(alterCnsis)
 		if err != nil {
 			return err
 		}
-	
+
 		return nil
-	}
+	})
 }
