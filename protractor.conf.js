45e4b99550dc129dd6312ab541cafa70be8ff33f
Irfan Habib <irfan.habib@suse.com>
2018-08-21 12:17:55 +0100

    bump up default wait

--- protractor.conf.js
+++ protractor.conf.js
@@ -53,7 +53,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 40000,
     print: function () {}
   },
   params: secrets,
