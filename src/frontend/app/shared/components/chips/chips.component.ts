beb378c0873dcb5de8f513a1dcc2922a9f5905af
Richard Cox <richard.cox@suse.com>
2018-08-24 18:15:34 +0100

    WIP

--- src/frontend/app/shared/components/chips/chips.component.ts
+++ src/frontend/app/shared/components/chips/chips.component.ts
@@ -16,6 +16,10 @@ export class AppChip<T = string> implements IAppChip<T> {
   hideClearButton$?: Observable<boolean>;
   busy?: Observable<boolean>;
   color?: string;
+  url?: {
+    link: string,
+    params?: string
+  };
 }
 
 @Component({
--- src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-service-instance-apps-attached/table-cell-service-instance-apps-attached.component.ts
+++ src/frontend/app/shared/components/list/list-types/cf-spaces-service-instances/table-cell-service-instance-apps-attached/table-cell-service-instance-apps-attached.component.ts
@@ -1,28 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 
 import { IServiceInstance } from '../../../../../../core/cf-api-svc.types';
 import { APIResource } from '../../../../../../store/types/api.types';
+import { AppChip } from '../../../../chips/chips.component';
 import { TableCellCustom } from '../../../list.types';
+import { first, map } from 'rxjs/operators';
 
-interface BoundApp {
-  appName: string;
-  url: string;
-}
 @Component({
   selector: 'app-table-cell-service-instance-apps-attached',
   templateUrl: './table-cell-service-instance-apps-attached.component.html',
   styleUrls: ['./table-cell-service-instance-apps-attached.component.scss']
 })
-export class TableCellServiceInstanceAppsAttachedComponent extends TableCellCustom<APIResource<IServiceInstance>> {
-  boundApps: BoundApp[];
+export class TableCellServiceInstanceAppsAttachedComponent extends TableCellCustom<APIResource<IServiceInstance>> implements OnInit {
+
+  boundApps$: Observable<AppChip[]>;
+  config$ = new BehaviorSubject(null);
+  row$ = new BehaviorSubject(null);
+
+  @Input('config')
+  set config(config: any) {
+    this.config$.next(config);
+  }
 
   @Input('row')
-  set row(row: any) {
-    this.boundApps = row ? row.entity.service_bindings.map(binding => {
-      return {
-        appName: binding.entity.app.entity.name,
-        url: `/applications/${binding.entity.cfGuid}/${binding.entity.app.metadata.guid}`,
-      };
-    }) : [];
+  set row(row: APIResource<IServiceInstance>) {
+    this.row$.next(row);
   }
+
+  ngOnInit(): void {
+    this.boundApps$ = combineLatest([
+      this.config$.asObservable().pipe(first()),
+      this.row$
+    ]).pipe(
+      map(([config, row]) => {
+        console.log('!!!!!!!!!');
+        return row ? row.entity.service_bindings.map(binding => {
+          return {
+            value: binding.entity.app.entity.name,
+            url: {
+              link: `/applications/${binding.entity.cfGuid}/${binding.entity.app.metadata.guid}`,
+              params: config.breadcrumbs,
+            }
+          };
+        }) : [];
+      }),
+      first()
+    );
+  }
+
 }
