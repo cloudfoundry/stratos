0b1124be75de9ccbf4b5f7021a98eb2cc6fc0bec
Neil MacDougall <neil.macdougall@suse.com>
2018-08-30 13:06:12 +0100

    Run through with tests

--- deploy/ci/travis/upload-e2e-test-report.sh
+++ deploy/ci/travis/upload-e2e-test-report.sh
@@ -15,23 +15,10 @@ chmod +x mc
 
 echo "Uploading test report...."
 
-mkdir ./e2e-reports
-echo "This is a test" > ./e2e-reports/test.txt
-echo "This is a test" > ./e2e-reports/test2.txt
-echo "This is a test" > ./e2e-reports/test3.txt
-
 echo "Configuring upload client"
 ./mc config host add s3 ${AWS_ENDPOINT} ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} --insecure
-
-echo "Listing contents..."
-./mc ls s3/${S3_BUCKET} --insecure
-
-ls ./e2e-reports
 
 echo "Uploading ..."
 # Sync the E2E reports
 ./mc cp --insecure -r e2e-reports s3/${S3_BUCKET}
-
-echo "Listing contents..."
-./mc ls s3/${S3_BUCKET} --insecure
 
