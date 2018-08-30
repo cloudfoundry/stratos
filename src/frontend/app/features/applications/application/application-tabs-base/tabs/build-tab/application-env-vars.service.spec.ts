00c12dbec70e36aeaeec19707d511af38c713ebc
Richard Cox <richard.cox@suse.com>
2018-08-30 15:58:54 +0100

    Fix e2e tests

--- src/frontend/app/features/applications/application/application-tabs-base/tabs/build-tab/application-env-vars.service.spec.ts
+++ src/frontend/app/features/applications/application/application-tabs-base/tabs/build-tab/application-env-vars.service.spec.ts
@@ -1,11 +1,19 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
+import { PaginationMonitorFactory } from '../../../../../../shared/monitors/pagination-monitor.factory';
+import { createBasicStoreModule } from '../../../../../../test-framework/store-test-helper';
 import { ApplicationEnvVarsHelper } from './application-env-vars.service';
 
 describe('ApplicationEnvVarsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApplicationEnvVarsHelper]
+      providers: [
+        ApplicationEnvVarsHelper,
+        PaginationMonitorFactory,
+      ],
+      imports: [
+        createBasicStoreModule()
+      ]
     });
   });
 
--- src/frontend/app/features/applications/deploy-application/deploy-application-options-step/deploy-application-options-step.component.spec.ts
+++ src/frontend/app/features/applications/deploy-application/deploy-application-options-step/deploy-application-options-step.component.spec.ts
@@ -1,5 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { CoreModule } from '../../../../core/core.module';
+import { SharedModule } from '../../../../shared/shared.module';
+import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
+import {
+  ApplicationEnvVarsHelper,
+} from '../../application/application-tabs-base/tabs/build-tab/application-env-vars.service';
 import { DeployApplicationOptionsStepComponent } from './deploy-application-options-step.component';
 
 describe('DeployApplicationOptionsStepComponent', () => {
@@ -8,9 +16,17 @@ describe('DeployApplicationOptionsStepComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DeployApplicationOptionsStepComponent ]
+      declarations: [DeployApplicationOptionsStepComponent],
+      providers: [ApplicationEnvVarsHelper],
+      imports: [
+        CoreModule,
+        SharedModule,
+        RouterTestingModule,
+        createBasicStoreModule(),
+        NoopAnimationsModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
