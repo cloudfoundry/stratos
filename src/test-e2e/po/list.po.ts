016b80ff44e3fe895b0513437faa5e3fe11fbb0f
Neil MacDougall <nwmac@users.noreply.github.com>
2018-08-31 07:29:06 +0100

    Remove trailing spaces

--- src/test-e2e/po/list.po.ts
+++ src/test-e2e/po/list.po.ts
@@ -64,7 +64,7 @@ export class ListTableComponent extends Component {
       return rows[index].element(by.css('.mat-radio-button')).click();
     });
   }
-  
+
   waitUntilNotBusy() {
     return Component.waitUntilNotShown(
       this.locator.element(by.css('.table-row__deletion-bar-wrapper'))
