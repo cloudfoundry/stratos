905fb7d37796e40a1c6b20b7487df95733118bdf
Richard Cox <richard.cox@suse.com>
2018-09-11 13:12:03 +0100

    Bug fix + e2e hardening - Fix exception in ServiceInstanceCardComponent caused by row() being called before ngOnInit - Use standard e2e entity name in services tests - Harden edit service instance test, clean up any reference to tests that might survive following a failure -

--- src/test-e2e/helpers/e2e-helpers.ts
+++ src/test-e2e/helpers/e2e-helpers.ts
@@ -21,7 +21,8 @@ export class E2EHelpers {
   constructor() { }
 
   // This makes identification of acceptance test apps easier in case they leak
-  static createCustomName = (prefix: string, isoTime?: string) => prefix + '.' + (isoTime || (new Date()).toISOString());
+  static createCustomName = (prefix: string, isoTime?: string, trim = false) =>
+    prefix + '.' + (isoTime || (new Date()).toISOString().replace(/[-:.]+/g, ''))
 
   getHost(): string {
     return browser.baseUrl;
--- src/test-e2e/marketplace/services-helper-e2e.ts
+++ src/test-e2e/marketplace/services-helper-e2e.ts
@@ -4,7 +4,10 @@ import { CFResponse, createEmptyCfResponse } from '../../frontend/app/store/type
 import { e2e, E2ESetup } from '../e2e';
 import { CFHelpers } from '../helpers/cf-helpers';
 import { CFRequestHelpers } from '../helpers/cf-request-helpers';
+import { E2EHelpers } from '../helpers/e2e-helpers';
 import { CreateServiceInstance } from './create-service-instance.po';
+
+const customServiceLabel = E2EHelpers.e2eItemPrefix + process.env.USER;
 
 export class ServicesHelperE2E {
 
@@ -16,8 +19,9 @@ export class ServicesHelperE2E {
   constructor(public e2eSetup: E2ESetup, createServiceInstance: CreateServiceInstance = null) {
     this.cfRequestHelper = new CFRequestHelpers(e2eSetup);
     this.cfHelper = new CFHelpers(e2eSetup);
-    const testTime = (new Date()).toISOString();
-    this.serviceInstanceName = `serviceInstance-${testTime}`;
+    this.serviceInstanceName = E2EHelpers.createCustomName(customServiceLabel).toLowerCase();
+    expect(this.serviceInstanceName.length)
+      .toBeLessThanOrEqual(50, `Service name should not exceed 50 characters: ${this.serviceInstanceName}`);
     if (!!createServiceInstance) {
       this.createServiceInstance = createServiceInstance;
     }
@@ -25,6 +29,8 @@ export class ServicesHelperE2E {
 
   addPrefixToServiceName = (prefix: string) => {
     this.serviceInstanceName = `${prefix}-${this.serviceInstanceName}`;
+    expect(this.serviceInstanceName.length)
+      .toBeLessThanOrEqual(50, `Service name should not exceed 50 characters: ${this.serviceInstanceName}`);
   }
   setCreateServiceInstance = (createServiceInstance: CreateServiceInstance) => {
     this.createServiceInstance = createServiceInstance;
@@ -51,6 +57,13 @@ export class ServicesHelperE2E {
     );
   }
 
+  fetchServiceInstanceByName = (cfGuid: string, serviceInstanceName: string): promise.Promise<CFResponse> => {
+    return this.cfRequestHelper.sendCfGet(
+      cfGuid,
+      `service_instances?name=${serviceInstanceName}`
+    );
+  }
+
   createService = (serviceName: string, marketplaceMode = false, bindApp: string = null) => {
     this.createServiceInstance.waitForPage();
 
@@ -60,14 +73,14 @@ export class ServicesHelperE2E {
 
     // Select Service
     if (!marketplaceMode) {
-       // Select Service
-       this.setServiceSelection(serviceName);
-       this.createServiceInstance.stepper.next();
-     }
+      // Select Service
+      this.setServiceSelection(serviceName);
+      this.createServiceInstance.stepper.next();
+    }
 
-     // Select Service Plan
-     this.setServicePlan();
-     this.createServiceInstance.stepper.next();
+    // Select Service Plan
+    this.setServicePlan();
+    this.createServiceInstance.stepper.next();
 
     // Bind App
     this.createServiceInstance.stepper.isBindAppStepDisabled().then(bindAppDisabled => {
@@ -145,6 +158,15 @@ export class ServicesHelperE2E {
   }
 
   cleanUpServiceInstance(serviceInstanceName: string): promise.Promise<any> {
+    return this.cleanUpServiceInstances([serviceInstanceName]);
+  }
+
+  cleanUpServiceInstances(serviceInstanceNames: string[]): promise.Promise<any> {
+    // Sleeping because the service instance may not be listed in the `get services` request
+    browser.sleep(1000);
+    if (serviceInstanceNames.length === 0) {
+      return promise.fullyResolved(createEmptyCfResponse());
+    }
     const getCfCnsi = this.cfRequestHelper.getCfGuid();
     let cfGuid: string;
     return getCfCnsi.then(guid => {
@@ -152,11 +174,12 @@ export class ServicesHelperE2E {
       return this.fetchServicesInstances(cfGuid);
     }).then(response => {
       const services = response.resources;
-      const serviceInstance = services.filter(service => service.entity.name === serviceInstanceName)[0];
-      if (serviceInstance) {
-        return this.deleteServiceInstance(cfGuid, serviceInstance.metadata.guid);
-      }
-      return promise.fullyResolved(createEmptyCfResponse());
+      const serviceInstances = services.filter(serviceInstance => {
+        return serviceInstanceNames.findIndex(name => name === serviceInstance.entity.name) >= 0;
+      });
+      return serviceInstances.length ?
+        promise.all(serviceInstances.map(serviceInstance => this.deleteServiceInstance(cfGuid, serviceInstance.metadata.guid))) :
+        promise.fullyResolved(createEmptyCfResponse());
     });
   }
 
