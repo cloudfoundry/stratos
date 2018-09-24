c3e807c731665a7b5746b96029f488cd9f407b67
Neil MacDougall <neil.macdougall@suse.com>
2018-09-24 09:03:24 +0100

    Tidy up. Fix display reference

--- deploy/ci/travis/run-e2e-tests.sh
+++ deploy/ci/travis/run-e2e-tests.sh
@@ -21,64 +21,40 @@ export CERTS_PATH=./dev-certs
 # There are two ways of running - building and deploying a full docker-compose deployment
 # or doing a local build and running that with sqlite
 
-E2E_TARGET="e2e-local"
-
 # Single arg to script can change whether we do a quick of full deploy
-RUN_TYPE=$1
+CAPTURE_VIDEO=$1
 
-if [ "${RUN_TYPE}" == "quick" ]; then
-  echo "Using local deployment for e2e tests"
-  # Quick deploy locally
-  # Start a local UAA - this will take a few seconds to come up in the background
-  docker run -d -p 8080:8080 splatform/stratos-uaa
+echo "Using local deployment for e2e tests"
+# Quick deploy locally
+# Start a local UAA - this will take a few seconds to come up in the background
+docker run -d -p 8080:8080 splatform/stratos-uaa
 
-  # Get go 1.0 and dep
-  curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-  chmod +x ~/bin/gimme
-  eval "$(gimme 1.9)"
-  curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-  go version
-  dep version
-  
-  npm run build
-  npm run build-backend
-  # Copy travis config.properties file
-  cp deploy/ci/travis/config.properties src/jetstream/
-  pushd src/jetstream
-  ./jetstream > backend.log &
-  popd
+# Get go 1.0 and dep
+curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+chmod +x ~/bin/gimme
+eval "$(gimme 1.9)"
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+go version
+dep version
 
-  E2E_TARGET="e2e -- --dev-server-target= --base-url=https://127.0.0.1:5443"
-else
-  echo "Using docker-compose deployment for e2e tests"
-  # Full deploy in docker compose - this is slow
-  # Move the node_modules folder - the docker build will remove it anyway
-  mv ./node_modules /tmp/node_modules
+npm run build
+npm run build-backend
+# Copy travis config.properties file
+cp deploy/ci/travis/config.properties src/jetstream/
+pushd src/jetstream
+./jetstream > backend.log &
+popd
 
-  echo "Building images locally"
-  ./deploy/docker-compose/build.sh -n -l
-  echo "Build Finished"
-  docker images
-
-  echo "Running Stratos in Docker Compose"
-  pushd deploy/ci/travis
-  docker-compose up -d
-  popd
-
-  # The build cleared node_modules, so move back the one we kept
-  #npm install
-  rm -rf ./node_modules
-  mv /tmp/node_modules ./node_modules
-fi
+E2E_TARGET="e2e -- --dev-server-target= --base-url=https://127.0.0.1:5443"
 
 # Test report folder name override
 TIMESTAMP=`date '+%Y%m%d-%H.%M.%S'`
 export E2E_REPORT_FOLDER="./e2e-reports/${TIMESTAMP}-Travis-Job-${TRAVIS_JOB_NUMBER}"
 
 # Capture video if configured
-if [ "$2" == "video" ]; then
+if [ "$CAPTURE_VIDEO" == "video" ]; then
   echo "Starting video capture"
-  ffmpeg -video_size 1280x768 -framerate 25 -f x11grab -i :0.0+0,0 ${E2E_REPORT_FOLDER}/ScreenCapture.mp4 &
+  ffmpeg -video_size 1280x768 -framerate 25 -f x11grab -i ${DISPLAY} ${E2E_REPORT_FOLDER}/ScreenCapture.mp4 &
   FFMPEG=$!
 fi
 
@@ -88,25 +64,12 @@ npm run ${E2E_TARGET}
 RESULT=$?
 set -e
 
-if [ "${TRAVIS_EVENT_TYPE}" != "pull_request" ]; then
-  pushd deploy/ci/travis
-  # Uncomment to copy logs to the travis log
-  #docker-compose stop
-  #docker-compose logs mariadb
-  #docker-compose logs goose
-  #docker-compose logs proxy 
-  docker-compose down
-  popd
-fi
-
 # Copy the backend log to the test report folder if the tests failed
-if [ "${RUN_TYPE}" == "quick" ]; then
-  if [ $RESULT -ne 0 ]; then
-    cp src/jetstream/backend.log ${E2E_REPORT_FOLDER}/jetstream.log
-  fi
+if [ $RESULT -ne 0 ]; then
+  cp src/jetstream/backend.log ${E2E_REPORT_FOLDER}/jetstream.log
 fi
 
-if [ "$2" == "video" ]; then
+if [ "$CAPTURE_VIDEO" == "video" ]; then
   echo "Stopping video capture"
   kill -INT $FFMPEG
 fi
