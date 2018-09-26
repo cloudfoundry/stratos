c5fba744e4b86a37c71f18462d7b25e2fc87b2be
Neil MacDougall <neil.macdougall@suse.com>
2018-09-26 17:18:30 +0100

    Log times as tests run

--- deploy/ci/automation/runandrecord.sh
+++ deploy/ci/automation/runandrecord.sh
@@ -11,13 +11,12 @@ DIRPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo $DIRPATH
 
 cd "$DIRPATH/../../.."
-
 export E2E_REPORT_FOLDER=./e2e-reports
 
-#ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -draw_mouse 0 -i :99.0 "${E2E_REPORT_FOLDER}/ScreenCapture.mp4" >/dev/null 2>&1 &
-ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -draw_mouse 0 -i :99.0 "${E2E_REPORT_FOLDER}/ScreenCapture.mp4" 2>&1 &
+ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -draw_mouse 0 -i :99.0 "${E2E_REPORT_FOLDER}/ScreenCapture.mp4" >/dev/null 2>&1 &
 FFMPEG=$!
 
+export STRATOS_E2E_LOG_TIME=true
 ./node_modules/.bin/ng e2e --dev-server-target= --base-url=https://console.local.pcfdev.io ${SUITE}
 RESULT=$?
 
