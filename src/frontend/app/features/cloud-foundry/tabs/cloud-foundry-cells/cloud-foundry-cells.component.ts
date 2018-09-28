b410a838d6e04997bc63b8a39bd85ee9a4dfe37f
Richard Cox <richard.cox@suse.com>
2018-09-28 17:32:00 +0100

    Fix cells view

--- src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-cells/cloud-foundry-cells.component.ts
+++ src/frontend/app/features/cloud-foundry/tabs/cloud-foundry-cells/cloud-foundry-cells.component.ts
@@ -4,6 +4,7 @@ import {
   CfCellsListConfigService,
 } from '../../../../shared/components/list/list-types/cf-cells/cf-cells-list-config.service';
 import { ListConfig } from '../../../../shared/components/list/list.component.types';
+import { getActiveRouteCfCellProvider } from '../../cf.helpers';
 
 @Component({
   selector: 'app-cloud-foundry-cells',
@@ -13,7 +14,8 @@ import { ListConfig } from '../../../../shared/components/list/list.component.ty
     {
       provide: ListConfig,
       useClass: CfCellsListConfigService
-    }
+    },
+    getActiveRouteCfCellProvider,
   ]
 })
 export class CloudFoundryCellsComponent { }
