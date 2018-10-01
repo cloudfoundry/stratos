97375d5b6da1764039701c7cbbde34c10ec63d05
Neil MacDougall <nwmac@users.noreply.github.com>
2018-10-01 07:09:27 +0100

    Restore disabling waitForAngular

--- src/test-e2e/application/application-deploy-e2e.spec.ts
+++ src/test-e2e/application/application-deploy-e2e.spec.ts
@@ -141,6 +141,10 @@ describe('Application Deploy -', function () {
       overrides.fill({ name: testAppName, random_route: true });
 
       e2e.debugLog(`${loggingPrefix} Overrides Step - overrides set`);
+      
+      // Turn off waiting for Angular - the web socket connection is kept open which means the tests will timeout
+      // waiting for angular if we don't turn off.
+      browser.waitForAngularEnabled(false);
 
       // Press next to deploy the app
       deployApp.stepper.next();
@@ -161,6 +165,7 @@ describe('Application Deploy -', function () {
       // Should be app summary
       browser.wait(ApplicationBasePage.detect()
         .then(appSummary => {
+          browser.waitForAngularEnabled(true);        
           appSummary.waitForPage();
           appDetails.cfGuid = appSummary.cfGuid;
           appDetails.appGuid = appSummary.appGuid;
