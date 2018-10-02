03f6ce2d8248085e102a9d3c7fcf3f7fa8d36652
Nathan Jones <njones@suse.com>
2018-10-02 10:30:31 +0100

    Tidy up home component

--- src/frontend/app/features/home/home/home-page.component.ts
+++ src/frontend/app/features/home/home/home-page.component.ts
@@ -1,71 +1,12 @@
-import { MetricsAction } from './../../../store/actions/metrics.actions';
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { FetchApplicationMetricsAction, MetricQueryConfig } from '../../../store/actions/metrics.actions';
-import { AppState } from '../../../store/app-state';
-import { MetricsChartHelpers } from '../../../shared/components/metrics-chart/metrics.component.helpers';
-import { MetricsConfig } from '../../../shared/components/metrics-chart/metrics-chart.component';
-import { IMetricMatrixResult } from '../../../store/types/base-metric.types';
-import { IMetricApplication } from '../../../store/types/metric.types';
-import { MetricsLineChartConfig } from '../../../shared/components/metrics-chart/metrics-chart.types';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
-  instanceChartConfig: MetricsLineChartConfig;
+export class HomePageComponent {
 
-  constructor(private store: Store<AppState>) {
-    this.instanceChartConfig = this.buildChartConfig();
+  constructor() {
   }
-
-  instanceMetricConfig: MetricsConfig<IMetricMatrixResult<IMetricApplication>>[] = [
-    {
-      getSeriesName: result => `Instance ${result.metric.instance_index}`,
-      mapSeriesItemName: MetricsChartHelpers.getDateSeriesName,
-      sort: MetricsChartHelpers.sortBySeriesName,
-      // mapSeriesItemValue: (bytes) => (bytes / 1000000).toFixed(2);
-      metricsAction: new FetchApplicationMetricsAction(
-        '23b29b1a-1411-422d-9089-0dd51b1fe57a',
-        'rqljU7j5TF-v8_nyozXsd6kDUeU',
-        new MetricQueryConfig('firehose_container_metric_cpu_percentage')
-      )
-    },
-    {
-      getSeriesName: result => `Instance ${result.metric.instance_index}`,
-      mapSeriesItemName: MetricsChartHelpers.getDateSeriesName,
-      sort: MetricsChartHelpers.sortBySeriesName,
-      mapSeriesItemValue: (bytes) => (bytes / 1000000).toFixed(2),
-      metricsAction: new FetchApplicationMetricsAction(
-        '23b29b1a-1411-422d-9089-0dd51b1fe57a',
-        'rqljU7j5TF-v8_nyozXsd6kDUeU',
-        new MetricQueryConfig('firehose_container_metric_memory_bytes')
-      )
-    },
-    {
-      getSeriesName: result => `Instance ${result.metric.instance_index}`,
-      mapSeriesItemName: MetricsChartHelpers.getDateSeriesName,
-      sort: MetricsChartHelpers.sortBySeriesName,
-      mapSeriesItemValue: (bytes) => (bytes / 1000000).toFixed(2),
-      metricsAction: new FetchApplicationMetricsAction(
-        '23b29b1a-1411-422d-9089-0dd51b1fe57a',
-        'rqljU7j5TF-v8_nyozXsd6kDUeU',
-        new MetricQueryConfig('firehose_container_metric_disk_bytes')
-      )
-    }
-  ];
-
-
-  private buildChartConfig() {
-    const lineChartConfig = new MetricsLineChartConfig();
-    lineChartConfig.xAxisLabel = 'Time';
-    lineChartConfig.yAxisLabel = 'test';
-    return lineChartConfig;
-  }
-
-
-
-  ngOnInit() { }
 }
