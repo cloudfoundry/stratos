4bb8602c93870a461463898ef0a4894680ba906d
Richard Cox <richard.cox@suse.com>
2018-10-18 10:00:25 +0100

    Fix cc and unit test

--- src/frontend/app/shared/components/list/list-types/cf-cell-health/cf-cell-health-source.ts
+++ src/frontend/app/shared/components/list/list-types/cf-cell-health/cf-cell-health-source.ts
@@ -41,39 +41,36 @@ export class CfCellHealthDataSource extends ListDataSource<CfCellHealthEntry, IM
         if (!response || response.length === 0 || !response[0].data.result[0].values) {
           return [];
         }
-        const values = response[0].data.result[0].values;
-
-        // Create a new collection containing only the change of state
-        const newValues = values.reduce((res, value, index) => {
-          const timestamp = value['0'];
-          const state = value['1'];
-          if (index === 0) {
-            // Record the first entry
-            res.current = state;
-            res.collection.push({
-              timestamp,
-              state: state === '0' ? CfCellHealthState.INITIAL_HEALTHY : CfCellHealthState.INITIAL_UNHEALTHY
-            });
-          } else if (res.current !== state) {
-            // Record any change of state
-            res.current = state;
-            res.collection.push({
-              timestamp,
-              state: state === '0' ? CfCellHealthState.HEALTHY : CfCellHealthState.UNHEALTHY
-            });
-          }
-          return res;
-        }, { current: null, collection: [] });
-
-        return newValues.collection;
-
-        return values.map(res => ({
-          timestamp: res['0'],
-          state: Number(res['1'])
-        }));
+        return this.mapMetricsToStates(response[0].data.result[0].values);
       }),
       listConfig
     });
+  }
+
+  private mapMetricsToStates(values: [number, string][]): CfCellHealthEntry[] {
+    // Create a new collection containing only the change of state
+    const newValues = values.reduce((res, value, index) => {
+      const timestamp = value['0'];
+      const state = value['1'];
+      if (index === 0) {
+        // Record the first entry
+        res.current = state;
+        res.collection.push({
+          timestamp,
+          state: state === '0' ? CfCellHealthState.INITIAL_HEALTHY : CfCellHealthState.INITIAL_UNHEALTHY
+        });
+      } else if (res.current !== state) {
+        // Record any change of state
+        res.current = state;
+        res.collection.push({
+          timestamp,
+          state: state === '0' ? CfCellHealthState.HEALTHY : CfCellHealthState.UNHEALTHY
+        });
+      }
+      return res;
+    }, { current: null, collection: [] });
+
+    return newValues.collection;
   }
 
 }
