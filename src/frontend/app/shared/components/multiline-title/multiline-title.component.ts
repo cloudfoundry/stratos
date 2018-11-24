57007bc60f04dfe793fdf33314c8eb0493d9e905
Neil MacDougall <neil.macdougall@suse.com>
2018-11-24 14:59:49 +0000

    Limit card titles to two-lines with ellipsis/fade out

---
+++ src/frontend/app/shared/components/multiline-title/multiline-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multiline-title',
+  templateUrl: './multiline-title.component.html',
+  styleUrls: ['./multiline-title.component.scss']
+})
+export class MultilineTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
