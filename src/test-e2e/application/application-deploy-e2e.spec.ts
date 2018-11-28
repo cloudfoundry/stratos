13d6a6df37ad8a4072d8494ae1f54817cc351110
Neil MacDougall <neil.macdougall@suse.com>
2018-11-28 15:08:15 +0000

    E2E Fix

--- src/test-e2e/application/application-deploy-e2e.spec.ts
+++ src/test-e2e/application/application-deploy-e2e.spec.ts
@@ -1,7 +1,5 @@
 import { browser, promise } from 'protractor';
 
-import { IApp } from '../../frontend/app/core/cf-api.types';
-import { APIResource } from '../../frontend/app/store/types/api.types';
 import { ApplicationsPage } from '../applications/applications.po';
 import { e2e } from '../e2e';
 import { CFHelpers } from '../helpers/cf-helpers';
@@ -303,6 +301,7 @@ describe('Application Deploy -', function () {
     expect(appSummary.cardBuildInfo.stack.getValue()).toBe(testAppStack || defaultStack);
 
     appSummary.cardDeployInfo.waitForTitle('Deployment Info');
+    appSummary.cardDeployInfo.github.waitUntilShown('Waiting for GitHub deployment information');
     expect(appSummary.cardDeployInfo.github.isDisplayed()).toBeTruthy();
     appSummary.cardDeployInfo.github.getValue().then(commitHash => {
       expect(commitHash).toBeDefined();
