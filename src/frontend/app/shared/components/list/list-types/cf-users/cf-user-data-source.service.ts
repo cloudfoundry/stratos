d109f7450b05d24f0115438c878e15d5ffe5b7dc
Richard Cox <richard.cox@suse.com>
2018-12-03 17:40:33 +0000

    WIP

--- src/frontend/app/shared/components/list/list-types/cf-users/cf-user-data-source.service.ts
+++ src/frontend/app/shared/components/list/list-types/cf-users/cf-user-data-source.service.ts
@@ -2,16 +2,64 @@ import { Store } from '@ngrx/store';
 
 import { getRowMetadata } from '../../../../../features/cloud-foundry/cf.helpers';
 import { cfUserSchemaKey, entityFactory } from '../../../../../store/helpers/entity-factory';
-import { PaginatedAction } from '../../../../../store/types/pagination.types';
+import { PaginatedAction, PaginationEntityState } from '../../../../../store/types/pagination.types';
 import { ListDataSource } from '../../data-sources-controllers/list-data-source';
 import { ListConfig } from '../../list.component.types';
 import { AppState } from './../../../../../store/app-state';
 import { APIResource } from './../../../../../store/types/api.types';
 import { CfUser } from './../../../../../store/types/user.types';
 
+export const userListUserVisibleKey = 'showUsers';
+// TODO: RC location
+export enum UserListUsersVisible {
+  ALL = 'all',
+  WITH_ROLE = 'withRole',
+  NO_ROLE = 'noRole'
+}
+
+// export const userHas(): boolean {
+//   return false;
+// }
+
+export const userHasRole = (user: CfUser, roleProperty: string): boolean => {
+  return !!user[roleProperty] && !!user[roleProperty].length;
+};
+
+// const userHasAnyRole = (user: CfUser): boolean => {
+//   return userHasRole(user, 'organizations') ||
+//     userHasRole(user, 'spaces') ||
+//     userHasRole(user, 'managed_organizations') ||
+//     userHasRole(user, 'managed_spaces') ||
+//     userHasRole(user, 'audited_organizations') ||
+//     userHasRole(user, 'audited_spaces') ||
+//     userHasRole(user, 'billing_managed_organizations');
+// };
+
+function createUserVisibilityFilter(userHasRoles: (user: CfUser) => boolean):
+  (entities: APIResource<CfUser>[], paginationState: PaginationEntityState) => APIResource<CfUser>[] {
+  return (entities: APIResource<CfUser>[], paginationState: PaginationEntityState): APIResource<CfUser>[] => {
+    const filter: UserListUsersVisible = paginationState.clientPagination.filter.items[userListUserVisibleKey];
+    if (!filter || filter === UserListUsersVisible.ALL) {
+      return entities;
+    }
+    return entities.reduce((response, user) => {
+      const hasARole = userHasRoles(user.entity);
+      if ((filter === UserListUsersVisible.WITH_ROLE && hasARole) || (filter === UserListUsersVisible.NO_ROLE && !hasARole)) {
+        response.push(user);
+      }
+      return response;
+    }, []);
+  };
+}
+
 
 export class CfUserDataSourceService extends ListDataSource<APIResource<CfUser>> {
-  constructor(store: Store<AppState>, action: PaginatedAction, listConfigService: ListConfig<APIResource<CfUser>>) {
+  constructor(
+    store: Store<AppState>,
+    action: PaginatedAction,
+    listConfigService: ListConfig<APIResource<CfUser>>,
+    userHasRoles: (user: CfUser) => boolean
+  ) {
     super({
       store,
       action,
@@ -19,7 +67,7 @@ export class CfUserDataSourceService extends ListDataSource<APIResource<CfUser>>
       getRowUniqueId: getRowMetadata,
       paginationKey: action.paginationKey,
       isLocal: true,
-      transformEntities: [{ type: 'filter', field: 'entity.username' }],
+      transformEntities: [{ type: 'filter', field: 'entity.username' }, createUserVisibilityFilter(userHasRoles)],
       listConfig: listConfigService
     });
   }
