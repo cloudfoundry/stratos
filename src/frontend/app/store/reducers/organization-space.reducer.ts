24833567182d442f5a1581a22052490538f804a7
Nathan Jones <njones@suse.com>
2019-01-16 17:21:00 +0000

    Tidy up org spce reducer logic

--- src/frontend/app/store/reducers/organization-space.reducer.ts
+++ src/frontend/app/store/reducers/organization-space.reducer.ts
@@ -9,62 +9,81 @@ import {
 import { spaceSchemaKey } from '../helpers/entity-factory';
 import { APIResource, NormalizedResponse } from '../types/api.types';
 import { APISuccessOrFailedAction } from '../types/request.types';
-
+import { IRequestEntityTypeState } from '../app-state';
+type entityOrgType = APIResource<IOrganization<string[]>>;
 // Note - This reducer will be updated when we address general deletion of entities within inline lists (not paginated lists)
 export function updateOrganizationSpaceReducer() {
-  return function (state: APIResource, action: APISuccessOrFailedAction) {
+  return function (state: IRequestEntityTypeState<entityOrgType>, action: APISuccessOrFailedAction<NormalizedResponse>) {
     switch (action.type) {
       case DELETE_SPACE_SUCCESS:
         const deleteSpaceAction: DeleteSpace = action.apiAction as DeleteSpace;
-        return updateOrgSpaces(state, deleteSpaceAction.orgGuid, deleteSpaceAction);
+        return removeSpaceFromOrg(state, deleteSpaceAction.orgGuid, deleteSpaceAction.guid);
       case CREATE_SPACE_SUCCESS:
-        const createSpaceAction: CreateSpace = action.apiAction as CreateSpace;
-        const response: NormalizedResponse = action.response as NormalizedResponse;
+        const createSpaceAction = action.apiAction as CreateSpace;
+        const response = action.response;
         const space = response.entities[spaceSchemaKey][response.result[0]];
-        return updateOrgSpaces(state, createSpaceAction.orgGuid, createSpaceAction, space);
+        return addSpaceToOrg(state, createSpaceAction.orgGuid, space);
     }
     return state;
   };
 }
 
-function updateOrgSpaces(state: APIResource, orgGuid: string, spaceAction: BaseSpaceAction, newSpace?: APIResource<ISpace>) {
-  if (!orgGuid) {
-    return state;
-  }
+function addSpaceToOrg(
+  state: IRequestEntityTypeState<entityOrgType>,
+  orgGuid: string,
+  newSpace: APIResource<ISpace>
+) {
+  const orgToModify = getOrg(state, orgGuid);
+  const newSpaces = [
+    ...orgToModify.entity.spaces,
+    newSpace.metadata.guid
+  ];
+  const mergedOrg = applySpacesToOrg(orgToModify, newSpaces);
+  return {
+    ...state,
+    [orgGuid]: mergedOrg
+  };
+}
 
-  const orgGuids = Object.keys(state);
-  if (orgGuids.indexOf(orgGuid) === -1) {
-    return state;
-  }
-
-  const newState = {};
-  orgGuids.forEach(currentGuid => {
-    const org: APIResource<IOrganization> = state[currentGuid];
-    if (currentGuid === orgGuid) {
-      const newSpaces: (APIResource<ISpace> | string)[] = [...org.entity.spaces];
-      if (newSpace) {
-        newSpaces.push(newSpace.metadata.guid);
-      } else {
-        if (spaceAction.removeEntityOnDelete) {
-          const spaceIndex = org.entity.spaces.findIndex(space => {
-            return typeof (space) === 'string' ? space === spaceAction.guid : space.metadata.guid === spaceAction.guid;
-          });
-          if (spaceIndex >= 0) {
-            newSpaces.splice(spaceIndex, 1);
-          }
-        }
-      }
-      const newOrg = {
-        ...org,
-        entity: {
-          ...org.entity,
-          spaces: newSpaces
-        }
-      };
-      newState[currentGuid] = newOrg;
-    } else {
-      newState[currentGuid] = org;
+function removeSpaceFromOrg(
+  state: IRequestEntityTypeState<entityOrgType>,
+  orgGuid: string,
+  spaceGuid: string
+) {
+  const orgToModify = getOrg(state, orgGuid);
+  const newSpaces = orgToModify.entity.spaces.reduce((spaceIds, spaceId) => {
+    if (spaceId !== spaceGuid) {
+      spaceIds.push(spaceId);
     }
-  });
-  return newState;
+    return spaceIds;
+  }, []);
+  const mergedOrg = applySpacesToOrg(orgToModify, newSpaces);
+  return applyModifyOrgToState(state, mergedOrg);
+}
+
+function applySpacesToOrg(org: entityOrgType, spaces: string[]): entityOrgType {
+  return {
+    ...org,
+    entity: {
+      ...org.entity,
+      spaces
+    }
+  };
+}
+
+function applyModifyOrgToState(state: IRequestEntityTypeState<entityOrgType>, org: entityOrgType) {
+  return {
+    ...state,
+    [org.metadata.guid]: org
+  };
+}
+
+function getOrg(
+  state: IRequestEntityTypeState<entityOrgType>,
+  orgGuid: string,
+) {
+  const {
+    [orgGuid]: newOrg
+  } = state;
+  return newOrg;
 }
