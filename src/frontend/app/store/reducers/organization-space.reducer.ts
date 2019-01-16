99f2d1a686fe349d85734b9657441fe1c0384ad8
Richard Cox <richard.cox@suse.com>
2019-01-16 11:57:19 +0000

    Fix null exception after creating a space in an new org - We previously nuked the org's space collection when creating or deleting an org - Validation was correctly then going to refetch this list, however waitForEntity is broken (see #2327) and emits an invalid action - Later code takes invalid action an tries to access space - Fix, aside from #2327, is yodate org space collection after space creation instead of nuking and waiting for validation to refetch Other fixes - Found sub leak in edit org stepper - endpoint service allApps is subbed to in many places, ensure we share

--- src/frontend/app/store/reducers/organization-space.reducer.ts
+++ src/frontend/app/store/reducers/organization-space.reducer.ts
@@ -1,22 +1,33 @@
 import { IOrganization, ISpace } from '../../core/cf-api.types';
-import { BaseSpaceAction, CREATE_SPACE_SUCCESS, DELETE_SPACE_SUCCESS } from '../actions/space.actions';
-import { APIResource } from '../types/api.types';
+import {
+  BaseSpaceAction,
+  CREATE_SPACE_SUCCESS,
+  CreateSpace,
+  DELETE_SPACE_SUCCESS,
+  DeleteSpace,
+} from '../actions/space.actions';
+import { spaceSchemaKey } from '../helpers/entity-factory';
+import { APIResource, NormalizedResponse } from '../types/api.types';
 import { APISuccessOrFailedAction } from '../types/request.types';
 
-// Note - This reducer will be updated when we address general entity relation deletion
+// Note - This reducer will be updated when we address general deletion of entities within inline lists (not paginated lists)
 export function updateOrganizationSpaceReducer() {
   return function (state: APIResource, action: APISuccessOrFailedAction) {
     switch (action.type) {
       case DELETE_SPACE_SUCCESS:
+        const deleteSpaceAction: DeleteSpace = action.apiAction as DeleteSpace;
+        return updateOrgSpaces(state, deleteSpaceAction.orgGuid, deleteSpaceAction);
       case CREATE_SPACE_SUCCESS:
-        const spaceAction: BaseSpaceAction = action.apiAction as BaseSpaceAction;
-        return deleteOrgSpaces(state, spaceAction.orgGuid, spaceAction);
+        const createSpaceAction: CreateSpace = action.apiAction as CreateSpace;
+        const response: NormalizedResponse = action.response as NormalizedResponse;
+        const space = response.entities[spaceSchemaKey][response.result[0]];
+        return updateOrgSpaces(state, createSpaceAction.orgGuid, createSpaceAction, space);
     }
     return state;
   };
 }
 
-function deleteOrgSpaces(state: APIResource, orgGuid: string, spaceAction: BaseSpaceAction) {
+function updateOrgSpaces(state: APIResource, orgGuid: string, spaceAction: BaseSpaceAction, newSpace?: APIResource<ISpace>) {
   if (!orgGuid) {
     return state;
   }
@@ -30,14 +41,17 @@ function deleteOrgSpaces(state: APIResource, orgGuid: string, spaceAction: BaseS
   orgGuids.forEach(currentGuid => {
     const org: APIResource<IOrganization> = state[currentGuid];
     if (currentGuid === orgGuid) {
-      let newSpaces: APIResource<ISpace>[] = null;
-      if (spaceAction.removeEntityOnDelete) {
-        const spaceIndex = org.entity.spaces.findIndex(space => {
-          return typeof (space) === 'string' ? space === spaceAction.guid : space.metadata.guid === spaceAction.guid;
-        });
-        if (spaceIndex >= 0) {
-          newSpaces = [...org.entity.spaces];
-          newSpaces.splice(spaceIndex, 1);
+      const newSpaces: (APIResource<ISpace> | string)[] = [...org.entity.spaces];
+      if (newSpace) {
+        newSpaces.push(newSpace.metadata.guid);
+      } else {
+        if (spaceAction.removeEntityOnDelete) {
+          const spaceIndex = org.entity.spaces.findIndex(space => {
+            return typeof (space) === 'string' ? space === spaceAction.guid : space.metadata.guid === spaceAction.guid;
+          });
+          if (spaceIndex >= 0) {
+            newSpaces.splice(spaceIndex, 1);
+          }
         }
       }
       const newOrg = {
