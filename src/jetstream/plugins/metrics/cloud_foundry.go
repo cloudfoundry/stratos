c82a0e1e4a8c4084a618c258b17ff50f6f682f45
Neil MacDougall <neil.macdougall@suse.com>
2019-01-25 18:53:24 +0000

    Update newer files since the PR was created

--- src/jetstream/plugins/metrics/cloud_foundry.go
+++ src/jetstream/plugins/metrics/cloud_foundry.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/cloudfoundry-incubator/stratos/src/jetstream/repository/interfaces"
@@ -64,7 +63,7 @@ func makePrometheusRequestInfos(c echo.Context, userGUID string, metrics map[str
 		req.UserGUID = userGUID
 		req.ResultGUID = metric.endpoint.GUID
 		req.EndpointGUID = metric.metrics.EndpointGUID
-		req.Method = c.Request().Method()
+		req.Method = c.Request().Method
 
 		addQueries := queries
 		if len(addQueries) > 0 {
@@ -114,7 +113,7 @@ func makePrometheusRequestURI(c echo.Context, prometheusOp string, modify string
 }
 
 func getEchoURL(c echo.Context) url.URL {
-	u := c.Request().URL().(*standard.URL).URL
+	u := c.Request().URL
 	return *u
 }
 
@@ -128,7 +127,7 @@ func (m *MetricsSpecification) getCloudFoundryMetrics(c echo.Context) error {
 	// User must be an admin of the Cloud Foundry
 	// Check each in the list and if any is not, then return an error
 	canAccessMetrics := true
-	for _, endpointID := range cnsiList
+	for _, endpointID := range cnsiList {
 		// Get token for the UserID and EndpointID
 		token, exists := m.portalProxy.GetCNSITokenRecord(endpointID, userGUID)
 		if !exists {
@@ -205,7 +204,7 @@ func (m *MetricsSpecification) getCloudFoundryCellMetrics(c echo.Context) error 
 		return errors.New("Unsupported prometheus query")
 	}
 
-	cnsiList := strings.Split(c.Request().Header().Get("x-cap-cnsi-list"), ",")
+	cnsiList := strings.Split(c.Request().Header.Get("x-cap-cnsi-list"), ",")
 
 	return m.makePrometheusRequest(c, cnsiList, "")
 }
