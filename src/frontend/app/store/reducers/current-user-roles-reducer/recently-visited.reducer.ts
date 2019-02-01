6ce826eb9b3a06981b342a6b4f4a190ccf3f738e
Nathan Jones <njones@suse.com>
2019-02-01 11:26:54 +0000

    Only store 500 recent entities

--- src/frontend/app/store/reducers/current-user-roles-reducer/recently-visited.reducer.ts
+++ src/frontend/app/store/reducers/current-user-roles-reducer/recently-visited.reducer.ts
@@ -1,6 +1,8 @@
 import { TRecentlyVisitedState } from '../../types/recently-visited.types';
 import { AddRecentlyVisitedEntityAction } from '../../actions/recently-visited.actions';
 
+const MAX_RECENT_COUNT = 500;
+
 export function recentlyVisitedReducer(
   state: TRecentlyVisitedState = [],
   action: AddRecentlyVisitedEntityAction
@@ -8,8 +10,15 @@ export function recentlyVisitedReducer(
   if (action.type === AddRecentlyVisitedEntityAction.ACTION_TYPE) {
     return [
       action.recentlyVisited,
-      ...state,
+      ...trimRecent(state),
     ];
+  }
+  return state;
+}
+
+function trimRecent(state: TRecentlyVisitedState) {
+  if (state.length > MAX_RECENT_COUNT) {
+    return state.slice(0, MAX_RECENT_COUNT - 1);
   }
   return state;
 }
