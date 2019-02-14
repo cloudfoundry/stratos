b4313d94378d756c5536c21aacec878fc190c2b3
Neil MacDougall <neil.macdougall@suse.com>
2019-02-14 19:13:37 +0000

    Fixes for SSO

---
+++ deploy/ci/automation/init-uaa-for-sso.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Configuring UAA for SSO"
+
+CF=https://api.${CF_DOMAIN}
+UAA=$(curl -k -s $CF | jq -r .links.uaa.href)
+echo "Using UAA Endpoint: ${UAA}"
+
+uaac target ${UAA}
+uaac token client get admin -s ${ADMIN_CLIENT_SECRET}
+
+uaac client update cf --redirect_uri=https://console.${CF_DOMAIN}/pp/v1/auth/sso_login_callback
+uaac client update cf --authorized_grant_types password,refresh_token,authorization_code
+
+echo "UAA SSO Configuration complete"
