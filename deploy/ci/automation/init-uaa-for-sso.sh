4aff33df02fb3443268a276a36fe10921c8e0a92
Neil MacDougall <neil.macdougall@suse.com>
2019-02-15 12:31:28 +0000

    Further script fixes for SSO

--- deploy/ci/automation/init-uaa-for-sso.sh
+++ deploy/ci/automation/init-uaa-for-sso.sh
@@ -7,11 +7,18 @@ if [ -z "${UAA_ENDPOINT}" ]; then
   exit 1
 fi
 
+ZONE_ARG=""
+if [ -n "${UAA_ZONE}" ]; then
+  echo "UAA ZONE: ${UAA_ZONE}"
+  ZONE_ARG="-z ${UAA_ZONE}"
+fi
+
 echo "Using UAA Endpoint: ${UAA_ENDPOINT}"
 uaac target ${UAA_ENDPOINT} --skip-ssl-validation
 uaac token client get admin -s ${ADMIN_CLIENT_SECRET}
 
-uaac client update cf --redirect_uri=https://console.${CF_DOMAIN}/pp/v1/auth/sso_login_callback
-uaac client update cf --authorized_grant_types password,refresh_token,authorization_code
+uaac client get cf ${ZONE_ARG}
+uaac client update cf --redirect_uri=https://console.${CF_DOMAIN}/pp/v1/auth/sso_login_callback ${ZONE_ARG}
+uaac client update cf --authorized_grant_types password,refresh_token,authorization_code ${ZONE_ARG}
 
 echo "UAA SSO Configuration complete"
