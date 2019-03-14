ad05c0b70f46c9c47c33b43ba9403472e1c4a461
Nathan Jones <njones@suse.com>
2019-03-14 09:28:57 +0000

    Another e2e fix

--- src/test-e2e/po/list.po.ts
+++ src/test-e2e/po/list.po.ts
@@ -210,7 +210,7 @@ export class ListHeaderComponent extends Component {
   }
 
   getSearchInputField(): ElementFinder {
-    return this.getRightHeaderSection().first(by.css('#listSearchFilter')).element(by.css('input'));
+    return this.getRightHeaderSection().all(by.css('#listSearchFilter')).first().element(by.css('input'));
   }
 
   setSearchText(text: string): promise.Promise<void> {
