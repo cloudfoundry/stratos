2d65cd735e2da515f552d0fa4e4091559da993c3
Richard Cox <richard.cox@suse.com>
2019-03-18 10:39:40 +0000

    Allow endpoint types to reference common core endpoint auth types - stops references of package code in common code - prep for moving metrics into own package (just need to register metricType obj via decorators)

--- src/frontend/packages/cloud-foundry/src/cloud-foundry.module.ts
+++ src/frontend/packages/cloud-foundry/src/cloud-foundry.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { StratosExtension } from '../../core/src/core/extension/extension-service';
 import { EndpointTypeExtensionConfig } from '../../core/src/core/extension/extension-types';
 import { urlValidationExpression } from '../../core/src/core/utils.service';
+import { EndpointAuthTypeNames } from '../../core/src/features/endpoints/endpoint-helpers';
 import { CfEndpointDetailsComponent } from './shared/components/cf-endpoint-details/cf-endpoint-details.component';
 import { CloudFoundryComponentsModule } from './shared/components/components.module';
 
@@ -15,7 +16,8 @@ const cloudFoundryEndpointTypes: EndpointTypeExtensionConfig[] = [{
   imagePath: '/core/assets/endpoint-icons/cloudfoundry.png',
   homeLink: (guid) => ['/cloud-foundry', guid],
   listDetailsComponent: CfEndpointDetailsComponent,
-  order: 0
+  order: 0,
+  authTypes: [EndpointAuthTypeNames.CREDS, EndpointAuthTypeNames.SSO]
 }];
 
 @StratosExtension({
