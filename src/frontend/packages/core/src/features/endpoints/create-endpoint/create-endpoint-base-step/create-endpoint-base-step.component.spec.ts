037008cbeb32ef24b0c23f5ea106af3edb2d6115
Richard Cox <richard.cox@suse.com>
2019-03-22 14:03:50 +0000

    Fix unit tests

--- src/frontend/packages/core/src/features/endpoints/create-endpoint/create-endpoint-base-step/create-endpoint-base-step.component.spec.ts
+++ src/frontend/packages/core/src/features/endpoints/create-endpoint/create-endpoint-base-step/create-endpoint-base-step.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { createBasicStoreModule } from '../../../../../test-framework/store-test-helper';
+import { CoreModule } from '../../../../core/core.module';
+import { SharedModule } from '../../../../shared/shared.module';
 import { CreateEndpointBaseStepComponent } from './create-endpoint-base-step.component';
 
 describe('CreateEndpointBaseStepComponent', () => {
@@ -8,9 +14,27 @@ describe('CreateEndpointBaseStepComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateEndpointBaseStepComponent ]
+      declarations: [
+        CreateEndpointBaseStepComponent,
+      ],
+      imports: [
+        CoreModule,
+        SharedModule,
+        RouterTestingModule,
+        createBasicStoreModule(),
+        NoopAnimationsModule
+      ],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            queryParams: {},
+            params: { type: 'metrics' }
+          }
+        }
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
