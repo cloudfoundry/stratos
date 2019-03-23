e42a80769bd3f0ffa4aef3992ff49247afa783e8
zyjiaobj <zyjiaobj@cn.ibm.com>
2019-03-23 19:48:00 +0800

    [autoscaler] fix travis errors

--- src/frontend/packages/core/src/shared/components/list/list-types/app-autoscaler-metric-chart/app-autoscaler-metric-chart-card/app-autoscaler-metric-chart-card.component.ts
+++ src/frontend/packages/core/src/shared/components/list/list-types/app-autoscaler-metric-chart/app-autoscaler-metric-chart-card/app-autoscaler-metric-chart-card.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
 import { ApplicationService } from '../../../../../../features/applications/application.service';
-import { ServiceActionHelperService } from '../../../../../data-services/service-action-helper.service';
-import { EnvVarViewComponent } from '../../../../env-var-view/env-var-view.component';
-import { CardCell, IListRowCell, IListRowCellData } from '../../../list.types';
+import { CardCell, IListRowCell } from '../../../list.types';
 import { APIResource } from '../../../../../../../../store/src/types/api.types';
 import {
   entityFactory,
@@ -13,6 +10,7 @@ import {
   GetAppAutoscalerAppMetricAction,
 } from '../../../../../../../../store/src/actions/app-autoscaler.actions';
 import { getPaginationObservables } from '../../../../../../../../store/src/reducers/pagination-reducer/pagination-reducer.helper';
+import { Observable } from 'rxjs';
 import { AppState } from '../../../../../../../../store/src/app-state';
 import { Store } from '@ngrx/store';
 import { PaginationMonitorFactory } from '../../../../../../shared/monitors/pagination-monitor.factory';
@@ -21,18 +19,19 @@ import {
 } from '../../../../../../../../store/src/types/app-autoscaler.types';
 import { buildLegendData } from '../../../../../../../../store/src/helpers/autoscaler-helpers';
 
-interface EnvVarData {
-  key: string;
-  value: string;
-}
 @Component({
   selector: 'app-app-autoscaler-metric-chart-card',
   templateUrl: './app-autoscaler-metric-chart-card.component.html',
   styleUrls: ['./app-autoscaler-metric-chart-card.component.scss']
 })
-export class AppAutoscalerMetricChartCardComponent extends CardCell<APIResource<any>> implements OnInit, IListRowCell {
 
-  listData: IListRowCellData[];
+export class AppAutoscalerMetricChartCardComponent extends CardCell<APIResource<any>> implements OnInit, IListRowCell {
+  listData: {
+    label: string;
+    data$: Observable<string>;
+    customStyle?: string;
+  }[];
+
   envVarUrl: string;
 
   comboBarScheme = {
@@ -58,9 +57,7 @@ export class AppAutoscalerMetricChartCardComponent extends CardCell<APIResource<
   };
 
   constructor(
-    private dialog: MatDialog,
     private appService: ApplicationService,
-    private serviceActionHelperService: ServiceActionHelperService,
     private store: Store<AppState>,
     private paginationMonitorFactory: PaginationMonitorFactory,
   ) {
@@ -93,27 +90,6 @@ export class AppAutoscalerMetricChartCardComponent extends CardCell<APIResource<
     this.appAutoscalerAppMetricLegend = this.getLegend2(this.row.entity);
     this.metricData$ = this.getAppMetric(this.row.metadata.guid, this.row.entity, this.paramsMetrics);
   }
-
-  showEnvVars = (envVarData: EnvVarData) => {
-    this.dialog.open(EnvVarViewComponent, {
-      data: envVarData,
-      disableClose: false
-    });
-  }
-
-  detach = () => {
-    this.serviceActionHelperService.detachServiceBinding(
-      [this.row],
-      this.row.entity.service_instance_guid,
-      this.appService.cfGuid
-    );
-  }
-
-  edit = () => this.serviceActionHelperService.editServiceBinding(
-    this.row.entity.service_instance_guid,
-    this.appService.cfGuid,
-    { appId: this.appService.appGuid }
-  )
 
   getAppMetric(metricName: string, trigger: any, params: any) {
     const action = new GetAppAutoscalerAppMetricAction(this.appService.appGuid,
