0a170c430915f7d154dadcac7a720ce464531f32
Richard Cox <richard.cox@suse.com>
2019-03-25 11:53:01 +0000

    Fix unit tests

--- src/frontend/packages/core/src/shared/components/cf-org-space-links/cf-org-space-links.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/cf-org-space-links/cf-org-space-links.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
 import { CfOrgSpaceLinksComponent } from './cf-org-space-links.component';
+
 
 describe('CfOrgSpaceLinksComponent', () => {
   let component: CfOrgSpaceLinksComponent;
@@ -8,9 +11,13 @@ describe('CfOrgSpaceLinksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CfOrgSpaceLinksComponent ]
+      declarations: [CfOrgSpaceLinksComponent],
+      imports: [
+        RouterTestingModule,
+        createBasicStoreModule()
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
