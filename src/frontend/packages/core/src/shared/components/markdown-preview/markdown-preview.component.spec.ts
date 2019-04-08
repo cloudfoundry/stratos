375c40ea500924e08565b7c96cca66191d64dec3
Richard Cox <richard.cox@suse.com>
2019-04-08 14:16:10 +0100

    Fix unit tests

--- src/frontend/packages/core/src/shared/components/markdown-preview/markdown-preview.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/markdown-preview/markdown-preview.component.spec.ts
@@ -1,5 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
+import { LoggerService } from '../../../core/logger.service';
 import { MarkdownPreviewComponent } from './markdown-preview.component';
 
 describe('MarkdownPreviewComponent', () => {
@@ -8,9 +12,15 @@ describe('MarkdownPreviewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MarkdownPreviewComponent ]
+      declarations: [MarkdownPreviewComponent],
+      providers: [LoggerService],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        createBasicStoreModule()
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
