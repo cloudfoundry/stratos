5c1c963953718d2e8152eb48a2ed0f6479101aff
Richard Cox <richard.cox@suse.com>
2019-04-08 13:50:56 +0100

    Only show the filter toggle if there's something to filter on

--- src/frontend/packages/core/src/features/home/home/home-page.component.ts
+++ src/frontend/packages/core/src/features/home/home/home-page.component.ts
@@ -3,8 +3,12 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AppState } from '../../../../../store/src/app-state';
+import { AppState, IRequestEntityTypeState } from '../../../../../store/src/app-state';
+import { IUserFavoritesGroups } from '../../../../../store/src/types/favorite-groups.types';
+import { IFavoriteMetadata, UserFavorite } from '../../../../../store/src/types/user-favorites.types';
 import { EndpointsService } from '../../../core/endpoints.service';
+import { LoggerService } from '../../../core/logger.service';
+import { UserFavoriteManager } from '../../../core/user-favorite-manager';
 
 @Component({
   selector: 'app-home-page',
@@ -15,13 +19,26 @@ export class HomePageComponent {
   public allEndpointIds$: Observable<string[]>;
   public haveRegistered$: Observable<boolean>;
 
+  public showFilterToggle$: Observable<boolean>;
   public showFilters = false;
 
-  constructor(endpointsService: EndpointsService, store: Store<AppState>) {
+  constructor(endpointsService: EndpointsService, store: Store<AppState>, logger: LoggerService) {
     this.allEndpointIds$ = endpointsService.endpoints$.pipe(
       map(endpoints => Object.values(endpoints).map(endpoint => endpoint.guid))
     );
     this.haveRegistered$ = endpointsService.haveRegistered$;
+
+    const manager = new UserFavoriteManager(store, logger);
+    this.showFilterToggle$ = manager.getAllFavorites().pipe(
+      map(([, favEntities]: [IUserFavoritesGroups, IRequestEntityTypeState<UserFavorite<IFavoriteMetadata, any>>]) => {
+        for (const favEntity of Object.values(favEntities)) {
+          if (favEntity.entityType !== 'endpoint') {
+            return true;
+          }
+        }
+        return false;
+      })
+    );
   }
 
   public toggleShowFilters() {
