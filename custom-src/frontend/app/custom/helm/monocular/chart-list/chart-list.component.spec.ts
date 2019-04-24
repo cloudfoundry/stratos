d46adb9c89edc67ddfe76fd6a5d1e7886b11295e
Richard Cox <richard.cox@suse.com>
2019-04-24 11:45:50 +0100

    Remove custom-src

--- custom-src/frontend/app/custom/helm/monocular/chart-list/chart-list.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ChartListComponent } from './chart-list.component';
-
-
-describe('Component: ChartList', () => {
-  it('should create an instance', () => {
-    const component = new ChartListComponent();
-    expect(component).toBeTruthy();
-  });
-});
--- custom-src/frontend/app/custom/helm/monocular/list-filters/list-filters.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ListFiltersComponent } from './list-filters.component';
-
-
-describe('Component: ListFilters', () => {
-  it('should create an instance', () => {
-    const component = new ListFiltersComponent();
-    expect(component).toBeTruthy();
-  });
-});
