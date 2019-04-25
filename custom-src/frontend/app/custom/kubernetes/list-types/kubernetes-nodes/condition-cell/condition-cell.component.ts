3879ceddd7b364c6933d9c491a622e4e3dcadc03
Nathan Jones <njones@suse.com>
2019-04-25 15:58:02 +0100

    Allow boolean component to handle unknown state

--- custom-src/frontend/app/custom/kubernetes/list-types/kubernetes-nodes/condition-cell/condition-cell.component.ts
+++ custom-src/frontend/app/custom/kubernetes/list-types/kubernetes-nodes/condition-cell/condition-cell.component.ts
@@ -9,22 +9,38 @@ import { KubernetesNode } from '../../../store/kube.types';
   styleUrls: ['./condition-cell.component.scss']
 })
 export class ConditionCellComponent extends TableCellCustom<KubernetesNode> implements OnInit {
-  condition: string;
+  public isTrue: boolean = null;
 
-  inverse = false;
+  public subtle = false;
+
+  public inverse = false;
 
   constructor() {
     super();
   }
 
   ngOnInit() {
-
-    const condition = this.row.status.conditions.filter(c => c.type === this.config.conditionType);
-    if (condition && condition.length === 1) {
-      this.condition = condition[0].status;
+    const condition = this.row.status.conditions.filter(c => c.type === this.config.conditionType)[0];
+    if (condition) {
+      switch (condition.status) {
+        case 'True':
+          this.isTrue = true;
+          break;
+        case 'False':
+          this.isTrue = false;
+          break;
+      }
     }
   }
 
+}
+
+export class SubtleConditionCellComponent extends ConditionCellComponent {
+
+  constructor() {
+    super();
+    this.subtle = true;
+  }
 }
 
 
