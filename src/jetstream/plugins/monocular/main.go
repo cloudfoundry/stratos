7df2a64f3bf495a66b3234c4f1e398d719ae07c2
Neil MacDougall <neil.macdougall@suse.com>
2019-05-02 14:49:48 +0100

    Re-enable Helm feature

--- src/jetstream/plugins/monocular/main.go
+++ src/jetstream/plugins/monocular/main.go
@@ -45,19 +45,11 @@ func (m *Monocular) GetChartStore() chartsvc.ChartSvcDatastore {
 // Init performs plugin initialization
 func (m *Monocular) Init() error {
 
-	return errors.New("Manually disabled")
-	// #150 - Uncomment to enable helm plugin
-	// ---------------------------------------
-	// m.ConfigureSQL()
-
-	// m.chartSvcRoutes = chartsvc.GetRoutes()
-
-	// m.InitSync()
-
-	// m.syncOnStartup()
-
-	// return nil
-	// ---------------------------------------
+	m.ConfigureSQL()
+	m.chartSvcRoutes = chartsvc.GetRoutes()
+	m.InitSync()
+	m.syncOnStartup()
+	return nil
 }
 
 func (m *Monocular) syncOnStartup() {
