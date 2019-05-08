75039e73e201f7b2faa74eff082edede336a18bd
Neil MacDougall <neil.macdougall@suse.com>
2019-05-08 11:41:35 +0100

    Add simple script to start UAA and All-In-One Stratos

---
+++ deploy/tools/stand-up-dev-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Uses Docker Compose to run a UAA and Stratos
+
+DIRPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+STRATOS="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+echo "Stratos Directory: ${STRATOS}"
+
+BUILD="true"
+
+while getopts "n" opt ; do
+  case $opt in
+    n)
+      BUILD="false"
+    ;;
+  esac
+done
+
+# Build the all-in-one container unless -n arg is provided
+if [ "$BUILD" == "true" ]; then
+  echo "Building Docker All-In-One Container Image..."
+  ${STRATOS}/build/store-git-metadata.sh
+  pushd ${STRATOS} > /dev/null
+  docker build -f deploy/Dockerfile.all-in-one . -t splatform/stratos
+  popd > /dev/null
+fi
+
+# Remove existing system
+docker-compose -f ${DIRPATH}/docker-compose.dev.yml down
+
+# Bring up new system
+docker-compose -f ${DIRPATH}/docker-compose.dev.yml up -d
+
+echo "OK"
