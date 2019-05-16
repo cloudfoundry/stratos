79f51c1f36b6c86ec01e93b41406ac4591c89f34
Richard Cox <richard.cox@suse.com>
2019-05-16 17:17:27 +0100

    Show header in non-admin no-connected endpoints page

--- src/frontend/packages/core/src/features/no-endpoints-non-admin/no-endpoints-non-admin.component.ts
+++ src/frontend/packages/core/src/features/no-endpoints-non-admin/no-endpoints-non-admin.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { TabNavService } from '../../../tab-nav.service';
 
 @Component({
   selector: 'app-no-endpoints-non-admin',
   templateUrl: './no-endpoints-non-admin.component.html',
   styleUrls: ['./no-endpoints-non-admin.component.scss']
 })
-export class NoEndpointsNonAdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NoEndpointsNonAdminComponent {
+  constructor(public tabNavService: TabNavService) { }
 }
