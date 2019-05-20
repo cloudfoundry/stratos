6209d21214eef09e5680da2df40b8d7eeb3cee61
Richard Cox <richard.cox@suse.com>
2019-05-20 10:05:35 +0100

    Fix unit test

--- src/frontend/packages/core/src/shared/components/page-header/show-page-header/show-page-header.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/page-header/show-page-header/show-page-header.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { TabNavService } from '../../../../../tab-nav.service';
+import { CoreModule } from '../../../../core/core.module';
 import { ShowPageHeaderComponent } from './show-page-header.component';
 
 describe('ShowPageHeaderComponent', () => {
@@ -8,9 +11,17 @@ describe('ShowPageHeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShowPageHeaderComponent ]
+      declarations: [ShowPageHeaderComponent],
+      providers: [
+        TabNavService
+      ],
+      imports: [
+        CoreModule,
+        RouterTestingModule
+      ]
+
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
