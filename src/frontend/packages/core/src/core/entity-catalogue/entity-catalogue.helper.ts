22507368391d9c9e94768f992d127b8019f00b86
Nathan Jones <njones@suse.com>
2019-05-21 10:11:37 +0100

    Fix connect/disconnect endpoint

--- src/frontend/packages/core/src/core/entity-catalogue/entity-catalogue.helper.ts
+++ src/frontend/packages/core/src/core/entity-catalogue/entity-catalogue.helper.ts
@@ -1,7 +1,13 @@
 export abstract class EntityCatalogueHelpers {
   static readonly endpointType = 'endpoint';
   static buildEntityKey(entityType: string, endpointType: string): string {
+    if (!entityType) {
+      return endpointType;
+    }
+    if (!endpointType) {
+      return entityType;
+    }
     // Camelcased to make it work better with the store.
-    return endpointType ? `${endpointType}${entityType.charAt(0).toUpperCase() + entityType.slice(1)}` : entityType;
+    return `${endpointType}${entityType.charAt(0).toUpperCase() + entityType.slice(1)}`;
   }
 }
