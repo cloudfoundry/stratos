324710a97038b7923f01416084897e6745762fa7
Kate E. Reinecke <50168367+kreinecke@users.noreply.github.com>
2019-06-21 16:07:39 +0100

    Move reducers to entity definitions in entity generator
    Signed-off-by: Kate E. Reinecke <50168367+kreinecke@users.noreply.github.com>

--- src/frontend/packages/store/src/reducers/api-request-reducers.generator.ts
+++ src/frontend/packages/store/src/reducers/api-request-reducers.generator.ts
@@ -1,15 +1,15 @@
 import { Action } from '@ngrx/store';
 
 import {
-  applicationEntityType,
-  appStatsEntityType,
-  appSummaryEntityType,
-  cfUserEntityType,
-  organizationEntityType,
-  routeEntityType,
-  serviceInstancesEntityType,
-  spaceEntityType,
-  userProvidedServiceInstanceEntityType,
+    applicationEntityType,
+    appStatsEntityType,
+    appSummaryEntityType,
+    cfUserEntityType,
+    organizationEntityType,
+    routeEntityType,
+    serviceInstancesEntityType,
+    spaceEntityType,
+    userProvidedServiceInstanceEntityType,
 } from '../../../cloud-foundry/src/cf-entity-factory';
 import { getCFEntityKey } from '../../../cloud-foundry/src/cf-entity-helpers';
 import { STRATOS_ENDPOINT_TYPE, userFavoritesEntitySchema } from '../../../core/src/base-entity-schemas';
@@ -39,77 +39,79 @@ import { endpointDisconnectUserReducer, userReducer, userSpaceOrgReducer } from 
  */
 
 const requestActions = [
-  RequestTypes.START,
-  RequestTypes.SUCCESS,
-  RequestTypes.FAILED,
-  RequestTypes.UPDATE
+    RequestTypes.START,
+    RequestTypes.SUCCESS,
+    RequestTypes.FAILED,
+    RequestTypes.UPDATE
 ] as IRequestArray;
 
 function chainReducers(baseReducer, extraReducers) {
-  return (state, action) => {
-    let newState = baseReducer(state, action);
-    let nextState;
-    Object.keys(extraReducers).forEach(key => {
-      nextState = extraReducers[key].reduce((s, reducer) => {
-        return reducer(s, action);
-      }, newState[key]);
-      if (nextState !== newState[key]) {
-        newState = {
-          ...newState,
-          ...{
-            [key]: nextState
-          }
-        };
-      }
-    });
-    return newState;
-  };
+    return (state, action) => {
+        let newState = baseReducer(state, action);
+        let nextState;
+        Object.keys(extraReducers).forEach(key => {
+            nextState = extraReducers[key].reduce((s, reducer) => {
+                return reducer(s, action);
+            }, newState[key]);
+            if (nextState !== newState[key]) {
+                newState = {
+                    ...newState,
+                    ...{
+                        [key]: nextState
+                    }
+                };
+            }
+        });
+        return newState;
+    };
 }
 
 export function requestReducer(state: IRequestState, action: Action) {
-  const baseRequestReducer = requestReducerFactory(requestActions);
-  const extraReducers = {
-    [appStatsEntityType]: [appStatsReducer]
-  };
-  return chainReducers(baseRequestReducer, extraReducers)(state, action);
+    const baseRequestReducer = requestReducerFactory(requestActions);
+    const extraReducers = {
+        [appStatsEntityType]: [appStatsReducer]
+    };
+    return chainReducers(baseRequestReducer, extraReducers)(state, action);
 }
 
 function getInternalEntityKey(type: string) {
-  return entityCatalogue.getEntityKey(STRATOS_ENDPOINT_TYPE, type);
+    return entityCatalogue.getEntityKey(STRATOS_ENDPOINT_TYPE, type);
 }
 
 // TODO Add these reducers to the catalogue
 export function requestDataReducer(state: BaseRequestDataState, action: Action) {
-  const baseDataReducer = requestDataReducerFactory(requestActions);
+    const baseDataReducer = requestDataReducerFactory(requestActions);
 
-  const extraReducers = {
-    [getCFEntityKey(cfUserEntityType)]: [userReducer, endpointDisconnectUserReducer],
-    [getCFEntityKey(routeEntityType)]: [routeReducer],
-    [getCFEntityKey(serviceInstancesEntityType)]: [serviceInstanceReducer],
-    [getCFEntityKey(userProvidedServiceInstanceEntityType)]: [serviceInstanceReducer],
-    [getInternalEntityKey(endpointStoreNames.type)]: [systemEndpointsReducer],
-    [getCFEntityKey(appSummaryEntityType)]: [updateAppSummaryRoutesReducer],
-    [getCFEntityKey(applicationEntityType)]: [
-      updateApplicationRoutesReducer(),
-      endpointDisconnectApplicationReducer()
-    ],
-    [getCFEntityKey(spaceEntityType)]: [
-      updateSpaceQuotaReducer,
-      endpointDisconnectApplicationReducer(),
-      applicationAddRemoveReducer(),
-      userSpaceOrgReducer(true)
-    ],
-    [getCFEntityKey(organizationEntityType)]: [
-      updateOrganizationQuotaReducer,
-      updateOrganizationSpaceReducer(),
-      endpointDisconnectApplicationReducer(),
-      userSpaceOrgReducer(false)
-    ],
-    [getInternalEntityKey(userFavoritesEntitySchema.entityType)]: [
-      addOrUpdateUserFavoriteMetadataReducer,
-      deleteUserFavoriteMetadataReducer
-    ]
-  };
+    const extraReducers = getAllEntityReducers();
 
-  return chainReducers(baseDataReducer, extraReducers)(state, action);
+    const extraReducers = {
+        [getCFEntityKey(cfUserEntityType)]: [userReducer, endpointDisconnectUserReducer],
+        [getCFEntityKey(routeEntityType)]: [routeReducer],
+        [getCFEntityKey(serviceInstancesEntityType)]: [serviceInstanceReducer],
+        [getCFEntityKey(userProvidedServiceInstanceEntityType)]: [serviceInstanceReducer],
+        [getInternalEntityKey(endpointStoreNames.type)]: [systemEndpointsReducer],
+        [getCFEntityKey(appSummaryEntityType)]: [updateAppSummaryRoutesReducer],
+        [getCFEntityKey(applicationEntityType)]: [
+            updateApplicationRoutesReducer(),
+            endpointDisconnectApplicationReducer()
+        ],
+        [getCFEntityKey(spaceEntityType)]: [
+            updateSpaceQuotaReducer,
+            endpointDisconnectApplicationReducer(),
+            applicationAddRemoveReducer(),
+            userSpaceOrgReducer(true)
+        ],
+        [getCFEntityKey(organizationEntityType)]: [
+            updateOrganizationQuotaReducer,
+            updateOrganizationSpaceReducer(),
+            endpointDisconnectApplicationReducer(),
+            userSpaceOrgReducer(false)
+        ],
+        [getInternalEntityKey(userFavoritesEntitySchema.entityType)]: [
+            addOrUpdateUserFavoriteMetadataReducer,
+            deleteUserFavoriteMetadataReducer
+        ]
+    };
+
+    return chainReducers(baseDataReducer, extraReducers)(state, action);
 }
