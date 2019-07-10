b11a3e5b9162d1f1261ef163e22309b353226d94
Nathan Jones <njones@suse.com>
2019-07-10 14:38:45 +0100

    Tidy up imports

--- src/frontend/packages/store/src/reducers/api-request-reducers.generator.ts
+++ src/frontend/packages/store/src/reducers/api-request-reducers.generator.ts
@@ -1,17 +1,5 @@
 import { Action } from '@ngrx/store';
-
-import {
-  applicationEntityType,
-  appStatsEntityType,
-  appSummaryEntityType,
-  cfUserEntityType,
-  organizationEntityType,
-  routeEntityType,
-  serviceInstancesEntityType,
-  spaceEntityType,
-  userProvidedServiceInstanceEntityType,
-} from '../../../cloud-foundry/src/cf-entity-factory';
-import { getCFEntityKey } from '../../../cloud-foundry/src/cf-entity-helpers';
+import { appStatsEntityType } from '../../../cloud-foundry/src/cf-entity-factory';
 import { STRATOS_ENDPOINT_TYPE, userFavoritesEntitySchema } from '../../../core/src/base-entity-schemas';
 import { entityCatalogue } from '../../../core/src/core/entity-catalogue/entity-catalogue.service';
 import { endpointStoreNames } from '../types/endpoint.types';
@@ -22,16 +10,8 @@ import { requestReducerFactory } from './api-request-reducer/request-reducer.fac
 import { IRequestArray } from './api-request-reducer/types';
 import { appStatsReducer } from './app-stats-request.reducer';
 import { addOrUpdateUserFavoriteMetadataReducer, deleteUserFavoriteMetadataReducer } from './favorite.reducer';
-import { applicationAddRemoveReducer } from './application-add-remove-reducer';
-import { updateApplicationRoutesReducer } from './application-route.reducer';
-import { endpointDisconnectRemoveEntitiesReducer } from './endpoint-disconnect-application.reducer';
-import { updateOrganizationQuotaReducer } from './organization-quota.reducer';
-import { updateOrganizationSpaceReducer } from './organization-space.reducer';
-import { routeReducer, updateAppSummaryRoutesReducer } from './routes.reducer';
-import { serviceInstanceReducer } from './service-instance.reducer';
-import { updateSpaceQuotaReducer } from './space-quota.reducer';
-import { endpointDisconnectUserReducer, userReducer, userSpaceOrgReducer } from './users.reducer';
 import { systemEndpointsReducer } from './system-endpoints.reducer';
+
 
 /**
  * This module uses the request data reducer and request reducer factories to create
@@ -88,33 +68,5 @@ export function requestDataReducer(state: BaseRequestDataState, action: Action) 
     addOrUpdateUserFavoriteMetadataReducer,
     deleteUserFavoriteMetadataReducer
   ]);
-
-  /*     const extraReducers = {
-          [getCFEntityKey(cfUserEntityType)]: [userReducer, endpointDisconnectUserReducer],
-          [getCFEntityKey(routeEntityType)]: [routeReducer],
-          [getCFEntityKey(serviceInstancesEntityType)]: [serviceInstanceReducer],
-          [getCFEntityKey(userProvidedServiceInstanceEntityType)]: [serviceInstanceReducer],
-          [getInternalEntityKey(endpointStoreNames.type)]: [systemEndpointsReducer],
-          [getCFEntityKey(appSummaryEntityType)]: [updateAppSummaryRoutesReducer],
-          [getCFEntityKey(applicationEntityType)]: [
-              updateApplicationRoutesReducer(),
-              endpointDisconnectApplicationReducer()
-          ],
-          [getCFEntityKey(spaceEntityType)]: [
-              updateSpaceQuotaReducer,
-              endpointDisconnectApplicationReducer(),
-              applicationAddRemoveReducer(),
-              userSpaceOrgReducer(true)
-          ],
-          [getCFEntityKey(organizationEntityType)]: [
-              updateOrganizationQuotaReducer,
-              updateOrganizationSpaceReducer(),
-              endpointDisconnectApplicationReducer(),
-              userSpaceOrgReducer(false)
-          ],
-          [getInternalEntityKey(userFavoritesEntitySchema.entityType)]: [
-              addOrUpdateUserFavoriteMetadataReducer,
-              deleteUserFavoriteMetadataReducer
-          ] */
   return chainReducers(baseDataReducer, extraReducers)(state, action);
 }
