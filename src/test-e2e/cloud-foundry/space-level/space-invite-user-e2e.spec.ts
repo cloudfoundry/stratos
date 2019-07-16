ed0890adce6b8365d8c0b67cdbab98703ff8a34f
Richard Cox <richard.cox@suse.com>
2019-07-16 13:41:02 +0100

    Removed fdescribe and comments

--- src/test-e2e/cloud-foundry/space-level/space-invite-user-e2e.spec.ts
+++ src/test-e2e/cloud-foundry/space-level/space-invite-user-e2e.spec.ts
@@ -5,7 +5,7 @@ import { CFHelpers } from '../../helpers/cf-helpers';
 import { setupInviteUserTests } from '../invite-users-e2e.helper';
 import { CfSpaceLevelPage } from './cf-space-level-page.po';
 
-fdescribe('CF - Space - Invite User - ', () => {
+describe('CF - Space - Invite User - ', () => {
   let spacePage: CfSpaceLevelPage;
 
   const navToSpaceUserList = (cfHelper: CFHelpers, defaultCf: E2EConfigCloudFoundry) => {
@@ -25,33 +25,3 @@ fdescribe('CF - Space - Invite User - ', () => {
   setupInviteUserTests(true, navToSpaceUserList, navToCfSummary);
 
 });
-
-
-// fdescribe('test', () => {
-
-//   beforeAll(() => {
-//     const defaultCf = e2e.secrets.getDefaultCFEndpoint();
-//     const setup = e2e.setup(ConsoleUserType.admin)
-//       .clearAllEndpoints()
-//       .registerDefaultCloudFoundry()
-//       .connectAllEndpoints(ConsoleUserType.admin)
-//       .connectAllEndpoints(ConsoleUserType.user);
-//     const page = new CFPage();
-//     page.sideNav.goto(SideNavMenuItem.CloudFoundry);
-
-//     return promise.fullyResolved(true).then(() => promise.rejected('REASON'));
-//   });
-
-//   it('1', () => {
-//     console.log('1');
-//   });
-
-//   it('2', () => {
-//     console.log('2');
-//   });
-
-
-//   // it('long', () => {
-//   //   return promise.defer().promise;
-//   // });
-// });
