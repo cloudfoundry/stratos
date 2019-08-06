2e557ea65fc16fe296f19b76816e456f463ab580
Richard Cox <richard.cox@suse.com>
2019-08-06 13:38:53 +0100

    Improve application 'view' tests - don't reload page so often - if wait for card status fails, print card status

--- src/test-e2e/application/application-view-e2e.spec.ts
+++ src/test-e2e/application/application-view-e2e.spec.ts
@@ -81,181 +81,181 @@ describe('Application View -', () => {
     });
   });
 
-  it('Walk tabs', () => {
-    appSummary.navigateTo();
-
-    appSummary.goToInstancesTab();
-    appSummary.goToRoutesTab();
-    appSummary.goToLogStreamTab();
-    appSummary.goToServicesTab();
-    appSummary.goToVariablesTab();
-    appSummary.goToEventsTab();
-    appSummary.goToSummaryTab();
-  });
-
-  describe('Summary Tab -', () => {
+  describe('Tabs', () => {
     beforeAll(() => {
-      appSummary.navigateTo();
-      appSummary.waitForPage();
+      ApplicationsPage.goToAppSummary(appName, CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
     });
 
-    it('Status', () => {
-      appSummary.cardStatus.waitForStatus('Incomplete');
+    it('Walk tabs', () => {
+      appSummary.goToInstancesTab();
+      appSummary.goToRoutesTab();
+      appSummary.goToLogStreamTab();
+      appSummary.goToServicesTab();
+      appSummary.goToVariablesTab();
+      appSummary.goToEventsTab();
+      appSummary.goToSummaryTab();
     });
 
-    it('Instances', () => {
-      appSummary.cardInstances.waitForRunningInstancesText('0 / 1');
+    describe('Summary Tab -', () => {
+      it('Status', () => {
+        appSummary.goToSummaryTab();
+        appSummary.cardStatus.waitForStatus('Incomplete');
+      });
+
+      it('Instances', () => {
+        appSummary.cardInstances.waitForRunningInstancesText('0 / 1');
+      });
+
+      it('App Running', () => {
+        appSummary.cardUptime.waitForTitle('Application is not running');
+      });
+
+      it('Info', () => {
+        expect(appSummary.cardInfo.memQuota.getValue()).toBe('23 MB');
+        expect(appSummary.cardInfo.diskQuota.getValue()).toBe('35 MB');
+        expect(appSummary.cardInfo.appState.getValue()).toBe('STOPPED');
+        expect(appSummary.cardInfo.packageState.getValue()).toBe('PENDING');
+        expect(appSummary.cardInfo.services.getValue()).toBe('0');
+        expect(appSummary.cardInfo.routes.getValue()).toBe('0');
+      });
+
+      it('Cf', () => {
+        const defaultCf = e2e.secrets.getDefaultCFEndpoint();
+
+        expect(appSummary.cardCfInfo.cf.getValue()).toBe(defaultCf.name);
+        expect(appSummary.cardCfInfo.org.getValue()).toBe(defaultCf.testOrg);
+        expect(appSummary.cardCfInfo.space.getValue()).toBe(defaultCf.testSpace);
+      });
+
+      it('Build Info', () => {
+        expect(appSummary.cardBuildInfo.buildPack.getValue()).toBe('-');
+        expect(appSummary.cardBuildInfo.stack.getValue()).toBe(defaultStack);
+      });
+
+      it('Deployment Info', () => {
+        appSummary.cardDeployInfo.waitForTitle('Deployment Info');
+        expect(appSummary.cardDeployInfo.getContent()).toBe('None');
+      });
     });
 
-    it('App Running', () => {
-      appSummary.cardUptime.waitForTitle('Application is not running');
+    describe('Instances Tab -', () => {
+      let appInstances: ApplicationPageInstancesTab;
+
+      beforeAll(() => {
+        appInstances = new ApplicationPageInstancesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
+        appInstances.goToInstancesTab();
+        appInstances.waitForPage();
+      });
+
+      it('Status', () => {
+        appInstances.cardStatus.waitForStatus('Incomplete');
+      });
+
+      it('Instances', () => {
+        appInstances.cardInstances.waitForRunningInstancesText('0 / 1');
+      });
+
+      it('App Running', () => {
+        appInstances.cardUsage.waitForTitle('Application is not running');
+      });
+
+      it('Empty Instances Table', () => {
+        expect(appInstances.list.empty.getDefault().isDisplayed()).toBeTruthy();
+        expect(appInstances.list.empty.getDefault().getComponent().getText()).toBe('There are no application instances');
+      });
+
     });
 
-    it('Info', () => {
-      expect(appSummary.cardInfo.memQuota.getValue()).toBe('23 MB');
-      expect(appSummary.cardInfo.diskQuota.getValue()).toBe('35 MB');
-      expect(appSummary.cardInfo.appState.getValue()).toBe('STOPPED');
-      expect(appSummary.cardInfo.packageState.getValue()).toBe('PENDING');
-      expect(appSummary.cardInfo.services.getValue()).toBe('0');
-      expect(appSummary.cardInfo.routes.getValue()).toBe('0');
+    describe('Routes Tab -', () => {
+      let appRoutes: ApplicationPageRoutesTab;
+
+      beforeAll(() => {
+        appRoutes = new ApplicationPageRoutesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
+        appRoutes.goToRoutesTab();
+        appRoutes.waitForPage();
+      });
+
+      it('Empty Routes Table', () => {
+        expect(appRoutes.list.empty.getDefault().isPresent()).toBeFalsy();
+        expect(appRoutes.list.empty.getDefault().getComponent().isPresent()).toBeFalsy();
+        expect(appRoutes.list.empty.getCustom().getComponent().isDisplayed()).toBeTruthy();
+        expect(appRoutes.list.empty.getCustomLineOne()).toBe('This application has no routes');
+      });
+
+      it('Should be able to cancel from Add Route', () => {
+        expect(appRoutes.list.header.getAdd().isDisplayed()).toBeTruthy();
+        appRoutes.list.header.getAdd().click();
+
+        const addRoutePage = new CreateRoutesPage(CFHelpers.cachedDefaultCfGuid, app.metadata.guid, app.entity.space_guid);
+        expect(addRoutePage.isActivePage()).toBeTruthy();
+        expect(addRoutePage.header.getTitleText()).toBe('Create Route');
+        expect(addRoutePage.type.getSelected().getText()).toBe('Create and map new route');
+
+        expect(addRoutePage.stepper.canCancel()).toBeTruthy();
+        addRoutePage.stepper.cancel();
+
+        // Should return back to App Routes
+        appRoutes.waitForPage();
+      });
     });
 
-    it('Cf', () => {
-      const defaultCf = e2e.secrets.getDefaultCFEndpoint();
+    // Events tab tests should come before anything else adds events
+    describe('Events Tab -', () => {
+      let appEvents: ApplicationPageEventsTab;
 
-      expect(appSummary.cardCfInfo.cf.getValue()).toBe(defaultCf.name);
-      expect(appSummary.cardCfInfo.org.getValue()).toBe(defaultCf.testOrg);
-      expect(appSummary.cardCfInfo.space.getValue()).toBe(defaultCf.testSpace);
+      beforeAll(() => {
+        appEvents = new ApplicationPageEventsTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
+        appEvents.goToEventsTab();
+        appEvents.waitForPage();
+      });
+
+      it('One row in events table', () => {
+        expect(appEvents.list.empty.isDisplayed()).toBeFalsy();
+        expect(appEvents.list.isTableView()).toBeTruthy();
+        expect(appEvents.list.getTotalResults()).toBe(1);
+        expect(appEvents.list.table.getCell(0, 1).getText()).toBe('audit\napp\ncreate');
+        expect(appEvents.list.table.getCell(0, 2).getText()).toBe('person\nadmin');
+      });
+
     });
 
-    it('Build Info', () => {
-      expect(appSummary.cardBuildInfo.buildPack.getValue()).toBe('-');
-      expect(appSummary.cardBuildInfo.stack.getValue()).toBe(defaultStack);
+    describe('Variables Tab -', () => {
+      let appVariables: ApplicationPageVariablesTab;
+
+      beforeAll(() => {
+        appVariables = new ApplicationPageVariablesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
+        appVariables.goToVariablesTab();
+        appVariables.waitForPage();
+      });
+
+      it('Empty Variables Table', () => {
+        expect(appVariables.list.empty.getDefault().isPresent()).toBeTruthy();
+        expect(appVariables.list.empty.getDefault().getComponent().getText()).toBe('There are no variables');
+      });
+
+      it('CRUD Env Var', () => {
+        appVariables.list.empty.waitUntilShown();
+
+        // Add Env Var
+        const envVarName = E2EHelpers.createCustomName('envVar');
+        const envVarValue = 'new env var value';
+        appVariables.addVariable(envVarName, envVarValue);
+
+        expect(appVariables.list.table.getRows().count()).toBe(1);
+        expect(appVariables.list.table.getCell(0, 1).getText()).toBe(envVarName);
+        expect(appVariables.list.table.getCell(0, 2).getText()).toBe(envVarValue);
+
+        // Edit Env Var
+        const envVarValueEdited = `${envVarValue}-edited`;
+        appVariables.editVariable(0, envVarValueEdited);
+        expect(appVariables.list.table.getCell(0, 2).getText()).toBe(envVarValueEdited);
+
+        // Delete Env Var
+        appVariables.deleteVariable(0, envVarName);
+        appVariables.list.empty.waitUntilShown();
+      });
+
     });
-
-    it('Deployment Info', () => {
-      appSummary.cardDeployInfo.waitForTitle('Deployment Info');
-      expect(appSummary.cardDeployInfo.getContent()).toBe('None');
-    });
-  });
-
-  describe('Instances Tab -', () => {
-    let appInstances: ApplicationPageInstancesTab;
-
-    beforeAll(() => {
-      appInstances = new ApplicationPageInstancesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
-      appInstances.navigateTo();
-      appInstances.waitForPage();
-    });
-
-    it('Status', () => {
-      appInstances.cardStatus.waitForStatus('Incomplete');
-    });
-
-    it('Instances', () => {
-      appInstances.cardInstances.waitForRunningInstancesText('0 / 1');
-    });
-
-    it('App Running', () => {
-      appInstances.cardUsage.waitForTitle('Application is not running');
-    });
-
-    it('Empty Instances Table', () => {
-      expect(appInstances.list.empty.getDefault().isDisplayed()).toBeTruthy();
-      expect(appInstances.list.empty.getDefault().getComponent().getText()).toBe('There are no application instances');
-    });
-
-  });
-
-  describe('Routes Tab -', () => {
-    let appRoutes: ApplicationPageRoutesTab;
-
-    beforeAll(() => {
-      appRoutes = new ApplicationPageRoutesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
-      appRoutes.navigateTo();
-      appRoutes.waitForPage();
-    });
-
-    it('Empty Routes Table', () => {
-      expect(appRoutes.list.empty.getDefault().isPresent()).toBeFalsy();
-      expect(appRoutes.list.empty.getDefault().getComponent().isPresent()).toBeFalsy();
-      expect(appRoutes.list.empty.getCustom().getComponent().isDisplayed()).toBeTruthy();
-      expect(appRoutes.list.empty.getCustomLineOne()).toBe('This application has no routes');
-    });
-
-    it('Should be able to cancel from Add Route', () => {
-      expect(appRoutes.list.header.getAdd().isDisplayed()).toBeTruthy();
-      appRoutes.list.header.getAdd().click();
-
-      const addRoutePage = new CreateRoutesPage(CFHelpers.cachedDefaultCfGuid, app.metadata.guid, app.entity.space_guid);
-      expect(addRoutePage.isActivePage()).toBeTruthy();
-      expect(addRoutePage.header.getTitleText()).toBe('Create Route');
-      expect(addRoutePage.type.getSelected().getText()).toBe('Create and map new route');
-
-      expect(addRoutePage.stepper.canCancel()).toBeTruthy();
-      addRoutePage.stepper.cancel();
-
-      // Should return back to App Routes
-      appRoutes.waitForPage();
-    });
-  });
-
-  // Events tab tests should come before anything else adds events
-  describe('Events Tab -', () => {
-    let appEvents: ApplicationPageEventsTab;
-
-    beforeAll(() => {
-      appEvents = new ApplicationPageEventsTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
-      appEvents.navigateTo();
-      appEvents.waitForPage();
-    });
-
-    it('One row in events table', () => {
-      expect(appEvents.list.empty.isDisplayed()).toBeFalsy();
-      expect(appEvents.list.isTableView()).toBeTruthy();
-      expect(appEvents.list.getTotalResults()).toBe(1);
-      expect(appEvents.list.table.getCell(0, 1).getText()).toBe('audit\napp\ncreate');
-      expect(appEvents.list.table.getCell(0, 2).getText()).toBe('person\nadmin');
-    });
-
-  });
-
-  describe('Variables Tab -', () => {
-    let appVariables: ApplicationPageVariablesTab;
-
-    beforeAll(() => {
-      appVariables = new ApplicationPageVariablesTab(CFHelpers.cachedDefaultCfGuid, app.metadata.guid);
-      appVariables.goToVariablesTab();
-      appVariables.waitForPage();
-    });
-
-    it('Empty Variables Table', () => {
-      expect(appVariables.list.empty.getDefault().isPresent()).toBeTruthy();
-      expect(appVariables.list.empty.getDefault().getComponent().getText()).toBe('There are no variables');
-    });
-
-    it('CRUD Env Var', () => {
-      appVariables.list.empty.waitUntilShown();
-
-      // Add Env Var
-      const envVarName = E2EHelpers.createCustomName('envVar');
-      const envVarValue = 'new env var value';
-      appVariables.addVariable(envVarName, envVarValue);
-
-      expect(appVariables.list.table.getRows().count()).toBe(1);
-      expect(appVariables.list.table.getCell(0, 1).getText()).toBe(envVarName);
-      expect(appVariables.list.table.getCell(0, 2).getText()).toBe(envVarValue);
-
-      // Edit Env Var
-      const envVarValueEdited = `${envVarValue}-edited`;
-      appVariables.editVariable(0, envVarValueEdited);
-      expect(appVariables.list.table.getCell(0, 2).getText()).toBe(envVarValueEdited);
-
-      // Delete Env Var
-      appVariables.deleteVariable(0, envVarName);
-      appVariables.list.empty.waitUntilShown();
-    });
-
   });
 
 });
