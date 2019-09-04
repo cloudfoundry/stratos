fd3908c6f7712eaa9dd4abb2c06db93bd147d493
Nathan Jones <njones@suse.com>
2019-09-04 16:16:38 +0100

    Added basic tests for failed entity handler

---
+++ src/frontend/packages/store/src/entity-request-pipeline/entity-request-base-handlers/fail-entity-request.handler.spec.ts
@@ -0,0 +1,61 @@
+import { failedEntityHandler } from './fail-entity-request.handler';
+import { StratosBaseCatalogueEntity } from '../../../../core/src/core/entity-catalogue/entity-catalogue-entity';
+import { EntitySchema } from '../../helpers/entity-schema';
+import { EntityRequestAction } from '../../types/request.types';
+
+describe('failedEntityHandlers', () => {
+  it('Should dispatch actions', () => {
+    const catalogueEntity = new StratosBaseCatalogueEntity({
+      type: 'test',
+      schema: new EntitySchema(
+        'test',
+        'endpoint'
+      ),
+      label: 'Entity',
+      labelPlural: 'Entities',
+    });
+    const spyDispatcher = jasmine.createSpy();
+    failedEntityHandler(
+      spyDispatcher,
+      catalogueEntity,
+      'fetch',
+      {
+        endpointType: 'end',
+        entityType: 'ent',
+        type: 'type',
+      } as EntityRequestAction,
+      {
+        success: false
+      },
+      false
+    );
+    expect(spyDispatcher).toHaveBeenCalledTimes(2);
+  });
+  it('Should dispatch actions', () => {
+    const catalogueEntity = new StratosBaseCatalogueEntity({
+      type: 'test',
+      schema: new EntitySchema(
+        'test',
+        'endpoint'
+      ),
+      label: 'Entity',
+      labelPlural: 'Entities',
+    });
+    const spyDispatcher = jasmine.createSpy();
+    failedEntityHandler(
+      spyDispatcher,
+      catalogueEntity,
+      'fetch',
+      {
+        endpointType: 'end',
+        entityType: 'ent',
+        type: 'type',
+      } as EntityRequestAction,
+      {
+        success: false
+      },
+      true
+    );
+    expect(spyDispatcher).toHaveBeenCalledWith(3);
+  });
+});
