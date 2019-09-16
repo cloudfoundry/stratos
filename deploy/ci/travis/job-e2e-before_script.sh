6f7a2a175680c6c35ac3db66b43298dd5f71bf67
Neil MacDougall <nwmac@users.noreply.github.com>
2019-09-16 18:35:32 +0100

    Fix E2E test failures due to chromedriver version issues (#3877)
    * Bump protractor version
    
    * Run webdriver update manually
    
    * Update dpkg
    
    * Update to newer distribution
    
    * Remove apt update of dpkg
    
    * Fix repository name for newer dist
    
    * Ensuer we use the correct version of Chrome when updating webdriver
    
    * Go back to protractor 5

--- deploy/ci/travis/job-e2e-before_script.sh
+++ deploy/ci/travis/job-e2e-before_script.sh
@@ -7,7 +7,9 @@ DIRPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../.. && pwd)"
 
 chmod +x ${DIRPATH}/deploy/ci/travis/run-e2e-tests.sh
 # We will install ffmpeg so we can capture a video of the display as the tests run
-sudo add-apt-repository -y ppa:mc3man/trusty-media
+# sudo apt-get update
+# sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:mc3man/bionic-media
 sudo apt-get -qq update
 
 if [ "${MAILCATCHER}" == "true" ]; then
