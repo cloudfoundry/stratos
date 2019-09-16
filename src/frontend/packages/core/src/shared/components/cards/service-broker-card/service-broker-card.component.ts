805b6b3f18f3a80d2fbc753c69efb47dc9ebb0ff
Richard Cox <richard.cox@suse.com>
2019-09-16 16:22:01 +0100

    Fix several service related bugs, some related to space scoped services - Tested using https://github.com/mattmcneeney/overview-broker - Fix service instance name when `extra` data does not contain display name - Fix some observable based bugs - Fix issue where space scoped services could not be created - Fix space scoped indicator in service summary service broker card - Fixed display of service plans in create space scoped service instance - Improved json schema form spacing

--- src/frontend/packages/core/src/shared/components/cards/service-broker-card/service-broker-card.component.ts
+++ src/frontend/packages/core/src/shared/components/cards/service-broker-card/service-broker-card.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { filter, map, switchMap, take, tap } from 'rxjs/operators';
+import { filter, first, map, switchMap, take } from 'rxjs/operators';
 
+import { GetSpace } from '../../../../../../store/src/actions/space.actions';
+import { entityFactory, spaceSchemaKey, spaceWithOrgKey } from '../../../../../../store/src/helpers/entity-factory';
+import { APIResource } from '../../../../../../store/src/types/api.types';
 import { IServiceBroker } from '../../../../core/cf-api-svc.types';
 import { ISpace } from '../../../../core/cf-api.types';
 import { EntityServiceFactory } from '../../../../core/entity-service-factory.service';
 import { ServicesService } from '../../../../features/service-catalog/services.service';
-import { APIResource } from '../../../../../../store/src/types/api.types';
-import { AppState } from '../../../../../../store/src/app-state';
-import { spaceSchemaKey, entityFactory, spaceWithOrgKey } from '../../../../../../store/src/helpers/entity-factory';
-import { GetSpace } from '../../../../../../store/src/actions/space.actions';
 
 @Component({
   selector: 'app-service-broker-card',
@@ -23,11 +21,10 @@ export class ServiceBrokerCardComponent {
   spaceLink: string[];
   serviceBroker$: Observable<APIResource<IServiceBroker>>;
   constructor(
-    private servicesService: ServicesService,
-    private store: Store<AppState>,
-    private entityServiceFactory: EntityServiceFactory
+    servicesService: ServicesService,
+    entityServiceFactory: EntityServiceFactory
   ) {
-    this.serviceBroker$ = this.servicesService.serviceBroker$;
+    this.serviceBroker$ = servicesService.serviceBroker$;
 
     this.serviceBroker$.pipe(
       filter(o => !!o),
@@ -36,25 +33,25 @@ export class ServiceBrokerCardComponent {
       filter(o => !!o),
       // Broker is space scoped
       switchMap(spaceGuid => {
-        const spaceService = this.entityServiceFactory.create<APIResource<ISpace>>(spaceSchemaKey,
+        const spaceService = entityServiceFactory.create<APIResource<ISpace>>(spaceSchemaKey,
           entityFactory(spaceWithOrgKey),
           spaceGuid,
-          new GetSpace(spaceGuid, this.servicesService.cfGuid),
+          new GetSpace(spaceGuid, servicesService.cfGuid),
           true
         );
         return spaceService.waitForEntity$;
       }),
-      tap(space => {
-        this.spaceLink = ['/cloud-foundry',
-          this.servicesService.cfGuid,
-          'organizations',
-          space.entity.entity.organization_guid,
-          'spaces',
-          space.entity.metadata.guid,
-          'summary'
-        ];
-        this.spaceName = space.entity.entity.name;
-      })
-    ).subscribe();
+      first()
+    ).subscribe(space => {
+      this.spaceLink = ['/cloud-foundry',
+        servicesService.cfGuid,
+        'organizations',
+        space.entity.entity.organization_guid,
+        'spaces',
+        space.entity.metadata.guid,
+        'summary'
+      ];
+      this.spaceName = space.entity.entity.name;
+    });
   }
 }
