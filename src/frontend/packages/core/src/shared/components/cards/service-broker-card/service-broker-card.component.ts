306ecb95a3203b270c1fd495fbcfa656be3add82
Richard Cox <richard.cox@suse.com>
2019-09-17 17:09:27 +0100

    Fix unit tests

--- src/frontend/packages/core/src/shared/components/cards/service-broker-card/service-broker-card.component.ts
+++ src/frontend/packages/core/src/shared/components/cards/service-broker-card/service-broker-card.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter, first, map, switchMap, take } from 'rxjs/operators';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { filter, map, switchMap, take, tap } from 'rxjs/operators';
 
 import { GetSpace } from '../../../../../../store/src/actions/space.actions';
 import { entityFactory, spaceSchemaKey, spaceWithOrgKey } from '../../../../../../store/src/helpers/entity-factory';
@@ -8,6 +8,7 @@ import { APIResource } from '../../../../../../store/src/types/api.types';
 import { IServiceBroker } from '../../../../core/cf-api-svc.types';
 import { ISpace } from '../../../../core/cf-api.types';
 import { EntityServiceFactory } from '../../../../core/entity-service-factory.service';
+import { safeUnsubscribe } from '../../../../core/utils.service';
 import { ServicesService } from '../../../../features/service-catalog/services.service';
 
 @Component({
@@ -15,43 +16,48 @@ import { ServicesService } from '../../../../features/service-catalog/services.s
   templateUrl: './service-broker-card.component.html',
   styleUrls: ['./service-broker-card.component.scss']
 })
-export class ServiceBrokerCardComponent {
+export class ServiceBrokerCardComponent implements OnDestroy {
 
   spaceName: string;
   spaceLink: string[];
   serviceBroker$: Observable<APIResource<IServiceBroker>>;
+  subs: Subscription[] = [];
+
   constructor(
     servicesService: ServicesService,
     entityServiceFactory: EntityServiceFactory
   ) {
     this.serviceBroker$ = servicesService.serviceBroker$;
 
-    this.serviceBroker$.pipe(
+    this.subs.push(this.serviceBroker$.pipe(
       filter(o => !!o),
       map(o => o.entity.space_guid),
       take(1),
       filter(o => !!o),
       // Broker is space scoped
-      switchMap(spaceGuid => {
-        const spaceService = entityServiceFactory.create<APIResource<ISpace>>(spaceSchemaKey,
+      switchMap(spaceGuid =>
+        entityServiceFactory.create<APIResource<ISpace>>(spaceSchemaKey,
           entityFactory(spaceWithOrgKey),
           spaceGuid,
           new GetSpace(spaceGuid, servicesService.cfGuid),
           true
-        );
-        return spaceService.waitForEntity$;
-      }),
-      first()
-    ).subscribe(space => {
-      this.spaceLink = ['/cloud-foundry',
-        servicesService.cfGuid,
-        'organizations',
-        space.entity.entity.organization_guid,
-        'spaces',
-        space.entity.metadata.guid,
-        'summary'
-      ];
-      this.spaceName = space.entity.entity.name;
-    });
+        ).waitForEntity$
+      ),
+      tap(space => {
+        this.spaceLink = ['/cloud-foundry',
+          servicesService.cfGuid,
+          'organizations',
+          space.entity.entity.organization_guid,
+          'spaces',
+          space.entity.metadata.guid,
+          'summary'
+        ];
+        this.spaceName = space.entity.entity.name;
+      })
+    ).subscribe());
+  }
+
+  ngOnDestroy() {
+    safeUnsubscribe(...this.subs);
   }
 }
