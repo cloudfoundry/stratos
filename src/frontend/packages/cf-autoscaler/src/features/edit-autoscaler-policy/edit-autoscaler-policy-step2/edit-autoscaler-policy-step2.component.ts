4c4dda82083e7314b96718cc34b6b29a1a85a7cd
Richard Cox <ricox@suse.com>
2019-09-19 13:30:25 +0100

    Fix sub leak - fixes #3881

--- src/frontend/packages/cf-autoscaler/src/features/edit-autoscaler-policy/edit-autoscaler-policy-step2/edit-autoscaler-policy-step2.component.ts
+++ src/frontend/packages/cf-autoscaler/src/features/edit-autoscaler-policy/edit-autoscaler-policy-step2/edit-autoscaler-policy-step2.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { safeUnsubscribe } from '../../../../../core/src/core/utils.service';
 import { ApplicationService } from '../../../../../core/src/features/applications/application.service';
 import {
   AutoscalerConstants,
@@ -29,7 +30,7 @@ import { EditAutoscalerPolicyService } from '../edit-autoscaler-policy-service';
     { provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }
   ]
 })
-export class EditAutoscalerPolicyStep2Component extends EditAutoscalerPolicy implements OnInit {
+export class EditAutoscalerPolicyStep2Component extends EditAutoscalerPolicy implements OnInit, OnDestroy {
 
   policyAlert = PolicyAlert;
   metricTypes = AutoscalerConstants.MetricTypes;
@@ -45,6 +46,7 @@ export class EditAutoscalerPolicyStep2Component extends EditAutoscalerPolicy imp
   private editMetricType = '';
   private editScaleType = 'upper';
   private editAdjustmentType = 'value';
+  private subs: Subscription[] = [];
 
   constructor(
     public applicationService: ApplicationService,
@@ -71,10 +73,9 @@ export class EditAutoscalerPolicyStep2Component extends EditAutoscalerPolicy imp
 
     this.metricUnit$ = this.metricUnitSubject.asObservable();
 
-    this.editTriggerForm.get('metric_type').valueChanges.pipe(
+    this.subs.push(this.editTriggerForm.get('metric_type').valueChanges.pipe(
       map(value => this.getMetricUnit(value)),
-      tap(unit => this.metricUnitSubject.next(unit))
-    ).subscribe();
+    ).subscribe(unit => this.metricUnitSubject.next(unit)));
   }
 
   addTrigger = () => {
@@ -197,5 +198,9 @@ export class EditAutoscalerPolicyStep2Component extends EditAutoscalerPolicy imp
 
   getMetricUnit(metricType: string) {
     return AutoscalerConstants.getMetricUnit(metricType);
+  }
+
+  ngOnDestroy() {
+    safeUnsubscribe(...this.subs);
   }
 }
