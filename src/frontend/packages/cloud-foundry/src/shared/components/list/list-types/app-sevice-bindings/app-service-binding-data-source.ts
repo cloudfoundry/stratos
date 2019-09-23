0659b5a5be9c2d55992fa47fdca3251b5df5e7c0
Richard Cox <richard.cox@suse.com>
2019-09-23 17:45:56 +0100

    Fix two actionBuilder params issues - Fix three places where `includeRelations` were passed incorrectly to actionBuilder - Fix bind service

--- src/frontend/packages/cloud-foundry/src/shared/components/list/list-types/app-sevice-bindings/app-service-binding-data-source.ts
+++ src/frontend/packages/cloud-foundry/src/shared/components/list/list-types/app-sevice-bindings/app-service-binding-data-source.ts
@@ -1,6 +1,5 @@
 import { Store } from '@ngrx/store';
 
-import { GetAppServiceBindings } from '../../../../../../../cloud-foundry/src/actions/application-service-routes.actions';
 import { CFAppState } from '../../../../../../../cloud-foundry/src/cf-app-state';
 import {
   applicationEntityType,
@@ -10,21 +9,21 @@ import {
   serviceInstancesEntityType,
   servicePlanEntityType,
 } from '../../../../../../../cloud-foundry/src/cf-entity-factory';
-import { ApplicationService } from '../../../../../../../cloud-foundry/src/features/applications/application.service';
-import { getRowMetadata } from '../../../../../../../cloud-foundry/src/features/cloud-foundry/cf.helpers';
-import { IServiceBinding } from '../../../../../../../core/src/core/cf-api-svc.types';
-import {
-  ListDataSource,
-} from '../../../../../../../core/src/shared/components/list/data-sources-controllers/list-data-source';
-import { IListConfig } from '../../../../../../../core/src/shared/components/list/list.component.types';
 import {
   createEntityRelationKey,
   createEntityRelationPaginationKey,
 } from '../../../../../../../cloud-foundry/src/entity-relations/entity-relations.types';
-import { APIResource } from '../../../../../../../store/src/types/api.types';
+import { ApplicationService } from '../../../../../../../cloud-foundry/src/features/applications/application.service';
+import { getRowMetadata } from '../../../../../../../cloud-foundry/src/features/cloud-foundry/cf.helpers';
+import { IServiceBinding } from '../../../../../../../core/src/core/cf-api-svc.types';
 import { entityCatalogue } from '../../../../../../../core/src/core/entity-catalogue/entity-catalogue.service';
-import { CF_ENDPOINT_TYPE } from '../../../../../../cf-types';
+import {
+  ListDataSource,
+} from '../../../../../../../core/src/shared/components/list/data-sources-controllers/list-data-source';
+import { IListConfig } from '../../../../../../../core/src/shared/components/list/list.component.types';
+import { APIResource } from '../../../../../../../store/src/types/api.types';
 import { PaginatedAction } from '../../../../../../../store/src/types/pagination.types';
+import { CF_ENDPOINT_TYPE } from '../../../../../../cf-types';
 
 export class AppServiceBindingDataSource extends ListDataSource<APIResource<IServiceBinding>> {
   static createGetAllServiceBindings(appGuid: string, cfGuid: string) {
@@ -32,12 +31,15 @@ export class AppServiceBindingDataSource extends ListDataSource<APIResource<ISer
     const serviceBindingEntity = entityCatalogue.getEntity(CF_ENDPOINT_TYPE, serviceBindingEntityType);
     const actionBuilder = serviceBindingEntity.actionOrchestrator.getActionBuilder('getAllForApplication');
     const getAppServiceBindingsAction = actionBuilder(
-      appGuid, cfGuid, paginationKey, [
-        createEntityRelationKey(serviceInstancesEntityType, servicePlanEntityType),
-        createEntityRelationKey(serviceInstancesEntityType, serviceEntityType),
-        createEntityRelationKey(serviceBindingEntityType, applicationEntityType),
-        createEntityRelationKey(serviceBindingEntityType, serviceInstancesEntityType),
-      ]) as PaginatedAction;
+      appGuid, cfGuid, paginationKey, {
+        includeRelations: [
+          createEntityRelationKey(serviceInstancesEntityType, servicePlanEntityType),
+          createEntityRelationKey(serviceInstancesEntityType, serviceEntityType),
+          createEntityRelationKey(serviceBindingEntityType, applicationEntityType),
+          createEntityRelationKey(serviceBindingEntityType, serviceInstancesEntityType),
+        ],
+        populateMissing: true
+      }) as PaginatedAction;
     return getAppServiceBindingsAction;
   }
 
--- src/frontend/packages/cloud-foundry/src/shared/components/list/list-types/cf-spaces-service-instances/cf-spaces-service-instances-data-source.ts
+++ src/frontend/packages/cloud-foundry/src/shared/components/list/list-types/cf-spaces-service-instances/cf-spaces-service-instances-data-source.ts
@@ -1,5 +1,6 @@
 import { Store } from '@ngrx/store';
 
+import { CF_ENDPOINT_TYPE } from '../../../../../../../cloud-foundry/cf-types';
 import { CFAppState } from '../../../../../../../cloud-foundry/src/cf-app-state';
 import {
   applicationEntityType,
@@ -12,32 +13,33 @@ import {
   spaceEntityType,
 } from '../../../../../../../cloud-foundry/src/cf-entity-factory';
 import {
-  ListDataSource,
-} from '../../../../../../../core/src/shared/components/list/data-sources-controllers/list-data-source';
-import { IListConfig } from '../../../../../../../core/src/shared/components/list/list.component.types';
-import {
   createEntityRelationKey,
   createEntityRelationPaginationKey,
 } from '../../../../../../../cloud-foundry/src/entity-relations/entity-relations.types';
-import { APIResource } from '../../../../../../../store/src/types/api.types';
-import { getRowMetadata } from '../../../../../features/cloud-foundry/cf.helpers';
-
-import { CF_ENDPOINT_TYPE } from '../../../../../../../cloud-foundry/cf-types';
-import { PaginatedAction } from '../../../../../../../store/src/types/pagination.types';
 import { entityCatalogue } from '../../../../../../../core/src/core/entity-catalogue/entity-catalogue.service';
+import {
+  ListDataSource,
+} from '../../../../../../../core/src/shared/components/list/data-sources-controllers/list-data-source';
+import { IListConfig } from '../../../../../../../core/src/shared/components/list/list.component.types';
+import { APIResource } from '../../../../../../../store/src/types/api.types';
+import { PaginatedAction } from '../../../../../../../store/src/types/pagination.types';
+import { getRowMetadata } from '../../../../../features/cloud-foundry/cf.helpers';
 
 export class CfSpacesServiceInstancesDataSource extends ListDataSource<APIResource> {
   constructor(cfGuid: string, spaceGuid: string, store: Store<CFAppState>, listConfig?: IListConfig<APIResource>) {
     const paginationKey = createEntityRelationPaginationKey(spaceEntityType, spaceGuid);
     const serviceInstanceEntity = entityCatalogue.getEntity(CF_ENDPOINT_TYPE, serviceInstancesEntityType);
     const actionBuilder = serviceInstanceEntity.actionOrchestrator.getActionBuilder('getAllInSpace');
-    const action = actionBuilder(spaceGuid, cfGuid, paginationKey, null, [
-      createEntityRelationKey(serviceInstancesEntityType, serviceBindingEntityType),
-      createEntityRelationKey(serviceInstancesEntityType, serviceEntityType),
-      createEntityRelationKey(serviceInstancesEntityType, servicePlanEntityType),
-      createEntityRelationKey(serviceInstancesEntityType, spaceEntityType),
-      createEntityRelationKey(serviceBindingEntityType, applicationEntityType),
-    ], true, false) as PaginatedAction;
+    const action = actionBuilder(spaceGuid, cfGuid, paginationKey, null, {
+      includeRelations: [
+        createEntityRelationKey(serviceInstancesEntityType, serviceBindingEntityType),
+        createEntityRelationKey(serviceInstancesEntityType, serviceEntityType),
+        createEntityRelationKey(serviceInstancesEntityType, servicePlanEntityType),
+        createEntityRelationKey(serviceInstancesEntityType, spaceEntityType),
+        createEntityRelationKey(serviceBindingEntityType, applicationEntityType),
+      ],
+      populateMissing: true
+    }) as PaginatedAction;
     super({
       store,
       action,
