a9d354ae1343975bd02d8d31c5ac1b5fd27092c3
Neil MacDougall <neil.macdougall@suse.com>
2019-10-01 11:50:03 +0100

    Fix for local user update profile

---
+++ build/tools/mysqldb-dev.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+echo "Startind MariaDB database for development"
+
+STRATOS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd)"
+echo $STRATOS_PATH
+
+docker stop stratos-db
+docker rm stratos-db
+
+ID=$(docker run --name stratos-db -d -e MYSQL_ROOT_PASSWORD=dbroot -p 3306:3306 splatform/stratos-mariadb)
+echo $ID
+
+rm -f dbsetup.sql init.sh
+cat <<EOF > dbsetup.sql
+CREATE DATABASE stratosdb;
+CREATE USER stratos IDENTIFIED BY 'strat0s';
+GRANT ALL PRIVILEGES ON stratosdb.* to 'stratos'@'%';
+EOF
+
+cat <<EOF > init.sh
+#!/usr/bin/env bash
+mysql -uroot -pdbroot < /dbsetup.sql
+EOF
+
+chmod +x init.sh
+docker cp ./dbsetup.sql ${ID}:/dbsetup.sql
+docker cp ./init.sh ${ID}:/init.sh
+rm dbsetup.sql init.sh
+
+echo "Just waiting a few seconds for the DB to come online ..."
+sleep 20
+
+docker exec -t ${ID} /init.sh
+
+echo "Database ready"
+
+mkdir -p ${STRATOS_PATH}/src/jetstream/db
+cp ${STRATOS_PATH}/deploy/db/dbconf.yml ${STRATOS_PATH}/src/jetstream/db
+
+if [ -f ${STRATOS_PATH}/src/jetstream/jetstream ]; then
+  ${STRATOS_PATH}/src/jetstream/jetstream --env=mariadb-dev up
+else
+  echo "Build the Stratos backend and run db migrations with:"
+  echo "  ./jetstream --env=mariadb-local up"
+fi
