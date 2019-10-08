99d4062c6ee0a4f19f4f435be47ec7e4f705e1ff
Richard Cox <richard.cox@suse.com>
2019-10-08 16:47:35 +0100

    Update migration name to avoid clashes, add PK to config table

--- src/jetstream/datastore/20191008121900_PrimaryKeys.go
+++ src/jetstream/datastore/20191008121900_PrimaryKeys.go
@@ -8,7 +8,7 @@ import (
 )
 
 func init() {
-	RegisterMigration(20190614121900, "PrimaryKeys", func(txn *sql.Tx, conf *goose.DBConf) error {
+	RegisterMigration(20191008121900, "PrimaryKeys", func(txn *sql.Tx, conf *goose.DBConf) error {
 
 		// Note: SQLite does not allow constraints to be added after table creation
 		if strings.Contains(conf.Driver.Name, "sqlite3") {
@@ -23,6 +23,12 @@ func init() {
 
 		addSetupConfigPrimaryKey := "ALTER TABLE console_config ADD CONSTRAINT PK_ConsoleConfig PRIMARY KEY (uaa_endpoint, console_admin_scope);"
 		_, err = txn.Exec(addSetupConfigPrimaryKey)
+		if err != nil {
+			return err
+		}
+
+		addConfigPrimaryKey := "ALTER TABLE config ADD CONSTRAINT PK_Config PRIMARY KEY (groupName, name);"
+		_, err = txn.Exec(addConfigPrimaryKey)
 		if err != nil {
 			return err
 		}
