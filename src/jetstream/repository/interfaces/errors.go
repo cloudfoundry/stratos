7b7b5064384f3963a263c96c219d19d02423da58
Neil MacDougall <neil.macdougall@suse.com>
2019-10-30 15:58:18 +0000

    Fix formatting of http error

--- src/jetstream/repository/interfaces/errors.go
+++ src/jetstream/repository/interfaces/errors.go
@@ -25,20 +25,20 @@ func (e ErrHTTPShadow) Error() string {
 }
 
 func NewHTTPError(status int, userFacingError string) error {
-	return NewHTTPShadowError(status, userFacingError, "", true)
+	return NewHTTPShadowError(status, userFacingError, "")
 }
 
 func NewHTTPShadowError(status int, userFacingError string, fmtString string, args ...interface{}) error {
 	//Only set the HTTPError field of the ErrHTTPShadow struct if we have been passed an error message intended for logging
 	httpErrorMsg := ""
-	if len(fmtString) > 0 {
+	if len(userFacingError) > 0 {
 		httpErrorMsg = fmt.Sprintf(`{"error":%q}`, userFacingError)
 	}
 	shadowError := ErrHTTPShadow{
 		UserFacingError: userFacingError,
 		HTTPError:       echo.NewHTTPError(status, httpErrorMsg),
 	}
-	if len(fmtString) > 0 {
+	if args != nil {
 		shadowError.LogMessage = fmt.Sprintf(fmtString, args...)
 	}
 	return shadowError
