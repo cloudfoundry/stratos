bef8c1f6480330485258928c80f958302b5e186c
Neil MacDougall <neil.macdougall@suse.com>
2019-12-02 13:38:50 +0000

    Fix front end unit tests

--- src/frontend/packages/core/src/shared/components/tile-selector-tile/tile-selector-tile.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/tile-selector-tile/tile-selector-tile.component.spec.ts
@@ -1,20 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TileSelectorTileComponent } from './tile-selector-tile.component';
+import { ITileImgConfig } from '../tile/tile-selector.types';
+import { MDAppModule } from '../../../core/md.module';
 
 describe('TileSelectorTileComponent', () => {
-  let component: TileSelectorTileComponent;
-  let fixture: ComponentFixture<TileSelectorTileComponent>;
+  let component: TileSelectorTileComponent<ITileImgConfig>;
+  let fixture: ComponentFixture<TileSelectorTileComponent<ITileImgConfig>>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MDAppModule
+      ],
       declarations: [ TileSelectorTileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TileSelectorTileComponent);
+    fixture = TestBed.createComponent(TileSelectorTileComponent) as ComponentFixture<TileSelectorTileComponent<ITileImgConfig>>;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
