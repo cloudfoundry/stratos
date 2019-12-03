d23158e6606fe92d6c7205cf46a8ea7f22491cfa
Neil MacDougall <neil.macdougall@suse.com>
2019-12-03 11:45:44 +0000

    Fix tokens table primary key

--- src/jetstream/datastore/20191008121900_PrimaryKeys.go
+++ src/jetstream/datastore/20191008121900_PrimaryKeys.go
@@ -10,12 +10,20 @@ import (
 func init() {
 	RegisterMigration(20191008121900, "PrimaryKeys", func(txn *sql.Tx, conf *goose.DBConf) error {
 
+		// Make sure all tokens have a CNSI guid
+		ensureTokensHaveCnsi := "UPDATE tokens SET cnsi_guid='STRATOS' WHERE token_type='uaa' and cnsi_guid IS NULL;"
+		_, err := txn.Exec(ensureTokensHaveCnsi)
+		if err != nil {
+			return err
+		}
+
 		// Note: SQLite does not allow constraints to be added after table creation
 		if strings.Contains(conf.Driver.Name, "sqlite3") {
 			return nil
 		}
 
-		addTokensPrimaryKey := "ALTER TABLE tokens ADD CONSTRAINT PK_Tokens PRIMARY KEY (user_guid, token_guid);"
+		// Need cnsi_guid to not be NULL in order to be able to create this primary key
+		addTokensPrimaryKey := "ALTER TABLE tokens ADD CONSTRAINT PK_Tokens PRIMARY KEY (user_guid, cnsi_guid, token_guid);"
 		_, err := txn.Exec(addTokensPrimaryKey)
 		if err != nil {
 			return err
