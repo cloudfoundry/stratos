18e6388fd20d03be3232e77ddbd7c4da006e53e5
Neil MacDougall <neil.macdougall@suse.com>
2019-12-11 10:14:52 +0000

    Fix issue with folder not existing

--- build/clean-test-reports.sh
+++ build/clean-test-reports.sh
@@ -6,3 +6,6 @@ rm -f "${STRATOS_PATH}/coverage/stratos-unittest-*.txt"
 rm -f "${STRATOS_PATH}/coverage/stratos-unittests.json"
 rm -f "${STRATOS_PATH}/coverage/stratos-unittests.txt"
 rm -f "${STRATOS_PATH}/coverage/stratos-exitcode.txt"
+
+# Create the coverage folder if needed
+mkdir -p "${STRATOS_PATH}/coverage"
