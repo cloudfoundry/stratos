f876328e6573cf1464005747e51562e77b5316aa
Neil MacDougall <neil.macdougall@suse.com>
2019-12-11 11:26:26 +0000

    Fix test report

--- build/show-test-reports.sh
+++ build/show-test-reports.sh
@@ -2,8 +2,20 @@
 
 STRATOS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 
+# Colours
+CYAN="\033[96m"
+YELLOW="\033[93m"
+RESET="\033[0m"
+BOLD="\033[1m"
+
+echo ""
+echo -e "${YELLOW}${BOLD}==========================================================================================="
+echo -e "== ${CYAN}STRATOS FRONT-END UNIT TEST RESULTS ${YELLOW}                                                  =="
+echo -e "===========================================================================================${RESET}"
+echo ""
+
 # Cat all of the files
-for f in "${STRATOS_PATH}/coverage/stratos-unittest-*.txt"
+for f in ${STRATOS_PATH}/coverage/stratos-unittest-*.txt
 do
   cat "${f}"
   echo ""
@@ -12,7 +24,7 @@ done
 cat "${STRATOS_PATH}/coverage/stratos-unittests.txt"
 echo ""
 
-if [ ! -f " ${STRATOS_PATH}/coverage/stratos-exitcode.txt" ]; then
+if [ ! -f "${STRATOS_PATH}/coverage/stratos-exitcode.txt" ]; then
   echo "ERORR: Exit code file does not exist"
   exit 1
 fi
