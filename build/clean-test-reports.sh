c7a0461d9520c07b30bb7e857dbb74b0b710e268
Neil MacDougall <neil.macdougall@suse.com>
2019-12-11 09:02:08 +0000

    Make paths more resilient to cwd

--- build/clean-test-reports.sh
+++ build/clean-test-reports.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-rm -f ./coverage/stratos-unittest-*.txt
-rm -f ./coverage/stratos-unittests.json
-rm -f ./coverage/stratos-unittests.txt
-rm -f ./coverage/stratos-exitcode.txt
+STRATOS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+rm -f "${STRATOS_PATH}/coverage/stratos-unittest-*.txt"
+rm -f "${STRATOS_PATH}/coverage/stratos-unittests.json"
+rm -f "${STRATOS_PATH}/coverage/stratos-unittests.txt"
+rm -f "${STRATOS_PATH}/coverage/stratos-exitcode.txt"
--- build/show-test-reports.sh
+++ build/show-test-reports.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+STRATOS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
 # Cat all of the files
-for f in ./coverage/stratos-unittest-*.txt
+for f in "${STRATOS_PATH}/coverage/stratos-unittest-*.txt"
 do
   cat "${f}"
   echo ""
 done
 
-cat "./coverage/stratos-unittests.txt"
+cat "${STRATOS_PATH}/coverage/stratos-unittests.txt"
 echo ""
 
-exitCode=$(cat ./coverage/stratos-exitcode.txt)
+exitCode=$(cat ${STRATOS_PATH}/coverage/stratos-exitcode.txt)
 echo "Exiting with exit code ${exitCode}"
 exit ${exitCode}
