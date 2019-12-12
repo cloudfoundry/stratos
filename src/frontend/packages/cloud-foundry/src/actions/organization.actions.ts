4432203af5d8c468891d6245b4cd8aa23b55b2eb
Richard Cox <richard.cox@suse.com>
2019-12-12 10:59:09 +0000

    Move createDefaultUserRelations to core file

--- src/frontend/packages/cloud-foundry/src/actions/organization.actions.ts
+++ src/frontend/packages/cloud-foundry/src/actions/organization.actions.ts
@@ -1,3 +1,5 @@
+import { HttpHeaders, HttpRequest } from '@angular/common/http';
+
 import { IUpdateOrganization } from '../../../core/src/core/cf-api.types';
 import { getActions } from '../../../store/src/actions/action.helper';
 import { PaginatedAction } from '../../../store/src/types/pagination.types';
@@ -17,8 +19,7 @@ import {
   EntityInlineParentAction,
 } from '../entity-relations/entity-relations.types';
 import { CFStartAction } from './cf-action.types';
-import { createDefaultUserRelations } from './user.actions.helpers';
-import { HttpRequest, HttpHeaders } from '@angular/common/http';
+import { createDefaultUserRelations } from './users.actions';
 
 export const GET_ORGANIZATION = '[Organization] Get one';
 export const GET_ORGANIZATION_SUCCESS = '[Organization] Get one success';
