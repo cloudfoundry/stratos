9d1751be30aea8da2e45e34ccaf78c6b19e7b141
Richard Cox <richard-cox@users.noreply.github.com>
2019-12-13 11:24:54 +0000

    Improvements to error/notification process (#4053)
    * Improvements to error/notification process
    - Show mini error message, as per create, when updating service instance
    - Handle all 5XX status codes as errors
    - Update messages to reflect varience of error states
    - Show single popover message on error dismiss all button
    - Correct error page selector
    
    * Treat calls to multiple endpoints as `global`
    - calls where we generate the endpoint lists (app wall, etc) were handled badly by jetstreamErrorHandler
    - this was due to the internal event storing endpoint id as `` instead of one per endpoint
    - this happened due to the list of generated endpoints being applied directly to request object in lower function
    - treat these as global for the moment

--- src/frontend/packages/store/src/entity-request-pipeline/entity-request-base-handlers/jetstream-error.handler.ts
+++ src/frontend/packages/store/src/entity-request-pipeline/entity-request-base-handlers/jetstream-error.handler.ts
@@ -3,7 +3,7 @@ import { SendEventAction } from '../../actions/internal-events.actions';
 import { RecursiveDeleteFailed } from '../../effects/recursive-entity-delete.effect';
 import { endpointSchemaKey } from '../../helpers/entity-factory';
 import { ApiRequestTypes, getFailApiRequestActions } from '../../reducers/api-request-reducer/request-helpers';
-import { InternalEventSeverity, InternalEventStateMetadata } from '../../types/internal-events.types';
+import { GLOBAL_EVENT, InternalEventSeverity, InternalEventStateMetadata } from '../../types/internal-events.types';
 import { EntityRequestAction } from '../../types/request.types';
 import { ActionDispatcher } from '../entity-request-pipeline.types';
 import { PipelineHttpClient } from '../pipline-http-client.service';
@@ -17,11 +17,30 @@ export function jetstreamErrorHandler(
   actionDispatcher: ActionDispatcher,
   recursivelyDeleting: boolean
 ) {
-  const endpointString = action.options.headers ? action.options.headers.get(PipelineHttpClient.EndpointHeader) || '' : '';
-  const endpointIds: string[] = endpointString.split(',');
-  endpointIds.forEach(endpoint =>
+  // This will never work for calls where endpoint list is automatically generated (list is applied to request object not action)
+  // For those cases treat as a global error
+  const endpointString = action.options.headers ? action.options.headers.get(PipelineHttpClient.EndpointHeader) || null : null;
+  const endpointIds: string[] = endpointString ? endpointString.split(',') : [];
+
+  if (endpointString) {
+    endpointIds.forEach(endpoint =>
+      actionDispatcher(
+        new SendEventAction<InternalEventStateMetadata>(endpointSchemaKey, endpoint, {
+          eventCode: error.status ? error.status + '' : '500',
+          severity: InternalEventSeverity.ERROR,
+          message: 'Jetstream API request error',
+          metadata: {
+            httpMethod: action.options.method as string,
+            errorResponse: error,
+            url: error.url || action.options.url,
+          },
+        }),
+      ),
+    );
+  } else {
+    // See #4054, in theory we should never hit this as we always know the endpoint id's
     actionDispatcher(
-      new SendEventAction<InternalEventStateMetadata>(endpointSchemaKey, endpoint, {
+      new SendEventAction<InternalEventStateMetadata>(GLOBAL_EVENT, catalogueEntity.entityKey, {
         eventCode: error.status ? error.status + '' : '500',
         severity: InternalEventSeverity.ERROR,
         message: 'Jetstream API request error',
@@ -31,8 +50,9 @@ export function jetstreamErrorHandler(
           url: error.url || action.options.url,
         },
       }),
-    ),
-  );
+    );
+  }
+
   const errorActions = getFailApiRequestActions(action, error, requestType, catalogueEntity, {
     endpointIds,
     url: error.url || action.options.url,
