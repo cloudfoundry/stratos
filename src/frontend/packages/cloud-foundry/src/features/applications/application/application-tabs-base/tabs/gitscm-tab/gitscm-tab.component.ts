d9e7fc53e0de6a6d3ad30f0feecac6ada06e64d5
Richard Cox <richard-cox@users.noreply.github.com>
2019-12-13 11:30:11 +0000

    Blitz - User Service & Deploy from git fixes (#4062)
    * Move createDefaultUserRelations to core file
    
    * Fix user service
    - ensure action builder constructors are correct (were fetching users for all endpoints instead of contextual one)
    - removed duplicate cf-user-service
    
    * Fix super sluggish behaviour when deploying from github
    - most of the git actions included the scm object
    - scm objects included httpClient
    - this lead to very large actions being dispatched
    - fix is to supply httpClient when needed
    
    * Fix getMatchingRepositories

--- src/frontend/packages/cloud-foundry/src/features/applications/application/application-tabs-base/tabs/gitscm-tab/gitscm-tab.component.ts
+++ src/frontend/packages/cloud-foundry/src/features/applications/application/application-tabs-base/tabs/gitscm-tab/gitscm-tab.component.ts
@@ -5,7 +5,6 @@ import { Store } from '@ngrx/store';
 import { Observable, of as observableOf, Subscription } from 'rxjs';
 import { distinctUntilChanged, filter, map, take, tap } from 'rxjs/operators';
 
-import { FetchBranchesForProject } from '../../../../../../../../cloud-foundry/src/actions/deploy-applications.actions';
 import { GitCommit, GitRepo } from '../../../../../../../../cloud-foundry/src/store/types/git.types';
 import { entityCatalogue } from '../../../../../../../../core/src/core/entity-catalogue/entity-catalogue.service';
 import { EntityService } from '../../../../../../../../core/src/core/entity-service';
@@ -112,10 +111,9 @@ export class GitSCMTabComponent implements OnInit, OnDestroy {
         const branchID = `${scmType}-${projectName}-${stProject.deploySource.branch}`;
         const gitBranchesEntity = entityCatalogue.getEntity(CF_ENDPOINT_TYPE, gitBranchesEntityType);
         const fetchBranchesActionBuilder = gitBranchesEntity.actionOrchestrator.getActionBuilder('get');
-        const fetchBranchesAction = fetchBranchesActionBuilder(branchID, null, { projectName, scm });
         this.gitBranchEntityService = this.entityServiceFactory.create(
           branchID,
-          new FetchBranchesForProject(scm, projectName)
+          fetchBranchesActionBuilder(branchID, null, { projectName, scm })
         );
 
         this.gitSCMRepo$ = this.gitSCMRepoEntityService.waitForEntity$.pipe(
