e6b73c033584ff319dce2f845c063b0dce957c61
Neil MacDougall <neil.macdougall@suse.com>
2020-01-13 13:15:05 +0000

    Fux unit tests

--- src/frontend/packages/core/src/shared/components/sidepanel-preview/sidepanel-preview.component.spec.ts
+++ src/frontend/packages/core/src/shared/components/sidepanel-preview/sidepanel-preview.component.spec.ts
@@ -3,9 +3,9 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoreTestingModule } from '../../../../test-framework/core-test.modules';
-import { createBasicStoreModule } from '../../../../test-framework/store-test-helper';
 import { LoggerService } from '../../../core/logger.service';
 import { SidepanelPreviewComponent } from './sidepanel-preview.component';
+import { createBasicStoreModule } from '@stratos/store/testing';
 
 describe('SidepanelPreviewComponent', () => {
   let component: SidepanelPreviewComponent;
