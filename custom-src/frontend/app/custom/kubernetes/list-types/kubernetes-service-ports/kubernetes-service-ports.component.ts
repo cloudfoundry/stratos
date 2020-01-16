d93fb5d70f1a8b04f72eb3eabf4d22cc5db5eb1c
Richard Cox <richard.cox@suse.com>
2020-01-16 13:48:27 +0000

    Remove link from service ports section of services table/cards

--- custom-src/frontend/app/custom/kubernetes/list-types/kubernetes-service-ports/kubernetes-service-ports.component.ts
+++ custom-src/frontend/app/custom/kubernetes/list-types/kubernetes-service-ports/kubernetes-service-ports.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component } from '@angular/core';
 
 import { CardCell } from '../../../../shared/components/list/list.types';
 import { KubeService } from '../../../kubernetes/store/kube.types';
@@ -9,27 +8,4 @@ import { KubeService } from '../../../kubernetes/store/kube.types';
   templateUrl: './kubernetes-service-ports.component.html',
   styleUrls: ['./kubernetes-service-ports.component.scss']
 })
-export class KubernetesServicePortsComponent extends CardCell<KubeService> {
-  private pRow: KubeService;
-  public kubeService$: Observable<KubeService>;
-  // Used when extending this class
-  public endpointId: string;
-
-  @Input() set row(row: KubeService) {
-    if (!row) {
-      return;
-    }
-    this.pRow = row;
-    this.kubeService$ = of(row as KubeService);
-    /* tslint:disable-next-line:no-string-literal  */
-    this.endpointId = row['endpointId'];
-  }
-  get row(): KubeService {
-    return this.pRow;
-  }
-
-
-  getServiceLink(service: KubeService, port: { name: string }): string {
-    return `/kubernetes/svcproxy/${this.endpointId}/${service.metadata.namespace}/${service.metadata.name}/${port.name}`;
-  }
-}
+export class KubernetesServicePortsComponent extends CardCell<KubeService> { }
