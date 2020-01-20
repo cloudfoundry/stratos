9c128cac4257046857abcc0e0dee11bf4d831bbb
Richard Cox <richard.cox@suse.com>
2020-01-20 17:23:14 +0000

    Allow user to select from existing namespace or create one

--- custom-src/frontend/app/custom/helm/create-release/create-release.component.ts
+++ custom-src/frontend/app/custom/helm/create-release/create-release.component.ts
@@ -1,19 +1,35 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatTextareaAutosize } from '@angular/material';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, of, Subscription } from 'rxjs';
-import { delay, filter, first, map, pairwise, switchMap, tap } from 'rxjs/operators';
+import { PaginationMonitorFactory } from 'frontend/packages/store/src/monitors/pagination-monitor.factory';
+import { getPaginationObservables } from 'frontend/packages/store/src/reducers/pagination-reducer/pagination-reducer.helper';
+import { BehaviorSubject, combineLatest, Observable, of, Subscription } from 'rxjs';
+import {
+  delay,
+  distinctUntilChanged,
+  filter,
+  first,
+  map,
+  pairwise,
+  publishReplay,
+  refCount,
+  startWith,
+  switchMap,
+} from 'rxjs/operators';
 
 import { AppState } from '../../../../../store/src/app-state';
 import { entityCatalog } from '../../../../../store/src/entity-catalog/entity-catalog.service';
 import { EndpointsService } from '../../../core/endpoints.service';
+import { safeUnsubscribe } from '../../../core/utils.service';
 import { ConfirmationDialogConfig } from '../../../shared/components/confirmation-dialog.config';
 import { ConfirmationDialogService } from '../../../shared/components/confirmation-dialog.service';
-import { StepOnNextFunction } from '../../../shared/components/stepper/step/step.component';
+import { StepOnNextFunction, StepOnNextResult } from '../../../shared/components/stepper/step/step.component';
 import { KUBERNETES_ENDPOINT_TYPE } from '../../kubernetes/kubernetes-entity-factory';
+import { KubernetesNamespace } from '../../kubernetes/store/kube.types';
+import { CreateKubernetesNamespace, GetKubernetesNamespaces } from '../../kubernetes/store/kubernetes.actions';
 import { helmReleaseEntityKey } from '../../kubernetes/workloads/store/workloads-entity-factory';
 import { HelmInstall } from '../store/helm.actions';
 import { HelmInstallValues } from '../store/helm.types';
@@ -23,7 +39,7 @@ import { HelmInstallValues } from '../store/helm.types';
   templateUrl: './create-release.component.html',
   styleUrls: ['./create-release.component.scss'],
 })
-export class CreateReleaseComponent implements OnInit {
+export class CreateReleaseComponent implements OnInit, OnDestroy {
 
   // Confirmation dialog
   overwriteValuesConfirmation = new ConfirmationDialogConfig(
@@ -40,7 +56,10 @@ export class CreateReleaseComponent implements OnInit {
   validate$: Observable<boolean>;
 
   details: FormGroup;
+  namespaces$: Observable<string[]>;
   overrides: FormGroup;
+
+  private endpointChanged = new BehaviorSubject(null);
 
   @ViewChild('releaseNameInputField', { static: true }) releaseNameInputField: ElementRef;
   @ViewChild('overridesYamlTextArea', { static: true }) overridesYamlTextArea: ElementRef;
@@ -48,44 +67,109 @@ export class CreateReleaseComponent implements OnInit {
 
   public valuesYaml = '';
 
+  private subs: Subscription[] = [];
+
   constructor(
     private route: ActivatedRoute,
     public endpointsService: EndpointsService,
     private store: Store<AppState>,
     private httpClient: HttpClient,
     private confirmDialog: ConfirmationDialogService,
-    // private esf: EntityServiceFactory
+    private pmf: PaginationMonitorFactory
   ) {
     const chart = this.route.snapshot.params;
     this.cancelUrl = `/monocular/charts/${chart.repo}/${chart.chartName}/${chart.version}`;
 
-    this.kubeEndpoints$ = this.endpointsService.connectedEndpointsOfTypes(KUBERNETES_ENDPOINT_TYPE);
-
-    this.details = new FormGroup({
-      endpoint: new FormControl('', Validators.required),
-      releaseName: new FormControl('', Validators.required),
-      releaseNamespace: new FormControl('', Validators.required),
-    });
+    this.setupDetailsStep();
 
     this.overrides = new FormGroup({
       values: new FormControl('')
     });
 
-    this.validate$ = this.details.statusChanges.pipe(
-      map(() => this.details.valid)
-    );
-
     // Fetch the values.yaml for the Chart
     const valuesYamlUrl = `/pp/v1/chartsvc/v1/assets/${chart.repo}/${chart.chartName}/versions/${chart.version}/values.yaml`;
 
+    // TODO: RC Error handling
     this.httpClient.get(valuesYamlUrl, { responseType: 'text' }).subscribe(response => {
       this.valuesYaml = response;
     });
   }
 
+  private setupDetailsStep() {
+    this.kubeEndpoints$ = this.endpointsService.connectedEndpointsOfTypes(KUBERNETES_ENDPOINT_TYPE);
+
+    this.namespaces$ = this.endpointChanged.asObservable().pipe(
+      switchMap(endpoint => {
+        const action = new GetKubernetesNamespaces(endpoint);
+        return getPaginationObservables<KubernetesNamespace>({
+          store: this.store,
+          action,
+          paginationMonitor: this.pmf.create(action.paginationKey, action)
+        }).entities$;
+      }),
+      map((namespaces: KubernetesNamespace[]) => namespaces.map(namespace => namespace.metadata.name)),
+      publishReplay(1),
+      refCount(),
+    );
+
+    this.details = new FormGroup({
+      endpoint: new FormControl('', Validators.required),
+      releaseName: new FormControl('', Validators.required),
+      releaseNamespace: new FormControl(''),
+      createNamespace: new FormControl(false),
+    });
+    this.details.controls.createNamespace.disable();
+
+    const namespaceChanged$ = this.details.controls.releaseNamespace.valueChanges.pipe(
+      distinctUntilChanged()
+    );
+    const createNamespaceChanged$ = this.details.controls.createNamespace.valueChanges.pipe(
+      startWith(false),
+      distinctUntilChanged()
+    );
+
+    this.subs.push(
+      combineLatest([
+        this.namespaces$,
+        namespaceChanged$,
+        createNamespaceChanged$
+      ])
+        .pipe().subscribe(([namespaces, namespace, create]) => {
+          const namespaceExists = !!namespaces.find(val => val === namespace);
+          if (namespaceExists) {
+            // All is fine
+            this.details.controls.releaseNamespace.validator = () => null;
+            this.details.controls.createNamespace.setValue(false);
+            this.details.controls.createNamespace.disable();
+          } else if (!namespace) {
+            // Invalid - missing namespace
+            this.details.controls.releaseNamespace.validator = () => ({ required: true });
+            this.details.controls.createNamespace.disable();
+          } else if (!create) {
+            // Invalid - namespace doesn't exist and not creating
+            this.details.controls.releaseNamespace.validator = () => ({ namespaceDoesNotExist: true });
+            this.details.controls.createNamespace.enable();
+          } else {
+            // Valid - namespace doesn't exist but creating
+            this.details.controls.releaseNamespace.validator = () => null;
+            // this.details.controls.createNamespace.disable();
+          }
+          this.details.controls.releaseNamespace.updateValueAndValidity();
+        })
+    );
+
+    this.subs.push(
+      this.details.controls.endpoint.valueChanges.subscribe(val => {
+        this.endpointChanged.next(val);
+      })
+    );
+
+    this.validate$ = this.details.statusChanges.pipe(
+      map(() => this.details.valid)
+    );
+  }
+
   public useValuesYaml() {
-
-
     if (this.overrides.value.values.length !== 0) {
       this.confirmDialog.open(this.overwriteValuesConfirmation, () => {
         this.replaceWithValuesYaml();
@@ -102,17 +186,15 @@ export class CreateReleaseComponent implements OnInit {
 
   ngOnInit() {
     // Auto select endpoint if there is only one
-    this.kubeEndpoints$.pipe(
-      first(),
-      tap(ep => {
-        if (ep.length === 1) {
-          this.details.controls.endpoint.setValue(ep[0].guid, { onlySelf: true });
-          setTimeout(() => {
-            this.releaseNameInputField.nativeElement.focus();
-          }, 1);
-        }
-      })
-    ).subscribe();
+    this.kubeEndpoints$.pipe(first()).subscribe(ep => {
+      if (ep.length > 1) {
+        this.details.controls.endpoint.setValue(ep[0].guid, { onlySelf: true });
+        this.endpointChanged.next(ep[0].guid);
+        setTimeout(() => {
+          this.releaseNameInputField.nativeElement.focus();
+        }, 1);
+      }
+    });
   }
 
   onEnterOverrides = () => {
@@ -123,6 +205,28 @@ export class CreateReleaseComponent implements OnInit {
   }
 
   submit: StepOnNextFunction = () => {
+
+    const createNamespace$ = this.details.controls.createNamespace.value ? this.createNamespace() : of({ success: true });
+
+    return createNamespace$.pipe(
+      switchMap(createRes => {
+        if (!createRes.success) {
+          return createRes;
+        }
+        return this.installChart();
+      })
+    );
+  }
+
+  createNamespace(): Observable<StepOnNextResult> {
+    // TODO: RC namespace exists from previous run??
+    const action = new CreateKubernetesNamespace();
+    return of({
+      success: false
+    });
+  }
+
+  installChart(): Observable<StepOnNextResult> {
     // Build the request body
     const values: HelmInstallValues = {
       ...this.details.value,
@@ -154,7 +258,10 @@ export class CreateReleaseComponent implements OnInit {
         message: !result.error ? '' : result.message
       })),
     );
+  }
 
+  ngOnDestroy() {
+    safeUnsubscribe(...this.subs);
   }
 
 }
