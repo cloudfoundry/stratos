5736f8477ef0a561d9424b17a7ee8654b4584aed
Richard Cox <richard.cox@suse.com>
2020-04-20 13:49:37 +0100

    Re-add tests that now work

--- src/frontend/packages/core/src/features/endpoints/connect-endpoint/connect-endpoint.component.spec.ts
+++ src/frontend/packages/core/src/features/endpoints/connect-endpoint/connect-endpoint.component.spec.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { createBasicStoreModule } from '@stratos/store/testing';
 
 import { CoreTestingModule } from '../../../../test-framework/core-test.modules';
-import { createBasicStoreModule } from '@stratos/store/testing';
 import { CoreModule } from '../../../core/core.module';
 import { SharedModule } from '../../../shared/shared.module';
 import { ConnectEndpointComponent } from './connect-endpoint.component';
 
-// TODO: Fix after metrics has been sorted - STRAT-152
-xdescribe('ConnectEndpointComponent', () => {
+describe('ConnectEndpointComponent', () => {
   let component: ConnectEndpointComponent;
   let fixture: ComponentFixture<ConnectEndpointComponent>;
 
--- src/frontend/packages/core/src/features/endpoints/create-endpoint/create-endpoint.component.spec.ts
+++ src/frontend/packages/core/src/features/endpoints/create-endpoint/create-endpoint.component.spec.ts
@@ -1,21 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { createBasicStoreModule } from '@stratos/store/testing';
 
 import { TabNavService } from '../../../../tab-nav.service';
 import { CoreTestingModule } from '../../../../test-framework/core-test.modules';
-import { createBasicStoreModule } from '@stratos/store/testing';
 import { CoreModule } from '../../../core/core.module';
+import { SidePanelService } from '../../../shared/services/side-panel.service';
 import { SharedModule } from '../../../shared/shared.module';
 import { ConnectEndpointComponent } from '../connect-endpoint/connect-endpoint.component';
 import { CreateEndpointCfStep1Component } from './create-endpoint-cf-step-1/create-endpoint-cf-step-1.component';
 import { CreateEndpointConnectComponent } from './create-endpoint-connect/create-endpoint-connect.component';
 import { CreateEndpointComponent } from './create-endpoint.component';
-import { HttpClientModule } from '@angular/common/http';
 
-// TODO: Fix after metrics has been sorted - STRAT-152
-xdescribe('CreateEndpointComponent', () => {
+describe('CreateEndpointComponent', () => {
   let component: CreateEndpointComponent;
   let fixture: ComponentFixture<CreateEndpointComponent>;
 
@@ -47,7 +47,9 @@ xdescribe('CreateEndpointComponent', () => {
             }
           }
         }
-      }, TabNavService],
+      },
+        TabNavService,
+        SidePanelService],
     })
       .compileComponents();
   }));
