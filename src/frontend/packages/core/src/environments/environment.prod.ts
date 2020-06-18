2291273b309a737244ae1259a0c73e73dd6d868b
Neil MacDougall <neil.macdougall@suse.com>
2020-06-18 13:39:17 +0100

    Address PR feedback

--- src/frontend/packages/core/src/environments/environment.prod.ts
+++ src/frontend/packages/core/src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
+import { cfAPIVersion, proxyAPIVersion } from '../../../store/src/jetstream';
 import { LogLevel } from './../../../store/src/actions/log.actions';
 
 export const environment = {
   production: true,
   logLevel: LogLevel.WARN,
-  proxyAPIVersion: 'v1',
-  cfAPIVersion: 'v2',
+  proxyAPIVersion,
+  cfAPIVersion,
   logToConsole: true,
   logEnableConsoleActions: false,
   showObsDebug: false,
