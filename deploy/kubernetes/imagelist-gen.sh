f34497f1ee6e3cea9967fd456793d0d65f829753
Neil MacDougall <neil.macdougall@suse.com>
2020-06-23 11:25:30 +0100

    Fix merge issue

--- deploy/kubernetes/imagelist-gen.sh
+++ deploy/kubernetes/imagelist-gen.sh
@@ -52,15 +52,15 @@ printf "${CYAN}Chart folder contents:${RESET}\n"
 ls -alR
 echo ""
 
-# Add any customizations
-addCustomizations
-
 helm template -f ${__DIRNAME}/imagelist.values.yaml ${CHART_FOLDER} | grep "image:" | grep --extended --only-matching '([^"/[:space:]]+/)?[^"/[:space:]]+/[^:[:space:]]+:[a-zA-Z0-9\._-]+' | sort | uniq | awk -F'/' '{print $2}' > imagelist.txt
-
 if [ $? -ne 0 ]; then
   echo -e "${BOLD}${RED}ERROR: Failed to render Helm Chart in order to generate image list"
   exit 1
 fi
+
+# Add any customizations to the image list
+# Mainly used if there are unreferenced images that need to be included
+addCustomizations
 
 popd > /dev/null
 
