b31092d9d3bd37c74f40791b605881c60b7bf250
Neil MacDougall <neil.macdougall@suse.com>
2020-06-23 17:05:15 +0100

    Fix backend unit tests

--- src/jetstream/mock_server_test.go
+++ src/jetstream/mock_server_test.go
@@ -38,6 +38,14 @@ type mockPGStore struct {
 	StoredSession *sessions.Session
 }
 
+func (m *mockPGStore) New(r *http.Request, name string) (*sessions.Session, error) {
+	session := &sessions.Session{
+		Values:  make(map[interface{}]interface{}),
+		Options: &sessions.Options{},
+	}
+	return session, nil
+}
+
 func (m *mockPGStore) Save(r *http.Request, w http.ResponseWriter, session *sessions.Session) error {
 	m.StoredSession = session
 	return nil
@@ -138,7 +146,6 @@ func setupPortalProxy(db *sql.DB) *portalProxy {
 		CFAdminIdentifier:    CFAdminIdentifier,
 		AuthEndpointType:     "remote",
 	}
-
 
 	pp := newPortalProxy(pc, db, nil, nil, env.NewVarSet())
 	pp.SessionStore = setupMockPGStore(db)
