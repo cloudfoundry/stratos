cd65b145a8c0cefde02f0a7f77b83d15bde21772
Neil MacDougall <neil.macdougall@suse.com>
2020-06-24 16:49:25 +0100

    Add dev doc and helper setup script

---
+++ build/tools/kube-terminal-dev.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Colours
+CYAN="\033[96m"
+YELLOW="\033[93m"
+RED="\033[91m"
+RESET="\033[0m"
+BOLD="\033[1m"
+
+# Program Paths:
+PROG=$(basename ${BASH_SOURCE[0]})
+PROG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+STRATOS_DIR="$( cd "${PROG_DIR}/../.." && pwd )"
+
+echo "Creating Service Account"
+SRC="${STRATOS_DIR}/deploy/kubernetes/console/templates/service-account.yaml"
+
+TEMPFILE=$(mktemp)
+cp $SRC $TEMPFILE
+sed -i.bak '/\s*helm/d' $TEMPFILE
+sed -i.bak '/\s*app\.kubernetes\.io\/version/d' $TEMPFILE
+sed -i.bak '/\s*app\.kubernetes\.io\/instance/d' $TEMPFILE
+sed -i.bak '/\s*{{-/d' $TEMPFILE
+
+kubectl apply -f $TEMPFILE
+
+# Service account should be created - now need to get token
+SECRET=$(kubectl get sa stratos -o json | jq -r '.secrets[0].name')
+TOKEN=$(kubectl get secret $SECRET -o json | jq -r '.data.token')
+echo "Token secret: $SECRET"
+echo "Token         $TOKEN"
+
+rm -f $TEMPFILE
+rm -f $TEMPFILE.bak
+
+# Create a namespace
+NS="stratos-dev"
+kubectl get ns $NS > /dev/null
+if [ $? -ne 0 ]; then
+  kubectl create ns $NS
+fi
+
+CFG=${STRATOS_DIR}/src/jetstream/config.properties
+touch $CFG
+
+echo -e "\n# Kubernetes Terminal Config for dev" >> $CFG
+echo "STRATOS_KUBERNETES_NAMESPACE=stratos-dev" >> $CFG
+echo "STRATOS_KUBERNETES_TERMINAL_IMAGE=splatform/stratos-kube-terminal:dev" >> $CFG
+echo "KUBE_TERMINAL_SERVICE_ACCOUNT_TOKEN=$TOKEN" >> $CFG
+
+MKUBE=$(minikube ip)
+if [ $? -eq 0 ]; then
+  echo "KUBERNETES_SERVICE_HOST=$MKUBE" >> $CFG
+  echo "KUBERNETES_SERVICE_PORT=8443" >> $CFG
+else
+  echo "KUBERNETES_SERVICE_HOST=" >> $CFG
+  echo "KUBERNETES_SERVICE_PORT=8443" >> $CFG
+fi
