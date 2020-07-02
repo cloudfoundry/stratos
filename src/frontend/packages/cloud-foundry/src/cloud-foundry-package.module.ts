5cf0b7f7dc93f58687f4d2990920238fed56f929
Richard Cox <richard.cox@suse.com>
2020-07-02 11:55:30 +0100

    Move CloudFoundrySharedModule out of CloudFoundryPackageModule

--- src/frontend/packages/cloud-foundry/src/cloud-foundry-package.module.ts
+++ src/frontend/packages/cloud-foundry/src/cloud-foundry-package.module.ts
@@ -5,10 +5,10 @@ import { MDAppModule } from '../../core/src/core/md.module';
 import { SharedModule } from '../../core/src/shared/shared.module';
 import { EntityCatalogModule } from '../../store/src/entity-catalog.module';
 import { generateCFEntities } from './cf-entity-generator';
-import { CloudFoundrySharedModule } from './shared/cf-shared.module';
 import { CfUserService } from './shared/data-services/cf-user.service';
 import { CloudFoundryService } from './shared/data-services/cloud-foundry.service';
 import { LongRunningCfOperationsService } from './shared/data-services/long-running-cf-op.service';
+import { GitSCMService } from './shared/data-services/scm/scm.service';
 import { ServiceActionHelperService } from './shared/data-services/service-action-helper.service';
 import { CloudFoundryUserProvidedServicesService } from './shared/services/cloud-foundry-user-provided-services.service';
 import { CloudFoundryStoreModule } from './store/cloud-foundry.store.module';
@@ -21,8 +21,6 @@ import { cfCurrentUserPermissionsService } from './user-permissions/cf-user-perm
     SharedModule,
     MDAppModule,
     CloudFoundryStoreModule,
-    // FIXME: Ensure that anything lazy loaded is not included here - #3675
-    CloudFoundrySharedModule,
   ],
   providers: [
     ...cfCurrentUserPermissionsService,
@@ -31,6 +29,7 @@ import { cfCurrentUserPermissionsService } from './user-permissions/cf-user-perm
     ServiceActionHelperService,
     LongRunningCfOperationsService,
     CloudFoundryUserProvidedServicesService,
+    GitSCMService,
   ]
 })
 export class CloudFoundryPackageModule { }
