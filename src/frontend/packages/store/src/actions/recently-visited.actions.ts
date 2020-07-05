ed9c6aed8075522698871a901c950f2b77922719
Neil MacDougall <neil_macdougall@hotmail.com>
2020-07-05 17:32:39 +0100

    Improve recent and favourites display

--- src/frontend/packages/store/src/actions/recently-visited.actions.ts
+++ src/frontend/packages/store/src/actions/recently-visited.actions.ts
@@ -1,19 +1,11 @@
 import { Action } from '@ngrx/store';
-import * as moment from 'moment';
 
-import { IRecentlyVisitedEntity, IRecentlyVisitedEntityDated } from '../types/recently-visited.types';
+import { IRecentlyVisitedEntity } from '../types/recently-visited.types';
 
 export class AddRecentlyVisitedEntityAction implements Action {
   static ACTION_TYPE = '[Recently visited] Add';
   public type = AddRecentlyVisitedEntityAction.ACTION_TYPE;
-  public date: number;
-  public recentlyVisited: IRecentlyVisitedEntityDated;
-  constructor(recentlyVisited: IRecentlyVisitedEntity) {
-    this.recentlyVisited = {
-      ...recentlyVisited,
-      date: moment().valueOf()
-    };
-  }
+  constructor(public recentlyVisited: IRecentlyVisitedEntity) {}
 }
 
 export class SetRecentlyVisitedEntityAction implements Action {
