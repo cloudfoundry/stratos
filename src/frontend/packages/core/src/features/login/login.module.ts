53f6ca5efc8bbe9f9c0a038aa8b34a180678dcdd
Neil MacDougall <nwmac@users.noreply.github.com>
2020-07-10 10:10:46 +0100

    Improve the logout experience (#4439)
    * Add support for including breaking changes in the changelog
    
    * Improve the logout experience
    
    * Fix unit tests

--- src/frontend/packages/core/src/features/login/login.module.ts
+++ src/frontend/packages/core/src/features/login/login.module.ts
@@ -4,6 +4,7 @@ import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login.routing';
+import { LogoutPageComponent } from './logout-page/logout-page.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginRoutingModule } from './login.routing';
     LoginRoutingModule
   ],
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    LogoutPageComponent
   ]
 })
 export class LoginModule { }
