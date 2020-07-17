700048d08dd16fb34228c6b9c20242da999a6104
Richard Cox <richard.cox@suse.com>
2020-07-17 14:08:47 +0100

    Fix path

--- electron/store.js
+++ electron/store.js
@@ -6,29 +6,26 @@ class Store {
   constructor(opts) {
     // Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
     // app.getPath('userData') will return a string of the user's app data directory path.
-    const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+    this.path = (electron.app || electron.remote.app).getPath('userData');
     // We'll use the `configName` property to set the file name and path.join to bring it all together as a string
-    this.path = path.join(userDataPath, opts.configName + '.json');
-    console.log('this.path: ', this.path)
+    this.filePath = path.join(this.path, opts.configName + '.json');
 
-    this.data = parseDataFile(this.path, opts.defaults);
+    this.data = parseDataFile(this.filePath, opts.defaults);
   }
 
   // This will just return the property on the `data` object
   get(key) {
-    console.log('get: ', this.data)
     return this.data[key];
   }
 
   // ...and this will set it
   set(key, val) {
     this.data[key] = val;
-    console.log('set: ', this.data)
     // Wait, I thought using the node.js' synchronous APIs was bad form?
     // We're not writing a server so there's not nearly the same IO demand on the process
     // Also if we used an async API and our app was quit before the asynchronous write had a chance to complete,
     // we might lose that data. Note that in a real app, we would try/catch this.
-    fs.writeFileSync(this.path, JSON.stringify(this.data));
+    fs.writeFileSync(this.filePath, JSON.stringify(this.data));
   }
 }
 
