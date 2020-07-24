8cde7b627cce568edf247aa603705e089f1fb4d7
Neil MacDougall <nwmac@users.noreply.github.com>
2020-07-24 14:01:31 +0100

    Patch 2.7.0 image to update nginx SSL configuration (#412)
    * Patch 2.7.0 image
    
    * Move patch files to better place

---
+++ deploy/suse/patches/nginx/README.md
@@ -0,0 +1,3 @@
+# 2.7.0 Patch
+
+These files were used to create a patch for 2.7.0 that fixes the TLS ciphers used by nginx.
---
+++ deploy/suse/patches/nginx/build.sh
@@ -0,0 +1,3 @@
+
+docker build -f Dockerfile . -t registry.suse.com/cap-staging/stratos-console-1:2.7.0-35f5964bd-cap
+docker push registry.suse.com/cap-staging/stratos-console-1:2.7.0-35f5964bd-cap
