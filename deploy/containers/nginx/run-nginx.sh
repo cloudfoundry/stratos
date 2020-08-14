9d89829e1cb48290e7ae577027692ee7a46a8ae0
Neil MacDougall <neil.macdougall@suse.com>
2020-08-14 10:19:44 +0100

    Change nginx ciphers and make configurable via helm chart values

--- deploy/containers/nginx/run-nginx.sh
+++ deploy/containers/nginx/run-nginx.sh
@@ -16,5 +16,23 @@ do
     sleep 1; 
 done
 
-echo "TLS certificate detected ... starting nginx."
+echo "TLS certificate detected OK"
+
+# Patch the config file with the desired ciphers and protocols
+echo "Setting nginx ciphers and protocols"
+
+DEFAULT_PROTOCOLS="TLSv1.2 TLSv1.3"
+DEFAULT_CIPHERS="HIGH:!aNULL:!MD5"
+
+NGINX_PROTOCOLS=${SSL_PROTOCOLS:-$DEFAULT_PROTOCOLS}
+NGINX_CIPHERS=${SSL_CIPHERS:-$DEFAULT_CIPHERS}
+
+echo "SSL Protocols : $NGINX_PROTOCOLS"
+echo "SSL Ciphers   : $NGINX_CIPHERS"
+
+sed -e 's/__PROTOCOLS__/'"${NGINX_PROTOCOLS}"'/g' /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
+sed -i.bak -e 's/__CIPHERS__/'"${NGINX_CIPHERS}"'/g' /etc/nginx/nginx.conf
+rm /etc/nginx/nginx.conf.bak
+
+echo "Starting nginx ..."
 nginx -g "daemon off;"
