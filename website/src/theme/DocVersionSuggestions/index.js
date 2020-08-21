44ffc6271432be9c47eb2851979255ef536e8049
Richard Cox <richard.cox@suse.com>
2020-08-21 17:29:01 +0100

    Add custom version of 'not latest released docs' message

---
+++ website/src/theme/DocVersionSuggestions/index.js
@@ -0,0 +1,65 @@
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { useActivePlugin, useActiveVersion, useDocVersionSuggestions } from '@theme/hooks/useDocs';
+import React from 'react';
+
+const useMandatoryActiveDocsPluginId = () => {
+  const activePlugin = useActivePlugin();
+
+  if (!activePlugin) {
+    throw new Error(
+      'DocVersionCallout is only supposed to be used on docs-related routes',
+    );
+  }
+
+  return activePlugin.pluginId;
+};
+
+const getVersionMainDoc = (version) =>
+  version.docs.find((doc) => doc.id === version.mainDocId);
+
+function DocVersionSuggestions() {
+  const {
+    siteConfig: { title: siteTitle },
+  } = useDocusaurusContext();
+  const pluginId = useMandatoryActiveDocsPluginId();
+  const activeVersion = useActiveVersion(pluginId);
+  const {
+    latestDocSuggestion,
+    latestVersionSuggestion,
+  } = useDocVersionSuggestions(pluginId); // No suggestion to be made
+
+  if (!latestVersionSuggestion) {
+    return <></>;
+  }
+
+  const activeVersionName = activeVersion.name; // try to link to same doc in latest version (not always possible)
+  // fallback to main doc of latest version
+
+  const suggestedDoc =
+    latestDocSuggestion ?? getVersionMainDoc(latestVersionSuggestion);
+  return (
+    <div className="alert alert--warning margin-bottom--md" role="alert">
+      {activeVersionName === 'next' ? (
+        <div>
+          This is unreleased documentation for {siteTitle}{' '}
+          <strong>{activeVersionName}</strong> version.
+        </div>
+      ) : (
+          <div>
+            This is documentation for {siteTitle}{' '}
+            <strong>v{activeVersionName}</strong>.
+          </div>
+        )}
+      <div className="margin-top--md">
+        For the latest released documentation see {' '}
+        <strong>
+          <Link to={suggestedDoc.path}>here</Link>
+        </strong>{' '}
+        ({latestVersionSuggestion.name}).
+      </div>
+    </div>
+  );
+}
+
+export default DocVersionSuggestions;
