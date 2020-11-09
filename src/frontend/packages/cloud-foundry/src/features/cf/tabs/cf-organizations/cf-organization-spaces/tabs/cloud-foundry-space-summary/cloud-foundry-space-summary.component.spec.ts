5665e6a622a1eccaddc42cd25b1458c6b20b75f8
Neil MacDougall <neil.macdougall@suse.com>
2020-11-09 09:29:22 +0000

    Fix test imports

--- src/frontend/packages/cloud-foundry/src/features/cf/tabs/cf-organizations/cf-organization-spaces/tabs/cloud-foundry-space-summary/cloud-foundry-space-summary.component.spec.ts
+++ src/frontend/packages/cloud-foundry/src/features/cf/tabs/cf-organizations/cf-organization-spaces/tabs/cloud-foundry-space-summary/cloud-foundry-space-summary.component.spec.ts
@@ -7,12 +7,6 @@ import {
 } from '../../../../../../../../test-framework/cloud-foundry-endpoint-service.helper';
 import { CloudFoundrySpaceServiceMock } from '../../../../../../../../test-framework/cloud-foundry-space.service.mock';
 import {
-  CardCfRecentAppsComponent,
-} from '../../../../../../../shared/components/cards/card-cf-recent-apps/card-cf-recent-apps.component';
-import {
-  CompactAppCardComponent,
-} from '../../../../../../../shared/components/cards/card-cf-recent-apps/compact-app-card/compact-app-card.component';
-import {
   CardCfSpaceDetailsComponent,
 } from '../../../../../../../shared/components/cards/card-cf-space-details/card-cf-space-details.component';
 import { CfUserService } from '../../../../../../../shared/data-services/cf-user.service';
@@ -22,6 +16,10 @@ import {
 import {
   CloudFoundryUserProvidedServicesService,
 } from '../../../../../../../shared/services/cloud-foundry-user-provided-services.service';
+import { CardCfRecentAppsComponent } from '../../../../../../home/card-cf-recent-apps/card-cf-recent-apps.component';
+import {
+  CompactAppCardComponent,
+} from '../../../../../../home/card-cf-recent-apps/compact-app-card/compact-app-card.component';
 import { CloudFoundryEndpointService } from '../../../../../services/cloud-foundry-endpoint.service';
 import { CloudFoundryOrganizationService } from '../../../../../services/cloud-foundry-organization.service';
 import { CloudFoundrySpaceService } from '../../../../../services/cloud-foundry-space.service';
