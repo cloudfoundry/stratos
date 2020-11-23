3160d6f886b826d99de3376ece85ea10d58f0062
Neil MacDougall <neil.macdougall@suse.com>
2020-11-23 14:21:54 +0000

    Fix favorite validation

--- src/frontend/packages/cloud-foundry/src/cf-entity-generator.ts
+++ src/frontend/packages/cloud-foundry/src/cf-entity-generator.ts
@@ -1,5 +1,6 @@
 import { Compiler, Injector } from '@angular/core';
 import { Action, Store } from '@ngrx/store';
+import { entityFetchedWithoutError } from '@stratosui/store';
 import moment from 'moment';
 import { combineLatest, Observable, of } from 'rxjs';
 import { first, map } from 'rxjs/operators';
