ab21c024bf9a0aec8fb4c3c499a30adcfe04238f
Neil MacDougall <neil.macdougall@suse.com>
2020-12-02 15:20:10 +0000

    Fix isse where latest is always null

--- src/frontend/packages/core/src/shared/services/snackbar.service.ts
+++ src/frontend/packages/core/src/shared/services/snackbar.service.ts
@@ -6,24 +6,31 @@ import { SnackBarReturnComponent } from '../components/snackbar-return/snackbar-
 
 /**
  * Service for showing snackbars
+ *
+ * Note: Only one snack bar is shown at a given time
  */
 @Injectable({
   providedIn: 'root',
 })
 export class SnackBarService {
 
+  private snackBars: MatSnackBarRef<SimpleSnackBar>[] = [];
+
   constructor(public snackBar: MatSnackBar) { }
 
-  // There can onlly be one snackbar on screen at once
-  private lastSnackBar: MatSnackBarRef<SimpleSnackBar>;
-
+  // Show a snack bar with the given message
+  // If closeMessage is supplied a button to dismiss the snack bar is shown and the duration is ignored
+  // If closeMessage is not supplied, no close button is shown and the snack bar will hide after the specified duration (default 5s)
   public show(message: string, closeMessage?: string, duration: number = 5000) {
     this.trackSnackBar(this.snackBar.open(message, closeMessage, {
       duration: closeMessage ? null : duration
     }));
   }
 
-  public showReturn(message: string, returnUrl: string | string[], returnLabel: string, duration?: number) {
+  // Show a snack bar with the given message and en extra button which when clicked navigates to the given URL
+  // A 'Dismiss' button is also always included in the snack bar
+  // The snack bar will disappear after the given duration if this is specified
+  public showWithLink(message: string, returnUrl: string | string[], returnLabel: string, duration?: number) {
     this.trackSnackBar(this.snackBar.openFromComponent(SnackBarReturnComponent, {
       duration,
       data: {
@@ -34,14 +41,17 @@ export class SnackBarService {
     }));
   }
 
+  // Hide the open snack bars
   public hide() {
-    if (this.lastSnackBar) {
-      this.lastSnackBar.dismiss();
-    }
+    this.snackBars.forEach(snackBar => snackBar.dismiss());
   }
 
   private trackSnackBar(snackBar: MatSnackBarRef<SimpleSnackBar>) {
-    this.lastSnackBar = snackBar;
-    snackBar.afterDismissed().pipe(first()).subscribe(() => this.lastSnackBar = null);
+    snackBar.afterOpened().pipe(first()).subscribe(() => {
+      this.snackBars.push(snackBar);
+    });
+    snackBar.afterDismissed().pipe(first()).subscribe(() => {
+      this.snackBars.shift();
+    });
   }
 }
