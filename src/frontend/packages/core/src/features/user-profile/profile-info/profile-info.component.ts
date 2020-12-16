b95b32948c7e2a28f559ac07db27ae1b4ee7dd57
Richard Cox <richard-cox@users.noreply.github.com>
2020-12-16 16:21:43 +0000

    Fix edit profile button visibility (#4839)
    - broken as part of electron changes
    - canEdit$ was incorrectly removed

--- src/frontend/packages/core/src/features/user-profile/profile-info/profile-info.component.ts
+++ src/frontend/packages/core/src/features/user-profile/profile-info/profile-info.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { ThemeService } from '../../../../../store/src/theme.service';
 import { UserProfileInfo } from '../../../../../store/src/types/user-profile.types';
+import { CurrentUserPermissionsService } from '../../../core/permissions/current-user-permissions.service';
+import { StratosCurrentUserPermissions } from '../../../core/permissions/stratos-user-permissions.checker';
 import { UserProfileService } from '../../../core/user-profile.service';
 import { UserService } from '../../../core/user.service';
 
@@ -24,9 +26,14 @@ export class ProfileInfoComponent {
     userProfileService: UserProfileService,
     public userService: UserService,
     public themeService: ThemeService,
+    private currentUserPermissionsService: CurrentUserPermissionsService,
   ) {
     this.isError$ = userProfileService.isError$;
     this.userProfile$ = userProfileService.userProfile$;
+
+    const canEdit = this.isError$.pipe(map(e => !e));
+    const hasEditPermissions = this.currentUserPermissionsService.can(StratosCurrentUserPermissions.EDIT_PROFILE);
+    this.canEdit$ = combineLatest([canEdit, hasEditPermissions]).pipe(map(([a, b]) => a && b));
 
     this.primaryEmailAddress$ = this.userProfile$.pipe(
       map((profile: UserProfileInfo) => userProfileService.getPrimaryEmailAddress(profile))
