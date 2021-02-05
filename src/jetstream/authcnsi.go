f0a53bbfd80d0dfb1fd34e9f9f03365e84065656
Thomas Quandt <thquad@gmail.com>
2021-02-05 12:08:07 +0100

    Prevent admins from connecting to user endpoints (#4753)
    Signed-off-by: Thomas Quandt <thquad@gmail.com>

--- src/jetstream/authcnsi.go
+++ src/jetstream/authcnsi.go
@@ -149,14 +149,26 @@ func (p *portalProxy) DoLoginToCNSI(c echo.Context, cnsiGUID string, systemShare
 		return nil, echo.NewHTTPError(http.StatusUnauthorized, "Could not find correct session value")
 	}
 
+	user, err := p.StratosAuthService.GetUser(userID)
+	if err != nil {
+		return nil, echo.NewHTTPError(http.StatusUnauthorized, "Can not connect - could not check user")
+	}
+
+	// admins are note allowed to connect to user created endpoints
+	if p.GetConfig().EnableUserEndpoints && len(cnsiRecord.Creator) > 0 {
+		cnsiUser, err := p.StratosAuthService.GetUser(cnsiRecord.Creator)
+		if err != nil {
+			return nil, echo.NewHTTPError(http.StatusUnauthorized, "Can not connect - endpoint creator has no account")
+		}
+
+		if !cnsiUser.Admin && user.Admin {
+			return nil, echo.NewHTTPError(http.StatusUnauthorized, "Can not connect - admins are not allowed to connect to user created endpoints")
+		}
+	}
+
 	// Register as a system endpoint?
 	if systemSharedToken {
 		// User needs to be an admin
-		user, err := p.StratosAuthService.GetUser(userID)
-		if err != nil {
-			return nil, echo.NewHTTPError(http.StatusUnauthorized, "Can not connect System Shared endpoint - could not check user")
-		}
-
 		if !user.Admin {
 			return nil, echo.NewHTTPError(http.StatusUnauthorized, "Can not connect System Shared endpoint - user is not an administrator")
 		}
