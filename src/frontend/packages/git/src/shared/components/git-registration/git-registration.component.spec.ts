b968bd8d48d4125c1b30faedfeffaa0581a8eb3f
Thomas Quandt <thquad@gmail.com>
2021-02-22 12:24:13 +0100

    Fix frontend unit test (#4753)
    Signed-off-by: Thomas Quandt <thquad@gmail.com>

--- src/frontend/packages/git/src/shared/components/git-registration/git-registration.component.spec.ts
+++ src/frontend/packages/git/src/shared/components/git-registration/git-registration.component.spec.ts
@@ -2,11 +2,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { SharedModule } from '@stratosui/core';
 import { getGitHubAPIURL, gitEntityCatalog, GITHUB_API_URL, GitSCMService } from '@stratosui/git';
-import { CATALOGUE_ENTITIES } from '@stratosui/store';
-import { createBasicStoreModule } from '@stratosui/store/testing';
+import { CATALOGUE_ENTITIES, entityCatalog, TestEntityCatalog } from '@stratosui/store';
 
 import { BaseTestModulesNoShared } from '../../../../../core/test-framework/core-test.helper';
 import { GitRegistrationComponent } from './git-registration.component';
+import { generateStratosEntities } from '../../../../../store/src/stratos-entity-generator';
 
 describe('GitRegistrationComponent', () => {
   let component: GitRegistrationComponent;
@@ -17,13 +17,21 @@ describe('GitRegistrationComponent', () => {
       declarations: [ GitRegistrationComponent ],
       imports: [
         ...BaseTestModulesNoShared,
-        createBasicStoreModule(),
         SharedModule,
       ],
       providers: [
         { provide: GITHUB_API_URL, useFactory: getGitHubAPIURL },
         GitSCMService,
-        { provide: CATALOGUE_ENTITIES, useFactory: () => gitEntityCatalog.allGitEntities(), multi: false },
+        { 
+          provide: CATALOGUE_ENTITIES, useFactory: () => {
+            const testEntityCatalog = entityCatalog as TestEntityCatalog;
+            testEntityCatalog.clear();
+            return [
+              ...generateStratosEntities(),
+              ...gitEntityCatalog.allGitEntities()
+            ];
+          }, multi: false 
+        },
         {
           provide: ActivatedRoute,
           useValue: {
