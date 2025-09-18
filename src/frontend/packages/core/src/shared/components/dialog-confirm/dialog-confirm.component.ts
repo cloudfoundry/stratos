99936fab8c281ec07bdc8eab2fe2a34bd6f92ecf
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-09-18 02:10:51 -0400

    Update dialog-confirm.component.ts

--- src/frontend/packages/core/src/shared/components/dialog-confirm/dialog-confirm.component.ts
+++ src/frontend/packages/core/src/shared/components/dialog-confirm/dialog-confirm.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@stratosui/core';
 
 import { environment } from '../../../environments/environment';
 import { ConfirmationDialogConfig, TypeToConfirm } from '../confirmation-dialog.config';
