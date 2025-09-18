f3176d18730f7854046d12559dca0ec8cfc2c75e
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-09-18 02:29:33 -0400

    Update public-api.ts

--- src/frontend/packages/core/src/public-api.ts
+++ src/frontend/packages/core/src/public-api.ts
@@ -19,3 +19,13 @@ export * from './shared/shared.module';
 export { LoginPageComponent } from './features/login/login-page/login-page.component';
 
 export { ProfileSettingsTypes } from './shared/components/profile-settings/profile-settings.component';
+
+// Tailwind Material Replacements
+export * from './shared/services/tailwind-material-replacements';
+export * from './shared/services/tailwind-snackbar.service';
+export * from './shared/services/tailwind-dialog.service';
+export * from './shared/services/tailwind-error-state-matcher';
+export * from './shared/services/tailwind-sidenav.service';
+export * from './shared/services/tailwind-sort.service';
+export * from './shared/services/tailwind-paginator.service';
+export * from './shared/services/tailwind-json-schema-form.service';
