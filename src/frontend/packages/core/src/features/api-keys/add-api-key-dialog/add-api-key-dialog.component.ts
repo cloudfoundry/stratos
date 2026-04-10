11504c1e3320d2368633ff17de410511aa547f55
Norman Abramovitz <norman-abramovitz@users.noreply.github.com>
2026-04-09 21:00:02 -0700

    WIP: ng update @angular/core@21 @angular/cli@21

--- src/frontend/packages/core/src/features/api-keys/add-api-key-dialog/add-api-key-dialog.component.ts
+++ src/frontend/packages/core/src/features/api-keys/add-api-key-dialog/add-api-key-dialog.component.ts
@@ -1,38 +1,54 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, signal, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { TailwindDialogRef } from '../../../shared/services/tailwind-dialog.service';
-import { CustomFormFieldComponent } from '../../../shared/components/custom-form-field/custom-form-field.component';
-import { AppProgressBarComponent } from '../../../shared/components/progress-bar/app-progress-bar.component';
-import { entityCatalog, stratosEntityCatalog, NormalizedResponse, ApiKey, RequestInfoState } from '@stratosui/store';
-import { Subscription } from 'rxjs';
-import { take, filter, map, pairwise, tap } from 'rxjs/operators';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  signal,
+  inject,
+} from "@angular/core";
 
-import { safeUnsubscribe } from '../../../core/utils.service';
-import { DialogErrorComponent } from '../../../shared/components/dialog-error/dialog-error.component';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import { TailwindDialogRef } from "../../../shared/services/tailwind-dialog.service";
+import { CustomFormFieldComponent } from "../../../shared/components/custom-form-field/custom-form-field.component";
+import { AppProgressBarComponent } from "../../../shared/components/progress-bar/app-progress-bar.component";
+import {
+  entityCatalog,
+  stratosEntityCatalog,
+  NormalizedResponse,
+  ApiKey,
+  RequestInfoState,
+} from "@stratosui/store";
+import { Subscription } from "rxjs";
+import { take, filter, map, pairwise, tap } from "rxjs/operators";
+
+import { safeUnsubscribe } from "../../../core/utils.service";
+import { DialogErrorComponent } from "../../../shared/components/dialog-error/dialog-error.component";
 
 interface AddApiKeyForm {
   comment: FormControl<string>;
 }
 
 @Component({
-  selector: 'app-add-api-key-dialog',
-  templateUrl: './add-api-key-dialog.component.html',
-  styleUrls: ['./add-api-key-dialog.component.scss'],
+  selector: "app-add-api-key-dialog",
+  templateUrl: "./add-api-key-dialog.component.html",
+  styleUrls: ["./add-api-key-dialog.component.scss"],
   standalone: true,
   imports: [
-    CommonModule,
     ReactiveFormsModule,
     CustomFormFieldComponent,
     AppProgressBarComponent,
-    DialogErrorComponent
+    DialogErrorComponent,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddApiKeyDialogComponent implements OnDestroy {
   private fb = inject(FormBuilder);
-  dialogRef = inject<TailwindDialogRef<ApiKey>>('TailwindDialogRef' as any);
-
+  dialogRef = inject<TailwindDialogRef<ApiKey>>("TailwindDialogRef" as any);
 
   public hasErrored = signal<string | null>(null);
   public isBusy = signal<boolean>(false);
@@ -43,7 +59,10 @@ export class AddApiKeyDialogComponent implements OnDestroy {
 
   constructor() {
     this.formGroup = this.fb.group<AddApiKeyForm>({
-      comment: new FormControl('', { nonNullable: true, validators: [Validators.required] })
+      comment: new FormControl("", {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
     });
   }
 
@@ -52,26 +71,32 @@ export class AddApiKeyDialogComponent implements OnDestroy {
   }
 
   submit() {
-    this.sub = stratosEntityCatalog.apiKey.api.create<RequestInfoState>(this.formGroup.controls.comment.value).pipe(
-      tap(() => {
-        this.isBusy.set(true);
-        this.hasErrored.set(null);
-      }),
-      pairwise(),
-      filter(([oldR, newR]) => oldR.creating && !newR.creating),
-      map(([, newR]) => newR),
-      tap(state => {
-        if (state.error) {
-          this.hasErrored.set(`Failed to create key: ${state.message}`);
-          this.isBusy.set(false);
-        } else {
-          const response: NormalizedResponse<ApiKey> = state.response;
-          const entityKey = entityCatalog.getEntityKey(stratosEntityCatalog.apiKey.actions.create(''));
-          this.dialogRef.close(response.entities[entityKey][response.result[0]]);
-        }
-      }),
-      take(1)
-    ).subscribe();
+    this.sub = stratosEntityCatalog.apiKey.api
+      .create<RequestInfoState>(this.formGroup.controls.comment.value)
+      .pipe(
+        tap(() => {
+          this.isBusy.set(true);
+          this.hasErrored.set(null);
+        }),
+        pairwise(),
+        filter(([oldR, newR]) => oldR.creating && !newR.creating),
+        map(([, newR]) => newR),
+        tap((state) => {
+          if (state.error) {
+            this.hasErrored.set(`Failed to create key: ${state.message}`);
+            this.isBusy.set(false);
+          } else {
+            const response: NormalizedResponse<ApiKey> = state.response;
+            const entityKey = entityCatalog.getEntityKey(
+              stratosEntityCatalog.apiKey.actions.create(""),
+            );
+            this.dialogRef.close(
+              response.entities[entityKey][response.result[0]],
+            );
+          }
+        }),
+        take(1),
+      )
+      .subscribe();
   }
-
 }
--- src/frontend/packages/kubernetes/src/kubernetes/kubernetes-namespace/kubernetes-namespace-preview/kubernetes-namespace-preview.component.ts
+++ src/frontend/packages/kubernetes/src/kubernetes/kubernetes-namespace/kubernetes-namespace-preview/kubernetes-namespace-preview.component.ts
@@ -1,59 +1,52 @@
-import { AsyncPipe, NgIf } from '@angular/common';
-import {Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { AppState } from '@stratosui/store';
-import { Observable } from 'rxjs';
+import { AsyncPipe } from "@angular/common";
+import { Component, inject, ChangeDetectionStrategy } from "@angular/core";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { AppState } from "@stratosui/store";
+import { Observable } from "rxjs";
 
-import { PreviewableComponent } from '../../../../../core/src/shared/previewable-component';
-import { BaseKubeGuid } from '../../kubernetes-page.types';
-import { KubernetesEndpointService } from '../../services/kubernetes-endpoint.service';
-import { KubernetesNamespaceService } from '../../services/kubernetes-namespace.service';
-import { KubernetesAnalysisService } from '../../services/kubernetes.analysis.service';
-import { KubernetesService } from '../../services/kubernetes.service';
+import { PreviewableComponent } from "../../../../../core/src/shared/previewable-component";
+import { BaseKubeGuid } from "../../kubernetes-page.types";
+import { KubernetesEndpointService } from "../../services/kubernetes-endpoint.service";
+import { KubernetesNamespaceService } from "../../services/kubernetes-namespace.service";
+import { KubernetesAnalysisService } from "../../services/kubernetes.analysis.service";
+import { KubernetesService } from "../../services/kubernetes.service";
 
 @Component({
-  selector: 'app-kubernetes-namespace-preview',
-  templateUrl: './kubernetes-namespace-preview.component.html',
+  selector: "app-kubernetes-namespace-preview",
+  templateUrl: "./kubernetes-namespace-preview.component.html",
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [AsyncPipe, NgIf, RouterLink],
+  imports: [AsyncPipe, RouterLink],
   providers: [
     {
       provide: BaseKubeGuid,
       useFactory: (activatedRoute: ActivatedRoute) => {
         return {
-          guid: activatedRoute.snapshot.params.endpointId
+          guid: activatedRoute.snapshot.params.endpointId,
         };
       },
-      deps: [
-        ActivatedRoute
-      ]
+      deps: [ActivatedRoute],
     },
     KubernetesService,
     KubernetesEndpointService,
     KubernetesNamespaceService,
     KubernetesAnalysisService,
-  ]
+  ],
 })
 export class KubernetesNamespacePreviewComponent implements PreviewableComponent {
-
   showAnalysis$: Observable<boolean>;
 
   link: string;
   private store = inject(Store<AppState>);
 
-
-
   constructor() {
-
-
-    this.showAnalysis$ = KubernetesAnalysisService.isAnalysisEnabled(this.store);
-
-
+    this.showAnalysis$ = KubernetesAnalysisService.isAnalysisEnabled(
+      this.store,
+    );
   }
 
-  setProps(props: { [key: string]: any; }): void {
+  setProps(props: { [key: string]: any }): void {
     const { resource, endpointId } = props;
     this.link = `/kubernetes/${endpointId}/resource/namespace/${resource.metadata.name}/analysis`;
   }
