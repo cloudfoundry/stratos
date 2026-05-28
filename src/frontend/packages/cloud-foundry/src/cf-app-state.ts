1075f2ba116d567272acb131cac61ef18a27b646
Norman Abramovitz <nabramovitz@juno.com>
2026-05-28 08:44:24 -0700

    refactor(users): delete UsersRoles ngrx subsystem
    With the wizards on the signal-native service, remove the manageUsersRoles
    slice entirely: actions, reducer, effects, selectors, their module
    registrations, the cf-app-state slice declaration, and the test-framework
    seed state. No consumers remain.

--- src/frontend/packages/cloud-foundry/src/cf-app-state.ts
+++ src/frontend/packages/cloud-foundry/src/cf-app-state.ts
@@ -1,10 +1,8 @@
 import { AppState } from '../../store/src/app-state';
 import { CFRequestDataState } from './cf-entity-types';
 import { CreateNewApplicationState } from './store/types/create-application.types';
-import { UsersRolesState } from './store/types/users-roles.types';
 
 // Care about CF entities? Use this one. CF version of app-state.ts equivalents
 export abstract class CFAppState extends AppState<CFRequestDataState> {
   createApplication!: CreateNewApplicationState;
-  manageUsersRoles!: UsersRolesState;
 }
--- src/frontend/packages/cloud-foundry/src/store/cloud-foundry.reducers.module.ts
+++ src/frontend/packages/cloud-foundry/src/store/cloud-foundry.reducers.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 
-import { cfUsersRolesReducer } from './reducers/cf-users-roles.reducer';
 import { createAppReducer } from './reducers/create-application.reducer';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('createApplication', createAppReducer),
-    StoreModule.forFeature('manageUsersRoles', cfUsersRolesReducer),
   ]
 })
 export class CloudFoundryReducersModule { }
