b43f77b0ff97c0564a361a979fd9df31d9b2f59d
Norman Abramovitz <nabramovitz@juno.com>
2026-06-19 12:01:39 -0700

    Extend ListSubNavComponent with selection-aware action buttons

--- src/frontend/packages/core/src/public-api.ts
+++ src/frontend/packages/core/src/public-api.ts
@@ -63,7 +63,7 @@ export { SteppersComponent } from './shared/components/stepper/steppers/steppers
 export { StepComponent, StepOnNextFunction } from './shared/components/stepper/step/step.component';
 export { PageSubNavComponent } from './shared/components/page-sub-nav/page-sub-nav.component';
 export { PageSubNavSectionComponent } from './shared/components/page-sub-nav-section/page-sub-nav-section.component';
-export { ListSubNavComponent, ListSubNavAddAction } from './shared/components/list-sub-nav/list-sub-nav.component';
+export { ListSubNavComponent, ListSubNavAddAction, ListSubNavAction } from './shared/components/list-sub-nav/list-sub-nav.component';
 export { IPageSideNavTab } from './features/dashboard/page-side-nav/page-side-nav.component';
 
 // Custom Material Replacement Components
